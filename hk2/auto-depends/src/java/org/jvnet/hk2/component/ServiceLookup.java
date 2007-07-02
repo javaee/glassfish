2782dd5e58f4fbc6bbaf0d7fee1a7a7faf302f0c
kohsuke <kohsuke@java.net>
2007-07-02 07:30:37 +0000

    doc improvement.

--- hk2/auto-depends/src/java/org/jvnet/hk2/component/ServiceLookup.java
+++ hk2/auto-depends/src/java/org/jvnet/hk2/component/ServiceLookup.java
@@ -1,5 +1,7 @@
 package org.jvnet.hk2.component;
 
+import org.jvnet.hk2.annotations.Contract;
+
 /**
  * Extract look up for services exposed from module(s).
  *
@@ -10,9 +12,10 @@ public interface ServiceLookup {
     /**
      * Returns a list of class implementing a specific contract interface.
      *
-     * @param serviceClass the service interface
+     * @param contractClass
+     *      {@link Contract} class/interface for which the services are looked up. 
      * @return the list of classes implementing the passed interface
      */
-    public abstract <T> Iterable<Class<? extends T>> getProvidersClass(Class<T> serviceClass);
+    public abstract <T> Iterable<Class<? extends T>> getProvidersClass(Class<T> contractClass);
 
 }
