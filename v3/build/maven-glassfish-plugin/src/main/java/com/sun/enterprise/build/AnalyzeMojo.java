c7cf11a8efd94e1b91822405bae038cb6a5a180c
Kohsuke Kawaguchi <kohsuke@java.net>
2007-11-27 01:32:54 +0000

    bug fixes to handle cases that I didn't antiticipate originally.
    svn path=/trunk/; revision=17605

--- v3/build/maven-glassfish-plugin/src/main/java/com/sun/enterprise/build/AnalyzeMojo.java
+++ v3/build/maven-glassfish-plugin/src/main/java/com/sun/enterprise/build/AnalyzeMojo.java
@@ -28,13 +28,19 @@ public class AnalyzeMojo extends AbstractGlassfishMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
         Map<String,Dep> ndd = determineAllNonDirectDependencies();
 
+        boolean foundIssue = false;
+
         // find duplicate
         for( Dependency d : (List<Dependency>)project.getDependencies() ) {
             Dep dep = ndd.get(toKey(d));
             if(dep!=null) {
                 getLog().warn("Dependency "+d+" is redundant. It's included through"+dep.trail);
+                foundIssue = true;
             }
         }
+
+        if(!foundIssue)
+            getLog().info("No redundant dependency found");
     }
 
     /**
@@ -47,6 +53,13 @@ public class AnalyzeMojo extends AbstractGlassfishMojo {
             try {
                 MavenProject p = loadPom(a);
                 for( Dependency d : (List<Dependency>)p.getDependencies() ) {
+                    if(d.isOptional())
+                        // it makes sense for distribution to include a maven module
+                        // that's declared as optional in its transitive dependencies 
+                        continue;
+                    if(d.getScope()!=null && d.getScope().equals("test"))
+                        // we can safely ignore test scope dependency from computation
+                        continue;
                     m.put(toKey(d),new Dep(d,a));
                 }
             } catch (ProjectBuildingException e) {
