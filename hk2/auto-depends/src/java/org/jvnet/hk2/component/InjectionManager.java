9c44434ad66bfbad0d9f0d01d740d620ecb38b23
kohsuke <kohsuke@java.net>
2007-11-27 21:55:12 +0000

    improving error diagnostics.

--- hk2/auto-depends/src/java/org/jvnet/hk2/component/InjectionManager.java
+++ hk2/auto-depends/src/java/org/jvnet/hk2/component/InjectionManager.java
@@ -46,69 +46,78 @@ public abstract class InjectionManager<T extends Annotation> {
      *      if injection failed for some reason.
      */
     public void inject(Object component, Class<T> type) throws ComponentException {
-        assert component!=null;
+        try {
+            assert component!=null;
 
-        // TODO: faster implementation needed.
+            // TODO: faster implementation needed.
 
-        Class currentClass = component.getClass();
-        while (!currentClass.equals(Object.class)) {
-            // get the list of the instances variable
-            for (Field field : currentClass.getDeclaredFields()) {
+            Class currentClass = component.getClass();
+            while (!currentClass.equals(Object.class)) {
+                // get the list of the instances variable
+                for (Field field : currentClass.getDeclaredFields()) {
 
-                T inject = field.getAnnotation(type);
-                if (inject == null)     continue;
+                    T inject = field.getAnnotation(type);
+                    if (inject == null)     continue;
 
-                Class fieldType = field.getType();
-                try {
-                    Object value = getValue(field, fieldType);
-                    if (value != null) {
-                        field.setAccessible(true);
-                        field.set(component, value);
-                    } else {
-                        if(!isOptional(inject))
-                            throw new UnsatisfiedDepedencyException(field);
+                    Class fieldType = field.getType();
+                    try {
+                        Object value = getValue(field, fieldType);
+                        if (value != null) {
+                            field.setAccessible(true);
+                            field.set(component, value);
+                        } else {
+                            if(!isOptional(inject))
+                                throw new UnsatisfiedDepedencyException(field);
+                        }
+                    } catch (ComponentException e) {
+                        if (!isOptional(inject)) {
+                            throw new UnsatisfiedDepedencyException(field,e);
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new ComponentException("Injection failed on " + field.toGenericString(), e);
                     }
-                } catch (ComponentException e) {
-                    if (!isOptional(inject)) {
-                        throw new UnsatisfiedDepedencyException(field,e);
-                    }
-                } catch (IllegalAccessException e) {
-                    throw new ComponentException("Injection failed on " + field.toGenericString(), e);
                 }
+                for (Method method : currentClass.getDeclaredMethods()) {
+                    T inject = method.getAnnotation(type);
+                    if (inject == null)     continue;
+
+                    if (method.getReturnType() != void.class) {
+                        throw new ComponentException("Injection failed on %s : setter method is not declared with a void return type",method.toGenericString());
+                    }
+
+                    Class<?>[] paramTypes = method.getParameterTypes();
+
+                    if (paramTypes.length > 1) {
+                        throw new ComponentException("injection failed on %s : setter method takes more than 1 parameter",method.toGenericString());
+                    }
+                    if (paramTypes.length == 0) {
+                        throw new ComponentException("injection failed on %s : setter method does not take a parameter",method.toGenericString());
+                    }
+
+                    try {
+                        Object value = getValue(method, paramTypes[0]);
+                        if (value != null) {
+                            method.setAccessible(true);
+                            method.invoke(component, value);
+                        } else {
+                            if (!isOptional(inject))
+                                throw new UnsatisfiedDepedencyException(method);
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new ComponentException("Injection failed on " + method.toGenericString(), e);
+                    } catch (InvocationTargetException e) {
+                        throw new ComponentException("Injection failed on " + method.toGenericString(), e);
+                    }
+                }
+                currentClass = currentClass.getSuperclass();
             }
-            for (Method method : currentClass.getDeclaredMethods()) {
-                T inject = method.getAnnotation(type);
-                if (inject == null)     continue;
-
-                if (method.getReturnType() != void.class) {
-                    throw new ComponentException("Injection failed on %s : setter method is not declared with a void return type",method.toGenericString());
-                }
-
-                Class<?>[] paramTypes = method.getParameterTypes();
-
-                if (paramTypes.length > 1) {
-                    throw new ComponentException("injection failed on %s : setter method takes more than 1 parameter",method.toGenericString());
-                }
-                if (paramTypes.length == 0) {
-                    throw new ComponentException("injection failed on %s : setter method does not take a parameter",method.toGenericString());
-                }
-
-                try {
-                    Object value = getValue(method, paramTypes[0]);
-                    if (value != null) {
-                        method.setAccessible(true);
-                        method.invoke(component, value);
-                    } else {
-                        if (!isOptional(inject))
-                            throw new UnsatisfiedDepedencyException(method);
-                    }
-                } catch (IllegalAccessException e) {
-                    throw new ComponentException("Injection failed on " + method.toGenericString(), e);
-                } catch (InvocationTargetException e) {
-                    throw new ComponentException("Injection failed on " + method.toGenericString(), e);
-                }
-            }
-            currentClass = currentClass.getSuperclass();
+        } catch (LinkageError e) {
+            // reflection could trigger additional classloading and resolution, so it can cause linkage error.
+            // report more information to assist diagnosis.
+            // can't trust component.toString() as the object could be in an inconsistent state.
+            LinkageError x = new LinkageError("Failed to inject " + component.getClass());
+            x.initCause(e);
+            throw x;
         }
     }
 
