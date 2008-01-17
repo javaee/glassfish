8b48e818a8d40b0f1858000eb2dfc81ef37338c5
kohsuke <kohsuke@java.net>
2008-01-17 22:29:17 +0000

    doc improvement.

--- hk2/auto-depends/src/java/com/sun/hk2/component/Holder.java
+++ hk2/auto-depends/src/java/com/sun/hk2/component/Holder.java
@@ -1,11 +1,23 @@
 package com.sun.hk2.component;
 
 /**
+ * Indirection to a value. That is, instead of referring to the value itself,
+ * this class allows you to obtain the value when you need it.
+ *
+ * <p>
+ * This is the basis for all the lazy computation.
+ *
+ *
+ *
  * @author Kohsuke Kawaguchi
  */
 public interface Holder<T> {
     T get();
 
+    /**
+     * {@link Holder} implementation that doesn't do any deferred computation,
+     * where the value is given in the constructor.
+     */
     public static final class Impl<T> implements Holder<T> {
         private final T t;
 
