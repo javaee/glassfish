894a7e956cbe45ca836eaade2e2b2f8a2e708a3e
kohsuke <kohsuke@java.net>
2008-01-18 22:04:26 +0000

    moving to the test package for clarity.

---
+++ hk2/test-harness/src/main/java/org/jvnet/hk2/test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Test harness for HK2.
+ *
+ * <p>
+ * This package contains classes that allow people to write JUnit-based unit test code
+ * that runs inside HK2 environment. 
+ */
+package org.jvnet.hk2.test;
