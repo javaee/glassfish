b46727fa5f3fa9a15d8917e998ef8d6437f6989d
kohsuke <kohsuke@java.net>
2008-01-18 22:06:59 +0000

    javadoc.

---
+++ hk2/auto-depends/src/java/org/jvnet/hk2/annotations/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * HK2 annotations that mark components and their required dependencies&#x2E; Primary API surface of HK2.
+ *
+ * <p>
+ * HK2 modules primarily interact with HK2 by using these annotations.
+ */
+package org.jvnet.hk2.annotations;
