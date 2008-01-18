29c281c628e06f9faa8d547f132011b80aa1bc6c
kohsuke <kohsuke@java.net>
2008-01-18 22:04:26 +0000

    moving to the test package for clarity.

---
+++ hk2/test-harness/src/main/java/org/jvnet/hk2/test/HK2TestCase.java
@@ -0,0 +1,23 @@
+package org.jvnet.hk2.test;
+
+import junit.framework.TestCase;
+import org.jvnet.hk2.annotations.Contract;
+import org.jvnet.hk2.annotations.Scoped;
+import org.jvnet.hk2.component.PerLookup;
+
+/**
+ * Base-class for all test cases in HK2.
+ *
+ * <p>
+ * At runtime all the services of this contract are discovered and run.
+ * The execution semantics of JUnit requires that a new instance be created
+ * for calling each <tt>testXXX</tt> methods, so the implementations
+ * must be {@link PerLookup} scope. 
+ *
+ * @author Kohsuke Kawaguchi
+ */
+@Contract
+@Scoped(PerLookup.class)
+public class HK2TestCase extends TestCase {
+
+}
