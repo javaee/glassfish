540c380298d3a26ffe574b51d145f08e7b0696e5
kohsuke <kohsuke@java.net>
2008-01-18 22:39:20 +0000

    javadoc

---
+++ hk2/auto-depends/src/java/org/jvnet/hk2/component/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Primary API surface of HK2&#x2E; Start with {@link Habitat}.
+ *
+ * <p>
+ * This package defines classes and interfaces that constitute the public contract of HK2,
+ * together with <a href="../annotations/">the annotations package</a>.
+ */
+package org.jvnet.hk2.component;
