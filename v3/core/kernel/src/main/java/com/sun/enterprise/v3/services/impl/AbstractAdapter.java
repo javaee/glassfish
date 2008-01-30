fec204b46824618bf4e489c7c1c01132461f3300
Jean francois Arcand <jfarcand@java.net>
2008-01-30 21:13:41 +0000

    Add licence, remove IDE stuff (no fonctional changes)
    svn path=/trunk/; revision=17819

--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/AbstractAdapter.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/AbstractAdapter.java
@@ -1,3 +1,26 @@
+/*
+ * The contents of this file are subject to the terms 
+ * of the Common Development and Distribution License 
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at 
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html or
+ * glassfish/bootstrap/legal/CDDLv1.0.txt.
+ * See the License for the specific language governing 
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL 
+ * Header Notice in each file and include the License file 
+ * at glassfish/bootstrap/legal/CDDLv1.0.txt.  
+ * If applicable, add the following below the CDDL Header, 
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
+ */
+
 package com.sun.enterprise.v3.services.impl;
 
 import com.sun.grizzly.tcp.Response;
@@ -12,11 +35,7 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 
 /**
- * Created by IntelliJ IDEA.
- * User: dochez
- * Date: Jan 16, 2008
- * Time: 3:30:51 PM
- * To change this template use File | Settings | File Templates.
+ * @author Jerome Dochez
  */
 public abstract class AbstractAdapter {
 
