0a4b32e229e2c9dc10735ea45db4b78678cdfb97
Jane Young <janey@java.net>
2008-01-30 01:54:29 +0000

    add dummy commands for disable and enable.
    svn path=/trunk/; revision=17815

---
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/deployment/EnableCommand.java
@@ -0,0 +1,64 @@
+/*
+ * The contents of this file are subject to the terms 
+ * of the Common Development and Distribution License 
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at 
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html or
+ * glassfish/bootstrap/legal/CDDLv1.0.txt.
+ * See the License for the specific language governing 
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL 
+ * Header Notice in each file and include the License file 
+ * at glassfish/bootstrap/legal/CDDLv1.0.txt.  
+ * If applicable, add the following below the CDDL Header, 
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
+ */
+
+package com.sun.enterprise.v3.deployment;
+
+import com.sun.enterprise.v3.server.V3Environment;
+import org.glassfish.api.admin.AdminCommand;
+import org.glassfish.api.admin.AdminCommandContext;
+import com.sun.enterprise.v3.server.ApplicationLifecycle;
+import com.sun.enterprise.util.LocalStringManagerImpl;
+import org.glassfish.api.ActionReport;
+import org.glassfish.api.I18n;
+import org.glassfish.api.Param;
+import org.jvnet.hk2.annotations.Inject;
+import org.jvnet.hk2.annotations.Service;
+
+/**
+ * Enable command
+ */
+@Service(name="enable")
+@I18n("enable.command")
+    
+public class EnableCommand extends ApplicationLifecycle implements AdminCommand {
+
+    final private static LocalStringManagerImpl localStrings = new LocalStringManagerImpl(EnableCommand.class);    
+
+    @Inject
+    V3Environment env;
+
+    @Param(primary=true, name="component")
+    String component = null;
+
+    /**
+     * Entry point from the framework into the command execution
+     * @param context context for the command.
+     */
+    public void execute(AdminCommandContext context) {
+        ActionReport report = context.getActionReport();
+        
+        report.setMessage(localStrings.getLocalString("enable.command.sucess", "{0} enabled successfully", component));
+        report.setActionExitCode(ActionReport.ExitCode.SUCCESS);
+        return;
+    }        
+}
