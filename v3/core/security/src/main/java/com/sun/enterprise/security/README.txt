284d8b224695592522951f0e53b327191d4c33de
Kumar Jayanti <v.b.kumar.jayanti@oracle.com>
2008-02-01 11:07:52 +0000

    Adding Security Module under Core. Initial Version, Integration with WebContainer yet to be worked out
    svn path=/trunk/; revision=17845

---
+++ v3/core/security/src/main/java/com/sun/enterprise/security/README.txt
@@ -0,0 +1,5 @@
+The LocalStrings here probably caters to different files which have moved to other modules. So just revisit all the LocalStrings files again.
+
+Merged all modules to one. Now trying to remove jauth package since it is a pre-jsr stuff and somehow i cannot locate the classes for that.
+
+Also removed wss directory that contains a JAXRPC systemhandler delegate or something
---
+++ v3/core/security/src/main/java/com/sun/enterprise/security/SecurityServicesUtil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sun.enterprise.security;
+
+import com.sun.enterprise.server.ServerContext;
+import com.sun.enterprise.server.pluggable.SecuritySupport;
+import org.glassfish.api.invocation.InvocationManager;
+import org.jvnet.hk2.annotations.Inject;
+import org.jvnet.hk2.annotations.Service;
+
+@Service
+public class SecurityServicesUtil {
+
+    @Inject(name="PE")
+    private SecuritySupport peSecSupport;
+    
+    public SecuritySupport getSecuritySupport() {
+        //TODO:V3 Add logic to differentiate between PE and EE cases ?
+        //if EE Impl class is in a different module then we can remove
+        // the named injection above.
+        return peSecSupport;
+    }
+}
