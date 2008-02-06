3f9fe96a788690f4cc30eb4a7d2198d5e2fb4f4c
kohsuke <kohsuke@java.net>
2008-02-06 20:03:17 +0000

    refactoring AptCompiler to make a room for more generic AptMojo, which in turn is used by GF admin-gui folks, for example.

--- hk2/hk2-maven-plugin/src/main/java/com/sun/enterprise/module/maven/AptCompiler.java
+++ hk2/hk2-maven-plugin/src/main/java/com/sun/enterprise/module/maven/AptCompiler.java
@@ -1,13 +1,10 @@
 package com.sun.enterprise.module.maven;
 
-import com.sun.enterprise.tools.apt.MetainfServiceGenerator;
 import org.codehaus.plexus.compiler.CompilerConfiguration;
 import org.codehaus.plexus.compiler.CompilerException;
 import org.codehaus.plexus.compiler.javac.JavacCompiler;
-import org.jvnet.hk2.config.generator.AnnotationProcessorFactoryImpl;
 
 import java.io.File;
-import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.List;
 
@@ -67,28 +64,6 @@ public class AptCompiler extends JavacCompiler {
         config.addCompilerCustomArgument("-s", sourceDest.getAbsolutePath());
         String[] args = buildCompilerArguments( config, sourceFiles );
 
-        return compileInProcess( args );
-    }
-
-    /**
-     * Compile the java sources in the current JVM, without calling an external executable,
-     * using <code>com.sun.tools.javac.Main</code> class
-     *
-     * @param args arguments for the compiler as they would be used in the command line javac
-     * @return List of CompilerError objects with the errors encountered.
-     * @throws CompilerException
-     */
-    protected List compileInProcess( String[] args ) throws CompilerException {
-        com.sun.tools.apt.Main aptTool = new com.sun.tools.apt.Main();
-        int r = aptTool.process(
-            new CompositeAnnotationProcessorFactory(
-                new MetainfServiceGenerator(),
-                new AnnotationProcessorFactoryImpl()
-            ), new PrintWriter(System.out,true),args);
-        if(r!=0)
-            throw new CompilerException("APT failed: "+r);
-
-        // TODO: should I try to parse the output?
-        return Collections.emptyList();
+        return AptInvoker.get().compileInProcess(args);
     }
 }
