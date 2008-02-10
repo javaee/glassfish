15739ada2f792aeecd41f195dcc0637ef7db120c
Jagadish Ramu <jagadish.ramu@oracle.com>
2008-02-10 04:37:56 +0000

    Initial code for v3 connector container
    svn path=/trunk/; revision=18000

--- v3/connectors/connectors-runtime/src/main/java/com/sun/enterprise/connectors/ConnectorRegistry.java
+++ v3/connectors/connectors-runtime/src/main/java/com/sun/enterprise/connectors/ConnectorRegistry.java
@@ -36,29 +36,29 @@
 
 package com.sun.enterprise.connectors;
 
-import com.sun.enterprise.config.serverbeans.*;
+import com.sun.enterprise.config.serverbeans.ResourceAdapterConfig;
+import com.sun.enterprise.config.serverbeans.SecurityMap;
 import com.sun.enterprise.deployment.ConnectorDescriptor;
 import com.sun.logging.LogDomains;
-import java.util.Collections;
+
 import javax.resource.spi.ManagedConnectionFactory;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.logging.*;
-import java.util.List;
 import java.util.Map;
-import java.util.ArrayList;
-import com.sun.enterprise.connectors.authentication.RuntimeSecurityMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
-/** 
-  * This is an registry class which holds various objects in hashMaps,
-  * hash tables, and vectors. These objects are updated and queried 
-  * during various funtionalities of rar deployment/undeployment, resource 
-  * creation/destruction
-  * Ex. of these objects are ResourcesAdapter instances, security maps for 
-  * pool managed connection factories.
-  * It follows singleton patter. i.e only one instance at any point of time.
-  * @author    Binod P.G and Srikanth P
-  * @version 
+/**
+ * This is an registry class which holds various objects in hashMaps,
+ * hash tables, and vectors. These objects are updated and queried
+ * during various funtionalities of rar deployment/undeployment, resource
+ * creation/destruction
+ * Ex. of these objects are ResourcesAdapter instances, security maps for
+ * pool managed connection factories.
+ * It follows singleton pattern. i.e only one instance at any point of time.
+ *
+ * @author Binod P.G and Srikanth P
  */
 
 public class ConnectorRegistry {
@@ -73,17 +73,20 @@ public class ConnectorRegistry {
      * String:resourceadapterName Vs ActiveResourceAdapter
      */
     protected final Map<String, ActiveResourceAdapter> resourceAdapters;
+
     protected final Map<String, PoolMetaData> factories;
     protected final Map<String, ResourceAdapterConfig> resourceAdapterConfig;
-    
-    /** Return the ConnectorRegistry instance
-     *  @return ConnectorRegistry instance which is a singleton
+
+    /**
+     * Return the ConnectorRegistry instance
+     *
+     * @return ConnectorRegistry instance which is a singleton
      */
     public static ConnectorRegistry getInstance() {
         _logger.fine("returning the connector registry");
         return connectorRegistryInstance;
     }
-   
+
     /**
      * Protected constructor.
      * It is protected as it follows singleton pattern.
@@ -93,91 +96,102 @@ public class ConnectorRegistry {
         resourceAdapters = Collections.synchronizedMap(new HashMap<String, ActiveResourceAdapter>());
         factories = Collections.synchronizedMap(new HashMap<String, PoolMetaData>());
         resourceAdapterConfig = Collections.synchronizedMap(new HashMap<String, ResourceAdapterConfig>());
-        _logger.log(Level.FINE,"initialized the connector registry");
+        _logger.log(Level.FINE, "initialized the connector registry");
     }
 
-    /** Adds the object implementing ActiveResourceAdapter
-     *  interface to the registry.
-     *  @param rarModuleName RarName which is the key
-     *  @param rar           ActiveResourceAdapter instance which is the value.
+    /**
+     * Adds the object implementing ActiveResourceAdapter
+     * interface to the registry.
+     *
+     * @param rarModuleName RarName which is the key
+     * @param rar           ActiveResourceAdapter instance which is the value.
      */
 
-    public void addActiveResourceAdapter( String rarModuleName, 
-                 ActiveResourceAdapter rar) {
-	resourceAdapters.put(rarModuleName,rar);
+    public void addActiveResourceAdapter(String rarModuleName,
+                                         ActiveResourceAdapter rar) {
+        resourceAdapters.put(rarModuleName, rar);
         _logger.log(Level.FINE,
-                    "Added the active resource adapter to connector registry", 
-                    rarModuleName);
+                "Added the active resource adapter to connector registry",
+                rarModuleName);
     }
 
-    /** Removes the object implementing ActiveResourceAdapter
-     *  interface from the registry.
-     *  This method is called whenever an active connector module
-     *  is removed from the Connector runtime. [eg. undeploy/recreate etc] 
-     *  @param rarModuleName RarName which is the key
-     *  @return true if successfully removed
-     *          false if deletion fails.
+    /**
+     * Removes the object implementing ActiveResourceAdapter
+     * interface from the registry.
+     * This method is called whenever an active connector module
+     * is removed from the Connector runtime. [eg. undeploy/recreate etc]
+     *
+     * @param rarModuleName RarName which is the key
+     * @return true if successfully removed
+     *         false if deletion fails.
      */
 
     public boolean removeActiveResourceAdapter(String rarModuleName) {
-       	Object o = resourceAdapters.remove( rarModuleName ); 
-	
-        if( o == null) {
+        Object o = resourceAdapters.remove(rarModuleName);
+
+        if (o == null) {
             _logger.fine("Failed to remove the resource adapter from connector registry" +
-               rarModuleName);
+                    rarModuleName);
             return false;
         } else {
-            _logger.log(Level.FINE,
-                 "removed the active resource adapter from connector registry",
-                 rarModuleName);
+            _logger.fine("removed the active resource adapter from connector registry" +
+                    rarModuleName);
             return true;
         }
     }
 
-    /** Retrieves the object implementing ActiveResourceAdapter interface
-     *  from the registry. Key is the rarName.
-     *  @param rarModuleName Rar name. It is the key
-     *  @return object implementing ActiveResourceAdapter interface
+    /**
+     * Retrieves the object implementing ActiveResourceAdapter interface
+     * from the registry. Key is the rarName.
+     *
+     * @param rarModuleName Rar name. It is the key
+     * @return object implementing ActiveResourceAdapter interface
      */
 
     public ActiveResourceAdapter getActiveResourceAdapter(
-                     String rarModuleName) {
-        if(rarModuleName != null) {
-            _logger.log(Level.FINE,
-               "returning/found the resource adapter from connector registry",
-               rarModuleName);
-            return  resourceAdapters.get(rarModuleName);
+            String rarModuleName) {
+        if (rarModuleName != null) {
+            _logger.fine(
+                    "returning/found the resource adapter from connector registry" +
+                            rarModuleName);
+            return resourceAdapters.get(rarModuleName);
         } else {
-            _logger.log(Level.FINE,
-               "Resourceadapter not found in connector registry.Returning null",
-               rarModuleName);
+            _logger.fine(
+                    "Resourceadapter not found in connector registry.Returning null" +
+                            rarModuleName);
             return null;
         }
     }
 
-    /** Checks if the MCF pertaining to the pool is instantiated and present
+    /**
+     * Checks if the MCF pertaining to the pool is instantiated and present
      * in the registry. Each pool has its own MCF instance.
+     *
      * @param poolName Name of the pool
      * @return true if the MCF is found.
      *         false if MCF is not found
      */
 
-    public boolean isMCFCreated (String poolName) {
-        boolean created = factories.containsKey( poolName );
+
+    public boolean isMCFCreated(String poolName) {
+        boolean created = factories.containsKey(poolName);
         _logger.fine("isMCFCreated " + poolName + " - " + created);
         return created;
     }
 
-    /** Remove MCF instance pertaining to the poolName from the registry.
-     *  @param poolName Name of the pool
-     *  @return true if successfully removed.
-     *          false if removal fails.
+
+    /**
+     * Remove MCF instance pertaining to the poolName from the registry.
+     *
+     * @param poolName Name of the pool
+     * @return true if successfully removed.
+     *         false if removal fails.
      */
 
     public boolean removeManagedConnectionFactory(String poolName) {
-        if(factories.remove(poolName) == null) {
+        if (factories.remove(poolName) == null) {
             _logger.log(Level.FINE,
-               "Failed to remove the MCF from connector registry.", poolName);
+                    "Failed to remove the MCF from connector registry.", poolName);
             return false;
         } else {
             _logger.fine("removeMCF " + poolName + " - " + !factories.containsKey(poolName));
@@ -185,70 +199,79 @@ public class ConnectorRegistry {
         }
     }
 
-    /** Add MCF instance pertaining to the poolName to the registry.
-     *  @param poolName Name of the pool
-     *  @param factory MCF instance to be added.
+    /**
+     * Add MCF instance pertaining to the poolName to the registry.
+     *
+     * @param poolName Name of the pool
+     * @param pmd      MCF instance to be added.
      */
-
-    public void addManagedConnectionFactory(String poolName, 
-                                    PoolMetaData pmd ) {
+    //TODO V3 why pool-meta-data is stored using addMCF() ??
+    public void addManagedConnectionFactory(String poolName,
+                                            PoolMetaData pmd) {
         factories.put(poolName, pmd);
-        _logger.fine("Added MCF to connector registry for: "+  poolName);
+        _logger.fine("Added MCF to connector registry for: " + poolName);
     }
 
-    /** Retrieve MCF instance pertaining to the poolName from the registry.
-     *  @param poolName Name of the pool
-     *  @return factory MCF instance retrieved.
+    /**
+     * Retrieve MCF instance pertaining to the poolName from the registry.
+     *
+     * @param poolName Name of the pool
+     * @return factory MCF instance retrieved.
      */
-    
+
+
     public ManagedConnectionFactory getManagedConnectionFactory(
-                     String poolName) {
-        if(poolName != null) {
+            String poolName) {
+        if (poolName != null) {
             _logger.log(Level.FINE,
-                 "Returning the MCF from connector registry.", poolName);
-            
-            PoolMetaData pmd =  factories.get( poolName );
-	    if (pmd != null) {
+                    "Returning the MCF from connector registry.", poolName);
+
+            PoolMetaData pmd = factories.get(poolName);
+            if (pmd != null) {
                 return pmd.getMCF();
             }
 
-        } 
+        }
         return null;
     }
-   
 
-    /** Checks whether the rar is already deployed i.e registered with
-     *  connector registry
-     *  @param rarModuleName rar Name.
-     *  @return true if rar is registered
-     *          false if rar is not registered.
+
+    /**
+     * Checks whether the rar is already deployed i.e registered with
+     * connector registry
+     *
+     * @param rarModuleName rar Name.
+     * @return true if rar is registered
+     *         false if rar is not registered.
      */
 
     public boolean isRegistered(String rarModuleName) {
         _logger.log(Level.FINE,
-            "Checking for MCF presence in connector registry.",rarModuleName);
+                "Checking for MCF presence in connector registry.", rarModuleName);
         return resourceAdapters.containsKey(rarModuleName);
     }
 
-    /** Gets the connector descriptor pertaining the rar
-     *  @param rarModuleName rarName
-     *  @return ConnectorDescriptor which represents the ra.xml
+    /**
+     * Gets the connector descriptor pertaining the rar
+     *
+     * @param rarModuleName rarName
+     * @return ConnectorDescriptor which represents the ra.xml
      */
 
     public ConnectorDescriptor getDescriptor(String rarModuleName) {
         ActiveResourceAdapter ar = null;
-        if(rarModuleName != null) {
-            ar =  resourceAdapters.get(rarModuleName);
+        if (rarModuleName != null) {
+            ar = resourceAdapters.get(rarModuleName);
         }
         if (ar != null) {
             _logger.log(Level.FINE,
-                 "Found/returing Connector descriptor in connector registry.", 
-                 rarModuleName);
+                    "Found/returing Connector descriptor in connector registry.",
+                    rarModuleName);
             return ar.getDescriptor();
-        } else { 
+        } else {
             _logger.log(Level.FINE,
-                 "Couldnot find Connector descriptor in connector registry.", 
-                 rarModuleName);
+                    "Couldnot find Connector descriptor in connector registry.",
+                    rarModuleName);
             return null;
         }
     }
@@ -261,6 +284,7 @@ public class ConnectorRegistry {
      *  @see SecurityMapUtils.processSecurityMaps(SecurityMap[])
      */
 
+/*  TODO V3
     public RuntimeSecurityMap getRuntimeSecurityMap(String poolName) {
         if(poolName != null) {
             _logger.log(Level.FINE,
@@ -270,71 +294,76 @@ public class ConnectorRegistry {
         } else {
             return null;
         }
-    }
+    }*/
 
-    /** Get the resource adapter config properties object registered with 
-     *  registry against the rarName.
-     *  @param rarName Name of the rar
-     *  @return ResourceAdapter configuration object
+    /**
+     * Get the resource adapter config properties object registered with
+     * registry against the rarName.
+     *
+     * @param rarName Name of the rar
+     * @return ResourceAdapter configuration object
      */
 
     public ResourceAdapterConfig getResourceAdapterConfig(String rarName) {
-        if(rarName != null) {
+        if (rarName != null) {
             _logger.log(Level.FINE,
-              "Returing the resourceadapter Config from registry.",rarName);
+                    "Returing the resourceadapter Config from registry.", rarName);
             return resourceAdapterConfig.get(rarName);
         } else {
             return null;
         }
     }
 
-    /** Add the resource adapter config properties object to registry 
-     *  against the rarName.
-     *  @param rarName Name of the rar
-     *  @param raConfig ResourceAdapter configuration object
+    /**
+     * Add the resource adapter config properties object to registry
+     * against the rarName.
+     *
+     * @param rarName  Name of the rar
+     * @param raConfig ResourceAdapter configuration object
      */
 
     public void addResourceAdapterConfig(String rarName,
-                 ResourceAdapterConfig raConfig) {
-        if( rarName != null) {
+                                         ResourceAdapterConfig raConfig) {
+        if (rarName != null) {
             _logger.log(Level.FINE,
-                 "Adding the resourceAdapter Config to connector registry.", 
-                 rarName);
-            resourceAdapterConfig.put(rarName,raConfig);
+                    "Adding the resourceAdapter Config to connector registry.",
+                    rarName);
+            resourceAdapterConfig.put(rarName, raConfig);
         }
     }
 
-    /** Remove the resource adapter config properties object from registry 
-     *  @param rarName Name of the rar
-     *  @return true if successfully deleted
-     *          false if deletion fails
+    /**
+     * Remove the resource adapter config properties object from registry
+     *
+     * @param rarName Name of the rar
+     * @return true if successfully deleted
+     *         false if deletion fails
      */
 
     public boolean removeResourceAdapterConfig(String rarName) {
-        if(resourceAdapterConfig.remove(rarName) == null) {
+        if (resourceAdapterConfig.remove(rarName) == null) {
             _logger.log(Level.FINE,
-                 "failed to remove the resourceAdapter config from registry.", 
-                 rarName);
+                    "failed to remove the resourceAdapter config from registry.",
+                    rarName);
             return false;
         } else {
             _logger.log(Level.FINE,
-                 "Removed the resourceAdapter config map from registry.", 
-                 rarName);
+                    "Removed the resourceAdapter config map from registry.",
+                    rarName);
             return true;
         }
     }
 
     /**
      * Returns all Active Resource Adapters in the connector runtime.
+     *
      * @return All active resource adapters in the connector runtime
      */
     public ActiveResourceAdapter[] getAllActiveResourceAdapters() {
-        return (ActiveResourceAdapter[])
-                this.resourceAdapters.values().
-                        toArray(new ActiveResourceAdapter[]{});
+        return this.resourceAdapters.values().toArray(new ActiveResourceAdapter[]{});
     }
 
-    public PoolMetaData getPoolMetaData( String poolName ) {
-        return  factories.get( poolName );
+    public PoolMetaData getPoolMetaData(String poolName) {
+        return factories.get(poolName);
     }
 }
