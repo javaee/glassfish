bef5a10797d1850cb9d30a970aa1e2b52bb39e7f
Jagadish Ramu <jagadish.ramu@oracle.com>
2008-02-10 04:46:38 +0000

    Initial code for v3 connector container (Without lazy-enlist, lazy-assoc support)
    svn path=/trunk/; revision=18011

--- v3/connectors/connectors-runtime/src/main/java/com/sun/enterprise/connectors/util/ConnectionPoolObjectsUtils.java
+++ v3/connectors/connectors-runtime/src/main/java/com/sun/enterprise/connectors/util/ConnectionPoolObjectsUtils.java
@@ -1,93 +1,91 @@
-
-
 /*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
- * The contents of this file are subject to the terms of either the GNU
- * General Public License Version 2 only ("GPL") or the Common Development
- * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License. You can obtain
- * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
- * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
- * language governing permissions and limitations under the License.
- * 
- * When distributing the software, include this License Header Notice in each
- * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
- * Sun designates this particular file as subject to the "Classpath" exception
- * as provided by Sun in the GPL Version 2 section of the License file that
- * accompanied this code.  If applicable, add the following below the License
- * Header, with the fields enclosed by brackets [] replaced by your own
- * identifying information: "Portions Copyrighted [year]
- * [name of copyright owner]"
- * 
- * Contributor(s):
- * 
- * If you wish your version of this file to be governed by only the CDDL or
- * only the GPL Version 2, indicate your decision by adding "[Contributor]
- * elects to include this software in this distribution under the [CDDL or GPL
- * Version 2] license."  If you don't indicate a single choice of license, a
- * recipient has the option to distribute your version of this file under
- * either the CDDL, the GPL Version 2 or to extend the choice of license to
- * its licensees as provided above.  However, if you add GPL Version 2 code
- * and therefore, elected the GPL Version 2 license, then the option applies
- * only if the new code is made subject to such option by the copyright
- * holder.
- */
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+*
+* Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+*
+* The contents of this file are subject to the terms of either the GNU
+* General Public License Version 2 only ("GPL") or the Common Development
+* and Distribution License("CDDL") (collectively, the "License").  You
+* may not use this file except in compliance with the License. You can obtain
+* a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+* or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+* language governing permissions and limitations under the License.
+*
+* When distributing the software, include this License Header Notice in each
+* file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+* Sun designates this particular file as subject to the "Classpath" exception
+* as provided by Sun in the GPL Version 2 section of the License file that
+* accompanied this code.  If applicable, add the following below the License
+* Header, with the fields enclosed by brackets [] replaced by your own
+* identifying information: "Portions Copyrighted [year]
+* [name of copyright owner]"
+*
+* Contributor(s):
+*
+* If you wish your version of this file to be governed by only the CDDL or
+* only the GPL Version 2, indicate your decision by adding "[Contributor]
+* elects to include this software in this distribution under the [CDDL or GPL
+* Version 2] license."  If you don't indicate a single choice of license, a
+* recipient has the option to distribute your version of this file under
+* either the CDDL, the GPL Version 2 or to extend the choice of license to
+* its licensees as provided above.  However, if you add GPL Version 2 code
+* and therefore, elected the GPL Version 2 license, then the option applies
+* only if the new code is made subject to such option by the copyright
+* holder.
+*/
 
 package com.sun.enterprise.connectors.util;
 
-import com.sun.enterprise.connectors.*;
-import com.sun.enterprise.deployment.runtime.connector.*;
-import com.sun.enterprise.deployment.*;
-import com.sun.enterprise.config.serverbeans.ElementProperty;
-import com.sun.enterprise.config.serverbeans.ServerTags;
-import com.sun.enterprise.config.ConfigBean;
-import com.sun.enterprise.connectors.util.ResourcesUtil;
+import com.sun.appserv.connectors.spi.ConnectorConstants;
+import com.sun.appserv.connectors.spi.ConnectorRuntimeException;
+import com.sun.appserv.connectors.spi.TransactionSupport;
+import com.sun.enterprise.connectors.ConnectorConnectionPool;
+import com.sun.enterprise.connectors.ConnectorRuntime;
+import com.sun.enterprise.deployment.ConnectorDescriptor;
+import com.sun.enterprise.deployment.ResourcePrincipal;
+import com.sun.enterprise.deployment.runtime.connector.ResourceAdapter;
+import com.sun.enterprise.deployment.runtime.connector.SunConnector;
 import com.sun.enterprise.util.i18n.StringManager;
-import com.sun.logging.*;
-import java.util.logging.Logger;
+import com.sun.logging.LogDomains;
+
+import javax.resource.spi.ManagedConnectionFactory;
+import javax.resource.spi.security.PasswordCredential;
+import javax.security.auth.Subject;
+import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.security.auth.Subject;
-import javax.resource.spi.security.PasswordCredential;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-
-import javax.resource.spi.ManagedConnectionFactory;
-import java.lang.reflect.Method; 
-
-import org.netbeans.modules.schema2beans.Common;
-
-import com.sun.appserv.connectors.spi.TransactionSupport;
-
 
 /**
- * This is an util class for creating poolObjects of the type 
+ * This is an util class for creating poolObjects of the type
  * ConnectorConnectionPool from ConnectorDescriptor and also using the
  * default values.
+ *
  * @author Srikanth P
- */ 
+ */
 
-public  final class ConnectionPoolObjectsUtils {
+public final class ConnectionPoolObjectsUtils {
     private ConnectionPoolObjectsUtils() { /* disallow instantiation */ }
-    
+
     public static final String ELEMENT_PROPERTY = "ElementProperty";
-    private static final Logger _logger = LogDomains.getLogger( LogDomains.RSR_LOGGER );
+    private static final Logger _logger = LogDomains.getLogger(LogDomains.RSR_LOGGER);
 
     private static final String VALIDATE_ATMOST_EVERY_IDLE_SECS =
- 	"com.sun.enterprise.connectors.ValidateAtmostEveryIdleSecs";
+            "com.sun.enterprise.connectors.ValidateAtmostEveryIdleSecs";
 
     private static final String validateAtmostEveryIdleSecsProperty = System.getProperty(VALIDATE_ATMOST_EVERY_IDLE_SECS);
 
-    private static final StringManager localStrings = 
-        StringManager.getManager( ConnectionPoolObjectsUtils.class);
+    private static final StringManager localStrings =
+            StringManager.getManager(ConnectionPoolObjectsUtils.class);
+
     /**
      * Creates default ConnectorConnectionPool consisting of default
-     * pool values. 
+     * pool values.
+     *
      * @param poolName Name of the pool
      * @return ConnectorConnectionPool created ConnectorConnectionPool instance
      */
@@ -118,6 +116,7 @@ public  final class ConnectionPoolObjectsUtils {
 
     /**
      * Sets default values for advanced pool properties<br>
+     *
      * @param connectorPoolObj Connector Connection Pool
      */
     private static void setDefaultAdvancedPoolAttributes(ConnectorConnectionPool connectorPoolObj) {
@@ -186,213 +185,143 @@ public  final class ConnectionPoolObjectsUtils {
      * Return the integer representation of the transaction-support attribure
      *
      * @param txSupport one of <br>
-     * <ul>
-     *  <li>NoTransaction</li>
-     *  <li>LocalTransaction</li>
-     *  <li>XATransaction</li>
-     * </ul>
+     *                  <ul>
+     *                  <li>NoTransaction</li>
+     *                  <li>LocalTransaction</li>
+     *                  <li>XATransaction</li>
+     *                  </ul>
      * @return one of
-     * <ul>
-     *  <li>ConnectorConstants.UNDEFINED_TRANSACTION_INT</li>
-     *  <li>ConnectorConstants.NO_TRANSACTION_INT</li>
-     *  <li>ConnectorConstants.LOCAL_TRANSACTION_INT</li>
-     *  <li>ConnectorConstants.XA_TRANSACTION_INT</li>
-     * </ul>
-     *
+     *         <ul>
+     *         <li>ConnectorConstants.UNDEFINED_TRANSACTION_INT</li>
+     *         <li>ConnectorConstants.NO_TRANSACTION_INT</li>
+     *         <li>ConnectorConstants.LOCAL_TRANSACTION_INT</li>
+     *         <li>ConnectorConstants.XA_TRANSACTION_INT</li>
+     *         </ul>
      */
-    public static int parseTransactionSupportString( String txSupport ) {
+    public static int parseTransactionSupportString(String txSupport) {
         int txSupportIntVal = ConnectorConstants.UNDEFINED_TRANSACTION_INT;
 
-	
-	if ( txSupport == null ) {
-	    if (_logger.isLoggable(Level.FINE)) {
-	        _logger.fine("txSupport is null");
-	    }
-	    return txSupportIntVal;
-	}
-	
-        if (_logger.isLoggable(Level.FINE) ) {
-	    _logger.fine("parseTransactionSupportString: passed in " + 
-	        "txSupport =>" + txSupport);
-	}
-	
-        if (ConnectorConstants.NO_TRANSACTION_TX_SUPPORT_STRING.equals(txSupport) ) {
-	    txSupportIntVal = ConnectorConstants.NO_TRANSACTION_INT;
-	} else if (ConnectorConstants.LOCAL_TRANSACTION_TX_SUPPORT_STRING.equals(txSupport)) {
-	    txSupportIntVal = ConnectorConstants.LOCAL_TRANSACTION_INT;
-	} else if (ConnectorConstants.XA_TRANSACTION_TX_SUPPORT_STRING.equals(txSupport)) {
-	    txSupportIntVal = ConnectorConstants.XA_TRANSACTION_INT;
-	}
 
-	return txSupportIntVal;
+        if (txSupport == null) {
+            if (_logger.isLoggable(Level.FINE)) {
+                _logger.fine("txSupport is null");
+            }
+            return txSupportIntVal;
+        }
+
+        if (_logger.isLoggable(Level.FINE)) {
+            _logger.fine("parseTransactionSupportString: passed in txSupport =>" + txSupport);
+        }
+
+        if (ConnectorConstants.NO_TRANSACTION_TX_SUPPORT_STRING.equals(txSupport)) {
+            txSupportIntVal = ConnectorConstants.NO_TRANSACTION_INT;
+        } else if (ConnectorConstants.LOCAL_TRANSACTION_TX_SUPPORT_STRING.equals(txSupport)) {
+            txSupportIntVal = ConnectorConstants.LOCAL_TRANSACTION_INT;
+        } else if (ConnectorConstants.XA_TRANSACTION_TX_SUPPORT_STRING.equals(txSupport)) {
+            txSupportIntVal = ConnectorConstants.XA_TRANSACTION_INT;
+        }
+
+        return txSupportIntVal;
     }
-    
+
     /**
      * A utility method to map TransactionSupport ints as represented
      * in ConnectorConstants to the new TransactionSupport enum
      */
     public static TransactionSupport getTransactionSupport(int ts) {
-    	switch (ts) {
-			case ConnectorConstants.NO_TRANSACTION_INT:
-				return TransactionSupport.NO_TRANSACTION;
-			case ConnectorConstants.LOCAL_TRANSACTION_INT:
-				return TransactionSupport.LOCAL_TRANSACTION;
-			case ConnectorConstants.XA_TRANSACTION_INT:
-				return TransactionSupport.XA_TRANSACTION;
-		}
-    	return null;
+        switch (ts) {
+            case ConnectorConstants.NO_TRANSACTION_INT:
+                return TransactionSupport.NO_TRANSACTION;
+            case ConnectorConstants.LOCAL_TRANSACTION_INT:
+                return TransactionSupport.LOCAL_TRANSACTION;
+            case ConnectorConstants.XA_TRANSACTION_INT:
+                return TransactionSupport.XA_TRANSACTION;
+        }
+        return null;
     }
 
     public static String getValueFromMCF(String prop, String poolName,
-	        ManagedConnectionFactory mcf) {
+                                         ManagedConnectionFactory mcf) {
         String result = null;
         try {
-            Method m = mcf.getClass().getMethod("get"+prop, (java.lang.Class[])null);
-            result = (String) m.invoke(mcf,(java.lang.Object[])null);
+            Method m = mcf.getClass().getMethod("get" + prop, (java.lang.Class[]) null);
+            result = (String) m.invoke(mcf, (java.lang.Object[]) null);
         } catch (Throwable t) {
             _logger.log(Level.FINE, t.getMessage(), t);
         }
-        
-	return result == null ? "" : result;
+
+        return result == null ? "" : result;
     }
-    
-    public static Subject createSubject(ManagedConnectionFactory mcf, 
-            final ResourcePrincipal prin) { 
-                                                                     
-       final Subject tempSubject = new Subject();
-        if( prin != null) {
+
+    public static Subject createSubject(ManagedConnectionFactory mcf,
+                                        final ResourcePrincipal prin) {
+
+        final Subject tempSubject = new Subject();
+        if (prin != null) {
             String password = prin.getPassword();
             if (password != null) {
                 final PasswordCredential pc =
-                  new PasswordCredential(prin.getName(),
-                  password.toCharArray());
+                        new PasswordCredential(prin.getName(),
+                                password.toCharArray());
                 pc.setManagedConnectionFactory(mcf);
                 AccessController.doPrivileged(new PrivilegedAction() {
-                public Object run() {
-                    tempSubject.getPrincipals().add(prin);
-                    tempSubject.getPrivateCredentials().add(pc);
-                    return null;
-                }
+                    public Object run() {
+                        tempSubject.getPrincipals().add(prin);
+                        tempSubject.getPrivateCredentials().add(pc);
+                        return null;
+                    }
                 });
-           }
+            }
         }
         return tempSubject;
     }
 
-
- 
-      /**
-      * Validates and sets the values for LazyConnectionEnlistment and LazyConnectionAssociation.
-      * @param lazyAssocString Property value
-      * @param adminPool  Config Bean
-      * @param conConnPool Connector Connection Pool
-      */
-     public static void setLazyEnlistAndLazyAssocProperties(String lazyAssocString, ConfigBean adminPool, ConnectorConnectionPool conConnPool){
- 
-         //Get LazyEnlistment value.
-         //To set LazyAssoc to true, LazyEnlist also need to be true.
-         //If LazyAssoc is true and LazyEnlist is not set, set it to true.
-         //If LazyAssoc is true and LazyEnlist is false, throw exception.
- 
-         ElementProperty[] o = (ElementProperty[])adminPool.getValues(ELEMENT_PROPERTY);
-         if (o == null) return ;
- 
-         ElementProperty lazyEnlistElement = null;
- 
-          for (int i=0; i < o.length; i++) {
-              if(o[i].getAttributeValue(Common.convertName(ServerTags.NAME)).equalsIgnoreCase("LAZYCONNECTIONENLISTMENT")) {
-                  lazyEnlistElement = o[i];
-                  break;
-              }
-          }
- 
-             boolean lazyAssoc = toBoolean( lazyAssocString, false );
- 
-             if(lazyEnlistElement != null){
- 
-                 boolean lazyEnlist = toBoolean(lazyEnlistElement.getValue(),false);
-                 if(lazyAssoc){
-                     if(lazyEnlist){
-                         conConnPool.setLazyConnectionAssoc( true) ;
-                         conConnPool.setLazyConnectionEnlist( true);
-                     }else{
-                          _logger.log(Level.SEVERE,"conn_pool_obj_utils.lazy_enlist-lazy_assoc-invalid-combination",conConnPool.getName());
-                         String i18nMsg = localStrings.getString(
-	        "cpou.lazy_enlist-lazy_assoc-invalid-combination");
-                         throw new RuntimeException(i18nMsg + conConnPool.getName());
-                     }
-                 }else{
-                     conConnPool.setLazyConnectionAssoc(false);
-                 }
-             }else{
-                 if(lazyAssoc){
-                     conConnPool.setLazyConnectionAssoc( true) ;
-                     conConnPool.setLazyConnectionEnlist( true);
-                 }else{
-                     conConnPool.setLazyConnectionAssoc( false) ;
-                 }
-             }
- 
- }
- 
-     private static boolean toBoolean( Object prop, boolean defaultVal ) {
-         if ( prop == null ) {
-             return defaultVal;
-         }
-         return Boolean.valueOf(((String) prop).toLowerCase());
-      }
-    
-    public static boolean isPoolSystemPool(com.sun.enterprise.config.serverbeans.ConnectorConnectionPool 
-            domainCcp){
-    	String poolName = domainCcp.getName();
-    	return isPoolSystemPool(poolName);
+    public static boolean isPoolSystemPool(com.sun.enterprise.config.serverbeans.ConnectorConnectionPool
+            domainCcp) {
+        String poolName = domainCcp.getName();
+        return isPoolSystemPool(poolName);
     }
-        
-    public static boolean isPoolSystemPool(String poolName){
+
+    public static boolean isPoolSystemPool(String poolName) {
         Pattern pattern = Pattern.compile("#");
         Matcher matcher = pattern.matcher(poolName);
-        
+
         // If the pool name does not contain #, return false
-        if (!matcher.find()){
-        	return false;
+        if (!matcher.find()) {
+            return false;
         }
-        
+
         matcher.reset();
-        
+
         String moduleNameFromPoolName = null;
-        int matchCount=0;
-        
-        while (matcher.find()){
-        	matchCount++;
-        	int patternStart = matcher.start();
-        	moduleNameFromPoolName = poolName.substring(0,patternStart);
+        int matchCount = 0;
+
+        while (matcher.find()) {
+            matchCount++;
+            int patternStart = matcher.start();
+            moduleNameFromPoolName = poolName.substring(0, patternStart);
         }
-        
+
         // If pool name contains more than 2 #, return false as the 
         // default system pool will have exacly one # for a standalone rar
         // and exactly two #s for an embedded rar
         ResourcesUtil resUtil = ResourcesUtil.createInstance();
-        switch (matchCount){
-        
-        case 1: 
-        	if (resUtil.belongToStandAloneRar(moduleNameFromPoolName))
-        		return true;
-        case 2: 
-        	if (resUtil.belongToEmbeddedRar(moduleNameFromPoolName))
-        		return true;
-        default: 
-        	return false;
+        switch (matchCount) {
+
+            case 1:
+                if (resUtil.belongToStandAloneRar(moduleNameFromPoolName))
+                    return true;
+            default:
+                return false;
         }
     }
 
-    public static int getTransactionSupportFromRaXml( String rarName ) throws
-            ConnectorRuntimeException
-    {
+    public static int getTransactionSupportFromRaXml(String rarName) throws
+            ConnectorRuntimeException {
         String txSupport =
-            ConnectorRuntime.getRuntime().getConnectorDescriptor( rarName ).
-            getOutboundResourceAdapter().getTransSupport() ;
+                ConnectorRuntime.getRuntime().getConnectorDescriptor(rarName).
+                        getOutboundResourceAdapter().getTransSupport();
 
-        return parseTransactionSupportString( txSupport );
+        return parseTransactionSupportString(txSupport);
     }
-
 }
