824fdae6f7218870137a6b65cbe0732f22201856
Anissa Lam <anissa.lam@oracle.com>
2008-02-21 01:35:23 +0000

    change package name
    svn path=/trunk/; revision=18139

---
+++ v3/admingui/core/src/main/java/org/glassfish/admingui/App.java
@@ -0,0 +1,18 @@
+package org.glassfish.admingui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static String now()
+    {
+        return new java.util.Date().toString();
+    }
+}
