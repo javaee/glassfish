3fd23c3cdd78cc51c43c624a9ad38a8d7ecd8219
Jerome Dochez <dochez@java.net>
2008-02-21 22:15:01 +0000

    added enabled test
    svn path=/trunk/; revision=18156

---
+++ v3/admin/config-api/src/test/java/com/sun/enterprise/configapi/tests/EnabledTest.java
@@ -0,0 +1,82 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.enterprise.configapi.tests;
+
+import com.sun.enterprise.config.serverbeans.HttpListener;
+import com.sun.enterprise.config.serverbeans.HttpService;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * HttpListener.getEnabled() API test
+ * 
+ * User: Jerome Dochez
+ * Date: Feb 21, 2008
+ * Time: 2:06:44 PM
+ */
+public class EnabledTest extends ConfigApiTest {
+
+
+    public String getFileName() {
+        return "DomainTest";
+    }
+
+    List<HttpListener> listeners = null;
+
+    @Before
+    public void setup() {
+        HttpService service = getHabitat().getComponent(HttpService.class);
+        assertTrue(service!=null);
+        listeners = service.getHttpListener();
+    }
+
+    @Test
+    public void enabled() {
+        for (HttpListener listener : listeners) {
+            logger.fine("Listener " + listener.getId() + " enabled " + listener.getEnabled());
+            if (listener.getId().equals("admin-listener")) {
+                assertFalse(new Boolean(listener.getEnabled()).booleanValue());
+            } else {
+                assertTrue(new Boolean(listener.getEnabled()).booleanValue());
+            }
+        }
+    }
+}
