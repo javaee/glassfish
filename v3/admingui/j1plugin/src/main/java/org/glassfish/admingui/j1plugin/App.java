fb62ca4c481f59d9d51f785b5af93b3882179fa2
Anissa Lam <anissa.lam@oracle.com>
2008-02-21 02:09:18 +0000

    Plugin Module for JavaOne 2008 HOL
    svn path=/trunk/; revision=18144

---
+++ v3/admingui/j1plugin/src/main/java/org/glassfish/admingui/j1plugin/App.java
@@ -0,0 +1,16 @@
+package org.glassfish.admingui.j1plugin;
+
+import org.glassfish.admingui.util.GuiUtil;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        GuiUtil.isEmpty("test");  // just to test if it can call method in core.
+        System.out.println( "Hello World!" );
+    }
+    
+}
