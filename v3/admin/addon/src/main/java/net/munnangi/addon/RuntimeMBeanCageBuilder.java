2073c0eb653e98a46eab1e789e695bfe0ec2e9f1
Kohsuke Kawaguchi <kohsuke@java.net>
2008-02-28 22:56:14 +0000

    display what's begin caged.
    svn path=/trunk/; revision=18336

--- v3/admin/addon/src/main/java/net/munnangi/addon/RuntimeMBeanCageBuilder.java
+++ v3/admin/addon/src/main/java/net/munnangi/addon/RuntimeMBeanCageBuilder.java
@@ -2,9 +2,11 @@ package net.munnangi.addon;
 
 import org.jvnet.hk2.component.CageBuilder;
 import org.jvnet.hk2.component.Inhabitant;
+import org.jvnet.hk2.annotations.Service;
 
+@Service
 public class RuntimeMBeanCageBuilder implements CageBuilder {
     public void onEntered(Inhabitant<?> i) {
-        System.out.println("RuntimeMBeanCageBuilder: onEntered: time to create and register the mBean ...");
+        System.out.println("RuntimeMBeanCageBuilder: onEntered("+i.typeName()+"): time to create and register the mBean ...");
     }
 }
