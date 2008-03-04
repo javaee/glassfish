21a3502ef26f297bb98fc28c7a899c78d518da31
Mahesh Kannan <mahesh.kannan@oracle.com>
2008-03-04 23:52:39 +0000

    Modified the interface to handle different kinds of java: lookup
    svn path=/trunk/; revision=18505

--- v3/common/glassfish-api/src/main/java/org/glassfish/api/naming/NamedNamingObjectProxy.java
+++ v3/common/glassfish-api/src/main/java/org/glassfish/api/naming/NamedNamingObjectProxy.java
@@ -7,13 +7,12 @@ import org.jvnet.hk2.annotations.Contract;
  *         Date: Feb 28, 2008
  */
 @Contract
-public interface NamedNamingObjectProxy
-    extends NamingObjectProxy {
+public interface NamedNamingObjectProxy {
 
     /**
      * Returns the name that will be used to publish this object in the naming maanager
      * @return the name to bind
      */
-    public String getName();
+    public Object handle(String name);
     
 }
