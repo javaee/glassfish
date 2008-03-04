cd2ae67d69c9e36291ba3b413cd9e62288b1e250
Jane Young <janey@java.net>
2008-03-04 08:01:53 +0000

    change call to Main to AsadminMain The reason for this change is so that AsadminMain can be recognized using jsp.  Main class is generic.
    add copyright info.
    
    Reviewed by Kedar Mhaswade
    
    
    svn path=/trunk/; revision=18465

--- v3/distributions/nucleus-base/bin/asadmin.bat
+++ v3/distributions/nucleus-base/bin/asadmin.bat
@@ -1,8 +1,13 @@
 @echo off
 
+REM
+REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
+REM Use is subject to license terms.
+REM
+
 setlocal
 set AS_INSTALL=%~dp0\..
 set AS_INSTALL_LIB=%AS_INSTALL%\modules
 
-java -Dcom.sun.aas.installRoot=%AS_INSTALL% -cp "%AS_INSTALL_LIB%\admin-cli-10.0-SNAPSHOT.jar";"%AS_INSTALL_LIB%\cli-framework-10.0-SNAPSHOT.jar";"%AS_INSTALL_LIB%\glassfish-10.0-SNAPSHOT.jar" com.sun.enterprise.admin.cli.Main %*
+java -Dcom.sun.aas.installRoot=%AS_INSTALL% -cp "%AS_INSTALL_LIB%\admin-cli-10.0-SNAPSHOT.jar";"%AS_INSTALL_LIB%\cli-framework-10.0-SNAPSHOT.jar";"%AS_INSTALL_LIB%\glassfish-10.0-SNAPSHOT.jar" com.sun.enterprise.admin.cli.AsadminMain %*
 endlocal
