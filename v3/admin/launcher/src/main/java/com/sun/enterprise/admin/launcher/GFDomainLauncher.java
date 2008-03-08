ed2a2fdb7d02bd1009d449cd8a82d4df980567b5
Byron Nevins <byron.nevins@oracle.com>
2008-03-08 05:09:19 +0000

    Move Launcher classes to their own module
    svn path=/trunk/; revision=18668

---
+++ v3/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFDomainLauncher.java
@@ -0,0 +1,63 @@
+/*
+ * The contents of this file are subject to the terms 
+ * of the Common Development and Distribution License 
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at 
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html or
+ * glassfish/bootstrap/legal/CDDLv1.0.txt.
+ * See the License for the specific language governing 
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL 
+ * Header Notice in each file and include the License file 
+ * at glassfish/bootstrap/legal/CDDLv1.0.txt.  
+ * If applicable, add the following below the CDDL Header, 
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ */
+package com.sun.enterprise.admin.launcher;
+
+import com.sun.enterprise.module.bootstrap.BootException;
+import java.util.*;
+import com.sun.enterprise.glassfish.bootstrap.Main;
+
+/**
+ * GFDomainLauncher
+ * This class is a package-private subclass of GFLauncher designed for
+ * domain launching
+ * @author bnevins
+ */
+class GFDomainLauncher extends GFLauncher {
+
+    void internalLaunch() throws GFLauncherException {
+        try {
+            if (info.isEmbedded()) {
+                launchEmbedded();
+            }
+            else {
+                launchExternal();
+            }
+        }
+        catch (BootException ex) {
+            throw new GFLauncherException("unknownError", ex);
+        }
+    }
+
+    private void launchEmbedded() throws GFLauncherException, BootException {
+        Main main = new Main();
+        main.start(info.getArgsAsStringArray());
+        GFLauncherLogger.info("FinishedEmbedded", info.getDomainName());
+
+    }
+
+    private void launchExternal() throws GFLauncherException, BootException {
+        GFLauncherLogger.info("Launching Embedded -- External Launching not yet supported.");
+        launchEmbedded();
+    }
+}
+
---
+++ v3/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherLogger.java
@@ -0,0 +1,52 @@
+/*
+ * The contents of this file are subject to the terms 
+ * of the Common Development and Distribution License 
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at 
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html or
+ * glassfish/bootstrap/legal/CDDLv1.0.txt.
+ * See the License for the specific language governing 
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL 
+ * Header Notice in each file and include the License file 
+ * at glassfish/bootstrap/legal/CDDLv1.0.txt.  
+ * If applicable, add the following below the CDDL Header, 
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ */
+package com.sun.enterprise.admin.launcher;
+
+import com.sun.enterprise.universal.i18n.LocalStringsImpl;
+import java.util.logging.*;
+
+/**
+ *
+ * @author bnevins
+ */
+public class GFLauncherLogger {
+
+    public static void info(String msg, Object... objs)
+    {
+        logger.info(strings.get(msg, objs));
+    }
+    public static void severe(String msg, Object... objs)
+    {
+        logger.severe(strings.get(msg, objs));
+    }
+    public static void fine(String msg, Object... objs)
+    {
+        logger.fine(strings.get(msg, objs));
+    }
+
+    
+    private GFLauncherLogger() {
+    }
+    private final static Logger logger = Logger.getAnonymousLogger();
+    private final static LocalStringsImpl strings = new LocalStringsImpl(GFLauncherLogger.class);
+}
