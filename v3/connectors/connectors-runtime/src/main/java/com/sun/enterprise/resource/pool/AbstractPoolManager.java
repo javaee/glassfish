7d079387780ff2599b498cb7bd907594e0228bf0
Jagadish Ramu <jagadish.ramu@oracle.com>
2008-03-10 13:44:06 +0000

    Removing unsupported methods from AbstractPoolManager that are implemented by the subclass PoolManagerImpl.java
    svn path=/trunk/; revision=18705

--- v3/connectors/connectors-runtime/src/main/java/com/sun/enterprise/resource/pool/AbstractPoolManager.java
+++ v3/connectors/connectors-runtime/src/main/java/com/sun/enterprise/resource/pool/AbstractPoolManager.java
@@ -1,7 +1,41 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
 package com.sun.enterprise.resource.pool;
 
 import com.sun.enterprise.connectors.ConnectorConnectionPool;
-import com.sun.enterprise.resource.ResourceHandle;
 import com.sun.enterprise.resource.ResourceSpec;
 
 import javax.resource.ResourceException;
@@ -11,6 +45,9 @@ import java.util.logging.Logger;
 import com.sun.logging.LogDomains;
 import com.sun.appserv.connectors.spi.PoolingException;
 
+/**
+ * Abstract Pool manager for unimplemented features. Throws UnsupportedOperationException when invoked.
+ */
 public abstract class AbstractPoolManager implements PoolManager {
     /**
      * Obtain a transactional resource such as JDBC connection
@@ -27,19 +64,6 @@ public abstract class AbstractPoolManager implements PoolManager {
 
     static {
         _logger = LogDomains.getLogger(LogDomains.RSR_LOGGER);
-    }
-
-    public void badResourceClosed(ResourceHandle res) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void resourceErrorOccurred(ResourceHandle res) {
-        throw new UnsupportedOperationException();
-    }
-
-
-    public void putbackBadResourceToPool(ResourceHandle h) {
-        throw new UnsupportedOperationException();
     }
 
     public void emptyResourcePool(ResourceSpec spec) {
@@ -75,10 +99,6 @@ public abstract class AbstractPoolManager implements PoolManager {
         throw new UnsupportedOperationException();
     }
 
-    public ResourcePool getPool(String poolName) {
-        throw new UnsupportedOperationException();
-    }
-
     public void setSelfManaged(String poolName, boolean flag) {
         throw new UnsupportedOperationException();
     }
@@ -86,7 +106,6 @@ public abstract class AbstractPoolManager implements PoolManager {
     public void lazyEnlist(ManagedConnection mc) throws ResourceException {
         throw new UnsupportedOperationException();
     }
-
 
     public ConcurrentHashMap getMonitoredPoolTable() {
         throw new UnsupportedOperationException();
