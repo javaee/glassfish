84ada75f569a6bd5458f6e04536b393a8cafd3a5
Lloyd Chambers <llc@java.net>
2008-03-17 22:08:20 +0000

    removed some header blocks, added @ParamNames
    svn path=/trunk/; revision=18910

--- v3/common/amx-api/src/main/java/com/sun/appserv/management/j2ee/J2EEManagedObject.java
+++ v3/common/amx-api/src/main/java/com/sun/appserv/management/j2ee/J2EEManagedObject.java
@@ -33,13 +33,6 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
- 
-/*
- * $Header: /cvs/glassfish/appserv-api/src/java/com/sun/appserv/management/j2ee/J2EEManagedObject.java,v 1.2 2007/05/05 05:30:51 tcfujii Exp $
- * $Revision: 1.2 $
- * $Date: 2007/05/05 05:30:51 $
- */
- 
 package com.sun.appserv.management.j2ee;
  
 
