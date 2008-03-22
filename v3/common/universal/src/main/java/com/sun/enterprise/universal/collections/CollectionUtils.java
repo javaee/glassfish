e603e49d1e7832413978c1c2a44196f5ef3701d9
Byron Nevins <byron.nevins@oracle.com>
2008-03-22 04:40:56 +0000

    I am tired of writing code that turns Map<String,String> into a list of printable strings like  a=b  c=d
     So I added some methods to do it.
     I also added a unit test
    
     QL Tests passed
     
    
    svn path=/trunk/; revision=19152

--- v3/common/universal/src/main/java/com/sun/enterprise/universal/collections/CollectionUtils.java
+++ v3/common/universal/src/main/java/com/sun/enterprise/universal/collections/CollectionUtils.java
@@ -39,4 +39,39 @@ public class CollectionUtils {
         }
         return map;
     }
+    
+    /**
+     * Tired of dumping a String representation of a Map?
+     * Then call me!
+     * @param map The map to turn into a printable String
+     * @return The pretty String 
+     */
+    public static String toString(Map<String,String> map) {
+        String[] arr = toStringArray(map);
+        StringBuilder sb = new StringBuilder();
+        
+        for(String s : arr) {
+            sb.append(s);
+            sb.append(EOL);
+        }
+        return sb.toString();
+    }
+
+    public static String[] toStringArray(Map<String,String> map) {
+        Set<String> set = map.keySet();
+        String[] ss = new String[map.size()];
+        int i = 0;
+        
+        for(String name : set) {
+            String value = map.get(name);
+            String s = name;
+            
+            if(value != null) {
+                s += "=" + value;
+            }
+            ss[i++] = s;
+        }
+        return ss;
+    }
+    private static final String EOL = System.getProperty("line.separator");
 }
