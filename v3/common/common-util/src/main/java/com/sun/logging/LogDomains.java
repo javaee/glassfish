7db51897d0cd5a01f4a771686df60f568e08fdcc
Kedar Mhaswade <km@java.net>
2008-04-10 20:28:47 +0000

    Removed inadvertent checking causing build failure
    svn path=/trunk/; revision=19791

--- v3/common/common-util/src/main/java/com/sun/logging/LogDomains.java
+++ v3/common/common-util/src/main/java/com/sun/logging/LogDomains.java
@@ -1,5 +1,3 @@
-could not open /dev/kbd to get keyboard type US keyboard assumed
-could not get keyboard type US keyboard assumed
 /*
  * The contents of this file are subject to the terms 
  * of the Common Development and Distribution License 
