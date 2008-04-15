de85c792420fd3e4d037263d059fcd87930ca4b9
kohsuke <kohsuke@java.net>
2008-04-15 18:31:09 +0000

    automation script to perform a release

---
+++ hk2/release.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -ex
+# Script to perform a new release with automatic version increment.
+# This script is meant to be run from Hudson.
+#
+# Because of the tricky dependency inside HK2, where a part of the build (hk2/hk2 in particular)
+# uses another part of the build (hk2-maven-plugin) as a build extension,
+# normal "mvn -B release:prepare release:perform" fails.
+#
+# This script works around this issue, and that's why it's very non-obvious.
+
+# First, get to the base line. This is probably not a requirement.
+mvn clean install
+
+# This starts the release preparation, but it eventually fails because
+# it's unable to resolve the maven-hk2-plugin that the release is going to build.
+mvn -B -P release release:prepare || true
+
+# At this point local POM has the release version set,
+# so we build it, in particular maven-hk2-plugin.
+mvn -P release-phase1 install
+
+# Now retry release:prepare and this shall run to the completion
+mvn -B -P release release:prepare
+
+# At this point POM has the next SNAPSHOT version set,
+# and unless I build maven-hk2-plugin again, the POM fails to load
+# when we run "release:perform" later. So do the build again.
+mvn -P release-phase1 install
+
+# finally a release
+mvn -B release:perform
