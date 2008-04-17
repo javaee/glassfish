0122948e6bcbe683bb9ced3862da03765c723184
Kohsuke Kawaguchi <kohsuke@java.net>
2008-04-17 16:49:30 +0000

    removed hard-coded version dependency.
    svn path=/branches/tp2/; revision=20189

--- ejb/ejb-container/src/main/java/org/glassfish/ejb/startup/EjbDeployer.java
+++ ejb/ejb-container/src/main/java/org/glassfish/ejb/startup/EjbDeployer.java
@@ -90,7 +90,7 @@ public class EjbDeployer
     @Override
     public MetaData getMetaData() {
         List<ModuleDefinition> apis = new ArrayList<ModuleDefinition>();
-        Module module = modulesRegistry.makeModuleFor("org.glassfish:javax.javaee", "10.0-SNAPSHOT");
+        Module module = modulesRegistry.makeModuleFor("org.glassfish:javax.javaee",null);
         if (module!=null) {
             apis.add(module.getModuleDefinition());
         }
