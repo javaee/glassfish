2aef06d22ab6b255ae89d125047ccea6ef30c0ae
Snjezana Sevo-Zenzerovic <snjezana.sevozenzerovic@oracle.com>
2008-04-25 20:46:39 +0000

    Changed default installation directory, integrated new progress panel images, fixed component naming and layout of summary panel.
    svn path=/branches/tp2/; revision=20573

--- installer/src/main/resources/wrappers/wrapper.sh
+++ installer/src/main/resources/wrappers/wrapper.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 echo "Extracting archive, please wait..."
-tail +11l $0 > tmp.jar
+tail +12l $0 > tmp.jar
 $JAVA_HOME/bin/jar xvf tmp.jar 
 rm tmp.jar
 chmod ugo+x product-installer.sh
 chmod ugo+x install/bin/engine-wrapper
+echo "InstallHome.directory.INSTALL_HOME=$HOME/glassfish-v3tp2" > install.properties
 sh product-installer.sh
 exit $?
