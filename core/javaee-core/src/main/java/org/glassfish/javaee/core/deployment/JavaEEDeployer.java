7ad6ec9d2d7f0b40ebeb70a24d25ad830e56b9aa
Kohsuke Kawaguchi <kohsuke@java.net>
2008-04-26 02:57:57 +0000

    no need to call the getSourceDir()
    svn path=/branches/kohsuke/post-tp2/; revision=20607

--- core/javaee-core/src/main/java/org/glassfish/javaee/core/deployment/JavaEEDeployer.java
+++ core/javaee-core/src/main/java/org/glassfish/javaee/core/deployment/JavaEEDeployer.java
@@ -322,27 +322,15 @@ public abstract class   JavaEEDeployer<T extends Container, U extends Applicatio
     // it is present. Application can be user application or system 
     // appliction.
     protected String getObjectType(DeploymentContext context) {
-        File appDir = context.getSourceDir();
-        FileInputStream fis = null;
         try{
-            File manifestFile = new File(appDir, JarFile.MANIFEST_NAME);
-            fis = new FileInputStream(manifestFile);
-            Manifest manifest = new Manifest(fis);
+            Manifest manifest = context.getSource().getManifest();
+            if(manifest==null)  return null;
             Attributes attrs = manifest.getMainAttributes();
             return attrs.getValue(APPLICATION_TYPE);
-        }catch(Exception e){
+        }catch(IOException e){
             // by default resource-type will be assigned "user".
             return null;
-        } finally {
-            try {
-                if (fis != null) {
-                    fis.close();
-                }
-            } catch (IOException ioe) {
-                // ignore ioe
-            }
         }
-
     }
  
     abstract protected RootDeploymentDescriptor getDefaultBundleDescriptor();
