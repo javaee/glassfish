9c761fa1ecbda80ef71cb65f1ccd134e5dae5089
llc <llc@java.net>
2008-04-29 00:04:55 +0000

    code OK'd by Jerome.
    - fixes the QueueFull problem; add() (non-blocking) was being called instead of put() (blocking)A
    - fixes the waitForDrain() method to use a CountDownLatch for correct and optimally efficient behavior
    - removed the broken method boolean pendingTransactionEvents() which did not return a correct result in any environment with multiple transactions (waited for more than the current state, possibly forever).
    - fixed threading bug whereby 'listeners' could be changed while PropertyChangeEvents were being processed.

--- hk2/config/src/java/org/jvnet/hk2/config/Transactions.java
+++ hk2/config/src/java/org/jvnet/hk2/config/Transactions.java
@@ -38,12 +38,10 @@ package org.jvnet.hk2.config;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Condition;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.CountDownLatch;
+
 import java.beans.PropertyChangeEvent;
 import java.lang.reflect.Proxy;
 
@@ -53,19 +51,43 @@ import java.lang.reflect.Proxy;
  *
  * @author Jerome Dochez
  */
-public class Transactions {
-
+public final class Transactions {
     final private static Transactions t = new Transactions();
+    
+    // NOTE: synchronization on the object itself
     final List<TransactionListener> listeners = new ArrayList<TransactionListener>();
+    
     // Use a blocking queue out of conveniene, in fact the synchornization is done with the Lock and condition below
-    final private BlockingQueue<List<PropertyChangeEvent>> pendingRecords = new ArrayBlockingQueue<List<PropertyChangeEvent>>(5);
-    Thread pump;
+    final private BlockingQueue<Job> pendingJobs = new ArrayBlockingQueue<Job>(5);
+    final Thread mNotififierThread;
 
     /**
-     * Lock and Condition to be able to monitor that all events have been
-     * dispatched to all listeners before declaring the events backlog is empty
+        A job contains an optional CountdownLatch so that a caller can learn when the
+        transaction has "cleared" by blocking until that time.
      */
-    final Semaphore semaphore = new Semaphore(100);
+    private static final class Job {
+        final List<PropertyChangeEvent> mEvents;
+        private final CountDownLatch mLatch;
+        
+        public Job( final List<PropertyChangeEvent> events, final CountDownLatch latch ) {
+            mEvents = events;
+            mLatch  = latch;
+        }
+        
+        public void waitForLatch() throws InterruptedException {
+            if ( mLatch != null ) {
+                mLatch.await();
+            }
+        }
+        
+        public void releaseLatch() {
+            if ( mLatch != null ) {
+                mLatch.countDown();
+            }
+        }
+    }
+
+
     /**
      * Returns the singleton service
      *
@@ -80,8 +102,10 @@ public class Transactions {
      *
      * @param listener to be added.
      */
-    public synchronized void addTransactionsListener(TransactionListener listener) {
-        listeners.add(listener);
+    public void addTransactionsListener(TransactionListener listener) {
+        synchronized(listeners) {
+            listeners.add(listener);
+        }
     }
 
     /**
@@ -89,73 +113,140 @@ public class Transactions {
      * @param listener the registered listener
      * @return true if the listener unregistration was successful
      */
-    public synchronized boolean removeTransactionsListener(TransactionListener listener) {
-        return listeners.remove(listener); 
+    public boolean removeTransactionsListener(TransactionListener listener) {
+        synchronized(listeners) {
+            return listeners.remove(listener);
+        }
+    }
+    
+    /**
+        To make a copy of the list, we must hold the lock while we copy it:
+        what happens if add/removeTransactionsListener is called while copying?!
+     */
+    public List<TransactionListener> currentListeners() {
+        synchronized(listeners) {
+            return new ArrayList<TransactionListener>(listeners);
+        }
     }
 
+
+    /** maintains prior semantics of add, and return immediately */
+    void addTransaction( final List<PropertyChangeEvent> events) {
+        addTransaction(events, false);
+    }
+        
     /**
      * Notification of a new transation completion
      *
      * @param events accumulated list of changes
+     * @param waitTillCleared  synchronous semantics; wait until all change events are sent
      */
-    void addTransaction(List<PropertyChangeEvent> events) {
+    void addTransaction(
+        final List<PropertyChangeEvent> events,
+        final boolean waitTillCleared ) {
+        
+        // create a CountDownLatch to implement waiting for events to actually be sent
+        final CountDownLatch latch = waitTillCleared ? new CountDownLatch(1) : null;
+        
+        final Job job = new Job( events, latch );
+        
+        // NOTE that this is put() which blocks, *not* add() which will not block and will
+        // throw an IllegalStateException if the queue is full.
         try {
-            semaphore.acquire();
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            pendingJobs.put(job);
+            job.waitForLatch();
+        } catch (InterruptedException e ) {
+            throw new RuntimeException(e);
         }
-        pendingRecords.add(events);
     }
 
+    
     /**
-     * Returns true if there are transaction events yet to be received by listeners
-     *
+        Returns true if there are transaction events yet to be received by listeners
+        <p>
+        The return value is meaningless in a threaded environment
+        unless  all events queued prior to this call have *actually been
+        sent to all listeners*, as opposed to having been pulled off the queue and about to be sent,
+        partially sent, etc.  This method should be removed, since waitForDrain() does what is needed.
      * @return true if the are pending events notifications.
-     */
     public boolean pendingTransactionEvents() {
-        return 100!=semaphore.availablePermits();
+        waitForDrain();
+        return false;
     }
+     */
 
     public void waitForDrain() {
-        while (pendingTransactionEvents());
+        // insert a dummy Job and block until is has been processed.  This guarantees
+        // that all prior jobs have finished
+        addTransaction( new ArrayList<PropertyChangeEvent>(), true );
+        // at this point all prior transactions are guaranteed to have cleared
     }
 
     public void shutdown() {
-        pump.interrupt();
+        mNotififierThread.interrupt();
     }
 
-    private Transactions() {
-        pump = new Thread() {
-            public void run() {
-                while (true) {
-                    try {
-                        List<PropertyChangeEvent> events = pendingRecords.take();
-                        for (TransactionListener listener : new ArrayList<TransactionListener>(listeners)) {
-                            try {
-                                listener.transactionCommited(events);
-                            } catch(Exception e) {
-                                e.printStackTrace();
-                            }
+    private final class ListenerNotifierThread extends Thread {
+        public ListenerNotifierThread() {}
+        
+        private void processJob( final Job job ) {
+            try {
+                final List<PropertyChangeEvent> events = job.mEvents;
+                if ( events.size() != 0 ) {
+                    // copy the 'listeners' list so as to avoid concurrency issues
+                    final List<TransactionListener> curListeners = currentListeners();
+                    
+                    for ( final TransactionListener listener : curListeners ) {
+                        try {
+                            listener.transactionCommited(events);
+                        } 
+                        catch(Exception e) {
+                            e.printStackTrace();
                         }
-                        for (PropertyChangeEvent evt : events) {
-                            Dom dom = (Dom) ((ConfigView) Proxy.getInvocationHandler(evt.getSource())).getMasterView();
-                            if (dom.getListeners() != null) {
-                                for (ConfigListener listener : dom.getListeners()) {
-                                    try {
-                                        listener.changed(events.toArray(new PropertyChangeEvent[events.size()]));
-                                    } catch (Exception e) {
-                                        e.printStackTrace();
-                                    }
+                    }
+                    
+                    for ( final PropertyChangeEvent evt : events) {
+                        final Dom dom = (Dom)((ConfigView) Proxy.getInvocationHandler(evt.getSource())).getMasterView();
+                        if (dom.getListeners() != null) {
+                            for ( final ConfigListener listener : dom.getListeners()) {
+                                try {
+                                    // create a new array each time to avoid any potential array changes?
+                                    listener.changed(events.toArray(new PropertyChangeEvent[events.size()]));
+                                } catch (Exception e) {
+                                    e.printStackTrace();
                                 }
                             }
                         }
-                        semaphore.release();
-                    } catch (InterruptedException e) {
-                        return;
                     }
                 }
             }
-        };
-        pump.start();
+            finally {
+                job.releaseLatch();
+            }
+        }
+        
+        public void run() {
+            while (true) {
+                try {
+                    final Job job  = pendingJobs.take();
+                    processJob(job);
+                }
+                catch (InterruptedException e)
+                {
+                    // do anything here?
+                }
+            }
+        }
+    }
+    
+    private Transactions() {
+        mNotififierThread = new ListenerNotifierThread(); 
+        mNotififierThread.start();
     }
 }
+
+
+
+
+
+
