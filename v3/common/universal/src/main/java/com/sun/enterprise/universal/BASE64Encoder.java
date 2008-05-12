f5eadfdb70f86c5dac9e02df41b4ab8e43fd88bd
Byron Nevins <byron.nevins@oracle.com>
2008-05-12 19:43:20 +0000

    * Added a do-nothing class that extends sun.misc.BASE64Encoder and sun.misc.BASE64Decoder.  Why?  Warnings are generated everywhere that  the Sun classes are used.  This generates lots of noise and it can't be fixed with @SuppressWarnings().  The idea is to call these new classes instead and the warning will only come out of this module.  It also makes it easy to implement a replacement  of the Sun classes w/o changing all the callers again.  -- The next step is to change all the callers to import these classes instead of the sun classes. * Added license comment to a few files
    svn path=/trunk/; revision=20794
    
    
    Former-commit-id: 5d6cc20bb3f6ae25c1f159060290eb0bb056ca1a

--- v3/common/universal/src/main/java/com/sun/enterprise/universal/BASE64Encoder.java
+++ v3/common/universal/src/main/java/com/sun/enterprise/universal/BASE64Encoder.java
@@ -34,75 +34,16 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+
 package com.sun.enterprise.universal;
 
-import java.util.*;
-
 /**
- * This class is needed so often in so many places.  It is centralized once and
- * for all here.
- * @param K key type
- * @param V value type
+ * Base64 encoding using sun.misc generates warning messages.
+ * It is centralized here to get rid of the warnings sprinkled around
+ * the rest of Glassfish.
+ * Also, we can replace the implementation whenever we like.
  * @author bnevins
  */
-public class NameValue<K, V> {
-    /**
-     * 
-     * @param k key
-     * @param v value
-     */
-    public NameValue(K k, V v)
-    {
-        if(k == null)
-            throw new NullPointerException();
-        
-        value = v;
-        key = k;
-    }
+public class BASE64Encoder  extends sun.misc.BASE64Encoder{
 
-    public K getName    () {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    public V setValue(V newValue) {
-        V oldValue = value;
-        value = newValue;
-        return oldValue;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof NameValue))
-            return false;
-        
-        NameValue nv = (NameValue) o;
-        Object k1 = getName();
-        Object k2 = nv.getName();
-        if (k1 == k2 || k1.equals(k2)) {
-            Object v1 = getValue();
-            Object v2 = nv.getValue();
-            
-            if (v1 == v2 || (v1 != null && v1.equals(v2)))
-                return true;
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return key.hashCode() ^ (value == null ? 0 : value.hashCode());
-    }
-
-    @Override
-    public String toString() {
-        return getName() + "=" + getValue();
-    }
-
-    private final K key;
-    private V value;
 }
-
