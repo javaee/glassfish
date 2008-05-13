51682e1615ab052eb02e1bf1b5b369cc176b03e9
Byron Nevins <byron.nevins@oracle.com>
2008-05-13 08:14:12 +0000

    Unit tests for BASE64 classes. It verifies that:
    (1) it works
    (2) it works EXACTLY the same as sun.misc
    
    
    svn path=/trunk/; revision=20799
    
    
    Former-commit-id: d343ec8f77122b211d6a15e69819a4933aa58e3d

---
+++ v3/common/universal/src/test/java/com/sun/enterprise/universal/BASE64DecoderTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sun.enterprise.universal;
+
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bnevins
+ */
+public class BASE64DecoderTest {
+    /* 
+     * make sure the Universal base64 works
+     */
+    @Test
+    public void testEncodeDecode() throws IOException{
+        BASE64Encoder encoder = new BASE64Encoder();
+        BASE64Decoder decoder = new BASE64Decoder();
+        
+        for(String s : ss) {
+            byte[] stringAsByteBuf = s.getBytes();
+            String enc = encoder.encode(stringAsByteBuf);
+            assertFalse(enc.equals(s));
+            byte[] decodedByteBuf = decoder.decodeBuffer(enc);
+            String dec = new String(decodedByteBuf);
+            assertEquals(dec, s);
+        }
+    }
+    
+    /* make sure the Universal base64 results match sun.misc
+     */
+    @Test
+    public void testEncodeDecodeAgainstSun() throws IOException{
+        com.sun.enterprise.universal.BASE64Encoder gfEncoder = 
+                new com.sun.enterprise.universal.BASE64Encoder();
+        com.sun.enterprise.universal.BASE64Decoder gfDecoder = 
+                new com.sun.enterprise.universal.BASE64Decoder();
+        sun.misc.BASE64Decoder sunDecoder = new sun.misc.BASE64Decoder();
+        sun.misc.BASE64Encoder sunEncoder = new sun.misc.BASE64Encoder();
+        
+        for(String s : ss) {
+            byte[] stringAsByteBuf = s.getBytes();
+            String gfEnc = gfEncoder.encode(stringAsByteBuf);
+            String sunEnc = sunEncoder.encode(stringAsByteBuf);
+            
+            assertEquals(gfEnc, sunEnc);
+            
+            byte[] gfDecodedByteBuf = gfDecoder.decodeBuffer(gfEnc);
+            byte[] sunDecodedByteBuf = sunDecoder.decodeBuffer(sunEnc);
+            
+            assertTrue(gfDecodedByteBuf.length == sunDecodedByteBuf.length);
+            
+            for(int i = 0; i < gfDecodedByteBuf.length; i++)
+                assertEquals(gfDecodedByteBuf[i], sunDecodedByteBuf[i]);
+
+            String gfDec = new String(gfDecodedByteBuf);
+            String sunDec = new String(sunDecodedByteBuf);
+            assertEquals(gfDec, s);
+            assertEquals(gfDec, sunDec);
+        }
+    }
+    
+    private static final String[] ss = new String[]
+    {
+        "foo", "QQ234bbVVc", "\n\n\r\f\n"
+    };
+}
