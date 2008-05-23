73599d786a48b1e3cb49d8b359fba0eedbd514c0
Snjezana Sevo-Zenzerovic <snjezana.sevozenzerovic@oracle.com>
2008-05-23 22:51:27 +0000

    Modified self-extracting wrapper to use random temporary directory and added registration jars to install engine classpath.
    svn path=/branches/tp2/; revision=20888

--- installer/src/main/resources/wrappers/wrapper.sh
+++ installer/src/main/resources/wrappers/wrapper.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+tmp=`mktemp -d`
 echo "Extracting archive, please wait..."
-tail +12l $0 > tmp.jar
+tail +15l $0 > /$tmp/tmp.jar
+cd $tmp
 $JAVA_HOME/bin/jar xvf tmp.jar 
 rm tmp.jar
 chmod ugo+x product-installer.sh
 chmod ugo+x install/bin/engine-wrapper
 echo "InstallHome.directory.INSTALL_HOME=$HOME/glassfish-v3tp2" > install.properties
 sh product-installer.sh
+rm -rf * 
 exit $?
