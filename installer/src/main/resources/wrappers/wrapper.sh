fd879de7b1d4cbc7134a53bf641ba8cb7d4765da
Snjezana Sevo-Zenzerovic <snjezana.sevozenzerovic@oracle.com>
2008-05-27 19:39:22 +0000

    Modified message.
    svn path=/branches/tp2/; revision=20893

--- installer/src/main/resources/wrappers/wrapper.sh
+++ installer/src/main/resources/wrappers/wrapper.sh
@@ -2,7 +2,7 @@
 
 tmp=`mktemp -d -t install.XXXX`
 if [ $? -ne 0 ]; then
-    echo "Can't create temporary directory, exiting..."
+    echo "Unable to create temporary directory, exiting..."
     exit 1
 fi
 echo "Extracting archive, please wait..."
@@ -14,5 +14,5 @@ chmod ugo+x product-installer.sh
 chmod ugo+x install/bin/engine-wrapper
 echo "InstallHome.directory.INSTALL_HOME=$HOME/glassfish-v3tp2" > install.properties
 sh product-installer.sh
-rm -rf $tmp/* 
+rm -rf ${tmp}/*
 exit $?
