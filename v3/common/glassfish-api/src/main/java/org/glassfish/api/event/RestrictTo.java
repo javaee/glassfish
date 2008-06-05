62157988c981472532de6a484ebd656eccdf3ea1
Jerome Dochez <dochez@java.net>
2008-06-05 05:20:47 +0000

    more event stuff, with event types and events facility
    svn path=/trunk/; revision=20967
    
    
    Former-commit-id: b5c98d42a982e74557385be9970c69f29c4cc6ba

---
+++ v3/common/glassfish-api/src/main/java/org/glassfish/api/event/RestrictTo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.glassfish.api.event;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Sometimes listeners are not interested in receiving all event types, forcing 
+ * them in checking the event type before doing any type of processing.
+ * 
+ * Alternatively, they can use this annotation to restrict the parameter type 
+ * or value they really are interested in. In this case, they can restrict
+ * to a event type name so that their listener is only called when such event
+ * type is dispatched.
+ * 
+ * @author Jerome Dochez
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RestrictTo {
+    
+    String value();
+}
