ba063a02213e0890bd2ed94746ed5bd5f558f35a
dochez <dochez@java.net>
2008-06-17 23:17:31 +0000

    removed the mvn deploy that was pushing SNAPSHOT to the repository

--- hk2/release.sh
+++ hk2/release.sh
@@ -39,7 +39,7 @@ mvn -B release:perform
 
 # when other people get the new workspace, they'll fail to resolve maven-hk2-plugin,
 # so we need some seed version to be out there.
-mvn deploy
+# mvn deploy
 
 # Once the bits are pushed and made visible, you just need to change v3/pom.xml <hk2.version> property
 # and GFv3 will pick up the new version of HK2.
