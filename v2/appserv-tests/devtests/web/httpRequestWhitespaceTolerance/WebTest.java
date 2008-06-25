a4c2f55c98a84198826ef2d367544a0d5d26f297
Shing Wai Chan <shing.wai.chan@oracle.com>
2008-06-25 21:03:25 +0000

    add new test case for Whitespace tolerance in http request https://grizzly.dev.java.net/issues/show_bug.cgi?id=163
    svn path=/trunk/; revision=21122
    
    
    Former-commit-id: 6e518b3e80273a264dd345e2417c2bb1b22bcf35

---
+++ v2/appserv-tests/devtests/web/httpRequestWhitespaceTolerance/WebTest.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.net.*;
+import com.sun.ejte.ccl.reporter.*;
+
+/**
+ * Unit test for:
+ *
+ *  https://grizzly.dev.java.net/issues/show_bug.cgi?id=163
+ *  ("Whitespace tolerance in http request")
+ *
+ */
+public class WebTest {
+
+    private static SimpleReporterAdapter stat
+        = new SimpleReporterAdapter("appserv-tests");
+
+    private static final String TEST_NAME
+        = "http-request-whitespace-tolerance";
+
+    private static final String EXPECTED = "Hello World";
+
+    private String host;
+    private String port;
+    private String contextRoot;
+
+    public WebTest(String[] args) {
+        host = args[0];
+        port = args[1];
+        contextRoot = args[2];
+    }
+    
+    public static void main(String[] args) {
+        stat.addDescription("Unit test for SSI Escape Character");
+        WebTest webTest = new WebTest(args);
+        webTest.doTest();
+        stat.printSummary(TEST_NAME);
+    }
+
+    public void doTest() {     
+        try { 
+            invoke();
+        } catch (Exception ex) {
+            stat.addStatus(TEST_NAME, stat.FAIL);
+            ex.printStackTrace();
+        }
+    }
+
+    private void invoke() throws Exception {
+
+        System.out.println("Host=" + host + ", port=" + port);        
+        Socket sock = new Socket(host, new Integer(port).intValue());
+        OutputStream os = sock.getOutputStream();
+        String get = "GET   " + contextRoot + "/index.jsp  HTTP/1.1\n";
+        System.out.println(get);
+        os.write(get.getBytes());
+        os.write("Host: localhost\n".getBytes());
+        os.write("\n".getBytes());
+
+        InputStream is = sock.getInputStream();
+        BufferedReader bis = new BufferedReader(new InputStreamReader(is));
+
+        String line = null;
+        boolean hasExpectedResponse = false;
+        while ((line = bis.readLine()) != null) {
+            System.out.println(line);
+            if (line.equals(EXPECTED)) {
+                hasExpectedResponse = true;
+                break;
+            }
+        }
+
+        if (hasExpectedResponse) {
+            stat.addStatus(TEST_NAME, stat.PASS);
+        } else {
+            System.err.println("Missing expected response: " + EXPECTED);
+            stat.addStatus(TEST_NAME, stat.FAIL);
+        }
+    }
+}
