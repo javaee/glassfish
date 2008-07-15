ee8b9b92284dfecad26b7220d680f1035f2a1e84
Sathyan Catari <scatari@java.net>
2008-07-15 07:20:42 +0000

    Enabling silent installer support on Unix platform. Parsing of command line arguments will be handled by Open Installer scripts. The wrapper script would just pass the arguments. Downside of this approach is that Open Installer tries to validate and support too many options that are out of scope for V3 GlassFish Installer. The validations will later be added to the wrapper script itself for MS2.
    svn path=/trunk/; revision=21252
    
    
    Former-commit-id: 88bf816dfa299a867b8ebc5b9d21c269314558d9

--- v3/installer/src/main/resources/wrappers/wrapper.sh
+++ v3/installer/src/main/resources/wrappers/wrapper.sh
@@ -6,13 +6,18 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 echo "Extracting archive, please wait..."
-tail +19l $0 > $tmp/tmp.jar
+tail +24l $0 > $tmp/tmp.jar
 cd $tmp
 $JAVA_HOME/bin/jar xvf tmp.jar 
 rm tmp.jar
 chmod ugo+x product-installer.sh
 chmod ugo+x install/bin/engine-wrapper
 echo "InstallHome.directory.INSTALL_HOME=$HOME/glassfish-v3tp2" > install.properties
+if [ $# -eq 0 ]
+then
 sh product-installer.sh
+else
+sh product-installer.sh $*
+fi
 rm -rf ${tmp}/*
 exit $?
