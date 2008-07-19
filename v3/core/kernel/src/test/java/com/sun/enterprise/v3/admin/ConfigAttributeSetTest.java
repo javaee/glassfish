be8595c85226e3e8a42f66d69e99c482f73f63a7
Jerome Dochez <dochez@java.net>
2008-07-19 08:51:52 +0000

    added Set command test, testing attribute set and event reception
    svn path=/trunk/; revision=21321
    
    
    Former-commit-id: 666f1a1da46880ce19a29d95e9a43bc7e505dec7

---
+++ v3/core/kernel/src/test/java/com/sun/enterprise/v3/admin/ConfigAttributeSetTest.java
@@ -0,0 +1,96 @@
+package com.sun.enterprise.v3.admin;
+
+import org.glassfish.tests.utils.ConfigApiTest;
+import org.glassfish.tests.utils.Utils;
+import org.glassfish.api.admin.AdminCommand;
+import org.glassfish.api.admin.AdminCommandContext;
+import org.glassfish.api.ActionReport;
+import org.jvnet.hk2.config.*;
+import org.jvnet.hk2.component.Habitat;
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.*;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+import java.beans.PropertyChangeEvent;
+
+import com.sun.enterprise.v3.common.HTMLActionReporter;
+import com.sun.enterprise.config.serverbeans.HttpService;
+import com.sun.enterprise.config.serverbeans.HttpListener;
+import junit.framework.Assert;
+
+/**
+ * test the set command
+ * @author Jerome Dochez
+ */
+public class ConfigAttributeSetTest  extends ConfigApiTest implements ConfigListener {
+
+    Habitat habitat = Utils.getNewHabitat(this);
+    PropertyChangeEvent event = null;
+
+    public DomDocument getDocument(Habitat habitat) {
+        return new TestDocument(habitat);
+    }
+
+    /**
+     * Returns the DomainTest file name without the .xml extension to load the test configuration
+     * from.
+     *
+     * @return the configuration file name
+     */
+    public String getFileName() {
+        return "DomainTest";
+    }     
+
+    @Test
+     public void simpleAttributeSetTest() {
+
+        CommandRunner runner = habitat.getComponent(CommandRunner.class);
+        assertNotNull(runner);
+
+        // let's find our target
+        HttpListener listener = null;
+        HttpService service = habitat.getComponent(HttpService.class);
+        for (HttpListener l : service.getHttpListener()) {
+            if ("http-listener-1".equals(l.getId())) {
+                listener = l;
+                break;
+            }
+        }
+        assertNotNull(listener);        
+
+        // Let's regiter a listener
+        ObservableBean bean = (ObservableBean) ConfigSupport.getImpl(listener);
+        bean.addListener(this);
+
+        Properties parameters = new Properties();
+        parameters.put("value", "8090");
+        parameters.put("DEFAULT", "configs.config.server-config.http-service.http-listener.http-listener-1.port");
+
+        ActionReport report = new HTMLActionReporter();
+
+        runner.doCommand("set",  parameters, report);
+                                                                                                                                                                                                                           
+        // check the result.
+        String port = listener.getPort();
+        assertEquals(port, "8090");
+
+       Transactions.get().waitForDrain();
+        
+        // finally
+        bean.removeListener(this);
+
+        // check we recevied the event
+        assertNotNull(event);
+        assertEquals("8080", event.getOldValue());
+        assertEquals("8090", event.getNewValue());
+        assertEquals("port", event.getPropertyName());
+        
+    }
+
+    public void changed(PropertyChangeEvent[] propertyChangeEvents) {
+        assertEquals("Array size", propertyChangeEvents.length, 1 );
+        event = propertyChangeEvents[0];
+    }
+}
