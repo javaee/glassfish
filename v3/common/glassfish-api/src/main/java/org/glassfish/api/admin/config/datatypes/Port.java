c62667853a62c6a351dbe64efe9cc383e167d469
Kedar Mhaswade <km@java.net>
2008-08-04 21:59:39 +0000

    Bumping up the glassfish-api version.
    Rest of the builds are unaffected (if I understand the build system correctly!).
    
    
    
    svn path=/trunk/; revision=21576
    
    
    Former-commit-id: c18f824b0dd33c80072c1bc31eb337ab227a7250

---
+++ v3/common/glassfish-api/src/main/java/org/glassfish/api/admin/config/datatypes/Port.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.glassfish.api.admin.config.datatypes;
+import org.jvnet.hk2.config.DataType;
+import org.jvnet.hk2.config.ValidationException;
+
+/** Represents a network port on a machine. It's modeled as a functional class.
+ * @author &#2325;&#2375;&#2342;&#2366;&#2352 (km@dev.java.net)
+ */
+public final class Port extends DataType {
+
+    /** Checks if given string represents a port. Does not allow any value other
+     *  than those between 0 and 65535 (both inclusive).
+     * @param value represents the value of the port. 
+     * @throws org.jvnet.hk2.config.ValidationException if the value does not represent
+     * integer value.
+     */
+    @Override
+    public void validate(String value) throws ValidationException {
+        try {
+            int port = Integer.parseInt(value);
+            if (port < 0 || port > 0xFFFF) //taken from ServerSocket.java
+                throw new ValidationException("value: " + port + "not applicable for Port [0, " + 0xFFFF + "] data type");
+        } catch(NumberFormatException e) {
+            throw new ValidationException(e);
+        }
+    }
+}
---
+++ v3/common/glassfish-api/src/main/java/org/glassfish/api/admin/config/datatypes/PositiveInteger.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.glassfish.api.admin.config.datatypes;
+
+import org.jvnet.hk2.config.DataType;
+import org.jvnet.hk2.config.ValidationException;
+
+/** Represents an integer from 1 to Integer.MAX_VALUE. 
+ *  It's modeled as a functional class.
+ * @author &#2325;&#2375;&#2342;&#2366;&#2352 (km@dev.java.net) 
+ */
+public class PositiveInteger extends DataType { //could extend NonNegativeInteger
+    
+    /** Validates the given value as a positive integer.
+     * @param value
+     * @throws org.jvnet.hk2.config.ValidationException
+     */
+    @Override
+    public void validate(String value) throws ValidationException {
+        try {
+            int number = Integer.parseInt(value);
+            if (number < 1 || number > Integer.MAX_VALUE) //taken from ServerSocket.java
+                throw new ValidationException("value: " + number + "not applicable for PositiveInteger [1, " + Integer.MAX_VALUE + "] data type");
+        } catch(NumberFormatException e) {
+            throw new ValidationException(e);
+        }
+    }
+}
