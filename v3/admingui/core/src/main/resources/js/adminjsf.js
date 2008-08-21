907677c570a543a0beb2655944e5c0f919e85b2f
Anissa Lam <anissa.lam@oracle.com>
2008-08-21 04:22:07 +0000

    issue# 5449.  Remove alert msg which is debug statement.
    svn path=/trunk/; revision=22019
    
    
    Former-commit-id: b018e697a943fc2cecd0975c2a6c7909683763f9

--- v3/admingui/core/src/main/resources/js/adminjsf.js
+++ v3/admingui/core/src/main/resources/js/adminjsf.js
@@ -83,7 +83,7 @@ function disableComponent(componentName, type) {
         component = getSelectElement(componentName);
     }
     else {
-        alert('new/edit realm');
+        //alert('new/edit realm');
         component = getTextElement(componentName);
         component.value='';
     }
