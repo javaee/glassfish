b88c2bfeb53d6f77ff3dbcee567f5bd7003a7441
Jerome Dochez <dochez@java.net>
2008-08-27 05:38:28 +0000

    upgraded hk2 to 0.3.16 to bring in new behaviour for IT 5438
    now config changes events are propagated to the immediate parent
    of the changed object on top of notifying registered listeners on 
    the object itself. 
    
    This is particularily useful when configuration like Property are
    changed, so you don't have to register an interest on each 
    property. 
    
    Added config-api unit test
    
    
    svn path=/trunk/; revision=22150
    
    
    Former-commit-id: 80ecd05186f287c890136b96886242c7125bc193

---
+++ v3/admin/config-api/src/test/java/com/sun/enterprise/configapi/tests/PropertyChangeListenerTest.java
@@ -0,0 +1,74 @@
+package com.sun.enterprise.configapi.tests;
+
+import org.jvnet.hk2.component.Habitat;
+import org.jvnet.hk2.config.*;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.glassfish.tests.utils.Utils;
+import com.sun.hk2.component.ConstructorWomb;
+import com.sun.enterprise.config.serverbeans.*;
+
+
+import java.beans.PropertyVetoException;
+import java.beans.PropertyChangeEvent;
+
+/**
+ * This test listen to a property change event only injecting the parent containing the property.
+ *
+ * @author Jerome Dochez
+ */
+public class PropertyChangeListenerTest  extends ConfigApiTest implements ConfigListener {
+
+    Habitat habitat;
+    boolean result = false;
+
+    public String getFileName() {
+        return "DomainTest";
+    }
+
+    @Before
+    public void setup() {
+        habitat = Utils.getNewHabitat(this);
+    }
+
+    @Test
+    public void propertyChangeEventReceptionTest() throws TransactionFailure {
+
+        HttpService  httpService = habitat.getComponent(HttpService.class);
+        assertNotNull(httpService);
+
+       // let's find a acceptable target.
+        VirtualServer target =null;
+        for (VirtualServer vs : httpService.getVirtualServer()) {
+            if (!vs.getProperty().isEmpty()) {
+                target = vs;
+                break;
+            }
+        }
+
+        assertNotNull(target);
+
+        ((ObservableBean) ConfigSupport.getImpl(target)).addListener(this);
+        final Property prop  = target.getProperty().get(0);
+
+        ConfigSupport.apply(new SingleConfigCode<Property>() {
+
+            public Object run(Property param) throws PropertyVetoException, TransactionFailure {
+                // first one is fine...
+                param.setValue(prop.getValue().toUpperCase());
+                return null;
+            }
+        }, prop);
+
+        System.out.println("Property = " + target.getProperty().get(0).getValue());
+        Transactions.get().waitForDrain();
+        assertTrue(result);
+        ((ObservableBean) ConfigSupport.getImpl(target)).removeListener(this);
+    }
+
+    public UnprocessedChangeEvents changed(PropertyChangeEvent[] events) {
+        result = true;
+        return null;
+    }
+}
