3b4180977c73fc64f3d3b993cf8f3ce938afe06c
Sathyan Catari <scatari@java.net>
2008-09-05 00:35:41 +0000

    Fix for 5853. Workaround for OI issue related to default pkg type. Explicitly setting pkg type to "zip" in the wrapper.
    svn path=/trunk/; revision=22379
    
    
    Former-commit-id: eef6d1bc580ebc4e3fe397abfc90152a94c32119

--- v3/installer/src/main/resources/wrappers/uninstall.sh
+++ v3/installer/src/main/resources/wrappers/uninstall.sh
@@ -20,14 +20,14 @@ fi
 #Check if config is available
 if [ ! -d ${CWD}/var/install/config/glassfish ]
 then
-	echo "Config directory is missing from this installation. Aboring uninstallation."
+	echo "Config directory is missing from this installation. Aborting uninstallation."
 	exit ${CONFIG_DIRECTORY_MISSING}
 fi
 
 #Check if metadata is available
 if [ ! -d ${CWD}/metadata ]
 then
-	echo "metadata directory is missing from this installation. Aboring uninstallation."
+	echo "metadata directory is missing from this installation. Aborting uninstallation."
 	exit ${METADATA_DIRECTORY_MISSING}
 fi
 }
@@ -36,7 +36,7 @@ fi
 fireUninstaller() {
 cd ${CWD}/install/bin
 #Pass in any additional arguments passed to the script.
-./uninstaller -s ${CWD}/var/install/config/glassfish -m file://${CWD}/metadata -p Default-Product-ID=glassfish -J "-Dorg.openinstaller.provider.configurator.class=org.openinstaller.provider.conf.InstallationConfigurator" $*
+./uninstaller -s ${CWD}/var/install/config/glassfish -m file://${CWD}/metadata -p Default-Product-ID=glassfish -p Pkg-Format=zip -J "-Dorg.openinstaller.provider.configurator.class=org.openinstaller.provider.conf.InstallationConfigurator" $*
 #Pass the exit code from OI back to the env.
 exit $?
 }
