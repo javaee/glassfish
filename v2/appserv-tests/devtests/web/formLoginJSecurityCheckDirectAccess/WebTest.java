fe6a4a44a70cd6d6e408ae6732be3f1d6643c1d8
Jan Luehe <jluehe@java.net>
2008-09-06 21:06:59 +0000

    Revised fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=5948 ("[devtest failure] form-login-jsecurity-check-direct-access: FAIL"):
    Changed test to become independent of admin-realm, so it won't break should admin name and password change again in the future
    
    svn path=/trunk/; revision=22439
    
    
    Former-commit-id: 051bf5c287d975df1811bf6d312524875eeffaae

--- v2/appserv-tests/devtests/web/formLoginJSecurityCheckDirectAccess/WebTest.java
+++ v2/appserv-tests/devtests/web/formLoginJSecurityCheckDirectAccess/WebTest.java
@@ -29,8 +29,8 @@ public class WebTest {
         host = args[0];
         port = args[1];
         contextRoot = args[2];
-        adminUser = "anonymous";
-        adminPassword = "";
+        adminUser = args[3];
+        adminPassword = args[4];
     }
     
     public static void main(String[] args) {
