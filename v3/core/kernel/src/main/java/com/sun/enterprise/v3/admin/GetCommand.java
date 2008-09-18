7bd7178f91eb9d0fe626a6a8d92435a4c673840b
Jerome Dochez <dochez@java.net>
2008-09-18 23:46:58 +0000

    bumpled hk2 to stop writing default values to domain.xml IT 5608 : aliased information lost when displaying get command output IT 6081 : default values not displayed with asadmin get command
    changed default domain.xml to remove all default-values.
     
    
    svn path=/trunk/; revision=22792
    
    
    Former-commit-id: 477080e6c40c67fa7895216b49f0fb0e818566f6

--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/admin/GetCommand.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/admin/GetCommand.java
@@ -1,9 +1,46 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
 package com.sun.enterprise.v3.admin;
 
 import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.annotations.Inject;
+import org.jvnet.hk2.annotations.Scoped;
 import org.jvnet.hk2.config.Dom;
 import org.jvnet.hk2.component.Habitat;
+import org.jvnet.hk2.component.PerLookup;
 import org.glassfish.api.admin.AdminCommand;
 import org.glassfish.api.admin.AdminCommandContext;
 import org.glassfish.api.ActionReport;
@@ -25,6 +62,7 @@ import org.glassfish.flashlight.datatree.MethodInvoker;
  * Time: 12:17:26 AM
  */
 @Service(name="get")
+@Scoped(PerLookup.class)
 public class GetCommand extends V2DottedNameSupport implements AdminCommand {
 
     @Inject
@@ -58,6 +96,10 @@ public class GetCommand extends V2DottedNameSupport implements AdminCommand {
         }
 
         // reset the pattern.
+        String prefix="";
+        if (!pattern.startsWith(parentNodes[0].relativeName)) {
+            prefix= pattern.substring(0, pattern.indexOf(parentNodes[0].relativeName)+1);
+        }
         pattern = parentNodes[0].relativeName;
 
         Map<Dom, String> matchingNodes = getMatchingNodes(dottedNames, pattern);
@@ -74,7 +116,7 @@ public class GetCommand extends V2DottedNameSupport implements AdminCommand {
                 if (matches(node.getValue(), pattern)) {
                     ActionReport.MessagePart part = report.getTopMessagePart().addChild();
                     part.setChildrenType("DottedName");
-                    part.setMessage(node.getValue() + "=" + node.getKey().attribute("value"));
+                    part.setMessage(prefix + node.getValue() + "=" + node.getKey().attribute("value"));
                 }
             }   else {
                 Map<String, String> attributes = getNodeAttributes(node.getKey(), pattern);
@@ -84,7 +126,7 @@ public class GetCommand extends V2DottedNameSupport implements AdminCommand {
                     if (matches(finalDottedName, pattern)) {
                         ActionReport.MessagePart part = report.getTopMessagePart().addChild();
                         part.setChildrenType("DottedName");
-                        part.setMessage(node.getValue() + "." + name.getKey() + "=" + name.getValue());
+                        part.setMessage(prefix + node.getValue() + "." + name.getKey() + "=" + name.getValue());
                     }
                 }
             }
