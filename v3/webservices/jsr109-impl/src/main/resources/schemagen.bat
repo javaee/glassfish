806118ac354180b5f39c9e7b50582056778524dd
Bhakti Mehta <bhaktimehta@java.net>
2008-09-19 20:39:35 +0000

    Initial checkin for scripts for webservices
    svn path=/trunk/; revision=22827
    
    
    Former-commit-id: 2cad6f30fa78d56b860b2b896126abcf75cd45ed

---
+++ v3/webservices/jsr109-impl/src/main/resources/schemagen.bat
@@ -0,0 +1,8 @@
+@echo off
+
+REM
+REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
+REM Use is subject to license terms.
+REM
+
+java -cp "%~dp0..\modules\jaxb.jar;%~dp0..\modules\webservices.jar" com.sun.tools.jxc.SchemaGeneratorFacade "%*"
