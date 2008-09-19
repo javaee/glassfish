c75994288ceee151609d24e9b7b575ad374fe6f0
Kin Man Chung <kinman.chung@oracle.com>
2008-09-19 18:26:54 +0000

    - Fix a typo
    svn path=/trunk/; revision=22812
    
    
    Former-commit-id: a926c8fe7bf7fca6039ac64e213c8d2bc28aa405

--- v3/distributions/nucleus-base/bin/jspc.bat
+++ v3/distributions/nucleus-base/bin/jspc.bat
@@ -5,6 +5,5 @@ REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
 REM
 
-java -cp "%~dp0..\modules\javax.javaee-@VERSION@.jar;%~dp0..\modules\ant-@VERSIO
-N@.jar;%~dp0..\modules\@EL_IMPL@;%~dp0..\modules\@JSP_IMPL@" org.apache.jasper.JspC -classpath "%~dp0..\modules\web\@JSTL_IMPL@;%~dp0..\modules\web\@JSF_IMPL@ "%*"
+java -cp "%~dp0..\modules\javax.javaee-@VERSION@.jar;%~dp0..\modules\ant-@VERSION@.jar;%~dp0..\modules\@EL_IMPL@;%~dp0..\modules\@JSP_IMPL@" org.apache.jasper.JspC -classpath "%~dp0..\modules\web\@JSTL_IMPL@;%~dp0..\modules\web\@JSF_IMPL@ "%*"
 
