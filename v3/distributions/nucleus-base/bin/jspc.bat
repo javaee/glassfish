2a7a2244b02a6adfd2b0d56ed34546e5780643b2
Kin Man Chung <kinman.chung@oracle.com>
2008-09-22 23:46:44 +0000

    - Modify, now that javax.javaee.jar is no more.
    svn path=/trunk/; revision=22872
    
    
    Former-commit-id: 9ce8e5374dfa6c684ca169bc34dc6e88c08b2fbd

--- v3/distributions/nucleus-base/bin/jspc.bat
+++ v3/distributions/nucleus-base/bin/jspc.bat
@@ -1,9 +1,18 @@
 @echo off
-
 REM
 REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
 REM
 
-java -cp "%~dp0..\modules\javax.javaee-@VERSION@.jar;%~dp0..\modules\ant-@VERSION@.jar;%~dp0..\modules\@EL_IMPL@;%~dp0..\modules\@JSP_IMPL@" org.apache.jasper.JspC -classpath "%~dp0..\modules\web\@JSTL_IMPL@;%~dp0..\modules\web\@JSF_IMPL@ "%*"
+set AS_INSTALL_LIB=%~dp0..\modules
+set ANT_LIB=%AS_INSTALL_LIB%\ant-@VERSION@.jar
+set SERVLET_API=%AS_INSTALL_LIB%\@SERVLET_API@
+set JSP_API=%AS_INSTALL_LIB%\@JSP_API@
+set JSP_IMPL=%AS_INSTALL_LIB%\@JSP_IMPL@
+set EL_IMPL=%AS_INSTALL_LIB%\@EL_IMPL@
+set JSTL_API=%AS_INSTALL_LIB%\@JSTL_API@
+set JSTL_IMPL=%AS_INSTALL_LIB%\@JSTL_IMPL@
+set JSF_API=%AS_INSTALL_LIB%\@JSF_API@
+set JSF_IMPL=%AS_INSTALL_LIB%\@JSF_IMPL@
 
+java -cp "%SERVLET_API%;%JSP_API%;%JSTL_API%;%JSF_API%;%ANT_LIB%;%EL_IMPL%;%JSP_IMPL%" org.apache.jasper.JspC -classpath "%JSTL_IMPL%;%JSF_IMPL%" %*
