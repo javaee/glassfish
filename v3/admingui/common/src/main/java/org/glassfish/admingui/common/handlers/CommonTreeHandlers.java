e544402e175a94ecf65afa3e2a21c590314d559e
Ken Paulsen <kenpaulsen@java.net>
2008-09-30 20:15:25 +0000

    * Added a big hack which walks the tree and changes the renderers of all   the components it find (including woodstock hidden components which are   not normally findable) to use Woodstock HTML renderers instead of   Woodstock widget renderers.  This is important because Woodstock widget   renderers do not work well with our Ajax.
    svn path=/trunk/; revision=23128
    
    
    Former-commit-id: 92c0895226536f75371565ee690cf71654d340e2

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/CommonTreeHandlers.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/CommonTreeHandlers.java
@@ -10,7 +10,11 @@ import org.glassfish.admingui.common.util.AMXUtil;
 import org.glassfish.admingui.common.tree.FilterTreeEvent;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
 
 /**
  *
@@ -49,6 +53,89 @@ public class CommonTreeHandlers {
         }
         return result;
     }
-     
-    
+
+     @Handler(id="removeWidgetsFromTree",
+     input={
+        @HandlerInput(name="tree", type=UIComponent.class, required=true)})
+    public static void removeWidgetTreeHack(HandlerContext handlerCtx) {
+	// Hack 4.3 WS tree so that it doesn't use widget components...
+	UIComponent treeComp = (UIComponent) handlerCtx.getInputValue("tree");
+	doHack(treeComp);
+    }
+
+    /**
+     *	Ugly hack, remove asap.
+     */
+    private static void doHack(UIComponent currComp) {
+	// ImageHyperlink hidden child (getImageFacet())
+	UIComponent hackComp = (UIComponent) currComp.getAttributes().get("imageFacet");
+	if (hackComp != null) {
+	    hackComp.setRendererType(hackComp.getFamily());
+	    doHack(hackComp);
+	}
+
+	// Content hyperlink...
+	hackComp = (UIComponent) currComp.getAttributes().get("contentHyperlink");
+	if (hackComp != null) {
+	    hackComp.setRendererType(hackComp.getFamily());
+	    doHack(hackComp);
+	}
+
+	// Turner: (NOTE: only calling the next method to see if I should replace the image)
+	FacesContext ctx = FacesContext.getCurrentInstance();
+	hackComp = (UIComponent) currComp.getAttributes().get("turnerImageHyperlink");
+	if (hackComp != null) {
+	    // FIXME: Replace with glassfish.ImageHyperlink (see our faces-config.xml)
+	    hackComp = ctx.getApplication().createComponent("com.sun.webui.jsf.IconHyperlink");
+	    hackComp.setId(currComp.getId().concat("_turner"));
+	    hackComp.setRendererType(hackComp.getFamily());
+	    currComp.getFacets().put("_turner", hackComp);
+	    doHack(hackComp);
+	}
+
+	// Node Image: (NOTE: only calling the next method to see if I should replace the image)
+	hackComp = (UIComponent) currComp.getAttributes().get("nodeImageHyperlink");
+	if (hackComp != null) {
+	    // FIXME: Replace with glassfish.ImageHyperlink (see our faces-config.xml)
+	    hackComp = ctx.getApplication().createComponent("com.sun.webui.jsf.ImageHyperlink");
+	    hackComp.setId(currComp.getId().concat("_image"));
+	    hackComp.setRendererType(hackComp.getFamily());
+	    currComp.getFacets().put("_image", hackComp);
+	    doHack(hackComp);
+	}
+
+	// imageKeys
+	List<UIComponent> hackList = (List<UIComponent>) currComp.getAttributes().get("imageKeys");
+	if (hackList != null) {
+	    for (UIComponent child : hackList) {
+		// WS component family values match their HTML renderer types...
+		// so copy family to rendererType
+		child.setRendererType(child.getFamily());
+		doHack(child);
+	    }
+	}
+
+	// Facets and children (recurses)
+	Iterator<UIComponent> it = currComp.getFacetsAndChildren();
+	UIComponent child = null;
+	while (it.hasNext()) {
+	    child = it.next();
+	    if (child.getClass().getName().equals("com.sun.webui.jsf.component.TreeNode")) {
+		// We have a TreeNode, do some special stuff...
+		// imageKeys
+		hackList = (List<UIComponent>) currComp.getAttributes().get("imageKeys");
+		for (UIComponent treeImage : hackList) {
+		    // WS component family values match their HTML renderer types...
+		    // so copy family to rendererType
+		    treeImage.setRendererType(treeImage.getFamily());
+		}
+	    } else {
+		// WS component family values match their HTML renderer types...
+		// so copy family to rendererType
+		child.setRendererType(child.getFamily());
+		/* Not sure how we get to this code-path in Deep's impl... */
+	    }
+	    doHack(child);
+	}
+    }
 }
