32e7d736a7b5307ae87bfa327d97be7ab2dbf756
Byron Nevins <byron.nevins@oracle.com>
2008-10-07 21:45:21 +0000

    Smoketest work continues...
    svn path=/trunk/; revision=23265
    
    
    Former-commit-id: d95c4038fedac07f9e6284b3f7a20da2de577438

--- v3/embedded/packager/smoketests/src/test/java/smoke/SmokeTest.java
+++ v3/embedded/packager/smoketests/src/test/java/smoke/SmokeTest.java
@@ -5,22 +5,96 @@
 
 package smoke;
 
+import java.io.*;
+import org.glassfish.embed.*;
 import org.glassfish.embed.AppServer;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 /**
- *
+ * Important Note:
+ *  Need a negative test case for bad port number -- or any initialization failure
+ * of AppServer.  Currently no exception is thrown back!!!!!
  * @author bnevins
  */
 public class SmokeTest {
-    private static AppServer myGF;
+    //private static AppServer myGF;
     
     @BeforeClass
     public static void setUpClass() {
+        banner("Starting SmokeTests for GFE-All");
+    }
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        banner("Finished SmokeTests for GFE-All");
+    }
+
+    //@Test (expected=IllegalArgumentException.class)
+    /*
+    @Test
+    public void badPortNumber() {
         try {
+            
+            // oops!  GF Core does a System.exit() on myGF.stop() !!!!
+            // Need to fork a VM for EVERY test  -- or use a different file for negative tests...
+            AppServer myGF = new AppServer(65999);
+            banner("NEED to change core API --> this should be a FAT error!");
+            myGF.stop();
+        }
+        catch(IllegalArgumentException e) {
+        }
+        
+        catch(Exception e) {
+        }
+    }
+    */
+    
+    /* 
+     * Another negative test -- this triggeres a stop in the "GF Cloud" which
+     * does a System.exit()
+     *
+    
+    @Test
+    public void badWar() throws Exception{
+        File qqq = new File("xxxxxx");
+        AppServer myGF = new AppServer(9999);
+        App app = null;
+        
+        try {
+            app = myGF.deploy(qqq);
+        }
+        catch(Exception e) {
+            System.out.println("Caught an Exception as Expected: " + e);
+            return;
+        }
+        fail("Did not get expected Exception from deploying garbage!");
+    }
+    */
+    
+    
+    @Test
+    public void deploySimpleWar() {
+        try {
+            File qqq = new File("target/test-classes/simple.war");
+            assertTrue(qqq.exists());
+            AppServer myGF = new AppServer(9999);
+            App app = myGF.deploy(qqq);
+            
+            // TODO -- get output at port 9999
+            
+            myGF.stop();
+        }
+        catch(Exception e) {
+            fail("Got an Exception: " + e);
+        }
+    }
+    
+    /*
+     public static void setUpClass() {
+        try {
+            banner("Starting SmokeTests for GFE-All");
         System.out.println("Starting AppServer on port 9999");
-        myGF = new AppServer(9999);
+        myGF = new AppServer(999999);
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -29,24 +103,28 @@ public class SmokeTest {
 
     @AfterClass
     public static void tearDownClass() throws Exception {
+        System.out.println("TearDownClass Here!");
         System.out.println("Stopping AppServer...");
         myGF.stop();
     }
-
+    
+    
     @Before
     public void setUp() throws Exception {
+        banner("Setup Here");
     }
 
     @After
     public void tearDown() throws Exception {
+        banner("Teardown Here");
     }
-
+*/
     
-    
-    
-    @Test
-    public void hello() {
-        System.out.println("HELLO WORLD!!!!");
+    private static void banner(String s) {
+        System.out.println("\n");
+        System.out.println("**************************************************");
+        System.out.println("****  " + s); 
+        System.out.println("**************************************************");
+        System.out.println("\n");
     }
-
 }
