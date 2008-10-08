35dfa210f905f74ce4eb950574685e76e13c6a20
Byron Nevins <byron.nevins@oracle.com>
2008-10-08 05:45:00 +0000

    Hopefully fixed UNIX bug where the tests would fail if run from [1] and succeed if run from [2]
    [1] v3/embedded/packager
    
    [2] v3/embedded/packager/smoketests
    
    
    svn path=/trunk/; revision=23271
    
    
    Former-commit-id: e45ee7f84cc640271f0422ddc11a98a15f69201b

--- v3/embedded/packager/smoketests/src/test/java/smoke/SmokeTest.java
+++ v3/embedded/packager/smoketests/src/test/java/smoke/SmokeTest.java
@@ -18,10 +18,12 @@ import static org.junit.Assert.*;
  * @author bnevins
  */
 public class SmokeTest {
-    //private static AppServer myGF;
-    
     @BeforeClass
     public static void setUpClass() {
+        // get the war files from the src/test/resources dir....
+        simpleWar = new File(SmokeTest.class.getClassLoader().getResource("simple.war").getPath());
+        System.out.println("simpleWar= " + simpleWar);
+        assertTrue(simpleWar.exists());
         banner("Starting SmokeTests for GFE-All");
     }
     @AfterClass
@@ -29,6 +31,70 @@ public class SmokeTest {
         banner("Finished SmokeTests for GFE-All");
     }
 
+    @Test
+    public void deploySimpleWar() {
+        try {
+            File qqq = new File("target/test-classes/simple.war");
+            
+            if(!qqq.exists())
+                qqq = new File("smoketests/target/test-classes/simple.war");
+            
+            assertTrue(qqq.exists());
+            AppServer myGF = new AppServer(9999);
+            App app = myGF.deploy(qqq);
+            
+            // TODO -- get output at port 9999
+            
+            myGF.stop();
+        }
+        catch(Exception e) {
+            fail("Got an Exception: " + e);
+        }
+    }
+    private static void banner(String s) {
+        System.out.println("\n");
+        System.out.println("**************************************************");
+        System.out.println("****  " + s); 
+        System.out.println("**************************************************");
+        System.out.println("\n");
+    }
+    private static File simpleWar;
+}
+
+
+
+    
+    /*
+     public static void setUpClass() {
+        try {
+            banner("Starting SmokeTests for GFE-All");
+        System.out.println("Starting AppServer on port 9999");
+        myGF = new AppServer(999999);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        System.out.println("TearDownClass Here!");
+        System.out.println("Stopping AppServer...");
+        myGF.stop();
+    }
+    
+    
+    @Before
+    public void setUp() throws Exception {
+        banner("Setup Here");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        banner("Teardown Here");
+    }
+*/
+    
     //@Test (expected=IllegalArgumentException.class)
     /*
     @Test
@@ -72,63 +138,5 @@ public class SmokeTest {
     */
     
     
-    @Test
-    public void deploySimpleWar() {
-        try {
-            File qqq = new File("target/test-classes/simple.war");
-            
-            if(!qqq.exists())
-                qqq = new File("smoketests/target/test-classes/simple.war");
-            
-            assertTrue(qqq.exists());
-            AppServer myGF = new AppServer(9999);
-            App app = myGF.deploy(qqq);
-            
-            // TODO -- get output at port 9999
-            
-            myGF.stop();
-        }
-        catch(Exception e) {
-            fail("Got an Exception: " + e);
-        }
-    }
-    
-    /*
-     public static void setUpClass() {
-        try {
-            banner("Starting SmokeTests for GFE-All");
-        System.out.println("Starting AppServer on port 9999");
-        myGF = new AppServer(999999);
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-        System.out.println("TearDownClass Here!");
-        System.out.println("Stopping AppServer...");
-        myGF.stop();
-    }
-    
-    
-    @Before
-    public void setUp() throws Exception {
-        banner("Setup Here");
-    }
 
-    @After
-    public void tearDown() throws Exception {
-        banner("Teardown Here");
-    }
-*/
-    
-    private static void banner(String s) {
-        System.out.println("\n");
-        System.out.println("**************************************************");
-        System.out.println("****  " + s); 
-        System.out.println("**************************************************");
-        System.out.println("\n");
-    }
-}
