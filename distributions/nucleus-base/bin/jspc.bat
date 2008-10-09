55e5f6e46788534bc2d14a2db8ee8c62a3a9cb30
Kin Man Chung <kinman.chung@oracle.com>
2008-10-09 21:49:33 +0000

    - 6479: jspc not working
    svn path=/branches/v3_prelude_release/; revision=23328

--- distributions/nucleus-base/bin/jspc.bat
+++ distributions/nucleus-base/bin/jspc.bat
@@ -6,13 +6,13 @@ REM
 
 set AS_INSTALL_LIB=%~dp0..\modules
 set ANT_LIB=%AS_INSTALL_LIB%\ant.jar
-set SERVLET_API=%AS_INSTALL_LIB%\servlet-api.jar
-set JSP_API=%AS_INSTALL_LIB%\jsp-api.jar
-set JSP_IMPL=%AS_INSTALL_LIB%\jsp-impl.jar
-set EL_IMPL=%AS_INSTALL_LIB%\el-impl.jar
-set JSTL_API=%AS_INSTALL_LIB%\jstl-api.jar
-set JSTL_IMPL=%AS_INSTALL_LIB%\jstl-impl.jar
+set SERVLET_API=%AS_INSTALL_LIB%\javax.servlet.jar
+set JSP_API=%AS_INSTALL_LIB%\javax.servlet.jsp.jar
+set JSP_IMPL=%AS_INSTALL_LIB%\web\jsp-impl.jar
+set EL_IMPL=%AS_INSTALL_LIB%\web\el-impl.jar
+set JSTL_API=%AS_INSTALL_LIB%\web\javax.servlet.jsp.jstl.jar
+set JSTL_IMPL=%AS_INSTALL_LIB%\web\jstl-impl.jar
 set JSF_API=%AS_INSTALL_LIB%\jsf-api.jar
-set JSF_IMPL=%AS_INSTALL_LIB%\jsf-impl.jar
+set JSF_IMPL=%AS_INSTALL_LIB%\web\jsf-impl.jar
 
 java -cp "%SERVLET_API%;%JSP_API%;%JSTL_API%;%JSF_API%;%ANT_LIB%;%EL_IMPL%;%JSP_IMPL%" org.apache.jasper.JspC -classpath "%JSTL_IMPL%;%JSF_IMPL%" %*
