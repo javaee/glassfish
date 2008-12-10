4b868fa7715076c010cfcc65afb67951b16fa2de
Jan Luehe <jluehe@java.net>
2008-12-10 23:52:46 +0000

    Renamed servletProgrammaticRegistration unit test to servletAndFilterProgrammaticRegistration
    svn path=/trunk/; revision=23864
    
    
    Former-commit-id: baa24b6e4b17374672b0e75b98e81b812209f55f

---
+++ v2/appserv-tests/devtests/web/servletAndFilterProgrammaticRegistration/WebTest.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.*;
+import com.sun.ejte.ccl.reporter.*;
+
+/*
+ * Unit test for programmatic registration of servlets (Servlet 3.0 feature).
+ */
+public class WebTest {
+
+    private static final String TEST_NAME = "servlet-programmatic-registration";
+
+    private static SimpleReporterAdapter stat
+        = new SimpleReporterAdapter("appserv-tests");
+
+    private String host;
+    private String port;
+    private String contextRoot;
+
+    public WebTest(String[] args) {
+        host = args[0];
+        port = args[1];
+        contextRoot = args[2];
+    }
+    
+    public static void main(String[] args) {
+
+        stat.addDescription("Unit test for programmatic registration of servlets");
+        WebTest webTest = new WebTest(args);
+
+        try {
+            webTest.doTest();
+            stat.addStatus(TEST_NAME, stat.PASS);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            stat.addStatus(TEST_NAME, stat.FAIL);
+        }
+
+	stat.printSummary();
+    }
+
+    public void doTest() throws Exception {
+        URL url = new URL("http://" + host  + ":" + port
+                          + contextRoot + "/newServlet");
+        System.out.println("Connecting to: " + url.toString());
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.connect();
+        int responseCode = conn.getResponseCode();
+        if (responseCode != 200) {
+            throw new Exception("Unexpected return code: " + responseCode);
+        }
+    }
+}
