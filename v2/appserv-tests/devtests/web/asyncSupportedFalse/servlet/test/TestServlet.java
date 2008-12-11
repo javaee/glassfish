8863b96f956228cb9da047871ab8030a0a40e70c
Jan Luehe <jluehe@java.net>
2008-12-11 16:22:11 +0000

    Removed redundant code
    svn path=/trunk/; revision=23878
    
    
    Former-commit-id: 6c48f51c507412a212e60869de0e7a42707a375f

--- v2/appserv-tests/devtests/web/asyncSupportedFalse/servlet/test/TestServlet.java
+++ v2/appserv-tests/devtests/web/asyncSupportedFalse/servlet/test/TestServlet.java
@@ -1,7 +1,6 @@
 package test;
 
 import java.io.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -10,8 +9,6 @@ public class TestServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res)
             throws IOException, ServletException {
 
-        boolean success = false;
-        
         if (req.getAttribute("abc") == null) {
             throw new ServletException("Filter not invoked");
         }
