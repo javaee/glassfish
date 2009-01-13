2c71240c9d3b4e000469ea4c227e01b302c48f50
Byron Nevins <byron.nevins@oracle.com>
2009-01-13 22:21:43 +0000

    Added setListings method
    svn path=/branches/embedded_v3_prelude_release/; revision=24289
    
    
    Former-commit-id: a0de35eed9e66f06b9109dec61149c7a970341b3

--- web/web-core/src/main/java/org/apache/catalina/servlets/DefaultServlet.java
+++ web/web-core/src/main/java/org/apache/catalina/servlets/DefaultServlet.java
@@ -346,6 +346,16 @@ public class DefaultServlet
         }
 
     }
+    /**
+     * Enables or disables directory listings for this DefaultServlet.
+     *
+     * @param listings true if directory listings are to be enabled, false
+     * otherwise
+     */
+    public void setListings(boolean listings) {
+        this.listings = listings;
+    }
+
 
 
     // ------------------------------------------------------ Protected Methods
