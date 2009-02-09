e53a22a0126d85dc861ce0c56ee5174c1ca3c8b0
ss141213 <ss141213@java.net>
2009-02-09 04:11:33 +0000

    1. Add a new plugin called osgiversion-maven-plugin to fix https://glassfish.dev.java.net/issues/show_bug.cgi?id=7158 This plugin is used to set a predefined property called project.osgi.version which can then be used in pom.xmls. This plugin is used during build of all OSGi artifacts in HK2 like core, config, auto-depends, etc. If we make this part of hk2-maven-plugin, it will cause a circular dependency like this: hk2-maven-plugin -> hk2-maven -> hk2-core -> hk2-maven-plugin. So, this is separated from hk2-maven-plugin.
    2. Use the above plugin during build.

---
+++ hk2/osgiversion-maven-plugin/src/main/java/com/sun/enterprise/module/maven/OsgiVersionMojo.java
@@ -0,0 +1,138 @@
+/*
+ * 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ * 
+ * Contributor(s):
+ * 
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package com.sun.enterprise.module.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.osgi.Maven2OsgiConverter;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+
+/**
+ * Converts the project version into the OSGi format and
+ * set that to "project.osgi.version" property.
+ * It can be configured to drop certain portions from the
+ * version. See {@link #dropVersionComponent}.
+ *
+ * @author Kohsuke Kawaguchi
+ * @goal compute-osgi-version
+ * @phase validate
+ * @requiresProject
+ */
+public class OsgiVersionMojo extends AbstractMojo {
+    /**
+     * The maven project.
+     *
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    protected MavenProject project;
+
+    /**
+     * Flag used to determine what components of the version will be used
+     * in OSGi version.
+     * An OSGi version has four parts as shown below:
+     * major.minor.micro.qualifer.
+     * It is not always desirable to use all four parts while
+     * exporting packages. In fact, maven version and OSGi version
+     * behave just opposite during version comparison as shown below:
+     * a maven version 1.2.3-SNAPSHOT is mapped to OSGi version 1.2.3.SNAPSHOT.
+     * In maven, 1.2.3 > 1.2.3-SNAPSHOT, but in OSGi, 1.2.3 < 1.2.3.SNAPSHOT.
+     * So, it is highly desirable to drop qualifier while computing the version.
+     * Instead of hardcoding the policy, we let user tell us what portions will
+     * be used in the OSGi version. If they ask us to drop minor, then only
+     * major will be used. Similarly, if they ask us to drop qualifier, then
+     * major, minor and micro portions will be used.
+     * @parameter
+     */
+    protected String dropVersionComponent;
+
+    /**
+     * @parameter default-value="project.osgi.version"
+     */
+    protected String versionPropertyName;
+
+    private enum VERSION_COMPONENT {major, minor, micro, qualifier};
+
+    /**
+     * @component
+     */
+    protected Maven2OsgiConverter converter;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        DefaultArtifactVersion projectVersion =
+                new DefaultArtifactVersion(project.getVersion());
+        VERSION_COMPONENT compToDrop = dropVersionComponent == null ?
+                null : VERSION_COMPONENT.valueOf(dropVersionComponent);
+
+        DefaultArtifactVersion newVersion = projectVersion;
+        if (compToDrop != null) {
+            switch (compToDrop) {
+                case major: {
+                    newVersion = new DefaultArtifactVersion("0.0.0");
+                    break;
+                }
+                case minor: {
+                    final int major = projectVersion.getMajorVersion();
+                    newVersion = new DefaultArtifactVersion(major +"");
+                    break;
+                }
+                case micro: {
+                    final int major = projectVersion.getMajorVersion();
+                    final int minor = projectVersion.getMinorVersion();
+                    newVersion = new DefaultArtifactVersion(major + "." + minor);
+                    break;
+                }
+                case qualifier: {
+                    final int major = projectVersion.getMajorVersion();
+                    final int minor = projectVersion.getMinorVersion();
+                    final int micro = projectVersion.getIncrementalVersion();
+                    newVersion = new DefaultArtifactVersion(major + "." + minor + "." + micro);
+                    break;
+                }
+            }
+        }
+        String v = converter.getVersion(newVersion.toString());
+
+        getLog().info("OSGi Version for "+project.getVersion()+" is "+v);
+        getLog().info("It is set in project property called "+ versionPropertyName);
+        project.getProperties().put(versionPropertyName,v);
+    }
+}
