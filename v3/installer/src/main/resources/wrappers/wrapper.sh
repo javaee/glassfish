46e0fc3d25a241918dc6c36ad981b60a21aa4f8c
Sathyan Catari <scatari@java.net>
2009-02-12 19:11:58 +0000

    Adjusting the tail command to include the new lines added to this script.
    svn path=/trunk/; revision=24757
    
    
    Former-commit-id: c1bb463f6140264f21e4db5115c7dfe5d7412927

--- v3/installer/src/main/resources/wrappers/wrapper.sh
+++ v3/installer/src/main/resources/wrappers/wrapper.sh
@@ -150,7 +150,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 echo "Extracting archive, please wait..."
-tail +163l $0 > $tmp/tmp.jar
+tail +166l $0 > $tmp/tmp.jar
 cd $tmp
 $my_jar xvf tmp.jar 
 $my_jar xvf ./Product/Packages/Engine.zip 
