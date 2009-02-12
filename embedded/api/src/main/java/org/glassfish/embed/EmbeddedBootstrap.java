f00a80e5a6b45bc80bb980a12976c28f42dbcdfb
Jennifer Galloway <jc129909>
2009-02-12 03:37:03 +0000

    Back out the previous change - Removed code that specifically disables Security service to fix the build.  Some unit tests were breaking.
    svn path=/branches/embedded_v3_prelude_release/; revision=24750
    
    
    Former-commit-id: 4f00699edb81d62b5752d6850cffd0f4a693c3a4

--- embedded/api/src/main/java/org/glassfish/embed/EmbeddedBootstrap.java
+++ embedded/api/src/main/java/org/glassfish/embed/EmbeddedBootstrap.java
@@ -99,7 +99,6 @@ class EmbeddedBootstrap extends Main{
 
             // in the webtier-only bundle, these components don't exist to begin with.
 
-            /** bnevins -- let us see what happens when Security is not dropped 2/11/09
             try {
                 // security code needs a whole lot more work to work in the modular environment.
                 // disabling it for now.
@@ -111,7 +110,7 @@ class EmbeddedBootstrap extends Main{
             catch (LinkageError e) {
                 // maybe we are running in the webtier only bundle
             }
-             */
+             
         }
 
         // override the location of default-web.xml
