ddf835e47de88fce02fd8c435ac5aca7fabb1d63
Kin Man Chung <kinman.chung@oracle.com>
2009-02-13 19:34:38 +0000

    - Update jspc script, as jars that used to be in web have been relocated.
    svn path=/trunk/; revision=24779
    
    
    Former-commit-id: ca264aba89f6816ab0982161578ead8516d43b7d

--- v3/distributions/nucleus-base/bin/jspc.bat
+++ v3/distributions/nucleus-base/bin/jspc.bat
@@ -1,6 +1,6 @@
 @echo off
 REM
-REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
+REM Copyright 1997-2009 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
 REM
 
@@ -8,11 +8,12 @@ set AS_INSTALL_LIB=%~dp0..\modules
 set ANT_LIB=%AS_INSTALL_LIB%\ant.jar
 set SERVLET_API=%AS_INSTALL_LIB%\javax.servlet.jar
 set JSP_API=%AS_INSTALL_LIB%\javax.servlet.jsp.jar
-set JSP_IMPL=%AS_INSTALL_LIB%\web\jsp-impl.jar
-set EL_IMPL=%AS_INSTALL_LIB%\web\el-impl.jar
-set JSTL_API=%AS_INSTALL_LIB%\web\javax.servlet.jsp.jstl.jar
-set JSTL_IMPL=%AS_INSTALL_LIB%\web\jstl-impl.jar
+set JSP_IMPL=%AS_INSTALL_LIB%\jsp-impl.jar
+set EL_IMPL=%AS_INSTALL_LIB%\el-impl.jar
+set JSTL_API=%AS_INSTALL_LIB%\javax.servlet.jsp.jstl.jar
+set JSTL_IMPL=%AS_INSTALL_LIB%\jstl-impl.jar
 set JSF_API=%AS_INSTALL_LIB%\jsf-api.jar
-set JSF_IMPL=%AS_INSTALL_LIB%\web\jsf-impl.jar
+set JSF_IMPL=%AS_INSTALL_LIB%\jsf-impl.jar
+set AS_LIB=%~dp0..\lib
 
-java -cp "%SERVLET_API%;%JSP_API%;%JSTL_API%;%JSF_API%;%ANT_LIB%;%EL_IMPL%;%JSP_IMPL%" org.apache.jasper.JspC -classpath "%JSTL_IMPL%;%JSF_IMPL%" %*
+java -cp "%SERVLET_API%;%JSP_API%;%JSTL_API%;%JSF_API%;%ANT_LIB%;%EL_IMPL%;%JSP_IMPL%;%AS_LIB%" org.apache.jasper.JspC -classpath "%JSTL_IMPL%;%JSF_IMPL%" -schemas "/schemas/" -dtds "/dtds/" %*
