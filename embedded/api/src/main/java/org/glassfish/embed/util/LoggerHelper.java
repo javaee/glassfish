938bf844986eac8878893138dd1be5b859bfdc9b
Jennifer Galloway <jc129909>
2009-02-24 20:51:41 +0000

    Remove log rotation.  Use one big server.log file.
    svn path=/branches/embedded_v3_prelude_release/; revision=24929
    
    
    Former-commit-id: fe0264d0feee7918cacce40b39fbdcab05521495

--- embedded/api/src/main/java/org/glassfish/embed/util/LoggerHelper.java
+++ embedded/api/src/main/java/org/glassfish/embed/util/LoggerHelper.java
@@ -192,7 +192,7 @@ public class LoggerHelper {
     public final static void setLogFile(String logFile) {
         FileHandler fh = null;
         try {
-            fh = new FileHandler(logFile, 1048576, 100, true);
+            fh = new FileHandler(logFile, true);
         } catch (IOException ex) {
             Logger.getLogger(LoggerHelper.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SecurityException ex) {
