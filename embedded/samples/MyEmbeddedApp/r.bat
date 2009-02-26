becbbff8698e0be5f53935479bbb7cc57f368fc9
Byron Nevins <byron.nevins@oracle.com>
2009-02-26 04:41:29 +0000

    Ancient sample which no longer works...
    svn path=/branches/embedded_v3_prelude_release/; revision=24979
    
    
    Former-commit-id: 4b2faa04369c169e5cee1a8fc255c100934e1bb2

--- embedded/samples/MyEmbeddedApp/r.bat
+++
@@ -1,9 +0,0 @@
-setlocal
-set api="C:\Documents and Settings\bnevins\.m2\repository\org\glassfish\embedded\glassfish-embedded-all\3.0-Prelude-SNAPSHOT\glassfish-embedded-all-3.0-Prelude-SNAPSHOT.jar"
-
-set classname=com.elf.MyEmbeddedApp.MyApp
-set cp=target/MyEmbeddedApp-1.0-SNAPSHOT.jar
-set cp=%cp%;%api%
-
-java -cp %cp% %classname%
-endlocal
