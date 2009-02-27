7919353ea95112655c88bcda52f0422a7a8f5fbd
Jan Luehe <jluehe@java.net>
2009-02-27 22:26:05 +0000

    Added unit test for AsyncContext#complete
    svn path=/trunk/; revision=25023
    
    
    Former-commit-id: e0ff872bde75dbd38c041e6ae0fc47d4ab07ba09

---
+++ v2/appserv-tests/devtests/web/asyncContextComplete/servlet/test/TestServlet.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class TestServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+
+        if (!req.isAsyncSupported()) {
+            throw new ServletException("Async not supported when it should");
+        }
+
+        final AsyncContext ac = req.startAsync();
+
+        Timer asyncTimer = new Timer("TestTimer", true);
+        asyncTimer.schedule(
+            new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        ac.getResponse().getWriter().println("Hello world");
+                        ac.getResponse().getWriter().flush();
+                        ac.complete();
+                    } catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    }
+                }
+            },
+	    5000);
+    }
+}
