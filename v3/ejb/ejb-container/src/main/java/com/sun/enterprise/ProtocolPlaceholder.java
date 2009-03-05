55e775fed9d24280d9dff681d1c77999eeb0ba82
Ken Saks <ksak@java.net>
2009-03-05 03:20:47 +0000

    Last checkin apparently removed the last two entries in package com.sun.enterprise.*.   Not sure why it didn't break my clean builds or the devbuild but hudson builds are breaking for classes with import statements for that package.  Adding a class back to that package until we can remove all references.
    svn path=/trunk/; revision=25107
    
    
    Former-commit-id: 562f8fd43f680f4f88e5cd7c7448dc88638f309f

---
+++ v3/ejb/ejb-container/src/main/java/com/sun/enterprise/ProtocolPlaceholder.java
@@ -0,0 +1,9 @@
+package com.sun.enterprise;
+
+/**
+ * Moving ProtocolManager and RemoteReferenceFactory to
+ * orb-connector made this package disappear.  Temporarily
+ * adding until we can track down all references to
+ * com.sun.enterprise.*;
+ */
+public class ProtocolPlaceholder {}
