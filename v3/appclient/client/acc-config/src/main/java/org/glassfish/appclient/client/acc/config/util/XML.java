22ae54d0f4acb30bc8b9b628896acea84e4f916f
Tim Quinn <tim.quinn@oracle.com>
2009-03-11 05:30:08 +0000

    Move JAXB generation of config classes to its own module, so security can depend on it as well
    svn path=/trunk/; revision=25266
    
    
    Former-commit-id: 6a39484fa7992288f227998fbef61205b41abcf2

---
+++ v3/appclient/client/acc-config/src/main/java/org/glassfish/appclient/client/acc/config/util/XML.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.glassfish.appclient.client.acc.config.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Tim
+ */
+public class XML {
+
+    private static final List<String> booleanTrueValues = Arrays.asList("yes", "on", "1", "true");
+
+    private static final List<String> providerTypeValues = Arrays.asList("client", "server", "client-server");
+
+    public static boolean parseBoolean(final String booleanText) {
+        return _parseBoolean(booleanText.trim());
+    }
+
+    private static boolean isWhiteSpace(char ch) {
+        return ch==' ' || ch == '\t';
+    }
+    /**
+     *
+     * <code><!ENTITY % boolean "(yes | no | on | off | 1 | 0 | true | false)"></code>
+     *
+     * @param literal
+     * @return
+     */
+    private static boolean _parseBoolean(final CharSequence literal) {
+        int i=0;
+        int len = literal.length();
+        char ch;
+        do {
+            ch = literal.charAt(i++);
+        } while(isWhiteSpace(ch) && i<len);
+
+        // if we are strict about errors, check i==len. and report an error
+
+        return booleanTrueValues.contains(literal.subSequence(i, len));
+    }
+
+    public static String parseProviderType(String providerType) {
+        if (providerTypeValues.contains(providerType)) {
+            return providerType;
+        }
+        throw new IllegalArgumentException(providerType);
+    }
+}
