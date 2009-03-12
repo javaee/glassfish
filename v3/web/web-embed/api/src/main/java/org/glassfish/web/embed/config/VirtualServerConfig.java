8d232892ae72e0aa574ee7f841bc58f58a769842
Jan Luehe <jluehe@java.net>
2009-03-12 19:55:47 +0000

    Added a few more configurable aspects of VirtualServer
    svn path=/trunk/; revision=25341
    
    
    Former-commit-id: 25644ea3251df64b690755f26d794f6d5e71914a

--- v3/web/web-embed/api/src/main/java/org/glassfish/web/embed/config/VirtualServerConfig.java
+++ v3/web/web-embed/api/src/main/java/org/glassfish/web/embed/config/VirtualServerConfig.java
@@ -46,6 +46,13 @@ public class VirtualServerConfig {
 
     private boolean ssoEnabled;
     private boolean accessLoggingEnabled;
+    private String defaultWebXml;
+    private String contextXmlDefault;
+    private boolean allowLinking;
+    private String allowRemoteAddress;
+    private String denyRemoteAddress;
+    private String allowRemoteHost;
+    private String denyRemoteHost;
 
     /**
      * Enables or disables Single-Sign-On.
@@ -84,4 +91,194 @@ public class VirtualServerConfig {
     public boolean isAccessLoggingEnabled() {
         return accessLoggingEnabled;
     }
+
+    /**
+     * Sets the location of the default web.xml configuration file.
+     *
+     * @param defaultWebXml the location of the default web.xml configuration
+     * file
+     */
+    public void setDefaultWebXml(String defaultWebXml) {
+        this.defaultWebXml = defaultWebXml;
+    }
+
+    /**
+     * Gets the location of the default web.xml configuration file.
+     *
+     * @return the location of the default web.xml configuration file, or
+     * <tt>null</tt> if <tt>setDefaultWebXml</tt> was never called on this 
+     * <tt>VirtualServerConfig</tt>
+     */
+    public String getDefaultWebXml() {
+        return defaultWebXml;
+    }
+
+    /**
+     * Sets the location of the default context.xml configuration file.
+     *
+     * @param contextXmlDefault the location of the default context.xml
+     * configuration file.
+     */
+    public void setContextXmlDefault(String contextXmlDefault) {
+        this.contextXmlDefault = contextXmlDefault;
+    }
+
+    /**
+     * Gets the location of the default context.xml configuration file.
+     *
+     * @return the location of the default context.xml configuration file,
+     * or <tt>null</tt> if <tt>setContextXmlDefault</tt> was never called
+     * on this <tt>VirtualServerConfig</tt>
+     */
+    public String getContextXmlDefault() {
+        return contextXmlDefault;
+    }
+
+    /**
+     * Enables or disables the serving of resources that are symbolic links.
+     *
+     * @param allowLinking true if resources that are symbolic links are
+     * to be served, false otherwise
+     */
+    public void setAllowLinking(boolean allowLinking) {
+        this.allowLinking = allowLinking;
+    }
+
+    /**
+     * Checks if resources that are symbolic links will be served.
+     *
+     * @return true if resources that are symbolic links will be served,
+     * false otherwise
+     */
+    public boolean isAllowLinking() {
+        return allowLinking;
+    }
+
+    /**
+     * Sets the comma-separated list of regular expression patterns that
+     * the remote client's IP address is compared to.
+     *
+     * <p>If this property is specified, the remote address must match for
+     * this request to be accepted. If this property is not specified,
+     * all requests are accepted unless the remote address matches a
+     * <tt>denyRemoteAddress</tt> pattern.
+     *
+     * @param allowRemoteAddress the comma-separated list of regular
+     * expression patterns that the remote client's IP address is compared
+     * to
+     */
+    public void setAllowRemoteAddress(String allowRemoteAddress) {
+        this.allowRemoteAddress = allowRemoteAddress;
+    }
+
+    /**
+     * Gets the comma-separated list of regular expression patterns that
+     * the remote client's IP address is compared to.
+     *
+     * @return the comma-separated list of regular expression patterns that
+     * the remote client's IP address is compared to, or <tt>null</tt>
+     * if <tt>setAllowRemoteAddress</tt> was never called on this 
+     * <tt>VirtualServerConfig</tt>
+     */
+    public String getAllowRemoteAddress() {
+        return allowRemoteAddress;
+    }
+
+    /**
+     * Sets the comma-separated list of regular expression patterns that
+     * the remote client's IP address is compared to.
+     *
+     * <p>If this property is specified, the remote address must not match
+     * for this request to be accepted. If this property is not specified,
+     * request acceptance is governed solely by the allowRemoteAddress
+     * property.
+     *
+     * @param denyRemoteAddress the comma-separated list of regular
+     * expression patterns that the remote client's IP address is
+     * compared to
+     */
+    public void setDenyRemoteAddress(String denyRemoteAddress) {
+        this.denyRemoteAddress = denyRemoteAddress;
+    }
+
+    /**
+     * Gets the comma-separated list of regular expression patterns that
+     * the remote client's IP address is compared to.
+     *
+     * @return the comma-separated list of regular expression patterns that
+     * the remote client's IP address is compared to, or <tt>null</tt>
+     * if <tt>setDenyRemoteAddress</tt> was never called on this 
+     * <tt>VirtualServerConfig</tt>
+     */
+    public String getDenyRemoteAddress() {
+        return denyRemoteAddress;
+    }
+
+    /**
+     * Sets the comma-separated list of regular expression patterns
+     * that the remote client's hostname (as returned by
+     * java.net.Socket.getInetAddress().getHostName()) is compared to.
+     *
+     * <p>If this property is specified, the remote hostname must match
+     * for this request to be accepted. If this property is not specified,
+     * all requests are accepted unless the remote hostname matches a
+     * <tt>denyRemoteHost</tt> pattern.
+     *
+     * @param allowRemoteHost the comma-separated list of regular
+     * expression patterns that the remote client's hostname (as returned
+     * by java.net.Socket.getInetAddress().getHostName()) is compared to
+     */
+    public void setAllowRemoteHost(String allowRemoteHost) {
+        this.allowRemoteHost = allowRemoteHost;
+    }
+
+    /**
+     * Gets the comma-separated list of regular expression patterns
+     * that the remote client's hostname (as returned by
+     * java.net.Socket.getInetAddress().getHostName()) is compared to.
+     *
+     * @return the comma-separated list of regular expression patterns
+     * that the remote client's hostname (as returned by
+     * java.net.Socket.getInetAddress().getHostName()) is compared to,
+     * or <tt>null</tt> if <tt>setAllowRemoteHost</tt> was never called
+     * on this <tt>VirtualServerConfig</tt>
+     */
+    public String getAllowRemoteHost() {
+        return allowRemoteHost;
+    }
+
+    /**
+     * Sets the comma-separated list of regular expression patterns that
+     * the remote client's hostname (as returned by
+     * java.net.Socket.getInetAddress().getHostName()) is compared to.
+     *
+     * <p>If this property is specified, the remote hostname must not
+     * match for this request to be accepted. If this property is not
+     * specified, request acceptance is governed solely by the
+     * <tt>allowRemoteHost</tt> property.
+     *
+     * @param denyRemoteHost the comma-separated list of regular
+     * expression patterns that the remote client's hostname
+     * (as returned by java.net.Socket.getInetAddress().getHostName())
+     * is compared to
+     *
+    public void setDenyRemoteHost(String denyRemoteHost) {
+        this.denyRemoteHost = denyRemoteHost;
+    }
+
+    /**
+     * Gets the comma-separated list of regular expression patterns that
+     * the remote client's hostname (as returned by
+     * java.net.Socket.getInetAddress().getHostName()) is compared to.
+     *
+     * @return the comma-separated list of regular expression patterns that
+     * the remote client's hostname (as returned by
+     * java.net.Socket.getInetAddress().getHostName()) is compared to,
+     * or <tt>null</tt> if <tt>setDenyRemoteHost</tt> was never called
+     * on this <tt>VirtualServerConfig</tt>
+     */
+    public String getDenyRemoteHost() {
+        return denyRemoteHost;
+    }
+
 }
