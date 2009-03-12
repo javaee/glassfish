ad16aeb5bf00e6872807a6fd149e380b7641148c
Tim Quinn <tim.quinn@oracle.com>
2009-03-12 13:46:37 +0000

    Add property copyright notice and add a method to convert from List<JAXB-generated Property> to java.util.Properties
    svn path=/trunk/; revision=25331
    
    
    Former-commit-id: 36c2406e150237faa1a0829362b6bef6193c25cd

--- v3/appclient/client/acc-config/src/main/java/org/glassfish/appclient/client/acc/config/util/XML.java
+++ v3/appclient/client/acc-config/src/main/java/org/glassfish/appclient/client/acc/config/util/XML.java
@@ -1,16 +1,53 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
  */
 
 package org.glassfish.appclient.client.acc.config.util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
+import org.glassfish.appclient.client.acc.config.Property;
 
 /**
+ * Logic used during JAXB conversions between XML and objects.
  *
- * @author Tim
+ * @author tjquinn
  */
 public class XML {
 
@@ -50,5 +87,21 @@ public class XML {
             return providerType;
         }
         throw new IllegalArgumentException(providerType);
+    }
+
+    /**
+     * Converts the XML property elements (with name and value) to a Properties
+     * object.
+     *
+     * @param props List of Property elements from the JAXB-converted
+     * client container element
+     * @return corresponding Properties object
+     */
+    public static Properties toProperties(final List<Property> props) {
+        Properties result = new Properties();
+        for (Property p : props) {
+            result.setProperty(p.getName(), p.getValue());
+        }
+        return result;
     }
 }
