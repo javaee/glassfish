aab8996f72e329e232b55eb645173e694b307f75
Jagadish Ramu <jagadish.ramu@oracle.com>
2009-03-16 15:15:57 +0000

    Porting redeployRar connector dev-test
    svn path=/trunk/; revision=25420
    
    
    Former-commit-id: d6c6e267fde6ac24eebf82f94491e858b67afae4

---
+++ v2/appserv-tests/devtests/connector/v3/redeployRar/app/src/ejb/VersionCheckerHome.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package beans;
+
+import javax.ejb.CreateException;
+import javax.ejb.EJBLocalHome;
+
+public interface VersionCheckerHome extends EJBLocalHome {
+    VersionChecker create() throws CreateException;
+}
