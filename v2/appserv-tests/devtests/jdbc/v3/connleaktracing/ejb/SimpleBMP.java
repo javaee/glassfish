3d9a1af2c2a5ffe394b5e22c3dad74fb529cef8a
Shalini Muthukrishnan <sm157516@java.net>
2009-03-23 12:14:18 +0000

    Adding devtests for JDBC : Connection leak tracing and JDBC resource enable/disable.
    svn path=/trunk/; revision=25589
    
    
    Former-commit-id: 71a8bda5ed6a36ae408e7caecb20ee1c1e827ece

---
+++ v2/appserv-tests/devtests/jdbc/v3/connleaktracing/ejb/SimpleBMP.java
@@ -0,0 +1,7 @@
+package com.sun.s1asdev.jdbc.connectionleaktracing.ejb;
+
+import javax.ejb.EJBLocalObject;
+
+public interface SimpleBMP extends EJBLocalObject {
+    public boolean test1();
+}
---
+++ v2/appserv-tests/devtests/jdbc/v3/resEnableDisable/ejb/SimpleBMP.java
@@ -0,0 +1,7 @@
+package com.sun.s1asdev.jdbc.resenabledisable.ejb;
+
+import javax.ejb.EJBLocalObject;
+
+public interface SimpleBMP extends EJBLocalObject {
+    public boolean test1();
+}
