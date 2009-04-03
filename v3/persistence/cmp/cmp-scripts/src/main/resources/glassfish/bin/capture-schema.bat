6d671e61c970659a21a0dd069c76b75b5d2697dc
Mitesh Meswani <mm110999@java.net>
2009-04-03 01:26:32 +0000

    Removed endlocal from the script to prevent issue https://glassfish.dev.java.net/issues/show_bug.cgi?id=6838
    svn path=/trunk/; revision=25857
    
    
    Former-commit-id: 527a568cb33b06f00f00107ba2108516abb563d7

--- v3/persistence/cmp/cmp-scripts/src/main/resources/glassfish/bin/capture-schema.bat
+++ v3/persistence/cmp/cmp-scripts/src/main/resources/glassfish/bin/capture-schema.bat
@@ -11,5 +11,3 @@ set AS_INSTALL=%~dp0..
 set AS_INSTALL_LIB=%AS_INSTALL%\modules
 
 java  -Xms24m -Xmx96m  -cp "%AS_INSTALL_LIB%\common-util.jar;%AS_INSTALL_LIB%\cmp-utility.jar;%AS_INSTALL_LIB%\cmp-support-ejb.jar;%AS_INSTALL_LIB%\cmp-ejb-mapping.jar;%AS_INSTALL_LIB%\dbschema-repackaged.jar;%CLASSPATH%" com.sun.jdo.spi.persistence.support.ejb.util.CaptureSchemaWrapper %*
-
-endlocal
