56e9c5d2a02328d9fd88f10452034748898e8b7a
Siraj Ghaffar <siraj.ghaffar@oracle.com>
2009-04-06 19:57:47 +0000

    Merging changes from embedded branch
    svn path=/trunk/; revision=25950
    
    
    Former-commit-id: 903eff08c75de4ce928570a1201e34e159296d6e

--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/server/ExecutorServiceFactory.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/server/ExecutorServiceFactory.java
@@ -4,6 +4,7 @@ import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.annotations.FactoryFor;
 import org.jvnet.hk2.component.Factory;
 import org.jvnet.hk2.component.ComponentException;
+import java.util.concurrent.ThreadFactory;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,6 +19,13 @@ import java.util.concurrent.Executors;
 public class ExecutorServiceFactory implements Factory {
 
     public Object getObject() throws ComponentException {
-        return Executors.newCachedThreadPool();
+        return Executors.newCachedThreadPool(new ThreadFactory() {
+            public Thread newThread(Runnable r) {
+                Thread t = Executors.defaultThreadFactory().newThread(r);
+                t.setDaemon(true);
+                return t;
+            }
+        }
+        );
     }
 }
--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/server/ScheduledExecutorServiceFactory.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/server/ScheduledExecutorServiceFactory.java
@@ -4,6 +4,7 @@ import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.annotations.FactoryFor;
 import org.jvnet.hk2.component.Factory;
 import org.jvnet.hk2.component.ComponentException;
+import java.util.concurrent.ThreadFactory;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Executors;
@@ -18,6 +19,14 @@ import java.util.concurrent.Executors;
 public class ScheduledExecutorServiceFactory implements Factory {
     
     public Object getObject() throws ComponentException {
-        return Executors.newSingleThreadScheduledExecutor();
+        return Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
+            public Thread newThread(Runnable r) {
+                    Thread t = Executors.defaultThreadFactory().newThread(r);
+                    t.setDaemon(true);
+                    return t;
+                }
+            }
+            );
+
     }
 }
