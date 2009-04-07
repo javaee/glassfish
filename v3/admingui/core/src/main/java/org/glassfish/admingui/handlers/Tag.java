c5067d65810e8c4f9e7274ce058f88dc38e17524
Jason Lee <jdlee@java.net>
2009-04-07 19:12:35 +0000

    Implement hashCode()
    svn path=/trunk/; revision=25968
    
    
    Former-commit-id: 039396c6b4981e850db8904759e9fd8f33d465f9

--- v3/admingui/core/src/main/java/org/glassfish/admingui/handlers/Tag.java
+++ v3/admingui/core/src/main/java/org/glassfish/admingui/handlers/Tag.java
@@ -45,6 +45,12 @@ import java.util.List;
  */
 public class Tag implements java.io.Serializable {
 
+    private static final long serialVersionUID = 7437635853139196986L;
+    private String tagViewId = null;
+    private String tagName = null;
+    private String displayName = null;
+    private List<String> users = null;
+
     /**
      *	<p> Default constructor.</p>
      */
@@ -55,23 +61,23 @@ public class Tag implements java.io.Serializable {
      *	<p> The constructor that should normally be used.</p>
      */
     public Tag(String tagName, String tagViewId, String displayName, String user) {
-	this.tagName = tagName;
-	this.tagViewId = tagViewId;
-	this.displayName = displayName;
-	if (user != null) {
-	    this.users = new ArrayList<String>();
-	    this.users.add(user);
-	}
+        this.tagName = tagName;
+        this.tagViewId = tagViewId;
+        this.displayName = displayName;
+        if (user != null) {
+            this.users = new ArrayList<String>();
+            this.users.add(user);
+        }
     }
 
     /**
      *	<p> Allows an additional user to be added as a Tag creator.</p>
      */
     public void addUser(String name) {
-	if (users == null) {
-	    users = new ArrayList<String>();
-	}
-	users.add(name);
+        if (users == null) {
+            users = new ArrayList<String>();
+        }
+        users.add(name);
     }
 
     /**
@@ -79,14 +85,14 @@ public class Tag implements java.io.Serializable {
      *	    may be null.</p>
      */
     public List<String> getUsers() {
-	return users;
+        return users;
     }
 
     /**
      *  <p>	Checks to see if the given user is an owner of this Tag.</p>
      */
     public boolean containsUser(String name) {
-	return (users == null) ? false : users.contains(name);
+        return (users == null) ? false : users.contains(name);
     }
 
     /**
@@ -102,13 +108,13 @@ public class Tag implements java.io.Serializable {
      *		user, or <code>null</code> if none.
      */
     public List<String> removeUser(String name) {
-	if (users != null) {
-	    users.remove(name);
-	    if (users.size() == 0) {
-		users = null;
-	    }
-	}
-	return users;
+        if (users != null) {
+            users.remove(name);
+            if (users.size() == 0) {
+                users = null;
+            }
+        }
+        return users;
     }
 
     /**
@@ -117,37 +123,45 @@ public class Tag implements java.io.Serializable {
      *	    Lists are still considered equal.  The Display Name is also of no
      *	    importance to this implementation of equality.</p>
      */
+    @Override
     public boolean equals(Object obj) {
-	boolean result = false;
-	if (obj instanceof Tag) {
-	    Tag testTag = (Tag) obj;
-	    result = getTagName().equals(testTag.getTagName())
-		    && getTagViewId().equals(testTag.getTagViewId());
-	}
-	return result;
+        boolean result = false;
+        if (obj instanceof Tag) {
+            Tag testTag = (Tag) obj;
+            result = getTagName().equals(testTag.getTagName()) && getTagViewId().equals(testTag.getTagViewId());
+        }
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + (this.tagViewId != null ? this.tagViewId.hashCode() : 0);
+        hash = 89 * hash + (this.tagName != null ? this.tagName.hashCode() : 0);
+        hash = 89 * hash + (this.displayName != null ? this.displayName.hashCode() : 0);
+        return hash;
     }
 
     /**
      *  <p>	String representation of this Tag.</p>
      */
+    @Override
     public String toString() {
-	return "[" + getTagName() + ", " + getTagViewId()
-		+ ", " + getDisplayName() + ", Users: {"
-		+ users + "}]";
+        return "[" + getTagName() + ", " + getTagViewId() + ", " + getDisplayName() + ", Users: {" + users + "}]";
     }
 
     /**
      *	<p> This provides access to the tag name.</p>
      */
     public String getTagName() {
-	return tagName;
+        return tagName;
     }
 
     /**
      *	<p> This provides access to the TagViewId value.</p>
      */
     public String getTagViewId() {
-	return tagViewId;
+        return tagViewId;
     }
 
     /**
@@ -155,18 +169,11 @@ public class Tag implements java.io.Serializable {
      *	    which represents the content of this <code>Tag</code> instance.</p>
      */
     public String getDisplayName() {
-	// FIXME: I have I18N concerns about this... perhaps it's acceptible to
-	// FIXME: store a String localized at the time the page is tagged.
-	// FIXME: Multiple language environments may not like this.  To fix
-	// FIXME: this correctly, we not only need the ValueExpression (i.e.
-	// FIXME: #{i18n.foo}), but we also need the resource bundle to use.
-	return displayName;
+        // FIXME: I have I18N concerns about this... perhaps it's acceptible to
+        // FIXME: store a String localized at the time the page is tagged.
+        // FIXME: Multiple language environments may not like this.  To fix
+        // FIXME: this correctly, we not only need the ValueExpression (i.e.
+        // FIXME: #{i18n.foo}), but we also need the resource bundle to use.
+        return displayName;
     }
-
-    private static final long serialVersionUID = 7437635853139196986L;
-
-    private String tagViewId = null;
-    private String tagName = null;
-    private String displayName = null;
-    private List<String> users = null;
 }
