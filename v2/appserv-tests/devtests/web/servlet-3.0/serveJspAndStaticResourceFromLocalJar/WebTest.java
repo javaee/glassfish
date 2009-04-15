1f0ef9d64305126047cdc37038dedaad4dfdc429
Jan Luehe <jluehe@java.net>
2009-04-15 16:45:45 +0000

    Amended unit test to cover static resource (commented out for time being)
    svn path=/trunk/; revision=26211
    
    
    Former-commit-id: 6094a7650574ff7a1118553fae395456f1808a3b

--- v2/appserv-tests/devtests/web/servlet-3.0/serveJspAndStaticResourceFromLocalJar/WebTest.java
+++ v2/appserv-tests/devtests/web/servlet-3.0/serveJspAndStaticResourceFromLocalJar/WebTest.java
@@ -3,22 +3,25 @@ import java.net.*;
 import com.sun.ejte.ccl.reporter.*;
 
 /*
- * Unit test for ServletContext#getResource, where the requested resource
- * is embedded in a JAR file inside WEB-INF/lib, and the resource path is
- * interpreted as relative to the META-INF/resources directory of that JAR
- * file.
+ * Unit test for serving JSP and static resources from
+ * WEB-INF/lib/[*.jar]/META-INF/resources
  *
  * In this unit test, the client makes a request for
  *   http://localhost:8080/abc.jsp
- * and the requested JSP is supposed to be found under
+ * and
+ *   http://localhost:8080/abc.txt
+ * and the requested resource is supposed to be served from
  *   WEB-INF/lib/nested.jar!META-INF/resources/abc.jsp
+ * (by the JspServlet) and
+ *   WEB-INF/lib/nested.jar!META-INF/resources/abc.txt
+ * (by the DefaultServlet), respectively.
  */
 public class WebTest {
 
     private static SimpleReporterAdapter stat =
         new SimpleReporterAdapter("appserv-tests");
     private static final String TEST_NAME =
-        "jsp-servlet-get-jsp-resource-from-local-jar";
+        "serve-jsp-and-static-resource-from-local-jar";
 
     private String host;
     private String port;
@@ -31,14 +34,16 @@ public class WebTest {
     }
     
     public static void main(String[] args) {
-        stat.addDescription("Unit test for ServletContext#getResource");
+        stat.addDescription("Unit test for serving JSP and static " +
+                            "resources from JAR inside WEB-INF/lib");
         new WebTest(args).doTest();
         stat.printSummary(TEST_NAME);
     }
 
     public void doTest() {
         try { 
-            invokeServlet();
+            invokeJspServlet();
+            //invokeDefaultServlet();
             stat.addStatus(TEST_NAME, stat.PASS);
         } catch (Exception ex) {
             stat.addStatus(TEST_NAME, stat.FAIL);
@@ -46,9 +51,21 @@ public class WebTest {
         }
     }
 
-    private void invokeServlet() throws Exception {
+    private void invokeJspServlet() throws Exception {
         URL url = new URL("http://" + host  + ":" +
                           port + contextRoot + "/abc.jsp");
+        System.out.println("Invoking URL: " + url.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.connect();
+        int responseCode = conn.getResponseCode();
+        if (responseCode != 200) {
+            throw new Exception("Unexpected response code: " + responseCode);
+        }
+    }
+
+    private void invokeDefaultServlet() throws Exception {
+        URL url = new URL("http://" + host  + ":" +
+                          port + contextRoot + "/abc.txt");
         System.out.println("Invoking URL: " + url.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.connect();
