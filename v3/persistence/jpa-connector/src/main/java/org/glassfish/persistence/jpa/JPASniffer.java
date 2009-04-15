2ed65fcf02ee4968b5515af4085b214a81774085
Mitesh Meswani <mm110999@java.net>
2009-04-15 19:37:49 +0000

    Instead of using ClassLoader, need to go through files to not flag a .ear twice Tests run sqe JPA tests + quicklook testDeployedAppPage is failing but should not be related to this change
    svn path=/trunk/; revision=26220
    
    
    Former-commit-id: 6b3fbb9574256f42c5cafc60474001b3b8a1148f

--- v3/persistence/jpa-connector/src/main/java/org/glassfish/persistence/jpa/JPASniffer.java
+++ v3/persistence/jpa-connector/src/main/java/org/glassfish/persistence/jpa/JPASniffer.java
@@ -31,7 +31,8 @@ import org.jvnet.hk2.annotations.Scoped;
 import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.component.Singleton;
 
-import java.net.URL;
+import java.util.Enumeration;
+import java.io.IOException;
 
 
 /**
@@ -51,7 +52,13 @@ public class JPASniffer  extends GenericSniffer implements Sniffer {
     }
 
     private static char SEPERATOR_CHAR = '/';
+    private static final String WEB_INF                  = "WEB-INF";
+    private static final String LIB                      = "lib";
+    private static final String WEB_INF_LIb              = WEB_INF + SEPERATOR_CHAR + LIB;
+    private static final String WEB_INF_CLASSSES         = WEB_INF + SEPERATOR_CHAR + "classes";
     private static final String META_INF_PERSISTENCE_XML = "META-INF" + SEPERATOR_CHAR + "persistence.xml";
+    private static final String WEB_INF_CLASSSES_META_INF_PERSISTENCE_XML = WEB_INF_CLASSSES + SEPERATOR_CHAR + META_INF_PERSISTENCE_XML;
+    private static final String JAR_SUFFIX = ".jar";
     /**
      * Returns true if the archive contains persistence.xml as defined by packaging rules of JPA
      * Tries to getResource("META-INF/persitsence.xml") on curernt classLoader. If it succeeds, currrent archive is a pu
@@ -61,13 +68,66 @@ public class JPASniffer  extends GenericSniffer implements Sniffer {
      * ejb.jar (the resource can be present in root of the jar),
      * .ear (the resource can be present in lib dir of the ear)
      */
-    @Override
     public boolean handles(ReadableArchive location, ClassLoader loader) {
-        // scan for persistence.xml in META-INF/persitennce.xml If at least one is found, this is
-        // a jpa archive
-        URL persistencexmlURL = loader.getResource(META_INF_PERSISTENCE_XML);
-        return  persistencexmlURL != null; 
+            boolean isJPAArchive = false;
+
+            // scan for persistence.xml in expected locations. If at least one is found, this is
+            // a jpa archive
+
+            //Scan for the war case
+            if(isEntryPresent(location, WEB_INF)) {
+                // First check for  "WEB-INF/classes/META-INF/persistence.xml"
+                isJPAArchive = isEntryPresent(location, WEB_INF_CLASSSES_META_INF_PERSISTENCE_XML);
+                if (!isJPAArchive) {
+                    // Check in WEB-INF/lib dir
+                    if (isEntryPresent(location, WEB_INF_LIb)) {
+                        Enumeration<String> entries = location.entries(WEB_INF_LIb);
+                        isJPAArchive = scanForPURootsInLibDir(location, entries);
+                    } // if (isEntryPresent(location, WEB_INF_LIb))
+                } // if (!isJPAArchive)
+            } else {
+                // This can be ejb jar or ear
+
+                //Check for ejb jar case
+                isJPAArchive = isEntryPresent(location, META_INF_PERSISTENCE_XML);
+
+                // ear case, scan in lib of ear
+                // TODO we are hardcoding scanning in "lib" dir for now. Check from Hong how can get user specified "lib" dir for an ear from dol
+                if (isEntryPresent(location, LIB)) {
+                    Enumeration<String> entries = location.entries(LIB);
+                    isJPAArchive = scanForPURootsInLibDir(location, entries);
+                } // if (isEntryPresent(location, WEB_INF_LIb))
+            }
+            return isJPAArchive;
+        }
+
+    private boolean scanForPURootsInLibDir(ReadableArchive parentArchive, Enumeration<String> entries) {
+        boolean puRootPresent = false;
+        while (entries.hasMoreElements() && !puRootPresent) {
+            String entryName = entries.nextElement();
+            if (entryName.endsWith(JAR_SUFFIX) && // a jar in lib dir
+                    entryName.indexOf(SEPERATOR_CHAR, WEB_INF_LIb.length() + 1 ) == -1 ) { // && not WEB-INf/lib/foo/bar.jar
+                try {
+                    ReadableArchive jarInLib = parentArchive.getSubArchive(entryName);
+                    puRootPresent = isEntryPresent(jarInLib, META_INF_PERSISTENCE_XML);
+                    jarInLib.close();
+                } catch (IOException e) {
+                    // Something went wrong while reading the jar. Do not attempt to scan it
+                } // catch
+            } // if (entryName.endsWith(JAR_SUFFIX))
+        } // while
+        return puRootPresent;
     }
+
+    private boolean isEntryPresent(ReadableArchive location, String entry) {
+            boolean entryPresent = false;
+            try {
+                entryPresent = location.exists(entry);
+            } catch (IOException e) {
+                // ignore
+            }
+            return entryPresent;
+        }
 
     public String[] getContainersNames() {
         return containers;
