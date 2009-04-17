528a930833a7332df4fece8682a918b417cd69f7
Byron Nevins <byron.nevins@oracle.com>
2009-04-17 07:40:09 +0000

    * Removed a really slow unit test on Windows. * Added a utility to fetch the pid of a JVM.
    svn path=/trunk/; revision=26319
    
    
    Former-commit-id: 7848d7cc4832ba85c33347667ddb209460284c8f

---
+++ v3/common/common-util/src/test/java/com/sun/enterprise/universal/process/JpsTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sun.enterprise.universal.process;
+
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bnevins
+ */
+public class JpsTest {
+
+    public JpsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws IOException, InterruptedException {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void atest() {
+        int pid = Jps.getPid("ASMain");
+        System.out.println("ZZZZZZZZZZZZZZZZASMain pid = " + pid);
+    }
+}
