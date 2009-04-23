94f2c73af6a288436766158d82480cdefbaf786d
Rebecca Searls <rebeccas@java.net>
2009-04-23 18:56:19 +0000

    remove obsolete params from startup cmd. Fix path for domainRoot
    svn path=/trunk/; revision=26598
    
    
    Former-commit-id: 1466f5fe90a48a2b3c01cb9fb7c3a39e895464df

--- v3/extras/upgrade/src/main/scripts/asupgrade.bat
+++ v3/extras/upgrade/src/main/scripts/asupgrade.bat
@@ -6,17 +6,18 @@ REM Use is subject to license terms.
 REM
 
 setlocal
-AS_INSTALL=%~dp0..\
+DIRNAME=%~dp0
+AS_INSTALL=%DIRNAME%\..\
 AS_INSTALL_LIB=%AS_INSTALL%/lib
 AS_INSTALL_MOD=%AS_INSTALL%/modules
 CONFIG_HOME=%AS_INSTALL%/config
 call "%CONFIG_HOME%\asenv.bat"
 
 REM override relative path from asenv.bat
-AS_INSTALL=%~dp0..\
+AS_INSTALL=%DIRNAME%\..\
 
 set Path=%AS_INSTALL%\bin;%PATH%
 
 REM note: $AS_DEF_DOMAINS_PATH is relative to config dir
-"%AS_JAVA%\bin\java" -Dcom.sun.aas.utool.LogLevel="INFO" -Dcom.sun.aas.installRoot="%AS_INSTALL%" -Dcom.sun.aas.domainRoot="%AS_INSTALL%\config\%AS_DEF_DOMAINS_PATH%" -Dcom.sun.aas.instanceRoot="%AS_INSTALL%" -Dcom.sun.aas.configRoot="%AS_CONFIG%" -Dcom.sun.aas.java.home="%AS_JAVA%" -jar "%AS_INSTALL_LIB%/upgradeTool.jar" %*
+"%AS_JAVA%\bin\java" -Dcom.sun.aas.utool.LogLevel="INFO" -Dcom.sun.aas.domainRoot="%DIRNAME%\%AS_DEF_DOMAINS_PATH%"  -Dcom.sun.aas.java.home="%AS_JAVA%" -jar "%AS_INSTALL_LIB%/upgradeTool.jar" %*
 endlocal
