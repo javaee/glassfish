774e7b5ae6b2391b2713ba698024085efc41a5b8
Lloyd Chambers <llc@java.net>
2009-04-28 22:17:48 +0000

    remvoing unused defunct interfaces
    svn path=/trunk/; revision=26679
    
    
    Former-commit-id: ff830a69080c9065cdd52215bc5449fee301c8ae

--- v3/common/amx-config/src/main/java/org/glassfish/admin/amx/intf/config/HttpServiceConfig.java
+++ v3/common/amx-config/src/main/java/org/glassfish/admin/amx/intf/config/HttpServiceConfig.java
@@ -45,207 +45,20 @@ import java.util.Map;
 
 /**
 	 Configuration for the &lt;http-service&gt; element.
-     @deprecated  Grizzly variants now exist
  */
 public interface HttpServiceConfig
 	extends ConfigElement, PropertiesAccess, DefaultValues, Singleton
 {
     public static final String AMX_TYPE = "http-service";
-	/**
-		@return Map of all HttpListenerConfig proxies, keyed by the name of the listener.
-		@see org.glassfish.admin.amx.base.Container#getContaineeMap
-	 */
-	public Map<String,HttpListenerConfig>		getHttpListener();
 	
 	/**
 		@return Map of all VirtualServerConfig proxies, keyed by the name of the virtual server.
 		@see org.glassfish.admin.amx.base.Container#getContaineeMap
 	 */
 	public Map<String,VirtualServerConfig>		getVirtualServer();
-	
-	
-// 
-// 	/**
-// 		Creates access-log element with the given params.
-// 		@param params
-// 		@return Proxy to the AccessLogConfig MBean
-// 		@see AccessLogConfigKeys
-// 	 */
-// 	public AccessLogConfig	createAccessLogConfig( Map<String,String> params );
-// 
-// 	/**
-// 		Removes access-log element.
-// 	 */
-// 	public void			removeAccessLog();
-// 
-// 	/**
-// 		Creates request-processing element with the given params.
-// 		@param params
-// 		@return Proxy to the RequestProcessingConfig MBean
-// 		@see RequestProcessingConfigKeys
-// 	 */
-// 	public RequestProcessingConfig	createRequestProcessingConfig( Map<String,String> params );
-// 
-// 	/**
-// 		Removes request-processing element.
-// 	 */
-// 	public void			removeRequestProcessing();
-// 
-// 	/**
-// 		@return Proxy to the KeepAliveConfig MBean
-// 	 */
-// 	public KeepAliveConfig	getKeepAlive();
-// 
-// 	/**
-// 		Creates keep-alive element with the given params.
-// 		@param params
-// 		@return Proxy to the KeepAliveConfig MBean
-// 		@see KeepAliveConfigKeys
-// 	 */
-// 	public KeepAliveConfig	createKeepAliveConfig( Map<String,String> params );
-// 
-// 	/**
-// 		Removes keep-alive element.
-// 	 */
-// 	public void			removeKeepAlive();
-// 
-// 	/**
-// 		Creates connection-pool element with the given params.
-// 		@param params
-// 		@return the ObjectName of the ConnectionPoolConfig
-// 		@see ConnectionPoolConfigKeys
-// 	 */
-// 	public ConnectionPoolConfig	createConnectionPoolConfig( Map<String,String> params );
-// 
-// 	/**
-// 		Removes connection-pool element.
-// 	 */
-// 	public void			removeConnectionPool();
-// 
-// 	/**
-// 		@return Proxy to the HTTPProtocolConfig MBean
-// 	 */
-// 	public HTTPProtocolConfig	getHTTPProtocol();
-// 
-// 	/**
-// 		Creates http-protocol element with the given params.
-// 		@param params
-// 		@return Proxy to the HTTPProtocolConfig MBean
-// 		@see HTTPProtocolConfigKeys
-// 	 */
-// 	public HTTPProtocolConfig	createHTTPProtocolConfig( Map<String,String> params );
-// 
-// 	/**
-// 		Removes http-protocol element.
-// 	 */
-// 	public void			removeHTTPProtocol();
-// 
-// 	/**
-// 		@return Proxy to the HTTPFileCacheConfig MBean
-// 	 */
-// 	public HTTPFileCacheConfig	getHTTPFileCache();
-// 
-// 	/**
-// 		Creates http-file-cache element with the given params.
-// 		@param params
-// 		@return Proxy to the HTTPFileCacheConfig MBean
-// 		@see HTTPFileCacheConfigKeys
-// 	 */
-// 	public HTTPFileCacheConfig	createHTTPFileCacheConfig( Map<String,String> params );
-// 
-// 	/**
-// 		Removes http-file-cache element.
-// 	 */
-// 	public void			removeHTTPFileCache();
-// 
-// 
-// 
-// 	/**
-// 		Create an <http-listener>
-// 		Parameters:
-// 		<ul>
-// 		<li>{@link HTTPListenerConfigKeys#ADDRESS_KEY}</li>
-// 		<li>{@link HTTPListenerConfigKeys#PORT_KEY}</li>
-// 		<li>{@link HTTPListenerConfigKeys#ENABLED_KEY}</li>
-// 		<li>{@link HTTPListenerConfigKeys#SECURITY_ENABLED_KEY}</li>
-// 		<li>{@link HTTPListenerConfigKeys#DEFAULT_VIRTUAL_SERVER_KEY}</li>
-// 		<li>{@link HTTPListenerConfigKeys#ACCEPTOR_THREADS_KEY}</li>
-// 		<li>{@link HTTPListenerConfigKeys#XPOWERED_BY_KEY}</li>
-// 		<li>{@link HTTPListenerConfigKeys#XPOWERED_BY_KEY}</li>
-// 		<li>{@link HTTPListenerConfigKeys#IGNORE_MISSING_REFERENCES_KEY} (for defaultVirtualServer)</li>
-// 		</ul>
-// 	 
-// 		
-// 		@param name 		the name (id) of the newly created listener
-// 		@param address		IP address of the listener
-// 		@param port			port of the listener
-// 		@param defaultVirtualServer		        The name of the default 
-// 		virtual server for this particular connection group.
-// 		@param serverName	
-// 		@param optional		optional parameters keyed by one of XXX_KEY
-// 		@return A proxy to the HttpListenerConfig MBean
-// 		@see HTTPListenerConfigKeys
-// 	 */
-// 	public HttpListenerConfig	createHTTPListenerConfig(
-// 							String	name,
-// 							String	address,
-// 							int		port,
-// 							String	defaultVirtualServer,
-// 							String	serverName,
-// 							Map<String,String>		optional );
-// 	
-// 	/**
-// 		Remove the &lt;http-listener>.  Will fail if the &lt;http-listener> is referred to by
-// 		other elements.
-// 		@param name		the name (id) of the http listener to be removed.
-// 	 */
-// 	public void			removeHTTPListenerConfig( String name );
-// 	
-// 	/**
-// 		Create a <virtual-server>.
-// 		Most fields will default to reasonable values. Legal keys include:
-// 		<ul>
-// 			{@link VirtualServerConfigKeys#HTTP_LISTENERS_KEY}</li>
-// 			{@link VirtualServerConfigKeys#STATE_KEY}</li>
-// 			{@link VirtualServerConfigKeys#DOC_ROOT_PROPERTY_KEY}</li> 
-// 			{@link VirtualServerConfigKeys#ACCESS_LOG_PROPERTY_KEY}</li> 
-// 		</ul>
-// 		
-// 		@param name 			the name (id) of the newly created virtual server
-// 		@param hosts			comma-separated list of hosts
-// 		@param optional			optional params
-// 		@return		A proxy to the VirtualServerConfig MBean.
-// 		@see VirtualServerConfigKeys
-// 	 */
-// 	public VirtualServerConfig	createVirtualServerConfig(
-// 							String	name,
-// 							String	hosts,
-// 							Map<String,String>	optional );
-// 	
-// 	/**
-// 		Remove the &lt;virtual-server&gt;.
-// 		Will fail if the &lt;virtual-server&gt is referred to by
-// 		other elements.
-// 		
-// 		@param virtualServerName
-// 	 */
-// 	public void			removeVirtualServerConfig( String virtualServerName );
-	
-	
 
 	/**
 		Get the AccessLogConfig.
 	 */
 	public AccessLogConfig	getAccessLog();
-
-	/**
-		Get the RequestProcessingConfig.
-	 */
-	public RequestProcessingConfig	getRequestProcessing();
-
-	/**
-		Get the ConnectionPoolConfig.
-	 */
-	public ConnectionPoolConfig	getConnectionPool();
-
 }
