8524116f4b5b004ed37d1f5b0192d918c9b81870
Bobby Bisset <bbissett@java.net>
2009-04-29 16:15:38 +0000

    Changed artifact name to upgradeTool and modified scripts to look for it in the modules directory rather than lib. Am still seeing class not found errors with HelpSet.
    svn path=/trunk/; revision=26709
    
    
    Former-commit-id: 891807262d26c67f11704aaf4eb644b0f5a74a1f

--- v3/extras/upgrade/src/main/scripts/asupgrade.bat
+++ v3/extras/upgrade/src/main/scripts/asupgrade.bat
@@ -19,5 +19,5 @@ AS_INSTALL=%DIRNAME%\..\
 set Path=%AS_INSTALL%\bin;%PATH%
 
 REM note: $AS_DEF_DOMAINS_PATH is relative to config dir
-"%AS_JAVA%\bin\java" -Dcom.sun.aas.utool.LogLevel="INFO" -Dcom.sun.aas.domainRoot="%DIRNAME%\%AS_DEF_DOMAINS_PATH%"  -Dcom.sun.aas.java.home="%AS_JAVA%" -jar "%AS_INSTALL_LIB%/upgradeTool.jar" %*
+"%AS_JAVA%\bin\java" -Dcom.sun.aas.utool.LogLevel="INFO" -Dcom.sun.aas.domainRoot="%DIRNAME%\%AS_DEF_DOMAINS_PATH%"  -Dcom.sun.aas.java.home="%AS_JAVA%" -jar "%AS_INSTALL_MOD%/upgradeTool.jar" %*
 endlocal
