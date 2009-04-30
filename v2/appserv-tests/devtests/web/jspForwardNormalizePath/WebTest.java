558b5814ea414671ca1c82fcbb8d2ea6601e2e5b
Shing Wai Chan <shing.wai.chan@oracle.com>
2009-04-30 19:01:01 +0000

    close stream
    svn path=/trunk/; revision=26777
    
    
    Former-commit-id: 0cefb970e919889bb260f2d2854aca1420ba6ec9

--- v2/appserv-tests/devtests/web/jspForwardNormalizePath/WebTest.java
+++ v2/appserv-tests/devtests/web/jspForwardNormalizePath/WebTest.java
@@ -47,24 +47,60 @@ public class WebTest {
     private void invoke() throws Exception {
 
         System.out.println("Host=" + host + ", port=" + port);        
-        Socket sock = new Socket(host, new Integer(port).intValue());
-        OutputStream os = sock.getOutputStream();
-        String get = "GET " + contextRoot + "/page.jsp?blah=/../WEB-INF/web.xml HTTP/1.1\n";
-        System.out.println(get);
-        os.write(get.getBytes());
-        os.write("Host: localhost\n".getBytes());
-        os.write("\n".getBytes());
-
-        InputStream is = sock.getInputStream();
-        BufferedReader bis = new BufferedReader(new InputStreamReader(is));
-
-        String line = null;
+        Socket sock = null;
+        OutputStream os = null;
+        InputStream is = null;
+        BufferedReader bis = null;
         boolean isExpected = false;
-        while ((line = bis.readLine()) != null) {
-            System.out.println(line);
-            if (line.equals(EXPECTED)) {
-                isExpected = true;
-                break;
+
+        try {
+            sock = new Socket(host, new Integer(port).intValue());
+            os = sock.getOutputStream();
+            String get = "GET " + contextRoot + "/page.jsp?blah=/../WEB-INF/web.xml HTTP/1.1\n";
+            System.out.println(get);
+            os.write(get.getBytes());
+            os.write("Host: localhost\n".getBytes());
+            os.write("\n".getBytes());
+
+            is = sock.getInputStream();
+            bis = new BufferedReader(new InputStreamReader(is));
+
+            String line = null;
+            while ((line = bis.readLine()) != null) {
+                System.out.println(line);
+                if (line.equals(EXPECTED)) {
+                    isExpected = true;
+                    break;
+                }
+            }
+        } finally {
+            try {
+                if (sock != null) {
+                    sock.close();
+                }
+            } catch(IOException ioe) {
+                // ignore
+            }
+            try {
+                if (os != null) {
+                    os.close();
+                }
+            } catch(IOException ioe) {
+                // ignore
+            }
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch(IOException ioe) {
+                // ignore
+            }
+            try {
+                if (bis != null) {
+                    bis.close();
+                }
+            } catch(IOException ioe) {
+                // ignore
             }
         }
 
