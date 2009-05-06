cf046e6cc2146b544f65be5e8b80c52a5edd92b1
Kedar Mhaswade <km@java.net>
2009-05-06 03:08:42 +0000

    GLASSFISH-8066
    This is the batch-file version. I haven't tested this yet because the
    windows build is taking forever and will solely rely on Hudson to do
    the job. This is not a norm but exception ;)
    
    
    svn path=/trunk/; revision=27057
    
    
    Former-commit-id: 406c2fc3a7db281aed52f2714b5cf30e9c581a27

--- v3/packager/nucleus-base/bin/asadmin.bat
+++ v3/packager/nucleus-base/bin/asadmin.bat
@@ -1,9 +1,16 @@
 @echo off
-
-REM
 REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
-REM
-
-java -jar "%~dp0..\modules\admin-cli.jar" %*
-
+REM Always use JDK 1.6 or higher
+REM Depends on Java from ..\config\asenv.bat
+setlocal
+call "%~dp0..\config\asenv.bat" 
+if "%AS_JAVA%x" == "x" goto UsePath
+set JAVA="%AS_JAVA%\bin\java"
+goto run
+:UsePath
+set JAVA=java
+:run
+echo "java is %JAVA%"
+%JAVA% -jar "%~dp0..\modules\admin-cli.jar" %*
+endlocal
