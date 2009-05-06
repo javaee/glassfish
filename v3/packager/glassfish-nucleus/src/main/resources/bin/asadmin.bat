c9634e598b7b325d279d0acfdbb6be3378404a7f
Snjezana Sevo-Zenzerovic <snjezana.sevozenzerovic@oracle.com>
2009-05-06 06:37:30 +0000

    Applying fix for issue 8066 to bin/asadmin scripts.
    svn path=/trunk/; revision=27069
    
    
    Former-commit-id: 7547afa90eaaa6a96e09b9642101a3904c8d55ae

--- v3/packager/glassfish-nucleus/src/main/resources/bin/asadmin.bat
+++ v3/packager/glassfish-nucleus/src/main/resources/bin/asadmin.bat
@@ -1,9 +1,16 @@
 @echo off
-
-REM
 REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
-REM
-
-java -jar "%~dp0..\glassfish\modules\admin-cli.jar" %*
+REM Always use JDK 1.6 or higher
+REM Depends on Java from ..\glassfish\config\asenv.bat
+setlocal
+call "%~dp0..\glassfish\config\asenv.bat"
+if "%AS_JAVA%x" == "x" goto UsePath
+set JAVA="%AS_JAVA%\bin\java"
+goto run
+:UsePath
+set JAVA=java
+:run
+echo "java is %JAVA%"
+ %JAVA% -jar "%~dp0..\glassfish\modules\admin-cli.jar" %*
 
