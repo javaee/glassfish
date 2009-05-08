12559bca883a0c2eb44d2cfbc9419aab1b9f3886
Byron Nevins <byron.nevins@oracle.com>
2009-05-08 00:34:07 +0000

    works...
    svn path=/branches/bnevins/; revision=27179

--- experimental/jna/MyJna/src/test/java/com/elf/AppTest.java
+++ experimental/jna/MyJna/src/test/java/com/elf/AppTest.java
@@ -18,6 +18,10 @@ import com.sun.jna.Native;
  * @author bnevins
  */
 public class AppTest {
+    @Before
+    public void setup() {
+
+    }
     @Test
     public void main() {
         System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZ");
@@ -26,18 +30,30 @@ public class AppTest {
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
     }
+   public interface POSIX extends Library {
+	public int chmod(String filename, int mode);
+	public int chown(String filename, int user, int group);
+	public int rename(String oldpath, String newpath);
+	public int kill(int pid, int signal);
+	public int link(String oldpath, String newpath);
+	public int mkdir(String path, int mode);
+	public int rmdir(String path);
+	POSIX INSTANCE = (POSIX) Native.loadLibrary("c", POSIX.class);
+   }
 
     public interface Kernel32 extends Library {
        // FREQUENCY is expressed in hertz and ranges from 37 to 32767
        // DURATION is expressed in milliseconds
        public boolean Beep(int FREQUENCY, int DURATION);
        public void Sleep(int DURATION);
+       public long GetCurrentProcess();
+       public int GetProcessId(long process);
        Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("kernel32", Kernel32.class);
    }
 
     public interface CLibrary extends Library {
         CLibrary INSTANCE = (CLibrary) Native.loadLibrary(
-        (Platform.isWindows() ? "msvcrt" : "c"), CLibrary.class);
+            (windows ? "msvcrt" : "c"), CLibrary.class);
         void printf(String format, Object... args);
     }
 
@@ -49,6 +65,16 @@ public class AppTest {
    }
 
     @Test
+    public void pid() {
+        int pid = Kernel32.INSTANCE.GetProcessId(Kernel32.INSTANCE.GetCurrentProcess());
+        System.out.println("************  PID= " + pid + "  **************");
+        System.out.println("************  PID= " + pid + "  **************");
+        System.out.println("************  PID= " + pid + "  **************");
+        System.out.println("************  PID= " + pid + "  **************");
+        Kernel32.INSTANCE.Sleep(3000);
+    }
+
+    @Test
     public void printf() {
         CLibrary.INSTANCE.printf("Hello, World from NATIVE CODE\n");
         CLibrary.INSTANCE.printf("Hello, World from NATIVE CODE\n");
@@ -56,4 +82,15 @@ public class AppTest {
         CLibrary.INSTANCE.printf("Hello, World from NATIVE CODE\n");
         CLibrary.INSTANCE.printf("Hello, World from NATIVE CODE\n");
     }
+
+    @Test
+    public void posix() {
+        if(windows)
+            return;
+
+        System.out.println("Created directory via Native calls: /tmp/foofoogoo ");
+        POSIX.INSTANCE.mkdir("/tmp/foofoogoo", 777);
+    }
+
+    private final static boolean windows = Platform.isWindows();
 }
