4d85d9a10e56871d1dc3c5bed1ef14423cd81b3f
Lloyd Chambers <llc@java.net>
2009-05-13 00:38:40 +0000

    updated javadoc
    svn path=/trunk/; revision=27295
    
    
    Former-commit-id: 322c844549c6f4f8041589a3f17903857bd5da59

--- v3/common/amx-config/src/main/java/org/glassfish/admin/amx/intf/config/AMXConfigHelper.java
+++ v3/common/amx-config/src/main/java/org/glassfish/admin/amx/intf/config/AMXConfigHelper.java
@@ -11,32 +11,34 @@ import javax.management.Descriptor;
 import javax.management.MBeanAttributeInfo;
 import org.glassfish.admin.amx.annotation.Stability;
 import org.glassfish.admin.amx.annotation.Taxonomy;
+import org.glassfish.admin.amx.config.AMXConfigConstants;
 import org.glassfish.admin.amx.config.AMXConfigProxy;
+import org.jvnet.hk2.config.Units;
 import static org.glassfish.admin.amx.config.AMXConfigConstants.*;
 
 /**
- * Various utilities for working with AMX config MBeans.
+ * Various shortcut methods for working with AMX config MBeans, particularly attributes
+ * and metadata.
  * @author llc
  */
 @Taxonomy(stability = Stability.UNCOMMITTED)
 public class AMXConfigHelper
 {
     private final AMXConfigProxy mAMX;
-    
+
     public AMXConfigHelper(final AMXConfigProxy amx)
     {
         mAMX = amx;
     }
 
-    
     /**
-        match all attributes that have a Descriptor field with the specified value.
-        (could be extended to use regexp for value and/or field name).
-        @param amx the proxy
-        @param fieldName name of the field in the Descriptor for each Attribute
-        @param value value of the field in the Descriptor for each Attribute
+    match all attributes that have a Descriptor field with the specified value.
+    (could be extended to use regexp for value and/or field name).
+    @param amx the proxy
+    @param fieldName name of the field in the Descriptor for each Attribute
+    @param value value of the field in the Descriptor for each Attribute
      */
-    public  Set<String> attributeNamesByDescriptorField(final String fieldName, final String value)
+    public Set<String> attributeNamesByDescriptorField(final String fieldName, final String value)
     {
         final Set<String> attrNames = new HashSet<String>();
         for (final MBeanAttributeInfo attrInfo : mAMX.extra().mbeanInfo().getAttributes())
@@ -49,7 +51,10 @@ public class AMXConfigHelper
         }
         return attrNames;
     }
-    
+
+    /**
+    Get simple attribute names; those that are not Element kind.
+     */
     public Set<String> simpleAttributes()
     {
         final String elementKind = org.jvnet.hk2.config.Element.class.getName();
@@ -58,91 +63,129 @@ public class AMXConfigHelper
         remaining.removeAll(elementNames);
         return remaining;
     }
-    
+
     /**
-        Get simple attributes; those that are not Element kind.
+    Get simple attributes; those that are not Element kind.
      */
     public Map<String, Object> simpleAttributesMap()
     {
         return simpleAttributesMap();
     }
-    
-    public final Descriptor attributeDescriptor(final String attrName) 
+
+    /**
+    Get the JMX Descriptor for an Attribute.
+     * @see AMXConfigConstants
+     */
+    public final Descriptor attributeDescriptor(final String attrName)
     {
         final MBeanAttributeInfo info = mAMX.extra().attributeInfo(attrName);
         return info == null ? null : info.getDescriptor();
     }
-    
-    public final Object attributeDescriptorField(final String attrName, final String fieldName) 
+
+    /**
+    Get the field value for an Attribute and specified field name, if any.
+     * @param attrName
+     * @param fieldName
+     * @see AMXConfigConstants
+     */
+    public final Object attributeDescriptorField(final String attrName, final String fieldName)
     {
         final Descriptor desc = attributeDescriptor(attrName);
         return desc == null ? null : desc.getFieldValue(fieldName);
     }
-    
+
     /**
-        Return the units (if any) for the specified attribute.
+    Return the units (if any) for the specified attribute.
+     * @param attrName
+    @see Units
      */
-    public String units( final String attrName ) {
-        return  (String)attributeDescriptorField( attrName, DESC_UNITS);
+    public String units(final String attrName)
+    {
+        return (String) attributeDescriptorField(attrName, DESC_UNITS);
     }
-    
+
     /**
-        Return the minimum value (if any) for the specified numeric attribute.
+    Return the minimum value (if any) for the specified numeric attribute.
+     * @param attrName
      */
-    public Long min( final String attrName ) {
-        return  (Long)attributeDescriptorField( attrName, DESC_MIN);
+    public Long min(final String attrName)
+    {
+        return (Long) attributeDescriptorField(attrName, DESC_MIN);
     }
-    
+
     /**
-        Return the maximum value (if any) for the specified numeric attribute.
+    Return the maximum value (if any) for the specified numeric attribute.
+     * @param attrName
      */
-    public Long max( final String attrName ) {
-        return  (Long)attributeDescriptorField( attrName, DESC_MAX);
+    public Long max(final String attrName)
+    {
+        return (Long) attributeDescriptorField(attrName, DESC_MAX);
     }
-    
+
     /**
-        Return the dataType (if any) for the specified attribute eg java.lang.Boolean.
+    Return the dataType (if any) for the specified attribute eg java.lang.Boolean,
+     * @param attrName
+     * java.lang.Integer, java.lang.Long, java.lang.String
      */
-    public String dataType( final String attrName ) {
-        return (String)attributeDescriptorField( attrName, DESC_DATA_TYPE);
+    public String dataType(final String attrName)
+    {
+        return (String) attributeDescriptorField(attrName, DESC_DATA_TYPE);
     }
-    
+
     /**
-        Return the regex pattern (if any) for the specified attribute.
+    Return the regex pattern (if any) for the specified attribute.
+     * @param attrName
      */
-    public String regexPattern( final String attrName ) {
-        return (String)attributeDescriptorField( attrName, DESC_PATTERN_REGEX);
+    public String regexPattern(final String attrName)
+    {
+        return (String) attributeDescriptorField(attrName, DESC_PATTERN_REGEX);
     }
-    
-    public boolean key(final String attrName) {
-        return Boolean.parseBoolean( "" + attributeDescriptorField( attrName, DESC_KEY) );
+
+    /** return true if the Attribute is a key value
+     * @param attrName
+     */
+    public boolean key(final String attrName)
+    {
+        return Boolean.parseBoolean("" + attributeDescriptorField(attrName, DESC_KEY));
     }
-    
-    public boolean notNull(final String attrName) {
-        return Boolean.parseBoolean( "" + attributeDescriptorField( attrName, DESC_NOT_NULL) );
+
+    /** return true if the Attribute may not be null (is required)
+     * @param attrName
+     */
+    public boolean notNull(final String attrName)
+    {
+        return Boolean.parseBoolean("" + attributeDescriptorField(attrName, DESC_NOT_NULL));
     }
-    
+
     /**
-        Return whether the attribute is required.
+    Return whether the attribute is required.  This can be implied by several
+     * different annotations or field; this method checks them all.
+     * @param attrName
      */
-    public Boolean required( final String attrName ) {
+    public Boolean required(final String attrName)
+    {
         return notNull(attrName) || key(attrName) ||
-            Boolean.parseBoolean( "" + attributeDescriptorField( attrName, DESC_REQUIRED) );
+               Boolean.parseBoolean("" + attributeDescriptorField(attrName, DESC_REQUIRED));
     }
-    
+
     /**
-        Return the xml name of the attribute.
+    Return the xml name of the attribute.
+     * @param attrName
      */
-    public String xmlName( final String attrName ) {
-        return (String)attributeDescriptorField( attrName, DESC_XML_NAME);
+    public String xmlName(final String attrName)
+    {
+        return (String) attributeDescriptorField(attrName, DESC_XML_NAME);
     }
-    
+
     /**
-        Return the xml name of the attribute.
+    Return the default value of the attribute, or null if no default value.
+     * @param attrName
      */
-    public String defaultValue( final String attrName ) {
-        return (String)attributeDescriptorField( attrName, DESC_DEFAULT_VALUE);
+    public String defaultValue(final String attrName)
+    {
+        return (String) attributeDescriptorField(attrName, DESC_DEFAULT_VALUE);
     }
+
 }
 
 
