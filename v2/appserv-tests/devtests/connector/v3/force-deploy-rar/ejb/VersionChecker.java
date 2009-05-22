c75802532ceab195fe73ed3e4aff0688fb5ec50d
Jagadish Ramu <jagadish.ramu@oracle.com>
2009-05-22 14:09:03 +0000

    New test for "undeploy --cascade" and "deploy --force" for a resource-adapter
    svn path=/trunk/; revision=27533
    
    
    Former-commit-id: 0b0b3b2a4fcd713d7f6970ddb9a1c8377f390789

---
+++ v2/appserv-tests/devtests/connector/v3/force-deploy-rar/ejb/VersionChecker.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package beans;
+
+import javax.ejb.EJBLocalObject;
+
+public interface VersionChecker extends EJBLocalObject {
+    int getVersion() ;
+    boolean lookupResource();
+}
