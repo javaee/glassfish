7425f198416bc1270426304d95b46282539e11c8
Kedar Mhaswade <km@java.net>
2009-05-24 17:29:23 +0000

    more clarifications
    svn path=/branches/asadmin/; revision=27566
    
    
    Former-commit-id: f5f672d5e9b5ad8a09761a464469f1589306b526

--- v3/common/glassfish-api/src/main/java/org/glassfish/api/admin/cli/OptionType.java
+++ v3/common/glassfish-api/src/main/java/org/glassfish/api/admin/cli/OptionType.java
@@ -1,15 +1,17 @@
 package org.glassfish.api.admin.cli;
 
-/** Represents the type of an option or an operand.  An OptionType tell the generic command line implementation about
+/** Represents the type of an option or an operand.  An OptionType tells the generic command line implementation about
  *  how to treat (interpret) a particular option or operand. Boolean options are special cased, because they are
- *  represented specially on the command line.
+ *  represented differently on the command line.
  * @author Kedar Mhaswade(km@dev.java.net)
  */
 public enum OptionType {
+  
   BOOLEAN,   // implies it can be specified as --name or -n or --name=true or --name=false or --no-name
+  DIRECTORY, // implies a folder
   FILE,      // implies a file upload
   PASSWORD,  // implies secret information that should be prompted for
-  PATH,      // implies operating system path to a file
+  FILE_PATH, // implies operating system path to a file
   STRING,    // generic string
   PROPERTY,  //format is a=b
   PROPERTIES //format is a=b:c=d  
