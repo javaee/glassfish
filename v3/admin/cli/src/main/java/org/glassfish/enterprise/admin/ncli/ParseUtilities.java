769e2f331d278916320faad47a58f57822d90813
Kedar Mhaswade <km@java.net>
2009-06-03 06:22:42 +0000

    - Added more unit tests. - Discovered an anomaly that makes '-' an invalid character for an option name. This is sad but true.   If '-' is allowed in an option name, an option named --no-lines is ambiguous in that it can   either mean a long boolean option named "lines" negated (i.e. a boolean option --lines=false) or   it can mean a non-boolean option whose value is specified as next argument (e.g. --no-lines 203). - Made the Regex's stricter for the same.
    Unit tests are a must.
    
    svn path=/branches/asadmin/; revision=27685
    
    
    Former-commit-id: cae3670830c84b007260586c55270eada3579b3e

--- v3/admin/cli/src/main/java/org/glassfish/enterprise/admin/ncli/ParseUtilities.java
+++ v3/admin/cli/src/main/java/org/glassfish/enterprise/admin/ncli/ParseUtilities.java
@@ -9,7 +9,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-/**
+/** Provides utility routines to parse the command line. The regular expressions defined in this class as
+ *  literal strings are the basis of command line parsing.
  * @author &#2325;&#2375;&#2342;&#2366;&#2352 (km@dev.java.net)
  */
 final class ParseUtilities {
@@ -17,11 +18,11 @@ final class ParseUtilities {
     private static final String NEG_LONG_PREFIX               = "--no-";
     private static final String CMD_NAME_REGEX                = "^[a-zA-Z]([-_\\w]*)?$";
     private static final String SHORT_BOOLEAN_OPTION_REGEX    = "^-[a-zA-Z](=([t][r][u][e]|[f][a][l][s][e]))?$";
-    private static final String LONG_BOOLEAN_OPTION_REGEX     = "^--[\\w && \\D][-_\\w]+(=([t][r][u][e]|[f][a][l][s][e]))?$";
-    private static final String NEG_LONG_BOOLEAN_OPTION_REGEX = "^" + NEG_LONG_PREFIX +"[\\w &&\\D][-_\\w]+$";
+    private static final String LONG_BOOLEAN_OPTION_REGEX     = "^--[\\w && \\D][_\\w]+(=([t][r][u][e]|[f][a][l][s][e]))?$";
+    private static final String NEG_LONG_BOOLEAN_OPTION_REGEX = "^" + NEG_LONG_PREFIX +"[\\w &&\\D][_\\w]+$";
 
     private static final String SHORT_NON_BOOLEAN_OPTION_REGEX = "^-[a-zA-Z](=(.)+)?$";
-    private static final String LONG_NON_BOOLEAN_OPTION_REGEX  = "^--[\\w && \\D][-_\\w]+(=(.)+)?$";
+    private static final String LONG_NON_BOOLEAN_OPTION_REGEX  = "^--[\\w && \\D][_\\w]+(=(.)+)?$";
 
     private static final String REMAINDER_ARE_OPERANDS_INDICATOR_REGEX = "^--$";
     
@@ -89,13 +90,26 @@ final class ParseUtilities {
      * @param s the option as it appears on command line, e.g. <code> -f or -f=value</code>
      * @return symbol of the option e.g. <code>f</code> from given param
      */
-    static String getOptionSymbolFromShortOption(String s) {
+    static char getOptionSymbolFromShortOption(String s) {
         if (!indicatesShortBooleanOption(s) &&
             !indicatesShortNonBooleanOption(s))
             throw new IllegalArgumentException(s + " does not specify a long option");
-        return s.substring(1, 2); //a symbol 's' is always specified as -s, -s=value
+        return s.charAt(1); //a symbol 's' is always specified as -s, -s=value
     }
 
+    static boolean hasOptionNameAndValue(String s) {
+        if (!indicatesOption(s))
+            return false;
+        return s.indexOf(OPTION_NAME_VALUE_SEPARATOR) != -1; //should be an option && should have at least one '='
+    }
+
+    static String getOptionValue(String s) {
+        //makes sense only if the given param has both name/symbol and value specified, delimited by an '='
+        if (hasOptionNameAndValue(s)) {
+            return s.substring(s.indexOf(OPTION_NAME_VALUE_SEPARATOR) + 1);
+        }
+        return null;
+    }
     static void file2Set(String file, Set<String> set) {
         BufferedReader reader = null;
         try {
@@ -123,7 +137,9 @@ final class ParseUtilities {
             }
         }
     }
+
     // ALL Private ...
+
     private static boolean matches(String s, String regex) {
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(s);
--- v3/admin/cli/src/test/java/org/glassfish/enterprise/admin/ncli/ParseUtilitiesTest.java
+++ v3/admin/cli/src/test/java/org/glassfish/enterprise/admin/ncli/ParseUtilitiesTest.java
@@ -1,37 +1,104 @@
 package org.glassfish.enterprise.admin.ncli;
 
-import static junit.framework.Assert.*;
+import static org.glassfish.enterprise.admin.ncli.ParseUtilities.*;
 import junit.framework.TestCase;
 
-import java.util.Set;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 /**
  * @author &#2325;&#2375;&#2342;&#2366;&#2352 (km@dev.java.net)
  */
 public class ParseUtilitiesTest extends TestCase {
 
-    public void testCommandName() {
+    public void testIndicatesCommandName1() {
         String cmd = "create-jdbc-resource";
-        assertEquals(ParseUtilities.indicatesCommandName(cmd), true);
+        assertEquals(indicatesCommandName(cmd), true);
 
         cmd = "-invalid-cmd-name";
-        assertEquals(ParseUtilities.indicatesCommandName(cmd), false);
+        assertEquals(indicatesCommandName(cmd), false);
 
         cmd = "0another-invalid";
-        assertEquals(ParseUtilities.indicatesCommandName(cmd), false);
+        assertEquals(indicatesCommandName(cmd), false);
     }
 
-    public void testLegacySupportCommandsAreValid() {
+    public void testIndicatesCommandName2() {
         //this is a combined test as it tests multiple things.
         Set<String> slc = new HashSet<String>();
-        ParseUtilities.file2Set(Constants.SUPPORTED_CMD_FILE_NAME, slc);
+        file2Set(Constants.SUPPORTED_CMD_FILE_NAME, slc);
         Iterator<String> it = slc.iterator();
         while(it.hasNext()) {
             String cmd = it.next();
-            System.out.println("Testing: " + cmd);
-            assertEquals(ParseUtilities.indicatesCommandName(cmd), true);
+            assertEquals(indicatesCommandName(cmd), true);
         }
+    }
+    
+    public void testIndicatesOption() {
+        String s = "--host"; //valid long option
+        assertEquals(indicatesOption(s), true);
+        s = "-f";  //valid short option
+        assertEquals(indicatesOption(s), true);
+        s = "-f=false"; //valid short option with value, separated by an '='
+        assertEquals(indicatesOption(s), true);
+        s = "-p=name=value"; //valid short option with property specification
+        assertEquals(indicatesOption(s), true);
+        s = "--prop=a=b:c=d";  //valid long option with multiple properties delimited with a ':'
+        assertEquals(indicatesOption(s), true);
+        s = "--no-force";
+        assertEquals(indicatesOption(s), true);
+
+        //invalid cases
+        s = "-prop"; //invalid long or short option
+        assertEquals(indicatesOption(s), false);
+        s = "foo"; //option must start with a - or -- or --no-
+        assertEquals(indicatesOption(s), false);
+        s = "-"; //short option should have at least a character
+        assertEquals(indicatesOption(s), false);
+        s = "--"; //long option should have at least 2 characters in name
+        assertEquals(indicatesOption(s), false);
+        s = "--f"; //neither a short option (one additional '-') or a long option (lack of a character)
+        assertEquals(indicatesOption(s), false);
+        s = "--no-f";
+        assertEquals(indicatesOption(s), false);
+        s = "--with-hyphen"; //unfortunately, '-' is not allowed in the name of the option
+        assertEquals(indicatesOption(s), false);
+        s = "-to"; //a symbol is should be one character long
+        assertEquals(indicatesOption(s), false);
+    }
+
+    public void testGetOptionNameFromLongOption() {
+        String s = "--host=localhost";
+        assertEquals(getOptionNameFromLongOption(s), "host");
+        s = "--host";
+        assertEquals(getOptionNameFromLongOption(s), "host");
+    }
+    public void testGetOptionSymbolFromShortOption() {
+        String s = "-h=129.23.44.224";
+        assertEquals(getOptionSymbolFromShortOption(s), 'h');
+        s = "-h";
+        assertEquals(getOptionSymbolFromShortOption(s), 'h');        
+    }
+
+    public void testHasOptionNameAndValue() {
+        String s = "-h=localhost";
+        assertEquals(hasOptionNameAndValue(s), true);
+        s = "--desc=application display name";
+        assertEquals(hasOptionNameAndValue(s), true);
+        s = "-h";
+        assertEquals(hasOptionNameAndValue(s), false);
+        s = "--host";
+        assertEquals(hasOptionNameAndValue(s), false);
+        s = "--with_equals=a=b";
+        assertEquals(hasOptionNameAndValue(s), true);
+    }
+
+    public void testGetOptionValue() {
+        String s = "-h=localhost";
+        assertEquals(getOptionValue(s), "localhost");
+        s = "--desc=application display name";
+        assertEquals(getOptionValue(s), "application display name");
+        s = "--prop=a=b";
+        assertEquals(getOptionValue(s), "a=b");        
     }
 }
