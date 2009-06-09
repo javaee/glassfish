2e25bc54ba9344c410c38cb376dd293cefabdb57
Kedar Mhaswade <km@java.net>
2009-06-09 03:23:25 +0000

    More javadoc, more clarifications of the regular expressions that are vital to parsing.
    svn path=/branches/asadmin/; revision=27756
    
    
    Former-commit-id: abe574714a8cf6a7b9bdc800cc04be8285bcf7fc

--- v3/admin/cli/src/main/java/org/glassfish/enterprise/admin/ncli/ParseUtilities.java
+++ v3/admin/cli/src/main/java/org/glassfish/enterprise/admin/ncli/ParseUtilities.java
@@ -28,41 +28,105 @@ final class ParseUtilities {
      */
     static final String CMD_NAME_REGEX                   = "^[a-zA-Z]([-_\\w]*)?$";
 
-    /** A <b> regex</b> for a boolean short option which can be specified as: <p>
+    /** A <b> regex</b> for a boolean short option (aka symbol) which can be specified as: <p>
      *  -c, or -c=true or -c=false, where c is exactly one letter between [a-z] or [A-Z].
      *
      */
     static final String SHORT_BOOLEAN_OPTION_REGEX       = "^-[a-zA-Z](=([t][r][u][e]|[f][a][l][s][e]))?$";
+
+    /** A <b> regex </b> for a list of boolean options combined. Put literally, this is a string
+     * starting with a hyphen followed by 2 or more chracters in the range [a-z] or [A-Z] and nothing else.
+     *
+     */
     static final String SHORT_BOOLEAN_OPTION_LIST_REGEX  = "^-[a-zA-Z][a-zA-Z]+$";
+
+    /** A <b> regex </b> for a boolean option with a long name (rather than a symbol). Put literally, it is two hyphens followed
+     *  by 2 or more characters with first character in the range [a-z], [A-Z], and subsequent character is one of
+     *  [a-zA-Z], underscore which is then followed by an optional string "=true" or "=false". Note that for a precise reason
+     * (i.e due to {@link #NEG_LONG_BOOLEAN_OPTION_REGEX}), a hyphen is not allowed in the name of such option.
+     *
+     */
     static final String LONG_BOOLEAN_OPTION_REGEX        = "^--[\\w && \\D][_\\w]+(=([t][r][u][e]|[f][a][l][s][e]))?$";
+
+    /** A <b> regex </b> based on {@link #NEG_LONG_PREFIX} for a boolean option with long name negated.
+     *  Put literally, it is a string that starts with NEG_LONG_PREFIX followed by a valid long name, derived from
+     *  #LONG_BOOLEAN_OPTION_REGEX. 
+     *
+     */
     static final String NEG_LONG_BOOLEAN_OPTION_REGEX    = "^" + NEG_LONG_PREFIX +"[\\w &&\\D][_\\w]+$";
 
+    /** A <b> regex </b> for a non-boolean option (which could be boolean as well, if the values specified are "true"
+     *  or "false"). Put literally, it is a hyphen followed by a single character in the range [a-zA-Z] and then
+     *  an optional specification of equal to sign and value (which can be anything one or more character long).
+     *
+     */
     static final String SHORT_NON_BOOLEAN_OPTION_REGEX   = "^-[a-zA-Z](=(.)+)?$";
+
+    /** A <b> regex </b> for a non-boolean option (which could be boolean as well, if the values specified are "true"
+     *  or "false"). Put literally, it is two hyphens followed by a valid option name based on #LONG_BOOLEAN_OPTION_REGEX then
+     *  an optional specification of equal to sign and value (which can be anything one or more character long).
+     */
     static final String LONG_NON_BOOLEAN_OPTION_REGEX    = "^--[\\w && \\D][_\\w]+(=(.)+)?$";
 
+    /** A fixed string "--" and nothing else.
+     */
     static final String REMAINDER_ARE_OPERANDS_INDICATOR_REGEX = "^--$";
-    
+
+    /** A separator for name and value of an option in the same argument. The first occurrance of this character
+     *  separates name and value of an option within the same string.
+     */
     static final String OPTION_NAME_VALUE_SEPARATOR    = "=";
-
-
 
     private ParseUtilities() {}
 
+    /** Tests if the argument represents a command name.
+     * @param s A string representing an argument on command line
+     * @return true if argument matches #CMD_NAME_REGEX, false otherwise
+     * @throws NullPointerException if argument is null
+     */
     static boolean indicatesCommandName(String s) {
         return matches(s, CMD_NAME_REGEX);
     }
-    
+
+    /** Tests if the argument represents any kind of <b> single</b> option. This excludes a boolean option list which
+     *  represents two or more boolean options.
+     * @param s A string representing an argument on command line
+     * @return true if argument matches #SHORT_BOOLEAN_OPTION_REGEX, #LONG_BOOLEAN_OPTION_REGEX, #NEG_LONG_BOOLEAN_OPTION_REGEX,
+     * #SHORT_NON_BOOLEAN_OPTION_REGEX, or #LONG_NON_BOOLEAN_OPTION_REGEX, false otherwise
+     * @throws NullPointerException if argument is null
+     */
     static boolean indicatesOption(String s) {
         return indicatesShortOption(s) || indicatesLongOption(s);
     }
 
+    /** Tests if the argument represents a boolean option list.
+     *
+     * @param s String representing argument
+     * @return true if argument matches #SHORT_BOOLEAN_OPTION_LIST_REGEX, false otherwise
+     * @throws NullPointerException if argument is null
+     */
     static boolean indicatesBooleanOptionList(String s) {
         return matches(s, SHORT_BOOLEAN_OPTION_LIST_REGEX);
     }
     
+    /** Tests if the argument represents any kind of <b> single</b> option. This excludes a boolean option list which
+     *  represents two or more boolean options.
+     * @param s A string representing an argument on command line
+     * @return true if argument matches #SHORT_BOOLEAN_OPTION_REGEX,
+     * #SHORT_BOOLEAN_OPTION_REGEX, or #SHORT_NON_BOOLEAN_OPTION_REGEX, false otherwise
+     * @throws NullPointerException if argument is null
+     */
     static boolean indicatesShortOption(String s) {
         return indicatesShortBooleanOption(s) || indicatesShortNonBooleanOption(s);
     }
+
+    /** Tests if the argument represents any kind of <b> single</b> option. This excludes a boolean option list which
+     *  represents two or more boolean options.
+     * @param s A string representing an argument on command line
+     * @return true if argument matches #SHORT_BOOLEAN_OPTION_REGEX, #LONG_BOOLEAN_OPTION_REGEX, #NEG_LONG_BOOLEAN_OPTION_REGEX,
+     * #SHORT_NON_BOOLEAN_OPTION_REGEX, or #LONG_NON_BOOLEAN_OPTION_REGEX, false otherwise
+     * @throws NullPointerException if argument is null
+     */
     static boolean indicatesLongOption(String s) {
         return indicatesLongBooleanOption(s) || indicatesNegativeLongBooleanOption(s) || indicatesLongNonBooleanOption(s);
     }
