4edebfab67200b13a568d9eac96dab038ad5f7e3
Jennifer Galloway <jc129909>
2009-06-16 14:02:59 +0000

    Convert JVMTelemetry class to StatsProvider classes.
    svn path=/trunk/; revision=27908
    
    
    Former-commit-id: f581fc285653198b3bef8ac44a62f918c04536e0

---
+++ v3/admin/monitor/src/main/java/org/glassfish/admin/monitor/jvm/JVMGCStatsProvider.java
@@ -0,0 +1,86 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.glassfish.admin.monitor.jvm;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+import org.glassfish.gmbal.Description;
+import org.glassfish.gmbal.ManagedAttribute;
+import org.glassfish.gmbal.ManagedObject;
+
+/* jvm.garbage-collectors */
+@ManagedObject
+@Description( "JVM Garbage Collectors Statistics" )
+public class JVMGCStatsProvider {
+    
+    private List<GarbageCollectorMXBean> gcBeanList = ManagementFactory.getGarbageCollectorMXBeans();
+
+    public String[] getGCNames() {
+        String[] names = {};
+        int i = 0;
+        for (GarbageCollectorMXBean gcBean : gcBeanList) {
+            names[i] = gcBean.getName();
+            i++;
+        }
+        return names;
+    }
+
+    @ManagedAttribute(id="collectioncount-count")
+    @Description( "total number of collections that have occurred" )
+    public long[] getCollectionCount() {
+        long[] counts = {};
+        int i = 0;
+        for (GarbageCollectorMXBean gcBean : gcBeanList) {
+            counts[i] = gcBean.getCollectionCount();
+            i++;
+        }
+        return counts;
+    }
+
+    @ManagedAttribute(id="collectiontime-count")
+    @Description( "approximate accumulated collection elapsed time in milliseconds" )
+    public long[] getCollectionTime() {
+        long[] times = {};
+        int i = 0;
+        for (GarbageCollectorMXBean gcBean : gcBeanList) {
+            times[i] = gcBean.getCollectionTime();
+            i++;
+        }
+        return times;
+    }
+}
