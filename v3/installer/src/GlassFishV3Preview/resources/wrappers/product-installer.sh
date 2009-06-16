b0f23a2e5de03f77c4d3f763eda9220efcd9c8a1
Sathyan Catari <scatari@java.net>
2009-06-16 20:41:34 +0000

    Fixes for 8399 and 8529. The following changes are done to uninstall scripts.
    -Usage altered to support only required parameters for uninstall.
    -Uninstaller now require jdk 1.6 to run.
    -Approprirate exit codes added to support automation.
    -Support for silent installation enabled.
    -Usage message in installer altered to exclude uninstallation strings.
    -Open installer wrappers readjusted.
    
    
    svn path=/trunk/; revision=27924
    
    
    Former-commit-id: 9b813b0eba24ea153e8a2b507ef5ba365152e516

--- v3/installer/src/GlassFishV3Preview/resources/wrappers/product-installer.sh
+++ v3/installer/src/GlassFishV3Preview/resources/wrappers/product-installer.sh
@@ -96,7 +96,7 @@ LOGDIR=/tmp
 usage() {
   ${CAT} <<EOF
 
-Usage: <GlassFish installation/uninstallation program.> [-options]
+Usage: <GlassFish installation program> [-options]
 where options include:
     -a <answerfile>
         run this program in silent mode using the answerfile provided, should be used with -s option.
