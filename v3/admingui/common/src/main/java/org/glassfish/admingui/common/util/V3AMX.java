223a6139f57b36b95e6c28f2f03f079ff624eebd
Anissa Lam <anissa.lam@oracle.com>
2009-06-23 00:15:33 +0000

    - add more util method.  This file should be separated into another util class.
    svn path=/trunk/; revision=28068
    
    
    Former-commit-id: 57661cbd65e8c4cb49916f006341f8b9b1a182a9

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/util/V3AMX.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/util/V3AMX.java
@@ -5,8 +5,9 @@
 
 package org.glassfish.admingui.common.util;
 
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -20,20 +21,26 @@ import javax.servlet.ServletContext;
 import javax.management.MBeanServer;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
+
 import org.glassfish.admin.amx.base.DomainRoot;
+import org.glassfish.admin.amx.base.RealmsMgr;
+import org.glassfish.admin.amx.config.AMXConfigProxy;
 import org.glassfish.admin.amx.core.AMXProxy;
 import org.glassfish.admin.amx.core.proxy.AMXBooter;
 import org.glassfish.admin.amx.core.proxy.ProxyFactory;
-import org.glassfish.admin.amx.intf.config.Config;
-import org.glassfish.admin.amx.intf.config.Configs;
+import org.glassfish.admin.amx.intf.config.AMXConfigHelper;
 import org.glassfish.admin.amx.intf.config.ApplicationRef;
+import org.glassfish.admin.amx.intf.config.Config;
+import org.glassfish.admin.amx.intf.config.ConfigTools;
+import org.glassfish.admin.amx.intf.config.Configs;
 import org.glassfish.admin.amx.intf.config.Domain;
-
-
-import org.glassfish.admin.amx.intf.config.VirtualServer;
 import org.glassfish.admin.amx.intf.config.HttpService;
+import org.glassfish.admin.amx.intf.config.PropertiesAccess;
+import org.glassfish.admin.amx.intf.config.Property;
+import org.glassfish.admin.amx.intf.config.VirtualServer;
 import org.glassfish.admin.amx.intf.config.grizzly.NetworkConfig;
 import org.glassfish.admin.amx.intf.config.grizzly.NetworkListener;
+
 import org.jvnet.hk2.component.Habitat;
 
 /**
@@ -111,6 +118,10 @@ public class V3AMX {
         return mbeanServer;
     }
 
+    public RealmsMgr getRealmsMgr(){
+        return domainRoot.getExt().getRealmsMgr();
+    }
+
     public static Config getServerConfig(String configName){
         if ((configName == null) || configName.equals(""))
                 configName = "server-config";
@@ -166,6 +177,200 @@ public class V3AMX {
 	} catch (Exception  ex) {
             throw new RuntimeException(ex);
         }
+    }
+    
+    public static Map getAttrsMap(Object objectNameStr){
+        
+            ObjectName amxObjectName = null;
+        try{
+            if (objectNameStr instanceof ObjectName){
+                amxObjectName = (ObjectName) objectNameStr;
+            }else{
+                amxObjectName = new ObjectName((String)objectNameStr);
+            }
+            AMXProxy  amx = (AMXProxy) V3AMX.getInstance().getProxyFactory().getProxy(amxObjectName);
+            AMXConfigHelper helper = new AMXConfigHelper((AMXConfigProxy) amx);
+            final Map<String,Object> attrs = helper.simpleAttributesMap();
+            return attrs;
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return new HashMap();
+        }
+    }
+
+        /**
+     *  Change the property of a config mbean
+     *  If the property Value is null or empty, this property will be removed.
+     *  If the property  already exist,  the property value will be updated if the value is different.
+     *  If the property doesn't exist, a new property will be created.
+     *
+     * @param config    mbean whose Property element to be changed
+     * @param propName  name of the property
+     * @param propValue value of this property
+     *
+     */
+    public static void setPropertyValue(AMXConfigProxy config, String propName, String propValue) {
+        Map<String, Property> pMap = ((PropertiesAccess)config).getProperty();
+        if (pMap.containsKey(propName)) {
+            if (GuiUtil.isEmpty(propValue)) {
+                config.removeChild("property", propName);
+            } else {
+                //don't change the value if it is equal.
+                Property cp = ((PropertiesAccess)config).getProperty().get(propName);
+                if (!cp.getValue().equals(propValue)) {
+                    cp.setValue(propValue);
+                }
+            }
+        } else {
+            if (!GuiUtil.isEmpty(propValue)) {
+                Map attrs =new HashMap();
+                attrs.put(PROPERTY_NAME, propName);
+                attrs.put(PROPERTY_VALUE, propValue);
+                config.createChild("property", attrs);
+            }
+        }
+    }
+
+    public static void setProperties(String objectNameStr, List<Map<String,String>> propertyList, boolean sysProp){
+
+        try{
+            ObjectName objectName = new ObjectName(objectNameStr);
+            List newList = new ArrayList();
+            Set propertyNames = new HashSet();
+            final ConfigTools configTools = V3AMX.getInstance().getDomainRoot().getExt().child(ConfigTools.class);
+            if (propertyList.size()==0){
+                if (sysProp){
+                    configTools.clearSystemProperties(objectName);
+                }else{
+                    configTools.clearProperties(objectName);
+                }
+            }else{
+                for(Map<String, String> oneRow : propertyList){
+                    Map newRow = new HashMap();
+                    final String  name = oneRow.get(PROPERTY_NAME);
+                    if (GuiUtil.isEmpty(name)){
+                        continue;
+                    }
+                    if (propertyNames.contains(name)){
+                        throw new RuntimeException(GuiUtil.getMessage("msg.duplicatePropTableKey", new Object[]{name}));
+                        //GuiUtil.handleError(handlerCtx, GuiUtil.getMessage("msg.duplicatePropTableKey", new Object[]{name}));
+                    }else{
+                        propertyNames.add(name);
+                    }
+
+                    String value = oneRow.get(PROPERTY_VALUE);
+                    if (GuiUtil.isEmpty(value)){
+                        value = "";
+                    }
+                    newRow.put(PROPERTY_NAME,name);
+                    newRow.put(PROPERTY_VALUE,value);
+                    String desc = (String) oneRow.get(PROPERTY_DESC);
+                    if (! GuiUtil.isEmpty(desc)){
+                        newRow.put( PROPERTY_DESC,  desc);
+                    }
+                    newList.add(newRow);
+                }
+                if (sysProp){
+                    configTools.setSystemProperties(objectName, newList, true);
+                }else{
+                    configTools.setProperties(objectName, newList, true);
+                }
+            }
+        }catch(Exception ex){
+            throw new RuntimeException (ex);
+        }
+    }
+
+    /*
+     * update the properties of a config.
+     */
+    public static void updateProperties(AMXConfigProxy config, Map<String, String> newProps, List ignore) {
+
+        Map<String, Property> oldProps = ((PropertiesAccess)config).getProperty();
+        if (ignore == null) {
+            ignore = new ArrayList();
+        }
+        //Remove any property that is no longer in the new list
+        Iterator iter = oldProps.keySet().iterator();
+        while (iter.hasNext()) {
+            Object key = iter.next();
+            if (ignore.contains(key)) {
+                continue;
+            }
+            if (!newProps.containsKey(key)) {
+                config.removeChild("property", (String)key);
+            }
+        }
+
+        //update the value if the value is different or create a new property if it doesn't exist before
+        Map<String, Property> pMap = ((PropertiesAccess)config).getProperty();
+        for (String propName : newProps.keySet()) {
+            String propValue = newProps.get(propName);
+            if (pMap.containsKey(propName)) {
+                pMap.get(propName).setValue(propValue);
+            } else {
+                Map attrs =new HashMap();
+                attrs.put(PROPERTY_NAME, propName);
+                attrs.put(PROPERTY_VALUE, propValue);
+                config.createChild("property", attrs);
+            }
+        }
+    }
+
+    /**
+     * returns the Properties of a config, skipping those specified in the list thats passed in.
+     * This is mostly for edit where we want to treat particular properties differently, and don't
+     * show that in the Properties table.
+     * Normally, this is followed by updateProperites() with the ignore list the same as the skipList
+     * specified here when user does a Save.
+     */
+    public static Map<String, Property> getNonSkipPropertiesMap(PropertiesAccess config, List skipList) {
+        Map<String, Property> props = config.getProperty();
+        Map<String, Property> newMap = new HashMap<String, Property>();
+
+        for (String propsName : props.keySet()) {
+            if (skipList.contains(propsName)) {
+                continue;
+            }
+            newMap.put(propsName, props.get(propsName));
+        }
+        return newMap;
+    }
+    
+    public static String getPropValue( Map<String, Property> propMap, String key){
+        if (propMap.get(key) != null)
+            return propMap.get(key).getValue();
+        return "";
+    }
+
+    /*  converts a Property Map to a Map where the name is preceded by PropertiesAccess.PROPERTY_PREFIX.
+     *  This conversion is required when this Map is used as the optional parameter when creating a config.
+     *  refer to the java doc of PropertiesAccess in AMX javadoc
+     */
+    public static Map convertToPropertiesOptionMap(Map<String, String> props, Map<String, String> convertedMap) {
+        if (convertedMap == null) {
+            convertedMap = new HashMap();
+        }
+        if (props == null) {
+            return convertedMap;
+        }
+        Set<String> keySet = props.keySet();
+        for (String key : keySet) {
+            if (!GuiUtil.isEmpty((String) props.get(key))) {
+                convertedMap.put(PropertiesAccess.PROPERTY_PREFIX + key, (String) props.get(key));
+            }
+        }
+        return convertedMap;
+    }
+
+
+    public static Integer getAdminPort(){
+        String port = (String) getAttrsMap(AMX.ADMIN_LISTENER).get("Port");
+        if (port.startsWith("$")){
+            //TODO resolve token;
+            port = "4848";
+        }
+        return Integer.getInteger(port);
     }
 
 
@@ -306,6 +511,9 @@ public class V3AMX {
         skipAttr.add("Name");
         skipAttr.add("Children");
     }
+    private static final String PROPERTY_NAME = "Name";
+    private static final String PROPERTY_VALUE = "Value";
+    private static final String PROPERTY_DESC = "Description";
 
 
 }
