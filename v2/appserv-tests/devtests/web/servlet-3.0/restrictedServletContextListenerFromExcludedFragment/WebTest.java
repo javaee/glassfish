0209b2fd92afa8510c6bc04212813c7aace8d750
Jan Luehe <jluehe@java.net>
2009-06-26 17:16:04 +0000

    Added description of test scenario
    svn path=/trunk/; revision=28232
    
    
    Former-commit-id: 7ca853f68a0b3b83f38c1f07f607207b67e0a902

--- v2/appserv-tests/devtests/web/servlet-3.0/restrictedServletContextListenerFromExcludedFragment/WebTest.java
+++ v2/appserv-tests/devtests/web/servlet-3.0/restrictedServletContextListenerFromExcludedFragment/WebTest.java
@@ -7,7 +7,30 @@ import com.sun.ejte.ccl.reporter.*;
  *
  *  https://glassfish.dev.java.net/issues/show_bug.cgi?id=8565
  *  ("[SPEC] Special treatment of ServletContextListeners declared
- *  in TLDs of web-fragment JAR files omitted from <absolute-ordering>")
+ *  in TLDs of web-fragment JAR files omitted from <absolute-ordering>"):
+ *
+ * This unit test creates and deploys a WAR file that bundles two web
+ * fragments with names webFragment1 and webFragment2, respectively.
+ *
+ * Either fragment bundles a Tag Library Descriptor (TLD) resource
+ * declaring a ServletContextListener.
+ *
+ * The main web.xml declares an absolute ordering of the fragments, but
+ * omits webFragment1 from it.
+ * 
+ * Either ServletContextListener attempts to register a Servlet. However,
+ * since webFragment1 was omitted from the absolute ordering, the call
+ * to ServletContext#addServlet by its ServletContextListener must result
+ * in an IllegalStateException, as required by the Servlet spec. The
+ * ServletContextListener catches this exception and stores it as a
+ * ServletContext attribute.
+ *
+ * On the other hand, the Servlet registration by the ServletContextListener
+ * in webFragment2 is expected to succeed, and the client attempts to access
+ * this Servlet. When the Servlet is accessed, it checks for the presence of
+ * the ServletContext attribute stored by the ServletContextListener of 
+ * webFragment1, and throws a ServletException if the attribute is missing,
+ * causing the test to fail.
  */
 public class WebTest {
 
