5fb8a453f4d2cd94baac7ad982c8d066f196e2de
Ana Caballero <anac@java.net>
2009-06-26 00:38:19 +0000

    Adding method to get runtime manager.
    svn path=/trunk/; revision=28207
    
    
    Former-commit-id: 8de346e173c09bf5b430a94eda67e0dc293a3141

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/util/V3AMX.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/util/V3AMX.java
@@ -24,6 +24,7 @@ import javax.management.ObjectName;
 
 import org.glassfish.admin.amx.base.DomainRoot;
 import org.glassfish.admin.amx.base.Realms;
+import org.glassfish.admin.amx.base.RuntimeMgr;
 import org.glassfish.admin.amx.config.AMXConfigProxy;
 import org.glassfish.admin.amx.core.AMXProxy;
 import org.glassfish.admin.amx.core.proxy.AMXBooter;
@@ -120,6 +121,10 @@ public class V3AMX {
 
     public Realms getRealmsMgr(){
         return domainRoot.getExt().getRealms();
+    }
+    
+    public RuntimeMgr getRuntimeMgr(){
+        return domainRoot.getExt().getRuntime();
     }
 
     public static Config getServerConfig(String configName){
