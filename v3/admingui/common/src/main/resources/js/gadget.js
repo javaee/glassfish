ece6ab82e495d2cf87cbf4ec78170727adf2c7f1
Ken Paulsen <kenpaulsen@java.net>
2009-07-02 05:55:07 +0000

    * Added a very, very preliminary dashboard container.   - Currently has 2 hard-coded "gadgets": hello and goodbye   - Needs to have a way to add/remove gadgets   - L&F is not yet implemented
    * Added gadget.js JS file which has both stadard "gadget" api's and
      proprietary GlassFish api's for gadgets (and related functionality,
      such as making Ajax requests, etc.)  The only standard api currently
      supported is the "Prefs" api.  The most significant GlassFish api is
      the ability to invoke any "handler" via JS (this essentially gives
      us full access to every GlassFish API).
    
    * Added hello.xml / goodbye.xml -- 2 example gadgets.  They don't do
      much more than their name suggets.  hello.xml has some JS which
      invokes server-side api's and gets a response back in JS and displays
      it (lots of hidden work for something so simple).
    
    * Added "gadget.jsf".  This file does bootstraps a gadget XML file and
      ensure the enviroment is available for all the JS API's needed for a
      gadget.
    
    svn path=/trunk/; revision=28433
    
    
    Former-commit-id: 348f99e60bb3ffdf78eea90658295640bff74480

---
+++ v3/admingui/common/src/main/resources/js/gadget.js
@@ -0,0 +1,146 @@
+
+if (typeof(admingui) == 'undefined') {
+    admingui = {};
+}
+admingui.gadget = {
+    noop: function() {
+    },
+
+    /**
+     *	handler - The name of the handler to invoke.
+     *	args - An object containing properties / values for the parameters.
+     *	callback - A JS function that should be notified.
+     */
+    invoke: function(handler, args, callback) {
+	if (typeof(callback) == 'undefined') {
+	    callback = admingui.gadget.noop;
+	}
+	var button = document.getElementById("setPref");
+	var params = 'h=' + handler + '&a=';
+	for (var param in args) {
+	    // Create a String to represent all the parameters
+	    params += param + ':' + escape(args[param]) + ',';
+	}
+	DynaFaces.fireAjaxTransaction(
+	    button,
+	    {
+		execute:    button.id,
+		inputs:	    button.id,
+		parameters: params,
+		render:	    'gadgetResponse',
+		onComplete: callback,
+		// Experimental... this doesn't seem to cause problems in the
+		// browser, despite the fact that I expected it to:
+		asynchronous: false
+	    });
+	return false;
+    },
+
+    getResponse: function() {
+	var resp = document.getElementById('gadgetResponse');
+	var obj = eval('(' + resp.innerHTML + ')');
+	return obj;
+    }
+};
+
+if (typeof(gadgets) == 'undefined') {
+    // FIXME: I have to solve how to make these functions appear synchronous
+    // FIXME: when they are asynchronous when backed by Ajax
+    gadgets = {
+	Prefs: function(moduleId) {
+	    if (typeof(moduleId) == 'undefined') {
+		moduleId = 'default';
+	    }
+	    // outputs (i.e. "value") are not used, but must be supplied
+	    this.prefsHandlerOpts = {root: '/glassfish/gadget/' + moduleId, value:'gadgetPrefs'}
+	    this.moduleId = (typeof(moduleId) == 'undefined') ? 'GlassFish' : moduleId;
+	    this.getArray =
+		function(key) {
+		    // invoke Ajax to get preference
+		    this.prefsHandlerOpts.key = key;
+		    admingui.gadget.invoke("getPreference", this.prefsHandlerOpts);
+		    var resp = admingui.gadget.getResponse().value;
+		    if (typeof(resp) != 'object') {
+			// Array's show up as Objects...
+			resp = [resp];
+		    }
+		    return resp;
+		};
+	    this.getBool =
+		/* true iff preference == 'true' */
+		function(key) {
+		    // invoke Ajax to get preference
+		    this.prefsHandlerOpts.key = key;
+		    admingui.gadget.invoke("getPreference", this.prefsHandlerOpts);
+		    var resp = admingui.gadget.getResponse().value;
+		    resp = (resp == 'true');
+		    return resp;
+		};
+	    this.getCountry =
+		function() {
+		    // FIXME: Not implemented!
+		    return "US";
+		};
+	    this.getFloat =
+		function(key) {
+		    // invoke Ajax to get preference
+		    this.prefsHandlerOpts.key = key;
+		    admingui.gadget.invoke("getPreference", this.prefsHandlerOpts);
+		    var resp = admingui.gadget.getResponse().value;
+		    resp = parseFloat(resp);
+		    return resp;
+		};
+	    this.getInt =
+		function(key) {
+		    // invoke Ajax to get preference
+		    this.prefsHandlerOpts.key = key;
+		    admingui.gadget.invoke("getPreference", this.prefsHandlerOpts);
+		    var resp = admingui.gadget.getResponse().value;
+		    resp = parseInt(resp);
+		    alert(typeof(resp));
+		    return resp;
+		};
+	    this.getLang =
+		function() {
+		    // FIXME: Not implemented!
+		    return "en";
+		};
+	    this.getModuleId =
+		function() {
+		    return this.moduleId;
+		};
+	    this.getMsg =
+		function(key) {
+		    alert('Prefs.getMsg() tbd...');
+		    // FIXME: TBD...
+		    // invoke Ajax to get the message
+		    return key;
+		};
+	    this.getString =
+		function(key) {
+		    // invoke Ajax to get preference
+		    this.prefsHandlerOpts.key = key;
+		    admingui.gadget.invoke("getPreference", this.prefsHandlerOpts);
+		    var resp = admingui.gadget.getResponse().value;
+		    return resp;
+		};
+	    this.set =
+		function(key, val) {
+		    // invoke Ajax to set preference
+		    this.prefsHandlerOpts.key = key;
+		    this.prefsHandlerOpts.value = val;
+		    admingui.gadget.invoke("setPreference", this.prefsHandlerOpts);
+		};
+	    this.setArray =
+		function(key, val) {
+		    // invoke Ajax to get preference
+		    if (typeof(val) != 'object') {
+			val = [val];
+		    }
+		    this.prefsHandlerOpts.key = key;
+		    this.prefsHandlerOpts.value = val;
+		    admingui.gadget.invoke("setPreference", this.prefsHandlerOpts);
+		};
+	}
+    };
+}
