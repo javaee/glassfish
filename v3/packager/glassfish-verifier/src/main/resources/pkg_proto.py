f4360e0b85e2ceb73993c1f8b30a886dd02e843c
Jane Young <janey@java.net>
2009-07-08 06:35:18 +0000

    Fix broken build for IPS packaging of glassfish-verifier.
    svn path=/trunk/; revision=28716
    
    
    Former-commit-id: 9f340f47f3fcbf873dc55e57ac962ac7b0caab0c

--- v3/packager/glassfish-verifier/src/main/resources/pkg_proto.py
+++ v3/packager/glassfish-verifier/src/main/resources/pkg_proto.py
@@ -44,7 +44,7 @@ pkg = {
     "version"       : conf.glassfish_version,
     "attributes"    : {
                         "pkg.summary" : "GlassFish Verifier Tool",
-                        "pkg.description" : "GlassFish Verifier Description Placeholder. " conf.glassfish_description_long,
+                        "pkg.description" : "GlassFish Verifier Description Placeholder. "+conf.glassfish_description_long,
                         "info.classification" :  conf.glassfish_info_classification,
                       },
     "depends"       : { 
