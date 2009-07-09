59c681caf157f85830795404460e0dfaba97bf76
Shing Wai Chan <shing.wai.chan@oracle.com>
2009-07-09 21:39:08 +0000

    add test case for issue 8535
    svn path=/trunk/; revision=28813
    
    
    Former-commit-id: d6ba52cbcda7867b13ae88fc7ee33c16142feec8

---
+++ v2/appserv-tests/devtests/web/welcomePageExtensionMatch/WebTest.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.net.*;
+import java.net.HttpURLConnection;
+import com.sun.ejte.ccl.reporter.*;
+
+/*
+ * Unit test for Issue 8535: welcome-file ignores servlet mapping
+ *
+ */
+public class WebTest {
+
+    private static final String TEST_NAME = "welcome-page-extenson-match";
+    private static final String EXPECTED = "Hello";
+
+    private static SimpleReporterAdapter stat
+        = new SimpleReporterAdapter("appserv-tests");
+
+    private String host;
+    private String port;
+    private String contextRoot;
+
+    public WebTest(String[] args) {
+        host = args[0];
+        port = args[1];
+        contextRoot = args[2];
+    }
+    
+    public static void main(String[] args) {
+        stat.addDescription("Unit test for issue 8535");
+        WebTest webTest = new WebTest(args);
+        webTest.doTest();
+	    stat.printSummary();
+    }
+
+    public void doTest() {
+     
+        try {
+
+            URL url = new URL("http://" + host  + ":" + port
+                              + contextRoot + "/");
+            System.out.println("Connecting to: " + url.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                InputStream is = null;
+                BufferedReader reader = null;
+                boolean match = false;
+                try {
+                    is = conn.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(is));
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                        if (EXPECTED.equals(line)) {
+                            match = true;
+                            break;
+                        }
+                    }
+                } finally {
+                    if (is != null) {
+                        try {
+                            is.close();
+                        } catch(IOException ex) {
+                        }
+                    }
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch(IOException ex) {
+                        }
+                    }
+                }
+                stat.addStatus(TEST_NAME, ((match)? stat.PASS : stat.FAIL));
+            } else {
+                System.err.println("Incorrect response code: Expected: "
+                                   + HttpURLConnection.HTTP_OK
+                                   + ", received: " + responseCode);
+                stat.addStatus(TEST_NAME, stat.FAIL);
+            }
+
+        } catch( Exception ex) {
+            ex.printStackTrace();
+            stat.addStatus(TEST_NAME, stat.FAIL);
+        }
+    }
+
+}
---
+++ v2/appserv-tests/devtests/web/welcomePageExtensionMatch/servlet/TestServlet.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class TestServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+        res.getWriter().write("Hello");
+    }
+}
+
+
