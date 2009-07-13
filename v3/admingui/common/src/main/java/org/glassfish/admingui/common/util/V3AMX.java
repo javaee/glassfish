5dbac669ea607ac20b84b9a3141360810408dea1
Anissa Lam <anissa.lam@oracle.com>
2009-07-13 18:52:32 +0000

    AMXBooter.bootAMX() guarantees that AMX is loaded and ready to use;  no need to  call waitAMXReady().
    svn path=/trunk/; revision=29041
    
    
    Former-commit-id: 96027a63ca00b3339c0574ff3f89afc0e2c148e9

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/util/V3AMX.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/util/V3AMX.java
@@ -88,7 +88,6 @@ public class V3AMX {
             }
             AMXBooter.bootAMX(mbs);
             DomainRoot domainRoot = ProxyFactory.getInstance(mbs).getDomainRootProxy();
-            domainRoot.waitAMXReady();
             v3amx = new V3AMX( domainRoot, mbs);
 	}
         return v3amx;
