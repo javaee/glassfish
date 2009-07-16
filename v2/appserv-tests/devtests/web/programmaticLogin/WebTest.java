2c12adb8f7a30e4d9050c72b64780f593fc1b677
Jan Luehe <jluehe@java.net>
2009-07-16 17:30:56 +0000

    Simplified test and fixed its results reporting
    svn path=/trunk/; revision=29294
    
    
    Former-commit-id: 887c5bcc22551cdf8280db2b5718635a7225e754

--- v2/appserv-tests/devtests/web/programmaticLogin/WebTest.java
+++ v2/appserv-tests/devtests/web/programmaticLogin/WebTest.java
@@ -1,120 +1,64 @@
-
-import java.lang.*;
 import java.io.*;
 import java.net.*;
 
 import com.sun.ejte.ccl.reporter.*;
+
 /**
- * BugId 4862098: ProgrammaticLogin fails for webcontainer - regression on AS8
+ * Unit test for 4862098 ("ProgrammaticLogin fails for webcontainer -
+ * regression on AS8")
  */
-public class WebTest
-{
+public class WebTest {
     
-    private static int count = 0;
-    private static int EXPECTED_COUNT = 1;
-    
-    static SimpleReporterAdapter stat=
+    private static final String TEST_NAME = "programmatic-login";
+
+    private static final SimpleReporterAdapter stat =
         new SimpleReporterAdapter("appserv-tests");
 
-    public static void main(String args[])
-    {
+    private String host;
+    private String port;
+    private String contextRoot;
+    private String user;
+    private String password;
 
-        // The stat reporter writes out the test info and results
-        // into the top-level quicklook directory during a run.
-      
-        stat.addDescription("Standalone Servlet/Filter war test");
-
-        String host = args[0];
-        String portS = args[1];
-        String contextRoot = args[2];
-        String user = args[3];
-        String password = args[4];
-
-        int port = new Integer(portS).intValue();
-        String name;
-        
-        try {
-            goGet(host, port, "WEB-Programmatic-Login", contextRoot + "/ServletTest?user=testuser3&password=secret" );
-            
-            if (count != EXPECTED_COUNT){
-                stat.addStatus("Test UNPREDICTED-FAILURE", stat.FAIL);
-            }           
-        } catch (Throwable t) {
-            System.out.println(t.getMessage());
-            stat.addStatus("Test UNPREDICTED-FAILURE", stat.FAIL);
-        }
-
-        stat.printSummary("web/standalonewar---> expect 3 PASS");
+    public WebTest(String[] args) {
+        host = args[0];
+        port = args[1];
+        contextRoot = args[2];
+        user = args[3];
+        password = args[4];
     }
 
-    private static void goGet(String host, int port,
-                              String result, String contextPath)
-         throws Exception
-    {
-        Socket s = null;
-        OutputStream os = null;
-        InputStream is = null;
-        BufferedReader bis = null;
-        try{
-            s = new Socket(host, port);
-            os = s.getOutputStream();
+    public static void main(String[] args) {
 
-            System.out.println(("GET " + contextPath + " HTTP/1.0\n"));
-            os.write(("GET " + contextPath + " HTTP/1.0\n").getBytes());
-            os.write("\n".getBytes());
+        stat.addDescription("Unit test for 4862098");
         
-            is = s.getInputStream();
-            bis = new BufferedReader(new InputStreamReader(is));
-            String line = null;
+        WebTest webTest = new WebTest(args);
+        
+        try {
+            webTest.invoke();            
+            stat.addStatus(TEST_NAME, stat.PASS);
+        } catch (Throwable t) {
+            stat.addStatus(TEST_NAME, stat.FAIL);
+            t.printStackTrace();
+        }
 
-            int index;
-            while ((line = bis.readLine()) != null) {
-                index = line.indexOf(result);
-                System.out.println(line);
-                if (index != -1) {
-                    index = line.indexOf(":");
-                    String status = line.substring(index+1);
-                    
-                    if (status.equalsIgnoreCase("PASS")){
-                        stat.addStatus("web-programmatic-login: " + line.substring(0,index), stat.PASS);
-                    } else {
-                        stat.addStatus("web-programmatic-login: " + line.substring(0,index), stat.FAIL);                       
-                    }
-                    count++;
-                } 
-            }
-        } catch( Exception ex){
-            ex.printStackTrace();   
-            throw new Exception("Test UNPREDICTED-FAILURE");
-        } finally {
-            try {
-                if (s != null) {
-                    s.close();
-                }
-            } catch(IOException ioe) {
-                // ignore
-            }
-            try {
-                if (os != null) {
-                    os.close();
-                }
-            } catch(IOException ioe) {
-                // ignore
-            }
-            try {
-                if (is != null) {
-                    is.close();
-                }
-            } catch(IOException ioe) {
-                // ignore
-            }
-            try {
-                if (bis != null) {
-                    bis.close();
-                }
-            } catch(IOException ioe) {
-                // ignore
-            }
+        stat.printSummary(TEST_NAME);
+    }
+
+    private void invoke() throws Exception {
+
+        String uri = contextRoot +
+            "/ServletTest?user=testuser3&password=secret";
+        URL url = new URL("http://" + host  + ":" + port + uri);
+        System.out.println("Connecting to: " + url.toString());
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.connect();
+
+        int responseCode = conn.getResponseCode();
+        if (responseCode != 200) {
+            throw new Exception("Wrong response code. Expected: 200" +
+                ", received: " + responseCode);
         }
    }
   
--- v2/appserv-tests/devtests/web/programmaticLogin/servlet/ServletTest.java
+++ v2/appserv-tests/devtests/web/programmaticLogin/servlet/ServletTest.java
@@ -2,50 +2,22 @@ package test;
 
 import java.io.*;
 import java.net.*;
-import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import javax.naming.*;
-import javax.sql.*;
-import java.sql.*;
 
 import com.sun.appserv.security.ProgrammaticLogin;
 
 public class ServletTest extends HttpServlet {
 
-    private ServletContext context;
-    
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
-    }
-
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {        
-        System.out.println("[Servlet.Programmatin Login Test]");
-
-		String user = request.getParameter("user");
-		String password = request.getParameter("password"); 
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        String user = request.getParameter("user");
+        String password = request.getParameter("password"); 
         System.out.println("[user] " + user + " [password] " + password);
-
         ProgrammaticLogin login = new ProgrammaticLogin();
-        boolean value = ((Boolean)login.login(user, password, request, response)).booleanValue();
-
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
-		if (value){
-			out.println("WEB-Programmatic-Login:" + "PASS");
-		} else {
-			out.println("WEB-Programmatic-Login:" + "FAIL");
-		}
-
+        if (!login.login(user, password, request, response).booleanValue()) {
+            throw new ServletException("Login failed");
+        }
     }
 }
