3c0f635834f2621b8b7b819eed3daae01a35256a
Tim Quinn <tim.quinn@oracle.com>
2009-07-17 21:48:35 +0000

    Missed file in previous check-in.  Apologies.
    Tests: QL, ejb devtests, deployment devtests
    
    svn path=/trunk/; revision=29380
    
    
    Former-commit-id: ead58ad8707fb220ae4978e0dd0d61531dd9f174

---
+++ v3/appclient/server/core/src/main/java/org/glassfish/appclient/server/core/DeveloperContentHandler.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ * 
+ * Contributor(s):
+ * 
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.glassfish.appclient.server.core;
+
+import com.sun.enterprise.deployment.runtime.JnlpDocDescriptor;
+import java.util.Map;
+import org.glassfish.appclient.server.core.jws.servedcontent.DynamicContent;
+import org.glassfish.appclient.server.core.jws.servedcontent.StaticContent;
+
+/**
+ * Processes developer-provided content in (or directly or indirectly
+ * referenced from) an optional JNLP file included in the client or the EAR.
+ *
+ * @author tjquinn
+ */
+public class DeveloperContentHandler {
+
+    private final ClassLoader loader;
+    private final JnlpDocDescriptor jnlpDoc;
+    public static DeveloperContentHandler addDeveloperContent(
+            final ClassLoader loader,
+            final JnlpDocDescriptor jnlpDoc,
+            final Map<String,StaticContent> staticContent,
+            final Map<String,DynamicContent> dynamicContent) {
+
+        DeveloperContentHandler h = new DeveloperContentHandler(loader, jnlpDoc,
+                staticContent, dynamicContent);
+        h.process();
+        return h;
+    }
+
+    private DeveloperContentHandler(ClassLoader loader, JnlpDocDescriptor jnlpDoc,
+            final Map<String,StaticContent> staticContent,
+            final Map<String,DynamicContent> dynamicContent) {
+        this.loader = loader;
+        this.jnlpDoc = jnlpDoc;
+    }
+
+    private void process() {
+        
+    }
+
+}
