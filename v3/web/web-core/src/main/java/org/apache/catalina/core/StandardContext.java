2112ebcc101269cc562c33c559075de1d2a37013
Jan Luehe <jluehe@java.net>
2009-07-17 23:40:11 +0000

    Misc changes
    svn path=/trunk/; revision=29393
    
    
    Former-commit-id: 89b18dea90044cfad271f52dbb2c9bc023e6c159

--- v3/web/web-core/src/main/java/org/apache/catalina/core/StandardContext.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/core/StandardContext.java
@@ -157,17 +157,6 @@ public class StandardContext
         mySecurityManager = new MySecurityManager();
     }
 
-    /**
-     * set a new pipeline (restricted to use by EE code)
-     * HERCULES:add
-     */
-    public void restrictedSetPipeline(Pipeline pl) {
-        pl.setBasic(new StandardContextValve());
-        pipeline = pl;
-        hasCustomPipeline = true;
-    }
-
-
     // ----------------------------------------------------- Instance Variables
 
     /**
@@ -222,8 +211,7 @@ public class StandardContext
     /**
      * The Java class name of the CharsetMapper class to be created.
      */
-    private String charsetMapperClass =
-      "org.apache.catalina.util.CharsetMapper";
+    private String charsetMapperClass = CharsetMapper.class.getName();
 
     /**
      * The path to a file to save this Context information.
@@ -1384,6 +1372,15 @@ public class StandardContext
      */
     public Mapper getMapper() {
         return mapper;
+    }
+
+    /**
+     * Sets a new pipeline
+     */
+    public void restrictedSetPipeline(Pipeline pl) {
+        pl.setBasic(new StandardContextValve());
+        pipeline = pl;
+        hasCustomPipeline = true;
     }
 
     /**
@@ -6878,6 +6875,9 @@ public class StandardContext
              *  org.apache.catalina.core.ApplicationContextFacade
              * classContext[5]:
              *  Caller whose classloader to check
+             *
+             * NOTE: INDEX MUST BE ADJUSTED WHENEVER EXECUTION STACK
+             * CHANGES, E.G., DUE TO CODE BEING REORGANIZED
              */
             ClassLoader ccl = classContext[5].getClassLoader();
             if (ccl != null && ccl != webappLoader && 
