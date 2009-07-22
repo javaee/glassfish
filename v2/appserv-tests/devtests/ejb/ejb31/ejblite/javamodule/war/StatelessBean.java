93ce9c773e20b1fd36ca7d239da17b3f1601de84
Ken Saks <ksak@java.net>
2009-07-22 15:53:21 +0000

    added test code for ejb-jar.xml module-name and ejb-in-war naming
    svn path=/trunk/; revision=29558
    
    
    Former-commit-id: 50c9e597c4104248a515abed521c3ba43534d08b

--- v2/appserv-tests/devtests/ejb/ejb31/ejblite/javamodule/war/StatelessBean.java
+++ v2/appserv-tests/devtests/ejb/ejb31/ejblite/javamodule/war/StatelessBean.java
@@ -20,6 +20,9 @@ public class StatelessBean {
     @Resource(mappedName="java:app/foomanagedbean")
     private FooManagedBean foo2;
 
+    @EJB(name="stateless/singletonref")
+    private SingletonBean singleton;
+
     @PostConstruct
     private void init() {
 	System.out.println("In StatelessBean:init()");
@@ -31,6 +34,25 @@ public class StatelessBean {
 	FooManagedBean fmb = (FooManagedBean) 
 	    sessionCtx.lookup("java:module/foomanagedbean");
 
+	// Make sure dependencies declared in java:comp are visible
+	// via equivalent java:module entries since this is a 
+	// .war
+	SessionContext sessionCtx2 = (SessionContext)
+	    sessionCtx.lookup("java:module/env/com.acme.StatelessBean/sessionCtx");
+
+	SingletonBean singleton2 = (SingletonBean)
+	    sessionCtx2.lookup("java:module/env/stateless/singletonref");
+
+	// Lookup a comp env dependency declared by another ejb in the .war
+	SingletonBean singleton3 = (SingletonBean)
+	    sessionCtx2.lookup("java:comp/env/com.acme.SingletonBean/me");
+
+	// Lookup a comp env dependency declared by a servlet
+	FooManagedBean fmbServlet = (FooManagedBean)
+	    sessionCtx.lookup("java:comp/env/foo2ref");
+	FooManagedBean fmbServlet2 = (FooManagedBean)
+	    sessionCtx.lookup("java:module/env/foo2ref");
+
 	// Ensure that each injected or looked up managed bean 
 	// instance is unique
 	Object fooThis = foo.getThis();
@@ -40,6 +62,8 @@ public class StatelessBean {
 	System.out.println("fooThis = " + fooThis);
 	System.out.println("foo2This = " + foo2This);
 	System.out.println("fmbThis = " + fmbThis);
+	System.out.println("fmbServlet = " + fmbServlet);
+	System.out.println("fmbServlet2 = " + fmbServlet2);
 
 	if( ( fooThis == foo2This ) || ( fooThis == fmbThis  ) ||
 	    ( foo2This == fmbThis ) ) {
