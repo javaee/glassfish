a22b457638c41a178dc5a55f4813550886678234
Jagadish Ramu <jagadish.ramu@oracle.com>
2009-07-27 07:45:33 +0000

    Fixing and enabling MQ-CMT test case, updating total results count
    svn path=/trunk/; revision=29774
    
    
    Former-commit-id: c731cf7224e1b6e1b57e257a10122a5abce09d5d

--- v2/appserv-tests/devtests/connector/mq/cmt/ejb/SimpleMessageBean.java
+++ v2/appserv-tests/devtests/connector/mq/cmt/ejb/SimpleMessageBean.java
@@ -16,6 +16,7 @@ public class SimpleMessageBean implements MessageDrivenBean,
     private transient MessageDrivenContext mdc = null;
     private Context context;
     private static int i;
+    private final static Integer lock = new Integer("1");
 
     public SimpleMessageBean() {
         System.out.println("In SimpleMessageBean.SimpleMessageBean()");
@@ -34,12 +35,14 @@ public class SimpleMessageBean implements MessageDrivenBean,
     public void onMessage(Message inMessage) {
         TextMessage msg = null;
 
+        System.out.println("MQ-CMT about to updateDB");
 	updateDB();
+        System.out.println("MQ-CMT after updateDB");
 
         try {
             if (inMessage instanceof TextMessage) {
                 msg = (TextMessage) inMessage;
-                System.out.println("MESSAGE BEAN: Message received: "
+                System.out.println("MQ-CMT MESSAGE BEAN: Message received: "
                     + msg.getText());
             } else {
                 System.out.println("Message of wrong type: "
@@ -54,23 +57,29 @@ public class SimpleMessageBean implements MessageDrivenBean,
     }  // onMessage
 
     private void updateDB() {
+        synchronized(lock){
         try {
-	    Class.forName("com.inet.ora.OraDriver");
-            String url = "jdbc:inetora::wrx.india.sun.com:1521:dbsmpl1";
-	    java.sql.Connection con = DriverManager.getConnection(url,"connector", "connector");
+	    //Class.forName("com.inet.ora.OraDriver");
+	    Class.forName("org.apache.derby.jdbc.ClientDriver");
+            //String url = "jdbc:inetora::wrx.india.sun.com:1521:dbsmpl1";
+            String url = "jdbc:derby://localhost:1527/testdb;create=true;";
+	    java.sql.Connection con = DriverManager.getConnection(url,"dbuser", "dbpassword");
             ResultSet rs = con.createStatement().executeQuery("select exCount from mq_cmt_excpt");
 	    int count = 0;
 	    while (rs.next()){
 	        count = rs.getInt(1);
+                System.out.println("MQ-CMT updateDB : " + count);
 	    }
 	    rs.close();
 	    count++;
 	    String qry = "update mq_cmt_excpt set exCount="+ count ;
+            System.out.println("MQ-CMT updateDB : query : " + qry);
 	    con.createStatement().executeUpdate(qry);
 	    con.close();
 	} catch(Exception e) {
            System.out.println("Error:" + e.getMessage());
 	} 
+        }
     }
 
     public void ejbRemove() {
