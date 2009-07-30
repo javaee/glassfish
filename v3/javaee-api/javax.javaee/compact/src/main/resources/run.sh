37e98f17bd3f6cbcc665d7af229f530cfdcfd96d
Jane Young <janey@java.net>
2009-07-30 11:17:49 +0000

    add webservices javax api add assembly file to create web profile javaee-api artifact
    svn path=/trunk/; revision=29939
    
    
    Former-commit-id: b6848f6e1a8c953c46648fa2d72f944b5e56be90

--- v3/javaee-api/javax.javaee/compact/src/main/resources/run.sh
+++ v3/javaee-api/javax.javaee/compact/src/main/resources/run.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 rm javax.javaee-api-compact.jar
-rm -rf tmp
-mkdir tmp
 $1/bin/javac -XDprocess.packages -proc:only \
     -cp $2:. \
     -processor DIY \
     -Acom.sun.tools.javac.sym.Jar=$2 \
     -Acom.sun.tools.javac.sym.Dest=$3 \
     `jar tf $2 | grep '^javax/..*/$' | sed -e 's;/$;;' -e 's;/;.;g'`
-#cd tmp
-#jar cf ../javax.javaee-api-compact.jar javax
-#cd ..
+
