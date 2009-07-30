785cd93cf50418836e9c9f8db9322b72dddb3349
Ken Saks <ksak@java.net>
2009-07-30 17:42:11 +0000

    revert to original client code
    svn path=/trunk/; revision=29950
    
    
    Former-commit-id: 8683402ad33064b41f4fbcfb3eb34b03e9b97a24

--- v2/appserv-tests/devtests/ejb/ejb31/security/simple/client/Client.java
+++ v2/appserv-tests/devtests/ejb/ejb31/security/simple/client/Client.java
@@ -18,7 +18,7 @@ public class Client {
     private static String appName;
     private static String principal;
 
-    @EJB(mappedName="java:app/ejb-ejb31-security-simple-ejb/SingletonBean!com.acme.Hello") static Hello hello;
+    @EJB static Hello hello;
 
     public static void main(String args[]) {
 
@@ -50,11 +50,7 @@ public class Client {
 	    
 	    if( hello == null ) {
 		hello = (Hello) new InitialContext().lookup("com.acme.Hello");
-	    } else {
-		System.out.println("in appclient");
-		hello = (Hello) new InitialContext().lookup("java:app/ejb-ejb31-security-simple-ejb/SingletonBean!com.acme.Hello");
-	    }
-
+	    } 
 
 	    boolean pass;
 
