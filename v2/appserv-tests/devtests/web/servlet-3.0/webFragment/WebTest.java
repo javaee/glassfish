a5a9a5a14739d816556c1ae791bddb95448d8026
Jan Luehe <jluehe@java.net>
2009-08-03 20:16:02 +0000

    Test cleanup
    svn path=/trunk/; revision=30055
    
    
    Former-commit-id: 4e0567746ddd22c854ef2941d35c8c9ff41a6df6

--- v2/appserv-tests/devtests/web/servlet-3.0/webFragment/WebTest.java
+++ v2/appserv-tests/devtests/web/servlet-3.0/webFragment/WebTest.java
@@ -33,59 +33,54 @@ public class WebTest {
     public void doTest() {
         try { 
             invoke("http://" + host + ":" + port + contextRoot + "/mytest",
-                    TEST_NAME + "_urlPatternfromWeb", EXPECTED_RESPONSE);
+                   EXPECTED_RESPONSE);
             invoke("http://" + host + ":" + port + contextRoot + "/mytest2",
-                    TEST_NAME + "_envEntryFromWebFragment", EXPECTED_RESPONSE_2);
+                   EXPECTED_RESPONSE_2);
             invoke("http://" + host + ":" + port + contextRoot + "/wftest",
-                    TEST_NAME + "_urlPatternfomWebFragment", EXPECTED_RESPONSE);
+                   EXPECTED_RESPONSE);
+            stat.addStatus(TEST_NAME, stat.PASS);
         } catch (Exception ex) {
-            System.out.println(TEST_NAME + " test failed");
             stat.addStatus(TEST_NAME, stat.FAIL);
             ex.printStackTrace();
         }
     }
 
-    private void invoke(String url, String testName,
-            String expectedResponse) throws Exception {
+    private void invoke(String url, String expectedResponse) throws Exception {
         
         HttpURLConnection conn = (HttpURLConnection)
             (new URL(url)).openConnection();
 
         int code = conn.getResponseCode();
         if (code != 200) {
-            System.out.println("Unexpected return code: " + code);
-            stat.addStatus(testName, stat.FAIL);
-        } else {
-            InputStream is = null;
-            BufferedReader input = null;
-            String line = null;
+            throw new Exception("Unexpected return code: " + code);
+        }
+
+        InputStream is = null;
+        BufferedReader input = null;
+        String line = null;
+        try {
+            is = conn.getInputStream();
+            input = new BufferedReader(new InputStreamReader(is));
+            line = input.readLine();
+        } finally {
             try {
-                is = conn.getInputStream();
-                input = new BufferedReader(new InputStreamReader(is));
-                line = input.readLine();
-            } finally {
-                try {
-                    if (is != null) {
-                        is.close();
-                    }
-                } catch(IOException ioe) {
-                    // ignore
+                if (is != null) {
+                    is.close();
                 }
-                try {
-                    if (input != null) {
-                        input.close();
-                    }
-                } catch(IOException ioe) {
-                    // ignore
+            } catch(IOException ioe) {
+                // ignore
+            }
+            try {
+                if (input != null) {
+                    input.close();
                 }
+            } catch(IOException ioe) {
+                // ignore
             }
-            if (expectedResponse.equals(line)) {
-                stat.addStatus(testName, stat.PASS);
-            } else {
-                System.out.println("Wrong response. Expected: " + 
-                        EXPECTED_RESPONSE + ", received: " + line);
-                stat.addStatus(testName, stat.FAIL);
-            }
-        }    
+        }
+        if (!expectedResponse.equals(line)) {
+            throw new Exception("Wrong response. Expected: " + 
+                expectedResponse + ", received: " + line);
+        }
     }
 }
