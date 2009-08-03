46a62d9f3d8ce45bd0f30a247ec10ca05602506e
Jan Luehe <jluehe@java.net>
2009-08-03 22:31:16 +0000

    Removed unused imports
    svn path=/trunk/; revision=30065
    
    
    Former-commit-id: 8daf737161e87916556916377dd9c8dbe37d52a6

--- v2/appserv-tests/devtests/web/jrouteIdInCookieOrURL/servlet/TestServlet.java
+++ v2/appserv-tests/devtests/web/jrouteIdInCookieOrURL/servlet/TestServlet.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import com.sun.enterprise.web.monitor.*;
-import com.sun.enterprise.web.monitor.impl.*;
 import org.apache.catalina.connector.RequestFacade;
 
 public class TestServlet extends HttpServlet {
