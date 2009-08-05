bf8a80ff37df9bd13542443dbe31cb4500cd36fd
Jan Luehe <jluehe@java.net>
2009-08-05 20:40:48 +0000

    Prevent "java.lang.ClassCastException: com.sun.enterprise.web.pwc.connector.coyote.PwcCoyoteRequest cannot be cast to org.apache.catalina.core.ApplicationHttpRequest" during async dispatches
    Tests run: QL
    
    svn path=/trunk/; revision=30178
    
    
    Former-commit-id: 3b3e1ed9f859f069f4aca397b56fa4c723e4f240

--- v3/web/web-core/src/main/java/org/apache/catalina/connector/Request.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/connector/Request.java
@@ -3991,7 +3991,7 @@ public class Request
      */
     @Override
     public AsyncContext startAsync() throws IllegalStateException {
-        return startAsync(this, (Response) getResponse(), true);
+        return startAsync(getRequest(), getResponse().getResponse(), true);
     }
 
 
