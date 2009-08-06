5999fee676b0e478f0e8b49bc12d9fbcb1f98422
Jan Luehe <jluehe@java.net>
2009-08-06 03:31:18 +0000

    AsyncContext#hasOriginalRequestAndResponse clarifications
    svn path=/trunk/; revision=30195
    
    
    Former-commit-id: ff755c2b7445e2fecac0295cd6353c64bdc2a874

--- v3/web/web-core/src/main/java/org/apache/catalina/connector/Request.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/connector/Request.java
@@ -113,6 +113,8 @@ import org.apache.catalina.Session;
 import org.apache.catalina.Wrapper;
 
 import org.apache.catalina.authenticator.SingleSignOn;
+import org.apache.catalina.core.ApplicationHttpRequest;
+import org.apache.catalina.core.ApplicationHttpResponse;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.session.StandardSession;
@@ -3708,7 +3710,17 @@ public class Request
     public AsyncContext startAsync(ServletRequest servletRequest,
                                    ServletResponse servletResponse)
             throws IllegalStateException {
-        return startAsync(servletRequest, servletResponse, false);
+        // If original or container-wrapped request and response,
+        // AsyncContext#hasOriginalRequestAndResponse must return true;
+        // false otherwise (i.e., if application-wrapped)
+        if ((servletRequest instanceof RequestFacade ||
+                    servletRequest instanceof ApplicationHttpRequest) &&
+                (servletResponse instanceof ResponseFacade ||
+                    servletResponse instanceof ApplicationHttpResponse)) {
+            return startAsync(servletRequest, servletResponse, true);
+        } else {
+            return startAsync(servletRequest, servletResponse, false);
+        }
     }
 
     private Multipart getMultipart() {
