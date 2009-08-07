91610e07ff6be4b1116bf5a256a368b63c60c29d
Shalini Muthukrishnan <sm157516@java.net>
2009-08-07 12:19:13 +0000

    Adding lazy assoc test to the targets and updating result count
    svn path=/trunk/; revision=30245
    
    
    Former-commit-id: be53d92ae88db26621b919f391a88ed32664db04

--- v2/appserv-tests/devtests/jdbc/resultCount.sh
+++ v2/appserv-tests/devtests/jdbc/resultCount.sh
@@ -3,7 +3,7 @@
 FILE=$APS_HOME/test_resultsValid.xml
 echo "input file=$FILE"
 
-TOTAL=85
+TOTAL=87
 PASSED=`grep "pass" $FILE | wc -l`
 FAILED=`grep "fail" $FILE | wc -l`
 TOTAL_RUN=`expr $PASSED + $FAILED `
