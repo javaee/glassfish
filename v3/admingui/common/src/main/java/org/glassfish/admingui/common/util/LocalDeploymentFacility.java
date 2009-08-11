32b3fdff5ea8bf8a1285790ea0c5c7c654e2e8cd
Bill Shannon <bill.shannon@oracle.com>
2009-08-11 16:54:13 +0000

    A few more stragglers, to fix the build.
    svn path=/trunk/; revision=30315
    
    
    Former-commit-id: 13a00445a9646405e91f8ecafde3418097e366ae

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/util/LocalDeploymentFacility.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/util/LocalDeploymentFacility.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -42,7 +42,7 @@ import java.util.Properties;
 import org.glassfish.api.ActionReport;
 import org.glassfish.api.admin.CommandRunner;
 import org.glassfish.deployment.client.*;
-import com.sun.enterprise.cli.framework.CommandException;
+import com.sun.enterprise.admin.cli.CommandException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Map;
