3b04958288409c9740223213dd0f915db92dfd31
Bhakti Mehta <bhaktimehta@java.net>
2009-08-19 22:32:20 +0000

    Added test for issue 9168
    svn path=/trunk/; revision=30594
    
    
    Former-commit-id: fc0430ab05443dec22b78ec3c874ea6b99eb1763

--- v2/appserv-tests/devtests/webservice/annotations/wsctxt/endpoint/Hello.java
+++ v2/appserv-tests/devtests/webservice/annotations/wsctxt/endpoint/Hello.java
@@ -3,6 +3,10 @@ package endpoint;
 import javax.jws.WebService;
 import javax.annotation.Resource;
 import javax.xml.ws.WebServiceContext;
+import javax.servlet.ServletContext;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+
 
 @WebService
 public class Hello {
@@ -11,8 +15,15 @@ public class Hello {
 
     public String sayHello(String param) {
 	System.out.println("wsctxt-servlet wsc = " + wsc);
-	if(wsc != null)
-		return "WebSvcTest-Hello " + param;
+	if(wsc != null) {
+                 ServletContext sc =
+(ServletContext)wsc.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+                String a = null;
+                if (sc!= null ) {
+                 a = sc.getServletContextName();
+                }
+		return "Hello " + param +a;
+        }
 	return "WebService Context injection failed";
     }
 }
