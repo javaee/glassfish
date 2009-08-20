5779edc09da6afc5126244dc3f707ae6f75a0d97
Sreenivas Munnangi <msreddy@java.net>
2009-08-20 03:24:38 +0000

    monitoring config upgrade
    svn path=/trunk/; revision=30606
    
    
    Former-commit-id: de29480872337c781c73f0f01e034bb36802af57

--- v3/common/glassfish-api/src/main/java/org/glassfish/api/monitoring/MonitoringItem.java
+++ v3/common/glassfish-api/src/main/java/org/glassfish/api/monitoring/MonitoringItem.java
@@ -21,6 +21,18 @@ public interface MonitoringItem extends ConfigBeanProxy {
     public static final String LEVEL_LOW = "LOW";
     public static final String LEVEL_HIGH = "HIGH";
 
+    public static final String CONNECTOR_CONNECTION_POOL = "connector-connection-pool";
+    public static final String CONNECTOR_SERVICE = "connector-service";
+    public static final String EJB_CONTAINER = "ejb-container";
+    public static final String HTTP_SERVICE = "http-service";
+    public static final String JDBC_CONNECTION_POOL = "jdbc-connection-pool";
+    public static final String JMS_SERVICE = "jms-service";
+    public static final String JVM = "jvm";
+    public static final String ORB = "orb";
+    public static final String THREAD_POOL = "thread-pool";
+    public static final String TRANSACTION_SERVICE = "transaction-service";
+    public static final String WEB_CONTAINER = "web-container";
+
     /**
      * The monitoring level of this monitoring item 
      * @return String with values HIGH/LOW/OFF
