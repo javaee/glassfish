d3bd2ed107fc4031880114c367b6f62994ca342b
Lloyd Chambers <llc@java.net>
2009-08-21 18:34:00 +0000

    first pass at nav
    svn path=/trunk/; revision=30684
    
    
    Former-commit-id: ffc7057c2b81bc238599a655516cda4411a01ae4

--- v3/extras/jmxcmd/src/main/java/com/sun/cli/jmxcmd/cmd/NavCmd.java
+++ v3/extras/jmxcmd/src/main/java/com/sun/cli/jmxcmd/cmd/NavCmd.java
@@ -6,13 +6,7 @@ package com.sun.cli.jmxcmd.cmd;
 
 import javax.management.ObjectName;
 
-import org.glassfish.admin.amx.util.stringifier.ArrayStringifier;
 
-import com.sun.cli.jcmd.util.cmd.OptionsInfo;
-import com.sun.cli.jcmd.util.cmd.OptionsInfoImpl;
-import com.sun.cli.jcmd.util.cmd.OptionInfo;
-import com.sun.cli.jcmd.util.cmd.OptionInfoImpl;
-import com.sun.cli.jcmd.util.cmd.OptionDependency;
 
 import com.sun.cli.jcmd.framework.CmdEnv;
 import com.sun.cli.jcmd.framework.IllegalUsageException;
@@ -25,147 +19,245 @@ import com.sun.cli.jcmd.util.cmd.CmdInfos;
 import com.sun.cli.jcmd.util.cmd.CmdInfo;
 import com.sun.cli.jcmd.util.cmd.CmdInfoImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.glassfish.admin.amx.core.AMXProxy;
+import org.glassfish.admin.amx.core.proxy.ProxyFactory;
+import org.glassfish.admin.amx.util.CollectionUtil;
 import static com.sun.cli.jmxcmd.cmd.NavInfo.*;
 
 /**
-	Manages the default targets.
+Manages the default targets.
  */
 public class NavCmd extends JMXCmd
 {
-		public
-	NavCmd( final CmdEnv env )
-	{
-		super( env );
-	}
-	
-
-	static final class TargetCmdHelp extends CmdHelpImpl
-	{
-		public	TargetCmdHelp()	{ super( getCmdInfos() ); }
-		
-		private final static String	SYNOPSIS		= "file system analogy for MBeans";
-		private final static String	TARGET_TEXT		= "file system analogy for MBeans.";
-
-		
-		public String	getSynopsis()	{	return( formSynopsis( SYNOPSIS ) ); }
-		public String	getText()		{	return( TARGET_TEXT ); }
-	}
-
-		public CmdHelp
-	getHelp()
-	{
-		return( new TargetCmdHelp() );
-	}
-	
-	private final static String	CD_NAME		= "cd";
-	private final static String	PWD_NAME	= "pwd";
-	private final static String PUSHD_NAME	= "pushd";
-	private final static String POPD_NAME	= "popd";
-	private final static String LS_NAME	    = "ls";
-		
-	protected static final OperandsInfo	CD_OPERAND_INFO	= new OperandsInfoImpl( "<path>", 0 );
-		
-	private final static CmdInfo	CD_INFO	    = new CmdInfoImpl( CD_NAME, CD_OPERAND_INFO );
-	private final static CmdInfo	LS_INFO     = new CmdInfoImpl( LS_NAME );
-	private final static CmdInfo	PWD_INFO    = new CmdInfoImpl( PWD_NAME );
-	private final static CmdInfo	PUSHD_INFO  = new CmdInfoImpl( PUSHD_NAME );
-	private final static CmdInfo	POPD_INFO   = new CmdInfoImpl( POPD_NAME );
-		
-	
-		public static CmdInfos
-	getCmdInfos( )
-	{
-		return( new CmdInfos( CD_INFO, LS_INFO, PWD_INFO, PUSHD_INFO, POPD_INFO) );
-	}
-	    
-    public static final String NAV_INFO_KEY = "NavCmd.NavInfo";
-	
-    private void cd(final String dest )
+    public NavCmd(final CmdEnv env)
     {
-        final NavInfo info = getNavInfo();
-        info.setCurrentDir(dest);
+        super(env);
+    }
+
+    static final class NavCmdHelp extends CmdHelpImpl
+    {
+        public NavCmdHelp()
+        {
+            super(getCmdInfos());
+        }
+        private final static String SYNOPSIS = "file system analogy for MBeans";
+        private final static String TARGET_TEXT = "file system analogy for MBeans.";
+
+
+        public String getSynopsis()
+        {
+            return (formSynopsis(SYNOPSIS));
+        }
+
+
+        public String getText()
+        {
+            return (TARGET_TEXT);
+        }
+    }
+
+
+    public CmdHelp getHelp()
+    {
+        return (new NavCmdHelp());
+    }
+    private final static String CD_NAME = "cd";
+    private final static String PWD_NAME = "pwd";
+    private final static String CAT_NAME = "cat";
+    private final static String PUSHD_NAME = "pushd";
+    private final static String POPD_NAME = "popd";
+    private final static String LS_NAME = "ls";
+    protected static final OperandsInfo CD_OPERAND_INFO = new OperandsInfoImpl("<path>", 0);
+    private final static CmdInfo CD_INFO = new CmdInfoImpl(CD_NAME, CD_OPERAND_INFO);
+    private final static CmdInfo CAT_INFO = new CmdInfoImpl(CAT_NAME);
+    private final static CmdInfo LS_INFO = new CmdInfoImpl(LS_NAME);
+    private final static CmdInfo PWD_INFO = new CmdInfoImpl(PWD_NAME);
+    private final static CmdInfo PUSHD_INFO = new CmdInfoImpl(PUSHD_NAME);
+    private final static CmdInfo POPD_INFO = new CmdInfoImpl(POPD_NAME);
+
+
+    public static CmdInfos getCmdInfos()
+    {
+        return new CmdInfos(new CmdInfo[]
+            {
+                CD_INFO, LS_INFO, PWD_INFO, PUSHD_INFO, POPD_INFO, CAT_INFO
+            });
+    }
+    public static final String NAV_INFO_KEY = "NavCmd.NavInfo";
+
+
+    private ObjectName target()
+    {
+        return getNavInfo().resolve();
     }
     
+    
+    private AMXProxy targetProxy()
+    {
+        return ProxyFactory.getInstance( getConnection() ).getProxy( target() );
+    }
+
+
+    private void cd(final String dest)
+    {
+        final NavInfo info = getNavInfo();
+        info.cd(dest);
+        println( info.getCurrentDir() + " = " + info.resolve());
+    }
+
+
+    private void ls()
+    {
+        final AMXProxy amx = targetProxy();
+        
+        final String delim = ", ";
+        
+        final Set<AMXProxy> childrenSet = amx.childrenSet();
+        //final Map<String,Object> attrs = amx.attributesMap();
+        
+        final String attrNamesStr = CollectionUtil.toString( amx.attributeNames(), delim );
+        
+        final List<String> childItems = new ArrayList<String>();
+        for( final AMXProxy child : childrenSet)
+        {
+            final String nameProp = child.nameProp();
+            childItems.add( child.type() + (nameProp == null ? "" : "=" + nameProp) );
+        }
+        final String childrenStr = CollectionUtil.toString( childItems, delim );
+        
+        println( attrNamesStr );
+        println( "" );
+        if ( childrenSet.size() != 0 )
+        {
+            println( childrenStr );
+        }
+        
+        /*
+        try
+        {
+            final MBeanInfo info = getConnection().getMBeanInfo( target() );
+            println( info );
+        }
+        catch( final Exception e )
+        {
+            e.printStackTrace();
+        }
+        */
+    }
+
+
+    private void cat()
+    {
+        final NavInfo info = getNavInfo();
+        println("Not yet implemented");
+    }
+
+
     private NavInfo getNavInfo()
     {
         final Object o = envGet(NAV_INFO_KEY);
         NavInfo info = null;
-        if ( o instanceof String )
+        if (o instanceof String)
         {
-            info = new NavInfo();
-            info.setCurrentDir((String)o);
+            info = new NavInfo(getConnection());
+            info.setCurrentDir((String) o);
         }
-        else if ( o != null )
+        else if (o != null)
         {
-            info = (NavInfo)o;
+            info = (NavInfo) o;
         }
         else
         {
-            info = new NavInfo();
-        } 
-        
+            info = new NavInfo(getConnection());
+        }
+
         envPut(NAV_INFO_KEY, info, false);
         return info;
     }
+
+
+    private void connect()
+    {
+        try
+        {
+            establishProxy();
+            getNavInfo().setMBeanServerConnection( getConnection() );
+        }
+        catch( final Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
     
+
     /**
-        Commands are stubbed out, 
+    Commands are stubbed out,
      */
-		protected void
-	executeInternal()
-		throws Exception
-	{
-		final String [] operands	= getOperands();
-		final String	cmd	= getSubCmdNameAsInvoked();
-		
-		assert( operands != null );
-        
+    protected void executeInternal()
+        throws Exception
+    {
+        final String[] operands = getOperands();
+        final String cmd = getSubCmdNameAsInvoked();
+
+        assert (operands != null);
+
         final NavInfo navInfo = getNavInfo();
-		
-		if ( cmd.equals( CD_NAME ) )
-		{
-            if ( operands.length >= 2 )
+
+        if (cmd.equals(CD_NAME))
+        {
+            if (operands.length >= 2)
             {
-				throw new IllegalUsageException( cmd + " requires 0 or 1 arguments" );
+                throw new IllegalUsageException(cmd + " requires 0 or 1 arguments");
             }
+            connect();
             final String dir = operands.length == 0 ? DEFAULT_DIR : operands[0];
             cd(dir);
-            println(dir);
-		}
-        else if ( cmd.equals( LS_NAME ) )
+        }
+        else if (cmd.equals(LS_NAME))
         {
-            println( navInfo.getCurrentDir() );
-            establishProxy();
+            connect();
+            //println(navInfo.getCurrentDir());
             // list all attribute names and children
-            println( "Not yet implemented: " + cmd );
+            ls();
         }
-        else if ( cmd.equals( PWD_NAME ) )
+        else if (cmd.equals(PWD_NAME))
         {
-            println( navInfo.getCurrentDir() );
+            connect();
+            println(navInfo.getCurrentDir() + " = " + target() );
         }
-        else if ( cmd.equals( PUSHD_NAME ) )
+        else if (cmd.equals(PUSHD_NAME))
         {
-            navInfo.pushd( navInfo.getCurrentDir() );
-            println( navInfo.getCurrentDir() );
+            connect();
+            navInfo.pushd(navInfo.getCurrentDir());
+            println(navInfo.getCurrentDir());
         }
-        else if ( cmd.equals( POPD_NAME ) )
+        else if (cmd.equals(CAT_NAME))
         {
+            println("not implemented");
+        }
+        else if (cmd.equals(POPD_NAME))
+        {
+            connect();
             try
             {
                 final String dir = navInfo.popd();
                 navInfo.setCurrentDir(dir);
             }
-            catch( final Exception e)
+            catch (final Exception e)
             {
-                println( "Directory stack empty" );
+                println("Directory stack empty");
             }
-            println( navInfo.getCurrentDir() );
+            println(navInfo.getCurrentDir());
         }
-		else
-		{
-            println( "Unknown command: " + cmd );
-		}
-	}
+        else
+        {
+            println("Unknown command: " + cmd);
+        }
+    }
 }
 
 
