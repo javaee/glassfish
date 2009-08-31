fb12d64d2a1101fe3a7b1769d7c6b25261c5c158
Bobby Bisset <bbissett@java.net>
2009-08-31 16:37:34 +0000

    Potential fix for issue 9262. Need to commit and pull down build on a windows machine to test since I don't have one for development.
    svn path=/trunk/; revision=30964
    
    
    Former-commit-id: 3a5a3f71495db98ded2cf8283f2d71320dadf920

--- v3/extras/upgrade/upgrade-scripts/src/main/resources/glassfish/bin/asupgrade.bat
+++ v3/extras/upgrade/upgrade-scripts/src/main/resources/glassfish/bin/asupgrade.bat
@@ -6,18 +6,15 @@ REM Use is subject to license terms.
 REM
 
 setlocal
-DIRNAME=%~dp0
-AS_INSTALL=%DIRNAME%\..\
-AS_INSTALL_LIB=%AS_INSTALL%/lib
-AS_INSTALL_MOD=%AS_INSTALL%/modules
-CONFIG_HOME=%AS_INSTALL%/config
+set DIRNAME=%~dp0
+set AS_INSTALL=%DIRNAME%\..
+set AS_INSTALL_LIB=%AS_INSTALL%\lib
+set AS_INSTALL_MOD=%AS_INSTALL%\modules
+set CONFIG_HOME=%AS_INSTALL%\config
+
 call "%CONFIG_HOME%\asenv.bat"
 
-REM override relative path from asenv.bat
-AS_INSTALL=%DIRNAME%\..\
+set AS_JAVA=%JAVA_HOME%
 
-set Path=%AS_INSTALL%\bin;%PATH%
-
-REM note: $AS_DEF_DOMAINS_PATH is relative to config dir
 "%AS_JAVA%\bin\java" -Dcom.sun.aas.utool.LogLevel="INFO" -Dcom.sun.aas.domainRoot="%DIRNAME%\%AS_DEF_DOMAINS_PATH%"  -Dcom.sun.aas.java.home="%AS_JAVA%" -jar "%AS_INSTALL_MOD%/upgrade-tool.jar" %*
 endlocal
