8483e3590f3046d78210fa5ffa5bcbf78b669c5a
Jan Luehe <jluehe@java.net>
2009-09-03 16:58:09 +0000

    Retrofitted LifecycleException to leverage the exception chaining facilities of its Exception superclass
    Tests run: QL
    
    svn path=/trunk/; revision=31107
    
    
    Former-commit-id: bd6e63c802180ea090ea57012cb30d5b4d788970

--- v3/web/web-core/src/main/java/org/apache/catalina/LifecycleException.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/LifecycleException.java
@@ -77,9 +77,7 @@ public final class LifecycleException extends Exception {
      * Construct a new LifecycleException with no other information.
      */
     public LifecycleException() {
-
-        this(null, null);
-
+        super();
     }
 
 
@@ -89,9 +87,7 @@ public final class LifecycleException extends Exception {
      * @param message Message describing this exception
      */
     public LifecycleException(String message) {
-
-        this(message, null);
-
+        super(message);
     }
 
 
@@ -101,9 +97,7 @@ public final class LifecycleException extends Exception {
      * @param throwable Throwable that caused this exception
      */
     public LifecycleException(Throwable throwable) {
-
-        this(null, throwable);
-
+        super(throwable);
     }
 
 
@@ -115,70 +109,6 @@ public final class LifecycleException extends Exception {
      * @param throwable Throwable that caused this exception
      */
     public LifecycleException(String message, Throwable throwable) {
-
-        super();
-        this.message = message;
-        this.throwable = throwable;
-
+        super(message, throwable);
     }
-
-
-    //------------------------------------------------------ Instance Variables
-
-
-    /**
-     * The error message passed to our constructor (if any)
-     */
-    protected String message = null;
-
-
-    /**
-     * The underlying exception or error passed to our constructor (if any)
-     */
-    protected Throwable throwable = null;
-
-
-    //---------------------------------------------------------- Public Methods
-
-
-    /**
-     * Returns the message associated with this exception, if any.
-     */
-    public String getMessage() {
-
-        return (message);
-
-    }
-
-
-    /**
-     * Returns the throwable that caused this exception, if any.
-     */
-    public Throwable getThrowable() {
-
-        return (throwable);
-
-    }
-
-
-    /**
-     * Return a formatted string that describes this exception.
-     */
-    public String toString() {
-
-        StringBuffer sb = new StringBuffer("LifecycleException:  ");
-        if (message != null) {
-            sb.append(message);
-            if (throwable != null) {
-                sb.append(":  ");
-            }
-        }
-        if (throwable != null) {
-            sb.append(throwable.toString());
-        }
-        return (sb.toString());
-
-    }
-
-
 }
