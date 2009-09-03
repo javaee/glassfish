acd07b4f1011ba725a3ac3dcf8aa321a8832a647
Jan Luehe <jluehe@java.net>
2009-09-03 20:22:23 +0000

    Retrofitted ClientAbortException to leverage the exception chaining facilities of its IOException superclass
    Tests run: QL
    
    svn path=/trunk/; revision=31119
    
    
    Former-commit-id: 4092dd4f808c4a8424776d7972b58c9e030c3d6a

--- v3/web/web-core/src/main/java/org/apache/catalina/connector/ClientAbortException.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/connector/ClientAbortException.java
@@ -77,9 +77,7 @@ public final class ClientAbortException extends IOException {
      * Construct a new ClientAbortException with no other information.
      */
     public ClientAbortException() {
-
-        this(null, null);
-
+        super();
     }
 
 
@@ -89,9 +87,7 @@ public final class ClientAbortException extends IOException {
      * @param message Message describing this exception
      */
     public ClientAbortException(String message) {
-
-        this(message, null);
-
+        super(message);
     }
 
 
@@ -101,9 +97,7 @@ public final class ClientAbortException extends IOException {
      * @param throwable Throwable that caused this exception
      */
     public ClientAbortException(Throwable throwable) {
-
-        this(null, throwable);
-
+        super(throwable);
     }
 
 
@@ -115,70 +109,6 @@ public final class ClientAbortException extends IOException {
      * @param throwable Throwable that caused this exception
      */
     public ClientAbortException(String message, Throwable throwable) {
-
-        super();
-        this.message = message;
-        this.throwable = throwable;
-
+        super(message, throwable);
     }
-
-
-    //------------------------------------------------------ Instance Variables
-
-
-    /**
-     * The error message passed to our constructor (if any)
-     */
-    protected String message = null;
-
-
-    /**
-     * The underlying exception or error passed to our constructor (if any)
-     */
-    protected Throwable throwable = null;
-
-
-    //---------------------------------------------------------- Public Methods
-
-
-    /**
-     * Returns the message associated with this exception, if any.
-     */
-    public String getMessage() {
-
-        return (message);
-
-    }
-
-
-    /**
-     * Returns the cause that caused this exception, if any.
-     */
-    public Throwable getCause() {
-
-        return (throwable);
-
-    }
-
-
-    /**
-     * Return a formatted string that describes this exception.
-     */
-    public String toString() {
-
-        StringBuffer sb = new StringBuffer("ClientAbortException:  ");
-        if (message != null) {
-            sb.append(message);
-            if (throwable != null) {
-                sb.append(":  ");
-            }
-        }
-        if (throwable != null) {
-            sb.append(throwable.toString());
-        }
-        return (sb.toString());
-
-    }
-
-
 }
