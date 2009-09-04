624adf94ee1f1868a560760854c9a07b8efc3381
Jan Luehe <jluehe@java.net>
2009-09-04 17:19:51 +0000

    Moved enforcement logic of session cookie configuration from client to servlet
    svn path=/trunk/; revision=31146
    
    
    Former-commit-id: 7bbd121e18857657daca46f1cdf92295e0bfcbe0

--- v2/appserv-tests/devtests/web/servlet-3.0/sessionCookieConfigDeclarative/servlet/test/CreateSession.java
+++ v2/appserv-tests/devtests/web/servlet-3.0/sessionCookieConfigDeclarative/servlet/test/CreateSession.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -43,6 +43,48 @@ public class CreateSession extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+
         req.getSession(true);
+
+        String sessionCookie = res.getHeader("Set-Cookie");
+        if (sessionCookie == null) {
+            throw new ServletException("Missing Set-Cookie response header");
+        }
+
+        // name
+        if (sessionCookie.indexOf("MYJSESSIONID=") == -1) {
+            throw new ServletException("Missing session id");
+        }
+
+        // comment
+        if (sessionCookie.indexOf("Comment=myComment") == -1) {
+            throw new ServletException("Missing cookie comment");
+        }      
+
+        // domain
+        if (sessionCookie.indexOf("Domain=mydomain") == -1) {
+            throw new ServletException("Missing cookie domain");
+        }      
+
+        // path
+        if (sessionCookie.indexOf("Path=/myPath") == -1) {
+            throw new ServletException("Missing cookie path");
+        }      
+
+        // secure
+        if (sessionCookie.indexOf("Secure") == -1) {
+            throw new ServletException("Missing Secure attribute");
+        }      
+
+        // http-only
+        if (sessionCookie.indexOf("HttpOnly") == -1) {
+            throw new ServletException("Missing HttpOnly attribute");
+        }      
+
+        // max-age
+        if (sessionCookie.indexOf("Max-Age=123") == -1) {
+            throw new ServletException("Missing max-age");
+        }      
+
     }
 }
