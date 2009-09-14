96b5db808710ac31562612da3c5bb7da0ef36a9c
Anissa Lam <anissa.lam@oracle.com>
2009-09-14 06:07:34 +0000

    Change to sync up with the new anonymous login in Glassfish.  byPass login screen depending on getAnonymousUser() API.
    svn path=/trunk/; revision=31313
    
    
    Former-commit-id: 9647b028ff1190e1dc915114cf0b3fad64726f2c

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/CommonHandlers.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/CommonHandlers.java
@@ -366,7 +366,24 @@ public class CommonHandlers {
         handlerCtx.setOutputValue("unprocessedChanges", changes); 
     }
 
- 
+
+    /**
+     * <p> Test if anonymous user login is allowed <p>
+     * <p> Output value: "loginUser" -- Type: <code>java.lang.String</code> the anonymous user name </p>
+     * <p> Output value: "byPass" -- Type: <code>java.lang.Boolean</code>if GUI should bypass login form </p>
+     *
+     */
+    @Handler(id="testLoginBypass",
+    output={
+        @HandlerOutput(name="byPass", type=Boolean.class),
+        @HandlerOutput(name="loginUser", type=String.class)}
+    )
+    public void testLoginBypass(HandlerContext handlerCtx) {
+        String user = V3AMX.getInstance().getRealmsMgr().getAnonymousUser();
+        handlerCtx.setOutputValue("byPass", (user==null)? Boolean.FALSE: Boolean.TRUE);
+        handlerCtx.setOutputValue("loginUser", (user==null) ? "" : user);
+    }
+
     /**
      * <p> This handler sets a property on an object which is stored in an existing key
      * For example "advance.lazyConnectionEnlistment".  <strong>Note</strong>:  This does
