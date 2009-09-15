5f15566301449e194a0ec3835d50befed9aaa316
Jagadish Ramu <jagadish.ramu@oracle.com>
2009-09-15 10:28:23 +0000

    Issue Number : 9521 Description : make "class-name" attribute of admin-object-resource check in Sun-Resources-XML-Parser optional
    Tests Run : QL, connector-dev, jdbc-dev, manually tested with a sun-resources.xml sample
    
    
    svn path=/trunk/; revision=31347
    
    
    Former-commit-id: f9ee58e84a14edaa38ed64dcd683a3c6be87f1c7

--- v3/admin/cli/src/main/java/org/glassfish/admin/cli/resources/ResourcesXMLParser.java
+++ v3/admin/cli/src/main/java/org/glassfish/admin/cli/resources/ResourcesXMLParser.java
@@ -823,7 +823,10 @@ public class ResourcesXMLParser implements EntityResolver
         Node resTypeNode = attributes.getNamedItem(RES_TYPE);
         String resType = resTypeNode.getNodeValue();
         Node classNameNode = attributes.getNamedItem(ADMIN_OBJECT_CLASS_NAME);
-        String className = classNameNode.getNodeValue();
+        String className = null;
+        if(classNameNode != null){
+            className = classNameNode.getNodeValue();
+        }
         Node resAdapterNode = attributes.getNamedItem(RES_ADAPTER);
         String resAdapter = resAdapterNode.getNodeValue();
         Node enabledNode = attributes.getNamedItem(ENABLED);
@@ -832,7 +835,9 @@ public class ResourcesXMLParser implements EntityResolver
                     new Resource(Resource.ADMIN_OBJECT_RESOURCE);
         adminObjectResource.setAttribute(JNDI_NAME, jndiName);
         adminObjectResource.setAttribute(RES_TYPE, resType);
-        adminObjectResource.setAttribute(ADMIN_OBJECT_CLASS_NAME, className);
+        if(classNameNode != null){
+            adminObjectResource.setAttribute(ADMIN_OBJECT_CLASS_NAME, className);
+        }
         adminObjectResource.setAttribute(RES_ADAPTER, resAdapter);
 
         if (enabledNode != null) {
