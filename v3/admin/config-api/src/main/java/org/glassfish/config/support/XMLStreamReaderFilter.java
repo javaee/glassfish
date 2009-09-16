ff7253fc7cd2d40c699c03ae8ec4008db1b431d3
Nandini Ektare <ne110415@java.net>
2009-09-16 04:03:23 +0000

    Fix for issue 9127. Reviewed by Kohsuke Kawaguchi. QL passes.
    svn path=/trunk/; revision=31384
    
    
    Former-commit-id: c764833fbf5f2795c6debff26e2c6676ef1a9730

--- v3/admin/config-api/src/main/java/org/glassfish/config/support/XMLStreamReaderFilter.java
+++ v3/admin/config-api/src/main/java/org/glassfish/config/support/XMLStreamReaderFilter.java
@@ -36,8 +36,10 @@
  */
 package org.glassfish.config.support;
 
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
 import javax.xml.stream.util.StreamReaderDelegate;
 
 /**
@@ -63,11 +65,71 @@ abstract class XMLStreamReaderFilter extends StreamReaderDelegate {
 
     public int nextTag() throws XMLStreamException {
         while(true) {
-            int r = super.nextTag();
+            // Fix for issue 9127
+            // The following call to super.nextTag() is replaced with thisNextTag() 
+            int r = thisNextTag();
             if(r!=START_ELEMENT || !filterOut())
                 return r;
             skipTree();
         }
+    }
+
+    // Fix for issue 9127
+    // This method is a modified version of the super.nextTag()
+    // In addition to all other event types skipped in super.nextTag() in search for 
+    // START_ELEMENT this method also includes DTD eventType in the skip-list 
+    private int thisNextTag() throws XMLStreamException {
+        int eventType = super.next();
+        while((eventType == XMLStreamConstants.CHARACTERS && isWhiteSpace()) // skip whitespace
+        || (eventType == XMLStreamConstants.CDATA && isWhiteSpace())
+        // skip whitespace
+        || eventType == XMLStreamConstants.SPACE
+        || eventType == XMLStreamConstants.PROCESSING_INSTRUCTION
+        || eventType == XMLStreamConstants.COMMENT
+        || eventType == XMLStreamConstants.DTD
+        ) {
+            eventType = super.next();
+        }
+
+        if (eventType != XMLStreamConstants.START_ELEMENT && eventType != XMLStreamConstants.END_ELEMENT) {
+            throw new XMLStreamException(
+                    "found: " + getEventTypeString(eventType)
+                    + ", expected " + getEventTypeString(XMLStreamConstants.START_ELEMENT)
+                    + " or " + getEventTypeString(XMLStreamConstants.END_ELEMENT));
+        }
+
+        return eventType;
+
+    }
+
+    final static String getEventTypeString(int eventType) {
+        switch (eventType){
+            case XMLEvent.START_ELEMENT:
+                return "START_ELEMENT";
+            case XMLEvent.END_ELEMENT:
+                return "END_ELEMENT";
+            case XMLEvent.PROCESSING_INSTRUCTION:
+                return "PROCESSING_INSTRUCTION";
+            case XMLEvent.CHARACTERS:
+                return "CHARACTERS";
+            case XMLEvent.COMMENT:
+                return "COMMENT";
+            case XMLEvent.START_DOCUMENT:
+                return "START_DOCUMENT";
+            case XMLEvent.END_DOCUMENT:
+                return "END_DOCUMENT";
+            case XMLEvent.ENTITY_REFERENCE:
+                return "ENTITY_REFERENCE";
+            case XMLEvent.ATTRIBUTE:
+                return "ATTRIBUTE";
+            case XMLEvent.DTD:
+                return "DTD";
+            case XMLEvent.CDATA:
+                return "CDATA";
+            case XMLEvent.SPACE:
+                return "SPACE";
+        }
+        return "UNKNOWN_EVENT_TYPE, " + String.valueOf(eventType);
     }
 
     /**
