e35612fddb2acc5f32f92b0c34f584255bcf9190
Nandini Ektare <ne110415@java.net>
2009-09-18 18:21:57 +0000

    Fix for issue 7859. Added enum sort validation for the isolationLevel attribute of the jdbc connection pool.
    svn path=/trunk/; revision=31529
    
    
    Former-commit-id: 6fca5d947f252f87f0d4b2ef8409ee65c1bdc8f0

--- v3/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/JdbcConnectionPool.java
+++ v3/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/JdbcConnectionPool.java
@@ -287,6 +287,7 @@ public interface JdbcConnectionPool extends ConfigBeanProxy, Injectable, Resourc
      *         {@link String }
      */
     @Attribute
+    @Pattern(regexp="(read-uncommitted|read-committed|repeatable-read|serializable)")
     String getTransactionIsolationLevel();
 
     /**
