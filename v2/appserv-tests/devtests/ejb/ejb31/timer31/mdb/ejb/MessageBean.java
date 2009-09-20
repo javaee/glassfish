ce6fc28b7e57add60315398fe449834f0f8ed34d
Ken Saks <ksak@java.net>
2009-09-20 22:08:21 +0000

    added test for issue 9611 -- MDB isCallerInRole() operations allowed bug for timer callbacks
    svn path=/trunk/; revision=31595
    
    
    Former-commit-id: 1a2edd336452359f391061a215e82b206aaf458e

--- v2/appserv-tests/devtests/ejb/ejb31/timer31/mdb/ejb/MessageBean.java
+++ v2/appserv-tests/devtests/ejb/ejb31/timer31/mdb/ejb/MessageBean.java
@@ -7,17 +7,24 @@ import javax.ejb.Timer;
 import javax.interceptor.AroundTimeout;
 import javax.interceptor.InvocationContext;
 
+import javax.annotation.Resource;
+import javax.ejb.MessageDrivenContext;
 import javax.jms.MessageListener;
 import javax.jms.Message;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-
+import javax.annotation.security.*;
+import javax.ejb.EJBException;
 
 @MessageDriven(mappedName="jms/ejb_ejb31_timer31_mdb_InQueue", description="mymessagedriven bean description")
+ @RolesAllowed("foo")
 public class MessageBean implements MessageListener {
 
     @EJB 
     private SingletonBean singleton;
+
+    @Resource
+	private MessageDrivenContext mdc;
      
     @PostConstruct
     public void init() {
@@ -25,11 +32,23 @@ public class MessageBean implements MessageListener {
     }
 
     public void onMessage(Message message) {
+	System.out.println("In MessageBean::onMessage()");
+	System.out.println("getCallerPrincipal = " + mdc.getCallerPrincipal());
     }
 
     @Schedule(second="*/1", minute="*", hour="*")
     private void onTimeout() {
 	System.out.println("In MessageBean::onTimeout()");
+	System.out.println("getCallerPrincipal = " + mdc.getCallerPrincipal());
+
+	try {
+	    System.out.println("IsCallerInRole('foo')= " + 
+			       mdc.isCallerInRole("foo"));
+	    throw new EJBException("Expecting IllegalStateEXception for call to isCallerInRole() from timer callback");
+	} catch(IllegalStateException ise) {
+	    System.out.println("Successfully received exception for invocation of isCallerInRole from timer callback");
+	}
+	    
         if (singleton.getAroundTimeoutCalled(null)) {
 	    singleton.test1Passed();
         }
