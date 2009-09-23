27e3133757323cf0ffac8f2964695b82166e81ee
Bill Shannon <bill.shannon@oracle.com>
2009-09-23 23:06:23 +0000

    Make asadmin.bat work when command extensions aren't enabled. Fixes issue #9325.
    Reviewed by Sathyan and Byron.
    
    svn path=/trunk/; revision=31782
    
    
    Former-commit-id: 665ec9605fb5dd5f004ba15abaee0322ebed2e65

--- v3/packager/nucleus-base/bin/asadmin.bat
+++ v3/packager/nucleus-base/bin/asadmin.bat
@@ -3,7 +3,12 @@ REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
 REM Always use JDK 1.6 or higher
 REM Depends on Java from ..\config\asenv.bat
-setlocal
+VERIFY OTHER 2>nul
+setlocal ENABLEEXTENSIONS
+if ERRORLEVEL 0 goto ok
+echo "Unable to enable extensions"
+exit /B 1
+:ok
 call "%~dp0..\config\asenv.bat" 
 if "%AS_JAVA%x" == "x" goto UsePath
 set JAVA="%AS_JAVA%\bin\java"
