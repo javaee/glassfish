fde981c3fcbb698537de5bf2bdca69b00dceda07
Jean francois Arcand <jfarcand@java.net>
2009-09-29 16:15:08 +0000

    Fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=9690 ("[blocking]file-cache stats are not updated")
    The fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=9286
    
    re-enabled the optimization for skipping the double mapping of request|container, and exposed a new issue with the FileCache configuration. The complete fix will be in
    tegrated with 1.9.18c for file located under the docroot, and the current commit fix the issue when the file is included in the application was itself.
    
    + ALL QL passed.
    
    svn path=/trunk/; revision=32012
    
    
    Former-commit-id: c4ab613a70f8fcb9daaa8964376076d9a15a479e

--- v3/web/web-core/src/main/java/org/apache/catalina/connector/CoyoteAdapter.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -182,6 +182,7 @@ public class CoyoteAdapter
     /**
      * Service method.
      */
+    @Override
     public void service(com.sun.grizzly.tcp.Request req,
                         com.sun.grizzly.tcp.Response res)
             throws Exception {
@@ -357,6 +358,7 @@ public class CoyoteAdapter
      * Finish the response and close the connection based on the connection
      * header.
      */
+    @Override
     public void afterService(com.sun.grizzly.tcp.Request req,
                              com.sun.grizzly.tcp.Response res)
             throws Exception{
@@ -502,7 +504,9 @@ public class CoyoteAdapter
             /*mod_jk*/
             connector.getMapper().map(req.serverName(), decodedURI, 
                                   request.getMappingData());
-            request.updatePaths(request.getMappingData());
+            MappingData md = request.getMappingData();
+            req.setNote(MAPPING_DATA, md);
+            request.updatePaths(md);
             ctx = (Context) request.getMappingData().context;
         }
 
