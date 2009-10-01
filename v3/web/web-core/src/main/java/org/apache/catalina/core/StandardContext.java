f52e9b7f99e2dfe99a988abfec1c5f2527821d01
Jan Luehe <jluehe@java.net>
2009-10-01 04:29:05 +0000

    Make sure Throwable thrown by ServletContainerInitializer#onStartup is chained correctly
    Tests run: QL
    
    svn path=/trunk/; revision=32105
    
    
    Former-commit-id: f35c25b07aa3d37d46ddeb915592f01a8a0302a6

--- v3/web/web-core/src/main/java/org/apache/catalina/core/StandardContext.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/core/StandardContext.java
@@ -5372,11 +5372,12 @@ public class StandardContext
                     iniInstance.onStartup(
                         initializerList.get(initializer), ctxt);
                 } catch (Throwable t) {
-                    throw new LifecycleException(
+                    log.log(Level.SEVERE,
                         sm.getString(
                             "standardContext.servletContainerInitializer.error",
                             initializer.getCanonicalName()),
                         t);
+                    throw new LifecycleException(t);
                 }
             }
         } finally {
