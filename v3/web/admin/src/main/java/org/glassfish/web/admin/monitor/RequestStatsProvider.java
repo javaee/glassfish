dfea9c345f3c30b283b8102b3d7773243a546e7c
Shing Wai Chan <shing.wai.chan@oracle.com>
2009-10-02 19:52:47 +0000

    fix issue https://glassfish.dev.java.net/issues/show_bug.cgi?id=9010 ("WebStatsProvider still use org.glassfish.flashlight.statistics") reviewed by Jan Luehe
    svn path=/trunk/; revision=32197
    
    
    Former-commit-id: 33b728bbe093bd8cd9d8d61a986bfcd98a980ee1

--- v3/web/admin/src/main/java/org/glassfish/web/admin/monitor/RequestStatsProvider.java
+++ v3/web/admin/src/main/java/org/glassfish/web/admin/monitor/RequestStatsProvider.java
@@ -38,12 +38,10 @@ package org.glassfish.web.admin.monitor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.glassfish.external.statistics.CountStatistic;
-import org.glassfish.external.statistics.TimeStatistic;
+import org.glassfish.external.statistics.annotations.Reset;
 import org.glassfish.external.statistics.impl.CountStatisticImpl;
 import org.glassfish.external.statistics.impl.StatisticImpl;
 import org.glassfish.external.statistics.impl.TimeStatisticImpl;
-import org.glassfish.flashlight.statistics.*;
-import org.glassfish.flashlight.statistics.factory.TimeStatsFactory;
 import org.glassfish.external.probe.provider.annotations.*;
 import org.glassfish.gmbal.AMXMetadata;
 import org.glassfish.gmbal.Description;
@@ -69,34 +67,50 @@ public class RequestStatsProvider {
     //private Counter requestCount = CounterFactory.createCount();
     //Provides the cumulative value of the error count. The error count represents 
     //the number of cases where the response code was greater than or equal to 400.
-    private static final String PROCESSING_TIME_DESCRIPTION = 
-        "The request processing time (average, maximum, minimum, and total)";
-    private static final String REQUEST_COUNT_DESCRIPTION =
-        "Cumulative number of requests processed so far";
     private static final String ERROR_COUNT_DESCRIPTION =
         "Cumulative value of the error count, with error count representing the number of cases where the response code was greater than or equal to 400";
+    private static final String REQUEST_COUNT_DESCRIPTION =
+        "Cumulative number of requests processed so far";
+    private static final String MAX_TIME_DESCRIPTION =
+        "Longest response time for a request; not a cumulative value, but the largest response time from among the response times";
+    private static final String PROCESSING_TIME_DESCRIPTION = 
+        "Average request processing time";
 
-    private CountStatisticImpl requestCount = new CountStatisticImpl("RequestCount",
-            StatisticImpl.UNIT_COUNT, REQUEST_COUNT_DESCRIPTION);
     private CountStatisticImpl errorCount = new CountStatisticImpl("ErrorCount",
             StatisticImpl.UNIT_COUNT, ERROR_COUNT_DESCRIPTION);
+    private CountStatisticImpl requestCount = new CountStatisticImpl("RequestCount",
+            StatisticImpl.UNIT_COUNT, REQUEST_COUNT_DESCRIPTION);
+    private CountStatisticImpl maxTime = new CountStatisticImpl("MaxTime",
+            StatisticImpl.UNIT_COUNT, MAX_TIME_DESCRIPTION);
+    private CountStatisticImpl processingTime = new CountStatisticImpl("ProcessingTime",
+            StatisticImpl.UNIT_COUNT, PROCESSING_TIME_DESCRIPTION);
 
-    private TimeStats requestProcessTime = TimeStatsFactory.createTimeStatsMilli();
+    private TimeStatisticImpl requestProcessTime = new TimeStatisticImpl(0L, 0L, 0L, 0L,
+            "", "", "", System.currentTimeMillis(), -1L);
     private Logger logger;
     
     private String virtualServerName = null;
     private String moduleName = null;
 
+    private ThreadLocal<TimeStatData> timeStatDataLocal = new ThreadLocal<TimeStatData> (){
+        TimeStatData tsd;
+
+        protected TimeStatData initialValue (){
+            tsd = new TimeStatData ();
+            return tsd;
+        }
+        public TimeStatData get (){
+            if (tsd == null) {
+                tsd = new TimeStatData();
+            }
+            return tsd;
+        }
+    };
+
     public RequestStatsProvider(String appName, String vsName, Logger logger) {
         this.logger = logger;
         this.virtualServerName = vsName;
         this.moduleName = appName;
-    }
-
-    @ManagedAttribute(id="requestcount")
-    @Description(REQUEST_COUNT_DESCRIPTION)
-    public CountStatistic getRequestCount() {
-        return requestCount;
     }
 
     @ManagedAttribute(id="errorcount")
@@ -105,19 +119,30 @@ public class RequestStatsProvider {
         return errorCount;
     }
 
+    @ManagedAttribute(id="requestcount")
+    @Description(REQUEST_COUNT_DESCRIPTION)
+    public CountStatistic getRequestCount() {
+        requestCount.setCount(requestProcessTime.getCount());
+        return requestCount;
+    }
+
+    @ManagedAttribute(id="maxtime")
+    @Description(MAX_TIME_DESCRIPTION)
+    public CountStatistic getMaxTime() {
+        maxTime.setCount(requestProcessTime.getMaxTime());
+        return maxTime;
+    }
+
     @ManagedAttribute(id="processingtime")
     @Description(PROCESSING_TIME_DESCRIPTION)
-    public TimeStatistic getProcessingTime() {
-        return new TimeStatisticImpl(
-                (long) requestProcessTime.getTime(),
-                requestProcessTime.getMaximumTime(),
-                requestProcessTime.getMinimumTime(),
-                requestProcessTime.getTotalTime(),
-                "ProcessingTime",
-                StatisticImpl.UNIT_MILLISECOND,
-                PROCESSING_TIME_DESCRIPTION,
-                requestProcessTime.getStartTime(),
-                requestProcessTime.getLastSampleTime());
+    public CountStatistic getProcessingTime() {
+        long count = requestProcessTime.getCount();
+        long processTime = -1L;
+        if (count != 0) {
+            processTime = requestProcessTime.getTotalTime()/count;
+        }
+        processingTime.setCount(processTime);
+        return processingTime;
     }
 
     @ProbeListener("glassfish:web:http-service:requestStartEvent")
@@ -144,7 +169,7 @@ public class RequestStatsProvider {
                     hostName.equals(virtualServerName) &&
                     appName.equals(moduleName)){
                 //increment counts
-                requestProcessTime.entry();
+                timeStatDataLocal.get().setEntryTime(System.currentTimeMillis());
                 if (logger.isLoggable(Level.FINEST)) {
                     logger.finest(
                         "[TM]requestStartEvent resolved - virtual-server = " +
@@ -154,9 +179,8 @@ public class RequestStatsProvider {
                         serverPort);
                 }
             }
-        }
-        else {
-            requestProcessTime.entry();
+        } else {
+            timeStatDataLocal.get().setEntryTime(System.currentTimeMillis());
             if (logger.isLoggable(Level.FINEST)) {
                 logger.finest(
                     "[TM]requestStartEvent resolved - virtual-server = " +
@@ -191,8 +215,10 @@ public class RequestStatsProvider {
                     hostName.equals(virtualServerName) &&
                     appName.equals(moduleName)){
                 //increment counts
-                requestCount.increment();
-                requestProcessTime.exit();
+                TimeStatData tsd = timeStatDataLocal.get();
+                tsd.setExitTime(System.currentTimeMillis());
+                requestProcessTime.incrementCount(tsd.getTotalTime());
+
                 if (statusCode >= 400) {
                     errorCount.increment();
                 }
@@ -203,15 +229,14 @@ public class RequestStatsProvider {
                         " :appName = " + appName +
                         " : servlet = " + servletPath +
                         " : port = " + serverPort +
-                        " :Response code = " + statusCode +
-                        " :Response time = " +
-                        requestProcessTime.getTime());
+                        " :Response code = " + statusCode);
                 }
             }
-        }
-        else {
-            requestCount.increment();
-            requestProcessTime.exit();
+        } else {
+            TimeStatData tsd = timeStatDataLocal.get();
+            tsd.setExitTime(System.currentTimeMillis());
+            requestProcessTime.incrementCount(tsd.getTotalTime());
+
             if (statusCode >= 400) {
                 errorCount.increment();
             }
@@ -222,8 +247,7 @@ public class RequestStatsProvider {
                     contextPath + " : servlet = " +
                     servletPath + " : port = " +
                     serverPort  + " :Response code = " +
-                    statusCode + " :Response time = " +
-                    requestProcessTime.getTime());
+                    statusCode);
             }
         }
     }
@@ -239,6 +263,15 @@ public class RequestStatsProvider {
     
     public String getVSName() {
         return virtualServerName;
+    }
+
+    @Reset
+    public void reset() {
+        this.requestProcessTime.reset();
+        this.errorCount.reset();
+        this.maxTime.reset();
+        this.processingTime.reset();
+        this.requestCount.reset();
     }
 
 }
