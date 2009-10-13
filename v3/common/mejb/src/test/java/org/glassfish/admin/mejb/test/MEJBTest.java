a97b5490b9176bf690687f0428211b11882b6ae8
Lloyd Chambers <llc@java.net>
2009-10-13 18:56:28 +0000

    removed unused code
    svn path=/trunk/; revision=32702
    
    
    Former-commit-id: 4842962b931f3e3d001bc5b6bd368cc9a333bd83

--- v3/common/mejb/src/test/java/org/glassfish/admin/mejb/test/MEJBTest.java
+++ v3/common/mejb/src/test/java/org/glassfish/admin/mejb/test/MEJBTest.java
@@ -146,7 +146,6 @@ public class MEJBTest {
         }
     }
 
-
 		public static String
 	toString(
 		final Collection<?> c,
@@ -207,83 +206,6 @@ public class MEJBTest {
 		}
 	}
 
-/*
-    public static void main(String[] args) {
-        try {
-           // final Properties env = new Properties();
-            //env.put("java.naming.factory.initial", "com.sun.jndi.cosnaming.CNCtxFactory");
-            //env.put("java.naming.provider.url", "iiop://localhost:3700");
-            final Context initial = new InitialContext();
-
-            //final String mejbName = MEJBUtility.MEJB_DEFAULT_NAME;
-            final String mejbName = "java:global/MEJB/MEJBBean";
-            println("Looking up: " + mejbName);
-            final Object objref = initial.lookup(mejbName);
-           // println("Received from initial.lookup(): " + objref + " for " + mejbName);
-
-            final ManagementHome home = (ManagementHome)objref;
-            //println("ManagementHome: " + home + " for " + mejbName);
-            final MEJB mejb = (MEJB)home.create();
-            println("Got the MEJB");
-
-            new MEJBTest( mejb ).test();
-
-            mejb.remove();
-
-        } catch (Exception ex) {
-            System.err.println("Caught an unexpected exception!");
-            ex.printStackTrace();
-        }
-    }
-    
-    
-
-    public static void main(String[] args) {
-        try {
-            final String mejbName = "java:global/mejb/MEJBBean";
-            final String username = "admin";
-            final String password = "changeit";
-            System.out.println( "Authenticating with \"" + username + "\", \"" + password + "\"");
-            
-            final Properties props = new Properties();
-            props.setProperty("java.naming.factory.initial", "com.sun.enterprise.naming.SerialInitContextFactory");
-            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
-            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
-            props.setProperty( Context.SECURITY_PRINCIPAL, username);
-            props.setProperty( Context.SECURITY_CREDENTIALS, password);
-            final InitialContext initial = new InitialContext(props);
-            
-            println("Looking up: " + mejbName);
-            final Object objref = initial.lookup(mejbName);
-
-            final ManagementHome home = (ManagementHome) PortableRemoteObject.narrow(objref, ManagementHome.class);
-            try
-            {
-                final ManagementHome home2 = (ManagementHome)objref;
-            }
-            catch( final Exception e )
-            {
-                println("WARNING: (ManagementHome)PortableRemoteObject.narrow(objref, ManagementHome.class) works, but (ManagementHome)objref does not!" );
-            }
-            
-            //println("ManagementHome: " + home + " for " + mejbName);
-            final Management mejb = (Management)home.create();
-            println("Got the MEJB");
-
-            new MEJBTest( mejb ).test();
-
-            println( "Calling mejb.remove()" );
-            mejb.remove();
-
-        } catch (Exception ex) {
-            System.err.println("Caught an unexpected exception!");
-            ex.printStackTrace();
-        }
-        println( "Exiting main() forcibly" );
-        System.exit( -1 );
-    }
-
-*/
 
 
     public static void main(String[] args) {
