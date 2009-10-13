fb7048da3a0f02fb204466de59837a9c5a3b1752
km <km@java.net>
2009-10-13 18:08:32 +0000

    Adding a translator that can be leveraged by GlassFishDocument in glassfish code.

--- hk2/config/src/java/org/jvnet/hk2/config/Translator.java
+++ hk2/config/src/java/org/jvnet/hk2/config/Translator.java
@@ -56,4 +56,43 @@ public interface Translator {
             return str;
         }
     };
+    /**
+     * A translator that does translation from the system properties. Thus, any reference to an existing
+     * System Property like "${name}" will be replaced by its value, i.e. System.getProperty("name"). All
+     * found references are translated. If a System Property is not defined, its reference is returned verbatim.
+     * No escape sequences are handled.
+     */
+    public static final Translator SYS_PROP_TR = new Translator() {
+        public String translate(String s) {
+            StringBuilder sb = new StringBuilder();
+            int length = s.length();
+            int i = 0;
+            while(i < length) {
+                char c = s.charAt(i);
+                if (c == '$' && (i+1) < length && s.charAt(i+1) == '{') {
+                i += 2;
+                char cc='\0';
+                StringBuilder prop = new StringBuilder();
+                while (i < length && (cc=s.charAt(i)) != '}') {
+                  prop.append(cc);
+                  i++;
+                }
+                if (cc == '}') {
+                  String value = System.getProperty(prop.toString());
+                  if (value != null)
+                      sb.append(value);
+                  else //return reference to non-existent system-property verbatim
+                      sb.append("${" + prop + "}");
+                  i++;
+                } else { //we reached the end, no } found
+                  sb.append("${").append(prop);
+                }
+              } else {
+                sb.append(c);
+                i++;
+              }
+          }
+          return sb.toString();
+        }
+    };
 }
