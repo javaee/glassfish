946d9e6b037e8a2f8917efa2a570041693dbafca
Ken Saks <ksak@java.net>
2009-10-14 20:43:37 +0000

    fix test to match latest mejb app
    svn path=/trunk/; revision=32757
    
    
    Former-commit-id: 361f08e3e258b058bbc634877fec29f8b577226d

--- v2/appserv-tests/devtests/ejb/ejb31/singleton/hello/client/Client.java
+++ v2/appserv-tests/devtests/ejb/ejb31/singleton/hello/client/Client.java
@@ -34,29 +34,35 @@ public class Client {
 
 	try {
 
-	    // Try all three global JNDI names with PortableRemoteObject.narrow()
+	    // Ensure that MEJB is registered under all three of its JNDI names
 	    System.out.println("Looking up MEJB Homes");
 	    Object mh1Obj = new InitialContext().lookup("ejb/mgmt/MEJB");
+	    Object mh2Obj = new InitialContext().lookup("java:global/mejb/MEJBBean");
+	    Object mh3Obj = new InitialContext().lookup("java:global/mejb/MEJBBean!javax.management.j2ee.ManagementHome");
+
 	    System.out.println("mejb home obj 1 = " + mh1Obj);
+	    System.out.println("mejb home obj 2 = " + mh2Obj);
+	    System.out.println("mejb home obj 3 = " + mh3Obj);
+
+	    /**  Commenting out MEJB access now that the EJB is protected. 
 	    ManagementHome mh = (ManagementHome) PortableRemoteObject.narrow(mh1Obj, ManagementHome.class);
 	    System.out.println("mejb home 1 = " + mh);
 	    Management m1 = mh.create();
 	    System.out.println("mejb obj 1 = " + m1);
 
-	    Object mh2Obj = new InitialContext().lookup("java:global/mejb/MEJBBean");
-	    System.out.println("mejb home obj 2 = " + mh2Obj);
 	    ManagementHome mh2 = (ManagementHome) PortableRemoteObject.narrow(mh2Obj, ManagementHome.class);
 	    System.out.println("mejb home 2 = " + mh2);
 	    Management m2 = mh2.create();
 	    System.out.println("mejb obj 2 = " + m2);
 
-	    Object mh3Obj = new InitialContext().lookup("java:global/mejb/MEJBBean!org.glassfish.admin.mejb.MEJBHome");
+
 	    System.out.println("mejb home obj 3 = " + mh3Obj);
 	    ManagementHome mh3 = (ManagementHome) PortableRemoteObject.narrow(mh3Obj, ManagementHome.class);
-	    System.out.println("mejb home 3 = " + mh3);
+
 	    Management m3 = mh3.create();
 	    System.out.println("mejb obj 3 = " + m3);
 
+	    **/
 
 	    Hello hello = (Hello) new InitialContext().lookup("java:global/" + appName + "/SingletonBean");
 
