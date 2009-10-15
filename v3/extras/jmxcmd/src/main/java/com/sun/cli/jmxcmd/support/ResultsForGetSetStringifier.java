5f3e34ff8a1fc87ed3fbee35a89a6143de5e07c2
Lloyd Chambers <llc@java.net>
2009-10-15 16:12:50 +0000

    removed useless comment
    svn path=/trunk/; revision=32794
    
    
    Former-commit-id: a4c080a6b4490aac26c0a8a428f569efdc97ab35

--- v3/extras/jmxcmd/src/main/java/com/sun/cli/jmxcmd/support/ResultsForGetSetStringifier.java
+++ v3/extras/jmxcmd/src/main/java/com/sun/cli/jmxcmd/support/ResultsForGetSetStringifier.java
@@ -2,13 +2,6 @@
  * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
- 
-/*
- * $Header: /m/jws/jmxcmd/src/com/sun/cli/jmxcmd/support/ResultsForGetSetStringifier.java,v 1.5 2004/04/26 07:29:39 llc Exp $
- * $Revision: 1.5 $
- * $Date: 2004/04/26 07:29:39 $
- */
- 
 
 package com.sun.cli.jmxcmd.support;
 
