51febbdbc22f497f8b90c3bde6f30f9b3359e658
Marina Vatkina <marina.vatkina@oracle.com>
2009-10-17 22:07:29 +0000

    Added test for issue 10341.
    svn path=/trunk/; revision=32905
    
    
    Former-commit-id: a55a36bc971a2299fb85d3b6e14fbde9323b8f8c

--- v2/appserv-tests/devtests/ejb/ejb31/timer31/schedule_ann/ejb/StlesEJB.java
+++ v2/appserv-tests/devtests/ejb/ejb31/timer31/schedule_ann/ejb/StlesEJB.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Stateless
 public class StlesEJB implements Stles {
@@ -22,12 +23,19 @@ public class StlesEJB implements Stles {
     private static Map<String, ScheduleExpression> hm = new HashMap<String, ScheduleExpression>();
     private static Set<String> callers = new HashSet<String>();
     private static Set<String> expected_callers = new HashSet<String>();
+    private static Set<String> errors = new HashSet<String>();
+
+    private static AtomicBoolean verifying = new AtomicBoolean(false);
 
     static {
 
         ScheduleExpression se = new ScheduleExpression().second("*/5").minute("*").hour("*");
         expected_callers.add("timer-5-sec");
         hm.put("timer-5-sec", se);
+
+        se = new ScheduleExpression().second("*").minute("*").hour("*");
+        expected_callers.add("timer-every-sec");
+        hm.put("timer-every-sec", se);
 
         se = new ScheduleExpression().second("2/2").minute(" *   ").hour("*").dayOfWeek("0-7");
         expected_callers.add("another-timer-dayOfWeek=0-7");
@@ -36,6 +44,14 @@ public class StlesEJB implements Stles {
         se = new ScheduleExpression().second("3/2").minute(" *   ").hour("12-11");
         expected_callers.add("timer-NO-ARG-hour=12-11");
         hm.put("timer-NO-ARG-hour=12-11", se);
+
+        se = new ScheduleExpression().second("*/2").minute(" *   ").hour("*");
+        expected_callers.add("dd_timer1");
+        hm.put("dd_timer1", se);
+
+        se = new ScheduleExpression().second("*/4").minute(" *   ").hour("*");
+        expected_callers.add("dd_timer2");
+        hm.put("dd_timer2", se);
 
         se = new ScheduleExpression().second("*").minute("*").hour("*").year("3000,2007, 4096");
         hm.put("timer-year=3000,2007, 4096", se);
@@ -56,14 +72,24 @@ public class StlesEJB implements Stles {
     }
 
     public void verifyTimers() {
-        Collection<Timer> ts = timerSvc.getTimers();
-        Set<String> errors = new HashSet<String>();
-        for(Timer t : ts) {
-            ScheduleExpression s = hm.remove("" + t.getInfo());
-            if (s == null) {
-                errors.add("" + t.getInfo());
+        verifying.set(true);
+
+        if (!errors.isEmpty()) {
+            StringBuffer sb = new StringBuffer();
+            for (String e : errors) {
+                sb.append("" + e).append(", ");
             }
+            throw new EJBException("Timers SHOULD NOT expire for infos: " + sb.toString() );
+        }
+
+        Collection<Timer> ts = timerSvc.getTimers();
+        for(Timer t : ts) {
+            String info = "" + t.getInfo();
             t.cancel();
+            ScheduleExpression s = hm.remove(info);
+            if (s == null) {
+                errors.add(info);
+            }
         }
         
         if (!expected_callers.isEmpty()) {
@@ -124,6 +150,12 @@ public class StlesEJB implements Stles {
         }
 
         ScheduleExpression tse = t.getSchedule();
+        if (tse == null) {
+            throw new IllegalStateException("Timer Schedule for " + info + " is NULL!");
+        }
+        if (se0 == null) {
+            throw new IllegalStateException("Unknown Schedule for " + info );
+        }
         if (!tse.getSecond().trim().equals(se0.getSecond().trim()) ) {
             throw new IllegalStateException("Timer " + info + " SECONDS is not as expected: " + tse.getSecond());
         }
@@ -164,6 +196,7 @@ public class StlesEJB implements Stles {
 
     @Schedules ({
         @Schedule(second="*/5", minute="*", hour="*", info="timer-5-sec"),
+        @Schedule(second="*", minute="*", hour="*", info="timer-every-sec"),
         @Schedule(second="*", minute="*", hour="*", year="3000,2007, 4096", info="timer-year=3000,2007, 4096"),
         @Schedule(second="*", minute="*", hour="*", dayOfMonth="Last", info="timer-dayOfMonth=Last"),
         @Schedule(second="*", minute="*", hour="*", dayOfMonth="Last Sun", info="timer-dayOfMonth=Last Sun"),
@@ -171,29 +204,49 @@ public class StlesEJB implements Stles {
         @Schedule(dayOfMonth="29", month="FEB", year="2007-2011, 2013-2015, 2017-2019", info="timer-02-29-non-leap-year")
     })
     private void timeout(Timer t) {
-        if (("" + t.getInfo()).startsWith("another")) {
+        if (("" + t.getInfo()).startsWith("another") || ("" + t.getInfo()).startsWith("dd")) {
+            errors.add("" + t.getInfo());
             throw new IllegalStateException("Timer " + t.getInfo() + " is wrong for this timeout");
         }
-        verifyTimer(t);
+        if (!verifying.get()) { // skip if we are verifying calls at this time
+            verifyTimer(t);
+        }
     }
 
     @Schedule(second="2/2", minute="*", hour="*", dayOfWeek="0-7", info="another-timer-dayOfWeek=0-7")
     private void anothertimeout(Timer t) {
         if (!("" + t.getInfo()).startsWith("another")) {
+            errors.add("" + t.getInfo());
             throw new IllegalStateException("Timer " + t.getInfo() + " is wrong for this timeout");
         }
-        verifyTimer(t);
+        if (!verifying.get()) { // skip if we are verifying calls at this time
+            verifyTimer(t);
+        }
     }
 
     @Schedule(second="3/2", minute=" *   ", hour="12-11", info="timer-NO-ARG-hour=12-11")
     private void timeout() {
 
         System.out.println("in StlesEJB:NO-ARG timeout "  );
-        ScheduleExpression se = hm.get("timer-NO-ARG-hour=12-11");
-        if (se != null) {
-            callers.add("timer-NO-ARG-hour=12-11");
-            expected_callers.remove("timer-NO-ARG-hour=12-11");
+        if (!verifying.get()) { // skip if we are verifying calls at this time
+            ScheduleExpression se = hm.get("timer-NO-ARG-hour=12-11");
+            if (se != null) {
+                callers.add("timer-NO-ARG-hour=12-11");
+                expected_callers.remove("timer-NO-ARG-hour=12-11");
+            }
         }
 
     }
+
+    @Schedule(second="4/2", minute="*", hour="*", dayOfWeek="0-7", info="overridden-timer")
+    private void ddTimeout(Timer t) {
+        if (!("" + t.getInfo()).startsWith("dd_timer")) {
+            errors.add("" + t.getInfo());
+            throw new IllegalStateException("Timer " + t.getInfo() + " is wrong for this timeout");
+        }
+        if (!verifying.get()) { // skip if we are verifying calls at this time
+            verifyTimer(t);
+        }
+    }
+
 }
