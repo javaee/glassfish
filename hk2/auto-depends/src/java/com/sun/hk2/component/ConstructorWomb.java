6b9c22165baea9d319f2a638d1189b6686cb7b55
dochez <dochez@java.net>
2009-10-19 17:57:34 +0000

    removed Extract capability

--- hk2/auto-depends/src/java/com/sun/hk2/component/ConstructorWomb.java
+++ hk2/auto-depends/src/java/com/sun/hk2/component/ConstructorWomb.java
@@ -36,7 +36,6 @@
  */
 package com.sun.hk2.component;
 
-import org.jvnet.hk2.annotations.Extract;
 import org.jvnet.hk2.annotations.Scoped;
 import org.jvnet.hk2.component.ComponentException;
 import org.jvnet.hk2.component.Habitat;
@@ -46,9 +45,6 @@ import org.jvnet.hk2.component.PerLookup;
 import org.jvnet.hk2.component.Scope;
 import org.jvnet.hk2.component.Singleton;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 
@@ -93,121 +89,6 @@ public class ConstructorWomb<T> extends AbstractWombImpl<T> {
             }
         });
 
-        if(si!=null)
-            // extraction amounts to no-op if this is prototype scope. so skip that.
-            extract(t, si);
-
-    }
-
-    /**
-     * Extracts resources identified with {@link org.jvnet.hk2.annotations.Extract} annotations
-     * into the given scope.
-     *
-     * <p>
-     * This method is for the use within HK2, and not really meant
-     * to be used by application code.
-     *
-     * @param component the component we should extract resources from
-     * @throws ComponentException if the resource extract fail
-     */
-    public void extract(Object component, ScopeInstance si) throws ComponentException {
-        Class<?> currentClass = component.getClass();
-        while (!currentClass.equals(Object.class)) {
-            for (Field field : currentClass.getDeclaredFields()) {
-                Extract extract = field.getAnnotation(Extract.class);
-                if (extract == null)    continue;
-
-                try {
-                    field.setAccessible(true);
-                    Object value = field.get(component);
-                    Class<?> type = field.getType();
-
-//                    if (LOGGER.isLoggable(Level.FINER)) {
-//                        LOGGER.log(Level.FINER, "Extracting resource " + value + " returned from " + field);
-//                    }
-                    if (value!=null) {
-                        extractValue(value, si, extract.name(), type);
-                    } else {
-//                        if (LOGGER.isLoggable(Level.FINE)) {
-//                            LOGGER.log(Level.FINE, "Resource returned from " + field + " is null");
-//                        }
-                    }
-                } catch (IllegalArgumentException ex) {
-                    throw new ComponentException("Extraction failed on " + field, ex);
-
-                } catch (IllegalAccessException ex) {
-                    throw new ComponentException("Extraction failed on " + field, ex);
-                }
-            }
-            for (Method method : currentClass.getDeclaredMethods()) {
-                Extract extract = method.getAnnotation(Extract.class);
-                if (extract == null)    continue;
-
-                Class<?> type = method.getReturnType();
-                if (type == null) {
-                    throw new ComponentException("Extraction failed : %s has a void return type",method);
-                }
-                if (method.getParameterTypes().length > 0) {
-                    throw new ComponentException("Extraction failed : %s takes parameters, it should not",method);
-                }
-                try {
-                    method.setAccessible(true);
-                    Object value = method.invoke(component);
-//                    if (LOGGER.isLoggable(Level.FINER)) {
-//                        LOGGER.log(Level.FINER, "Extracting resource " + value + " returned from " + method);
-//                    }
-                    if (value!=null) {
-                        extractValue(value, si, extract.name(), type);
-                    } else {
-//                        if (LOGGER.isLoggable(Level.FINE)) {
-//                            LOGGER.log(Level.FINE, "Resource returned from " + method + " is null");
-//                        }
-                    }
-                } catch (IllegalArgumentException ex) {
-                    throw new ComponentException("Extraction failed on " + method.toGenericString(), ex);
-                } catch (InvocationTargetException ex) {
-                    throw new ComponentException("Extraction failed on " + method.toGenericString(), ex);
-                } catch (IllegalAccessException ex) {
-                    throw new ComponentException("Extraction failed on " + method.toGenericString(), ex);
-                }
-            }
-            currentClass = currentClass.getSuperclass();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private void extractValue(Object value, ScopeInstance si, String name,  Class type) {
-        if(value instanceof Iterable) {
-            for (Object o : (Iterable)value) {
-                extractSingleValue(o,name, type, si);
-            }
-        } else
-        if (type.isArray()) {
-            Object[] values = (Object[]) value;
-            for (Object o : values) {
-                extractSingleValue(o,name, type, si);
-            }
-        } else {
-            extractSingleValue(value, name, type, si);
-        }
-    }
-
-    private <T> void extractSingleValue(T o, String name, Class type, final ScopeInstance si) {
-        // TODO: name support. Wouldn't it be nice if Map<String,Object> extracts to named objects?
-        // or recognize the "Named" interface?
-        Inhabitant<T> i = new AbstractWombImpl<T>((Class<T>)o.getClass(),MultiMap.<String,String>emptyMap()) {
-            public T create(Inhabitant onBehalfOf) throws ComponentException {
-                // only look at objects already available in the scope.
-                // TODO: this obviously can return null. think about how to reconcile this semantics with the rest
-                return si.get(this);
-            }
-        };
-        if (name==null) {
-            habitat.add(i);
-        } else {
-            habitat.addIndex(i, type.getName(), name);
-        }
-        si.put(i,o);
     }
 
     /**
