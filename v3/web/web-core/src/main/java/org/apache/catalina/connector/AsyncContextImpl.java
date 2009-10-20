3d9f50a14bcbd2df798a7bc8d11dc4ec6b91051a
Jan Luehe <jluehe@java.net>
2009-10-20 17:07:49 +0000

    Fixed async-context-multiple-dispatch unit test failure
    svn path=/trunk/; revision=32998
    
    
    Former-commit-id: d994c9bbeb2a951431947e89df0a0dae0c8d8c52

--- v3/web/web-core/src/main/java/org/apache/catalina/connector/AsyncContextImpl.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/connector/AsyncContextImpl.java
@@ -316,7 +316,6 @@ public class AsyncContextImpl implements AsyncContext {
         this.isOriginalRequestAndResponse = isOriginalRequestAndResponse;
         isDispatchInProgress.set(false);
         setOkToConfigure(true);
-        setTimeout(DEFAULT_ASYNC_TIMEOUT_MILLIS);
         notifyAsyncListeners(AsyncEventType.START_ASYNC, null);
         if (isOriginalRequestAndResponse) {
             zeroArgDispatchTarget = getZeroArgDispatchTarget(origRequest);
