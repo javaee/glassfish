96d429951ab3dd998092d9e3ddb1c3afe4e71df7
Jan Luehe <jluehe@java.net>
2009-10-22 00:58:47 +0000

    Fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=10395 ("contextPath for default-web-app is correct in servlet, wrong when forwarded to jsp")
    Tests run: QL
    
    svn path=/trunk/; revision=33111
    
    
    Former-commit-id: 7f096c0e35ab425b44e147382ae4e6947b02f224

--- v3/web/web-core/src/main/java/org/apache/catalina/core/ApplicationDispatcher.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/core/ApplicationDispatcher.java
@@ -430,7 +430,6 @@ public final class ApplicationDispatcher
         else {
             ApplicationHttpRequest wrequest = (ApplicationHttpRequest)
                 wrapRequest(state);
-            String contextPath = context.getPath();
 
             // If the request is being FORWARD- or ASYNC-dispatched for 
             // the first time, initialize it with the required request
@@ -447,8 +446,17 @@ public final class ApplicationDispatcher
                                                hrequest.getPathInfo(),
                                                hrequest.getQueryString());
             }
- 
-            wrequest.setContextPath(contextPath);
+
+            String targetContextPath = context.getPath();
+            // START IT 10395
+            RequestFacade requestFacade = wrequest.getRequestFacade();
+            String originContextPath = requestFacade.getContextPath(false);
+            if (originContextPath != null &&
+                    originContextPath.equals(targetContextPath)) {
+                targetContextPath = hrequest.getContextPath();
+            }
+            // END IT 10395
+            wrequest.setContextPath(targetContextPath);
             wrequest.setRequestURI(requestURI);
             wrequest.setServletPath(servletPath);
             wrequest.setPathInfo(pathInfo);
@@ -461,7 +469,6 @@ public final class ApplicationDispatcher
 
             wrequest.recycle();
             unwrapRequest(state);
-
         }
     }
 
