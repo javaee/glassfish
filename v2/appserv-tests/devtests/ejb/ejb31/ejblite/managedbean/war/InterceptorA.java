ed951461f57d2691b6be883ae1385c73fb2cb18c
Ken Saks <ksak@java.net>
2009-10-22 22:10:47 +0000

    added @AroundInvoke in interceptor super class
    svn path=/trunk/; revision=33174
    
    
    Former-commit-id: cd86b0b576caa850312ea1d75ca9bbcde83e77a5

--- v2/appserv-tests/devtests/ejb/ejb31/ejblite/managedbean/war/InterceptorA.java
+++ v2/appserv-tests/devtests/ejb/ejb31/ejblite/managedbean/war/InterceptorA.java
@@ -13,7 +13,7 @@ import javax.interceptor.InvocationContext;
 import javax.interceptor.AroundInvoke;
 
 
-public class InterceptorA {
+public class InterceptorA extends InterceptorSuper {
 
 
     @PostConstruct
