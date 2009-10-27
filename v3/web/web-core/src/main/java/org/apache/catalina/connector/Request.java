ffbd3154d78641575e7076134239ef3b57c6b37b
Jean francois Arcand <jfarcand@java.net>
2009-10-27 21:02:34 +0000

    Fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=10008 ("AsyncListener.onError must be invoked when the client close the connection")
    Invoke the AsyncListener.onError() when the remote client close the connection
    
    + All QL passed
    + Tested with atmosphere-pubsub-0.4.war
    
    
    
    svn path=/trunk/; revision=33413
    
    
    Former-commit-id: 7b92fbf9a969d9c3d68637931251521c6641f74b

--- v3/web/web-core/src/main/java/org/apache/catalina/connector/Request.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/connector/Request.java
@@ -127,6 +127,9 @@ import org.apache.catalina.fileupload.Multipart;
 import com.sun.appserv.ProxyHandler;
 // END S1AS 6170450
 import com.sun.enterprise.security.integration.RealmInitializer;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
 import org.apache.catalina.authenticator.AuthenticatorBase;
 import org.apache.catalina.deploy.LoginConfig;
 
@@ -205,6 +208,11 @@ public class Request
         };
     }
     // ----------------------------------------------------- Instance Variables
+
+    // Is the remote browser closed the connection.
+    protected boolean clientClosedConnection = false;
+
+
     /**
      * The set of cookies associated with this Request.
      */
@@ -457,6 +465,10 @@ public class Request
     private String servletPath;
     private String pathInfo;
 
+    // Allow Grizzly to auto detect a remote close connection.
+    public final static boolean discardDisconnectEvent =
+            Boolean.getBoolean("com.sun.grizzly.discardDisconnect");
+
     // ----------------------------------------------------------- Constructor
     public Request() {
         // START OF SJSAS 6231069
@@ -496,6 +508,7 @@ public class Request
      * Release all object references, and initialize instance variables, in
      * preparation for reuse of this object.
      */
+    @Override
     public void recycle() {
 
         if (isAsyncStarted()) {
@@ -592,6 +605,7 @@ public class Request
         isAsyncSupported = true;
         asyncStarted.set(false);
         isAsyncComplete = false;
+        clientClosedConnection = false;
     }
 
     // -------------------------------------------------------- Request Methods
@@ -3598,7 +3612,14 @@ public class Request
 
                         @Override
                         public void cancelled(Request attachment) {
-                            attachment.asyncTimeout();
+                            if (attachment.clientClosedConnection &&
+                                    attachment.asyncContext != null) {
+                                attachment.asyncContext.notifyAsyncListeners(
+                                            AsyncContextImpl.AsyncEventType.ERROR,
+                                            null);
+                           } else {
+                                attachment.asyncTimeout();
+                            }
                         }
                     };
 
@@ -3845,6 +3866,28 @@ public class Request
                 log.log(Level.FINE, "RequestAttachement.resume: " + res);
             }
             completeProcessing();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        //@Override
+        public void handleSelectedKey(SelectionKey selectionKey) {
+            if (!selectionKey.isValid() || discardDisconnectEvent){
+                selectionKey.cancel();
+                return;
+            }
+            try {
+                ((Request)getAttachment()).clientClosedConnection = ((SocketChannel)selectionKey.channel()).
+                    read(ByteBuffer.allocate(1)) == -1;
+            } catch (IOException ex) {
+
+            } finally{
+                if (((Request)getAttachment()).clientClosedConnection){
+                   selectionKey.cancel();
+                   getCompletionHandler().cancelled(getAttachment());
+                }
+            }
         }
 
         void completeProcessing() {
