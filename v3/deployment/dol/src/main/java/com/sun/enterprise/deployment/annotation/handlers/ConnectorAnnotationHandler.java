b1b36c4200d2960abc01ce8505207a259657de42
Jagadish Ramu <jagadish.ramu@oracle.com>
2009-10-29 12:23:07 +0000

    Issue : 10635 Description : @AuthenticationMechanism annotations not processed
    Tests Run : QL (Web, Classic), connector-dev, connector-standalone-cts (Web, Classic)
    
    
    svn path=/trunk/; revision=33497
    
    
    Former-commit-id: efe6e657153a997e9b045a92faa73e5ac0e53202

--- v3/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/handlers/ConnectorAnnotationHandler.java
+++ v3/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/handlers/ConnectorAnnotationHandler.java
@@ -179,11 +179,12 @@ public class ConnectorAnnotationHandler extends AbstractHandler  {
                 if (!ignore) {
                     String credentialInterfaceName = ora.getCredentialInterfaceName(auth.credentialInterface());
                     //XXX: Siva: For now use the first provided description
+                    String description = "";
                     if(auth.description().length > 0){
-                        AuthMechanism authM = new AuthMechanism(auth.description()[0], authMechInt,
-                            credentialInterfaceName);
-                        ora.addAuthMechanism(authM);
+                        description = auth.description()[0];
                     }
+                    AuthMechanism authM = new AuthMechanism(description, authMechInt, credentialInterfaceName);
+                    ora.addAuthMechanism(authM);
                 }
             }
         }
