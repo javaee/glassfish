6cd8f1012762d7fa31287cf258b56313463c7a09
Siraj Ghaffar <siraj.ghaffar@oracle.com>
2009-10-30 13:14:01 +0000

    add all containers to the server
    svn path=/trunk/; revision=33559
    
    
    Former-commit-id: ecf8c68ca339ed4ab1f6d903e98aade00a43a60a

--- v3/tests/embedded/wrapper/src/main/java/org/glassfish/tests/embedded/EmbeddedMain.java
+++ v3/tests/embedded/wrapper/src/main/java/org/glassfish/tests/embedded/EmbeddedMain.java
@@ -2,14 +2,16 @@ package org.glassfish.tests.embedded;
 
 import org.glassfish.api.embedded.*;
 
-import org.glassfish.api.container.Sniffer;
-import java.util.*;
 import java.io.File;
-import java.util.logging.Logger;
+
+
+/** A 'wrapper' class that is used by QA to run tests on embedded. This class is used to start the domain
+ * in embedded mode from asadmin cli
+ *
+ */
 
 public class EmbeddedMain {
     public static void main(String[] args) {
-
 
        String installRoot = System.getenv("S1AS_HOME");
        if (installRoot == null) {
@@ -18,24 +20,22 @@ public class EmbeddedMain {
        }
        String instanceRoot = installRoot + "/domains/domain1";
        String domainXml = instanceRoot + "/config/domain.xml";
+
+        System.setProperty("com.sun.aas.instanceRootURI", "file:" + instanceRoot);
+        //System.setProperty("com.sun.aas.installRoot", installRoot );
+//        System.setProperty("com.sun.aas.instanceRoot", instanceRoot );
+
        EmbeddedFileSystem efs =
           new EmbeddedFileSystem.Builder().
-          setInstallRoot(new File(installRoot), true).
-          setInstanceRoot(new File(instanceRoot)).
-          setConfigurationFile(new File(domainXml)).
-          build();
+          installRoot(new File(installRoot), true).
+          instanceRoot(new File(instanceRoot)).
+          configurationFile(new File(domainXml), false).autoDelete(false).build();
 
         Server.Builder builder = new Server.Builder("asadmin");
-        //.logFile(new File("/export/home/log.log")).logger(true);
-        Server server = builder.setEmbeddedFileSystem(efs).build();
 
-        System.setProperty("com.sun.aas.instanceRootURI", instanceRoot + "/applications");
-
+        Server server = builder.embeddedFileSystem(efs).build();
         server.addContainer(ContainerBuilder.Type.all);
-
-        ArrayList<Sniffer> sniffers = new ArrayList<Sniffer>();
-        for (EmbeddedContainer c : server.getContainers()) {
-            sniffers.addAll(c.getSniffers());
-        }
     }
 }
+
+
