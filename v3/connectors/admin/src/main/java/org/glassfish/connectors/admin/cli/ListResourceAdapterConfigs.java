ac09fade3192320d7c8f49a29524fdfb1d0e6b1b
Jennifer Galloway <jc129909>
2009-11-04 21:58:56 +0000

    Issue 10338 - list-resource-adapter-configs missing --raname option - also add --verbose option to display properties
    svn path=/trunk/; revision=33833
    
    
    Former-commit-id: 7f8b068bd29d65bd7ead0bf9225ed50621092895

--- v3/connectors/admin/src/main/java/org/glassfish/connectors/admin/cli/ListResourceAdapterConfigs.java
+++ v3/connectors/admin/src/main/java/org/glassfish/connectors/admin/cli/ListResourceAdapterConfigs.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -38,6 +38,7 @@ package org.glassfish.connectors.admin.cli;
 import org.glassfish.api.admin.AdminCommand;
 import org.glassfish.api.admin.AdminCommandContext;
 import org.glassfish.api.I18n;
+import org.glassfish.api.Param;
 import org.glassfish.api.ActionReport;
 import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.annotations.Scoped;
@@ -46,7 +47,12 @@ import org.jvnet.hk2.component.PerLookup;
 import com.sun.enterprise.config.serverbeans.ResourceAdapterConfig;
 import com.sun.enterprise.util.LocalStringManagerImpl;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jvnet.hk2.config.types.Property;
 
 
 /**
@@ -59,6 +65,12 @@ import java.util.ArrayList;
 public class ListResourceAdapterConfigs implements AdminCommand {
 
     final private static LocalStringManagerImpl localStrings = new LocalStringManagerImpl(ListResourceAdapterConfigs.class);    
+
+    @Param(name="raname", optional=true)
+    String raName;
+
+    @Param(name="verbose", optional=true, defaultValue="false")
+    Boolean verbose;
 
     @Inject
     ResourceAdapterConfig[] resources;
@@ -74,23 +86,55 @@ public class ListResourceAdapterConfigs implements AdminCommand {
         final ActionReport report = context.getActionReport();
 
         try {
-            ArrayList<String> list = new ArrayList();
+            HashMap<String, List<Property>> raMap = new HashMap();
+            boolean raExists = false;
             for (ResourceAdapterConfig r : resources) {
-                list.add(r.getResourceAdapterName());
+                if (raName != null && !raName.isEmpty()) {
+                    if (r.getResourceAdapterName().equals(raName)) {
+                        raMap.put(raName, r.getProperty());
+                        raExists = true;
+                        break;
+                    }
+                } else {
+                    raMap.put(r.getResourceAdapterName(), r.getProperty());
+                }
             }
-            if (list.isEmpty()) {
+            if (raName != null && !raName.isEmpty() && !raExists) {
+                report.setMessage(localStrings.getLocalString("delete.resource.adapter.config.notfound",
+                        "Resource adapter {0} not found.", raName));
+                report.setActionExitCode(ActionReport.ExitCode.FAILURE);
+                return;
+            }
+            if (raMap.isEmpty()) {
                 final ActionReport.MessagePart part = report.getTopMessagePart().addChild();
                 part.setMessage(localStrings.getLocalString("nothingToList",
                     "Nothing to list."));
             } else {
-                for (String jndiName : list) {
-                    final ActionReport.MessagePart part = report.getTopMessagePart().addChild();
-                    part.setMessage(jndiName);
+                /**
+                  get the properties if verbose=true. Otherwise return the name.
+                 */
+                if (verbose) {
+                    for (Entry<String, List<Property>> raEntry : raMap.entrySet()) {
+                        final ActionReport.MessagePart part = report.getTopMessagePart().addChild();
+                        part.setMessage(raEntry.getKey());
+                        for (Property prop : raEntry.getValue()) {
+                            final ActionReport.MessagePart propPart = part.addChild();
+                            propPart.setMessage("\t" + prop.getName() + "=" + prop.getValue());
+                        }
+                    }
+                } else {
+                    for (String ra : raMap.keySet()) {
+                        final ActionReport.MessagePart part = report.getTopMessagePart().addChild();
+                        part.setMessage(ra);
+                    }
                 }
             }
         } catch (Exception e) {
-            report.setMessage(localStrings.getLocalString("list.resource.adapter.config.fail",
-                    "Unable to list resource adapter configs") + " " + e.getLocalizedMessage());
+            String failMsg = localStrings.getLocalString("list.resource.adapter.configs.fail",
+                    "Unable to list resource adapter configs.");
+            Logger.getLogger(ListResourceAdapterConfigs.class.getName()).log(Level.SEVERE,
+                    failMsg, e);
+            report.setMessage(failMsg + " " + e.getLocalizedMessage());
             report.setActionExitCode(ActionReport.ExitCode.FAILURE);
             report.setFailureCause(e);
             return;
