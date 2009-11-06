7ce58133c48fd227c4644f749c3603b43e796398
Shalini Muthukrishnan <sm157516@java.net>
2009-11-06 15:33:45 +0000

    Adding the dmmcf tests again and updating the result count.
    svn path=/trunk/; revision=33994
    
    
    Former-commit-id: ed4316b2bd6a4f564735ad0662ce676fc48f074f

--- v2/appserv-tests/devtests/jdbc/resultCount.sh
+++ v2/appserv-tests/devtests/jdbc/resultCount.sh
@@ -3,7 +3,7 @@
 FILE=$APS_HOME/test_resultsValid.xml
 echo "input file=$FILE"
 
-TOTAL=111
+TOTAL=119
 PASSED=`grep "pass" $FILE | wc -l`
 FAILED=`grep "fail" $FILE | wc -l`
 TOTAL_RUN=`expr $PASSED + $FAILED `
