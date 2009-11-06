52e98cd94abfe47a76768c00036567f0f3cb4545
Rama Pulavarthi <ramapulavarthi@java.net>
2009-11-06 22:46:45 +0000

    GLASSFISH-10849: Remove the quotes around %* to avoid sending all the args as single arg. Seems like these scripts are not tested on windows so far.
    svn path=/trunk/; revision=34029
    
    
    Former-commit-id: 6336411743e847f9ddb22854baee44863304874b

--- v3/webservices/webservices-scripts/src/main/resources/glassfish/bin/wscompile.bat
+++ v3/webservices/webservices-scripts/src/main/resources/glassfish/bin/wscompile.bat
@@ -5,4 +5,4 @@ REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
 REM
 
-java -Djava.endorsed.dirs="%~dp0..\modules\endorsed" -cp "%~dp0..\modules\webservices-osgi.jar;%~dp0..\modules\jaxb-osgi.jar;%~dp0..\modules\javax.mail.jar;%JAVA_HOME%/lib/tools.jar" com.sun.xml.rpc.tools.wscompile.Main "%*"
+java -Djava.endorsed.dirs="%~dp0..\modules\endorsed" -cp "%~dp0..\modules\webservices-osgi.jar;%~dp0..\modules\jaxb-osgi.jar;%~dp0..\modules\javax.mail.jar;%JAVA_HOME%/lib/tools.jar" com.sun.xml.rpc.tools.wscompile.Main %*
--- v3/webservices/webservices-scripts/src/main/resources/glassfish/bin/xjc.bat
+++ v3/webservices/webservices-scripts/src/main/resources/glassfish/bin/xjc.bat
@@ -5,4 +5,4 @@ REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
 REM
 
-java -Djava.endorsed.dirs="%~dp0..\modules\endorsed" -cp "%~dp0..\modules\webservices-osgi.jar;%~dp0..\modules\jaxb-osgi.jar" com.sun.tools.xjc.Driver "%*"
+java -Djava.endorsed.dirs="%~dp0..\modules\endorsed" -cp "%~dp0..\modules\webservices-osgi.jar;%~dp0..\modules\jaxb-osgi.jar" com.sun.tools.xjc.Driver %*
