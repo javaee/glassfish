0dd9192c5f68b586c9b25a515912833128ef5035
Roger Kitain <roger.kitain@oracle.com>
2009-11-11 00:40:46 +0000

    serialize InerceptorB
    svn path=/trunk/; revision=34248
    
    
    Former-commit-id: 5a72ed63e345fbac5f08b9e5fe5ab8239b801408

--- v3/tests/quicklook/weld/numberguess/src/java/numberguess/InterceptorB.java
+++ v3/tests/quicklook/weld/numberguess/src/java/numberguess/InterceptorB.java
@@ -1,5 +1,7 @@
 package numberguess;
 
+import java.io.Serializable;
+
 import javax.interceptor.*;
 import javax.annotation.*;
 
@@ -8,7 +10,7 @@ import javax.ejb.EJBException;
 
 import javax.inject.Inject;
 
-public class InterceptorB {
+public class InterceptorB implements Serializable{
 
     @EJB StatelessLocal sbean;
 
