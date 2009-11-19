487f50ee151cb463945c1b842d4f1ec1e76842ce
Shalini Muthukrishnan <sm157516@java.net>
2009-11-19 12:42:49 +0000

    Modifying the markconnectionasbad.xa test to get the monitoring values from amx mbeans using the new framework.
    svn path=/trunk/; revision=34530
    
    
    Former-commit-id: 4fecb866fa50249070fb18e79203f90b9138d624

--- v2/appserv-tests/devtests/jdbc/markconnectionasbad.xa/client/Client.java
+++ v2/appserv-tests/devtests/jdbc/markconnectionasbad.xa/client/Client.java
@@ -3,25 +3,20 @@ package com.sun.s1asdev.jdbc.markconnectionasbad.xa.client;
 import javax.naming.*;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
 
 import com.sun.s1asdev.jdbc.markconnectionasbad.xa.ejb.SimpleBMPHome;
 import com.sun.s1asdev.jdbc.markconnectionasbad.xa.ejb.SimpleBMP;
 import com.sun.ejte.ccl.reporter.SimpleReporterAdapter;
-import com.sun.appserv.management.client.AppserverConnectionSource;
-import com.sun.appserv.management.DomainRoot;
-import com.sun.appserv.management.config.ConfigConfig;
-import com.sun.appserv.management.config.ModuleMonitoringLevelsConfig;
-import com.sun.appserv.management.config.ModuleMonitoringLevelValues;
+import java.util.Collection;
 
 public class Client {
 
     SimpleReporterAdapter stat = new SimpleReporterAdapter();
-    public static final String NUM_CON_DESTROYED_COUNT = "numconndestroyed-count";
-    public static final String USER_PASSWORD = "adminadmin";
+    public static final String NUM_CON_DESTROYED_COUNT = "numconndestroyed";
     public static final int JMX_PORT = 8686;
-    public static final String DOTTED_NAME_GET = "dottedNameGet";
-    public static final String USER = "admin";
-    public static final String CONFIG = "server-config";
     public static final String HOST_NAME = "localhost";
 
 
@@ -43,94 +38,78 @@ public class Client {
         SimpleBMP simpleBMP = simpleBMPHome.create();
         stat.addDescription("Mark-Connection-As-Bad  ");
 
-        if (simpleBMP.test1() && getMonitorablePropertyOfConnectionPool("jdbc-unshareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 5) {
+        if (simpleBMP.test1() && getMonitorablePropertyOfConnectionPool("jdbc-unshareable-pool") == 5) {
             stat.addStatus(" Mark-Connection-As-Bad [XA - NoTx - UnShareable - ReadOnly] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [XA - NoTx - UnShareable - ReadOnly] : ", stat.FAIL);
         }
 
 
-        if (simpleBMP.test2() && getMonitorablePropertyOfConnectionPool("jdbc-unshareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 10) {
+        if (simpleBMP.test2() && getMonitorablePropertyOfConnectionPool("jdbc-unshareable-pool") == 10) {
             stat.addStatus(" Mark-Connection-As-Bad [XA - NoTx - UnShareable - Write] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [XA - NoTx - UnShareable - Write] : ", stat.FAIL);
         }
 
-        if (simpleBMP.test3() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 5) {
+        if (simpleBMP.test3() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool") == 5) {
             stat.addStatus(" Mark-Connection-As-Bad [XA - NoTx - Shareable - ReadOnly] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [XA - NoTx - Shareable - ReadOnly] : ", stat.FAIL);
         }
 
-        if (simpleBMP.test4() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 10) {
+        if (simpleBMP.test4() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool") == 10) {
             stat.addStatus(" Mark-Connection-As-Bad [XA - NoTx - Shareable - Write] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [XA - NoTx - Shareable - Write] : ", stat.FAIL);
         }
 
-        if (simpleBMP.test5() && getMonitorablePropertyOfConnectionPool("jdbc-unshareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 15) {
+        if (simpleBMP.test5() && getMonitorablePropertyOfConnectionPool("jdbc-unshareable-pool") == 15) {
             stat.addStatus(" Mark-Connection-As-Bad [XA - Tx - UnShareable - ReadOnly] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [XA - Tx - UnShareable - ReadOnly] : ", stat.FAIL);
         }
 
-        if (simpleBMP.test6() && getMonitorablePropertyOfConnectionPool("jdbc-unshareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 20) {
+        if (simpleBMP.test6() && getMonitorablePropertyOfConnectionPool("jdbc-unshareable-pool") == 20) {
             stat.addStatus(" Mark-Connection-As-Bad [XA - Tx - UnShareable - Write] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [XA - Tx - UnShareable - Write] : ", stat.FAIL);
         }
 
-        if (simpleBMP.test7() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 11) {
+        if (simpleBMP.test7() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool") == 11) {
             stat.addStatus(" Mark-Connection-As-Bad [XA - Tx - Shareable - ReadOnly] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [XA - Tx - Shareable - ReadOnly] : ", stat.FAIL);
         }
 
-        if (simpleBMP.test8() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 12) {
+        if (simpleBMP.test8() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool") == 12) {
             stat.addStatus(" Mark-Connection-As-Bad [XA - Tx - Shareable - Write] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [XA - Tx - Shareable - Write] : ", stat.FAIL);
         }
 
-        if (simpleBMP.test9() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 13 &&
-                getMonitorablePropertyOfConnectionPool("jdbc-local-pool",
-                        NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 1) {
+        if (simpleBMP.test9() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool") == 13 &&
+                getMonitorablePropertyOfConnectionPool("jdbc-local-pool") == 1) {
             stat.addStatus(" Mark-Connection-As-Bad [Local-XA -Shareable-Shareable - Write-Write] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [Local-XA -Shareable-Shareable - Write-Write] : ", stat.FAIL);
         }
 
-        if (simpleBMP.test10() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 14 &&
-                getMonitorablePropertyOfConnectionPool("jdbc-local-pool",
-                        NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG) == 2) {
+        if (simpleBMP.test10() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool") == 14 &&
+                getMonitorablePropertyOfConnectionPool("jdbc-local-pool") == 2) {
             stat.addStatus(" Mark-Connection-As-Bad [Local-XA -Shareable-Shareable - Read-Write] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [Local-XA -Shareable-Shareable - Read-Write] : ", stat.FAIL);
         }
 
-         if (simpleBMP.test11() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG )==15 &&
-                 getMonitorablePropertyOfConnectionPool("jdbc-local-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG )==3) {
+         if (simpleBMP.test11() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool") == 15 && 
+                 getMonitorablePropertyOfConnectionPool("jdbc-local-pool") == 3) {
             stat.addStatus(" Mark-Connection-As-Bad [Local-XA -Shareable-Shareable - Read-Read] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [Local-XA -Shareable-Shareable - Read-Read] : ", stat.FAIL);
         }
 
-         if (simpleBMP.test12() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG )==16 &&
-                 getMonitorablePropertyOfConnectionPool("jdbc-local-pool",
-                NUM_CON_DESTROYED_COUNT, HOST_NAME, JMX_PORT, USER, USER_PASSWORD, CONFIG )==4) {
+         if (simpleBMP.test12() && getMonitorablePropertyOfConnectionPool("jdbc-shareable-pool") == 16 && 
+                 getMonitorablePropertyOfConnectionPool("jdbc-local-pool") == 4) {
             stat.addStatus(" Mark-Connection-As-Bad [Local-XA -Shareable-Shareable - Write-Read] : ", stat.PASS);
         } else {
             stat.addStatus(" Mark-Connection-As-Bad [Local-XA -Shareable-Shareable - Write-Read] : ", stat.FAIL);
@@ -142,30 +121,21 @@ public class Client {
     }
 
 
-    public int getMonitorablePropertyOfConnectionPool(String poolName, String property, String hostName, int JMX_PORT, String user, String password, String configName) throws Exception {
-        AppserverConnectionSource appserver =
-                new AppserverConnectionSource(AppserverConnectionSource.PROTOCOL_RMI, hostName, JMX_PORT, user, password, null);
-        MBeanServerConnection connection = appserver.getJMXConnector(false).getMBeanServerConnection();
-        //System.out.println("Connected to JMX");
+    public int getMonitorablePropertyOfConnectionPool(String poolName) throws Exception {
 
-        DomainRoot domainRoot = appserver.getDomainRoot();
+	final String urlStr = "service:jmx:rmi:///jndi/rmi://" + HOST_NAME + ":" + JMX_PORT + "/jmxrmi";    
+        final JMXServiceURL url = new JMXServiceURL(urlStr);
 
-        final ConfigConfig config =
-                domainRoot.getDomainConfig().getConfigConfigMap().get(configName);
-
-        final ModuleMonitoringLevelsConfig mon = config.getMonitoringServiceConfig().getModuleMonitoringLevelsConfig();
-
-        mon.setJDBCConnectionPool(ModuleMonitoringLevelValues.HIGH);
+	final JMXConnector jmxConn = JMXConnectorFactory.connect(url);
+	final MBeanServerConnection connection = jmxConn.getMBeanServerConnection();
 
         ObjectName objectName =
-                new ObjectName("amx:j2eeType=X-MonitoringDottedNames,name=na");
+                new ObjectName("amx:pp=/mon/server-mon[server],type=jdbc-connection-pool-mon,name=resources/" + poolName);
 
-        String params[] = new String[]{String.class.getName()};
-        Object values[] = new Object[]{"server.resources." + poolName + "." + property};
-        javax.management.Attribute returnValue = (javax.management.Attribute) connection.invoke(objectName, DOTTED_NAME_GET, values, params);
+	javax.management.openmbean.CompositeDataSupport returnValue = 
+		(javax.management.openmbean.CompositeDataSupport) 
+		connection.getAttribute(objectName, NUM_CON_DESTROYED_COUNT);
 
-        System.out.println(DOTTED_NAME_GET + " : " + returnValue.getName() + " : " + returnValue.getValue());
-
-        return new Integer(returnValue.getValue().toString());
+        return new Integer(returnValue.get("count").toString());
     }
 }
