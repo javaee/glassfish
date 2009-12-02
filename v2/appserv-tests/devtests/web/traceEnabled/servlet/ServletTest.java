124defbe8d9f4401462fbc55c89e35766e4d052e
Jan Luehe <jluehe@java.net>
2009-12-02 19:38:51 +0000

    Removed redundant code
    svn path=/trunk/; revision=34852
    
    
    Former-commit-id: 4ba69960691f9ae77e1ebeb2d3d5e770954f647f

--- v2/appserv-tests/devtests/web/traceEnabled/servlet/ServletTest.java
+++ v2/appserv-tests/devtests/web/traceEnabled/servlet/ServletTest.java
@@ -41,19 +41,9 @@ import javax.servlet.http.*;
 
 public class ServletTest extends HttpServlet {
 
-    private ServletContext context;
-    
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        System.out.println("[Servlet.init]");        
-        context = config.getServletContext();
-        System.out.println("[Servlet.init] " + context.getMajorVersion());
-        
-    }
-
-    public void doTrace(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {        
-        System.out.println("[Servlet.doTrace]");
-        
+    public void doTrace(HttpServletRequest request,
+                        HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html");
     }
 
