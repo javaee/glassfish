bff9dc3e4b36838423e6c4968752ac69be8e38ea
Bhakti Mehta <bhaktimehta@java.net>
2009-12-08 21:49:00 +0000

    Fixed an error for batch files
    svn path=/trunk/; revision=35028
    
    
    Former-commit-id: dfb72983ac18f679910c28e6feaa45d60adf287b

--- v3/webservices/webservices-scripts/src/main/resources/glassfish/bin/wsgen.bat
+++ v3/webservices/webservices-scripts/src/main/resources/glassfish/bin/wsgen.bat
@@ -5,4 +5,4 @@ REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
 REM
 
-java $WSGEN_OPTS -Djava.endorsed.dirs="%~dp0..\modules\endorsed" -cp "%~dp0..\modules\webservices-osgi.jar;%~dp0..\modules\jaxb-osgi.jar" com.sun.tools.ws.WsGen %*
+java %WSGEN_OPTS% -Djava.endorsed.dirs="%~dp0..\modules\endorsed" -cp "%~dp0..\modules\webservices-osgi.jar;%~dp0..\modules\jaxb-osgi.jar" com.sun.tools.ws.WsGen %*
