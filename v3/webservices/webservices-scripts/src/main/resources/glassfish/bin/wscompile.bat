606bd17656a08367abf7002126bc59deb300adc0
Bhakti Mehta <bhaktimehta@java.net>
2009-12-11 19:01:46 +0000

    Fixed the wscompile scripts to take the WSCOMPILE_OPTS
    svn path=/trunk/; revision=35089
    
    
    Former-commit-id: 9f5265462915375bdea2a7d70989de1e2e6815bb

--- v3/webservices/webservices-scripts/src/main/resources/glassfish/bin/wscompile.bat
+++ v3/webservices/webservices-scripts/src/main/resources/glassfish/bin/wscompile.bat
@@ -5,4 +5,4 @@ REM Copyright 1997-2008 Sun Microsystems, Inc.  All rights reserved.
 REM Use is subject to license terms.
 REM
 
-java -Djava.endorsed.dirs="%~dp0..\modules\endorsed" -cp "%~dp0..\modules\webservices-osgi.jar;%~dp0..\modules\jaxb-osgi.jar;%~dp0..\modules\mail.jar;%JAVA_HOME%/lib/tools.jar" com.sun.xml.rpc.tools.wscompile.Main %*
+java %WSCOMPILE_OPTS% -Djava.endorsed.dirs="%~dp0..\modules\endorsed" -cp "%~dp0..\modules\webservices-osgi.jar;%~dp0..\modules\jaxb-osgi.jar;%~dp0..\modules\mail.jar;%JAVA_HOME%/lib/tools.jar" com.sun.xml.rpc.tools.wscompile.Main %*
