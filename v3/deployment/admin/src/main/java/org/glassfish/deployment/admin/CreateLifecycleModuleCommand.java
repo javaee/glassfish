4d60efae68a5b822a8cef22440c5f72061167de0
Lloyd Chambers <llc@java.net>
2010-01-04 23:50:39 +0000

    fixed copyright
    svn path=/trunk/; revision=35227
    
    
    Former-commit-id: d9fec5cb627cabb64a07e54a6dbdff701b329776

--- v3/deployment/admin/src/main/java/org/glassfish/deployment/admin/CreateLifecycleModuleCommand.java
+++ v3/deployment/admin/src/main/java/org/glassfish/deployment/admin/CreateLifecycleModuleCommand.java
@@ -13,9 +13,12 @@
  *
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
- * If applicable, add the following below the License Header, with the fields
- * enclosed by brackets [] replaced by your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
  *
  * Contributor(s):
  *
--- v3/deployment/admin/src/main/java/org/glassfish/deployment/admin/ListLifecycleModulesCommand.java
+++ v3/deployment/admin/src/main/java/org/glassfish/deployment/admin/ListLifecycleModulesCommand.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2008-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -13,9 +13,12 @@
  *
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
- * If applicable, add the following below the License Header, with the fields
- * enclosed by brackets [] replaced by your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
  *
  * Contributor(s):
  *
