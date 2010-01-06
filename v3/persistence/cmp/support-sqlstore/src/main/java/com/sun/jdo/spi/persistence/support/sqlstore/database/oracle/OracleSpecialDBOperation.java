a36ce8ae6575e82256a4188f33f163500547ed2d
Lloyd Chambers <llc@java.net>
2010-01-06 07:07:25 +0000

    fixed copyright date
    svn path=/trunk/; revision=35249
    
    
    Former-commit-id: 963c2594753e93e6633d26009a21f656b4f577d2

--- v3/persistence/cmp/support-sqlstore/src/main/java/com/sun/jdo/spi/persistence/support/sqlstore/database/oracle/OracleSpecialDBOperation.java
+++ v3/persistence/cmp/support-sqlstore/src/main/java/com/sun/jdo/spi/persistence/support/sqlstore/database/oracle/OracleSpecialDBOperation.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2009-2010 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -33,7 +33,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
- 
+
 /*
  * NOTE: This class is not included in the build,
  * as the Oracle JDBC driver must be present for
