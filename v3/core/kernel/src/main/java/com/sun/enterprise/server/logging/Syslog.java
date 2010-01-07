9aac515231015af231ca3d6baebb971e4dde6510
Carla Mott <carla.mott@oracle.com>
2010-01-07 02:09:28 +0000

    add correct correct  copy right header
    svn path=/trunk/; revision=35271
    
    
    Former-commit-id: bead174b39271dac465eef06d2e72fb31193fd0c

--- v3/core/kernel/src/main/java/com/sun/enterprise/server/logging/Syslog.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/server/logging/Syslog.java
@@ -32,9 +32,25 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ * Copyright 2009 Dustin Sallings
+ *
+ * Licensed under the MIT License, (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.opensource.org/licenses/mit-license.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-// Copyright (c) 1999 Dustin Sallings
 
 package com.sun.enterprise.server.logging;
 
