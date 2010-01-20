dbd868571a127b26e7191e4a03e848f7bbaea2ec
Dies Koper <dkoper@java.net>
2010-01-20 23:18:43 +0000

    Issue number: 9884 - SLSB activation error logged twice Submitted by: Dies Koper Reviewed by:  Kenneth Saks
    Changed second log statement to log the exception stacktrace only.
    
    
    svn path=/trunk/; revision=35411
    
    
    Former-commit-id: 3a03e0adec3040bcb5088d01582270e6b28d94ec

--- v3/ejb/ejb-container/src/main/java/com/sun/ejb/containers/StatefulSessionContainer.java
+++ v3/ejb/ejb-container/src/main/java/com/sun/ejb/containers/StatefulSessionContainer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -2407,8 +2407,7 @@ public final class StatefulSessionContainer
             }
             _logger.log(Level.SEVERE, "ejb.sfsb_activation_error",
                     new Object[]{sessionKey});
-            _logger.log(Level.SEVERE, "sfsb activation error. Key: "
-                    + sessionKey, ex);
+            _logger.log(Level.SEVERE, "", ex);
 
             throw new EJBException("Unable to activate EJB for key: "
                     + sessionKey, ex);
