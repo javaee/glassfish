0ca1667ed796344bc04f7dc90a7b43d494add5c2
Shing Wai Chan <shing.wai.chan@oracle.com>
2010-02-15 18:59:23 +0000

    Port fix for issue https://glassfish.dev.java.net/issues/show_bug.cgi?id=11149 ("[Embedded] deployment of application containing activation-1.1.jar fails when using uber-jar") to v3.0.1
    svn path=/branches/3.0.1/; revision=35643

--- web/war-util/src/main/java/org/glassfish/web/loader/WebappClassLoader.java
+++ web/war-util/src/main/java/org/glassfish/web/loader/WebappClassLoader.java
@@ -369,7 +369,13 @@ public class WebappClassLoader
      * and defined repositories.
      */
     public WebappClassLoader(URL[] urls, ClassLoader parent) {
-        super(urls, parent);
+        super(new URL[0], parent);
+        if (urls != null && urls.length > 0) {
+            for (URL url : urls) {
+                super.addURL(escapePlus(url));
+            }
+        }
+
         init();
     }
 
@@ -611,7 +617,7 @@ public class WebappClassLoader
     }
 
     public void addRepository(URL url) {
-        super.addURL(url);
+        super.addURL(escapePlus(url));
         hasExternalRepositories = true;
     }
 
@@ -2505,7 +2511,7 @@ public class WebappClassLoader
         } catch (IOException e) {
             // Ignore
         }
-        return realFile.toURI().toURL();
+        return escapePlus(realFile.toURI().toURL());
 
     }
 
@@ -2522,10 +2528,25 @@ public class WebappClassLoader
             // Ignore
         }
 
-        return file.toURI().toURL();
+        return escapePlus(file.toURI().toURL());
 
     }
 
+    private URL escapePlus(URL url) {
+        URL result = url;
+        if ("file".equals(url.getProtocol())) {
+            String path = url.getPath();
+            if (path != null && path.contains("+")) {
+                try {
+                    result = new URL(url.getProtocol(), url.getHost(),
+                        url.getPort(), path.replace("+", "%2B"));
+                } catch(MalformedURLException ex) {
+                    throw new IllegalStateException(ex);
+                }
+            }
+        }
+        return result;
+     }
 
     /**
      * Delete the specified directory, including all of its contents and
