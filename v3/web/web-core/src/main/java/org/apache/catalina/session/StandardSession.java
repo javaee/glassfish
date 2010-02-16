javaee/glassfish

commit 7eea492d50659e72067d35ddd07542cd54261bcd

Porting the SailFin related changes to V3.
Reviewed by: Jan Luehe
Ran QL.

svn path=/trunk/; revision=35290


Former-commit-id: f215d145dab3a718422fdd6ce9d470614c238a61

v3/web/web-core/src/main/java/org/apache/catalina/session/StandardSession.java
@@ -165,6 +165,26 @@ public class StandardSession
     protected static final Class<?> containerEventTypes[] =
         { String.class, Object.class };
 
+
+    /**
+     * The session identifier of the parent SipApplicationSession, if any
+     */
+    private String sipAppSessionId = null;
+
+
+    /**
+     * The BEKEY of this session, or <tt>null</tt>.
+     *
+     * <p>The BEKEY is used by the Converged Loadbalancer (CLB) in DCR mode
+     * for loadbalancing purposes, and supplied to the web container in the
+     * form of a request header.
+     *
+     * <p>See https://sailfin.dev.java.net/issues/show_bug.cgi?id=1647
+     * for additional details
+     */
+    private String beKey;
+
+
     /**
      * Descriptive information describing this Session implementation.
      */
@@ -387,6 +407,55 @@ public class StandardSession
             manager.add(this);
         tellNew();
     }
+
+
+/**
+     * Sets the BEKEY for this session
+     *
+     * <p>The BEKEY is used by the Converged Loadbalancer (CLB) in DCR mode
+     * for loadbalancing purposes, and supplied to the web container in the
+     * form of a request header.
+     *
+     * @param beKey the BEKEY for this session, or <tt>null</tt> if not
+     * present
+     */
+    public void setBeKey(String beKey) {
+        this.beKey = beKey;
+    }
+
+
+    /**
+     * Gets the BEKEY of this session
+     *
+     * @return the BEKEY of this session, or <tt>null</tt> if not present
+     */
+    public String getBeKey() {
+        return beKey;
+    }
+
+
+    /**
+     * Sets the id of the SipApplicationSession that is the parent of this
+     * StandardSession.
+     *
+     * @param The SipApplicationSession id
+     */
+    public void setSipApplicationSessionId(String id) {
+        sipAppSessionId = id;
+    }
+
+
+    /**
+     * Gets the id of the SipApplicationSession that is the parent of this
+     * StandardSession.
+     *
+     * @return The SipApplicationSession id, or null if this
+     * StandardSession does not have any SipApplicationSession parent
+     */
+    public String getSipApplicationSessionId() {
+        return sipAppSessionId;
+    }
+
 
 
     /**
@@ -1743,7 +1812,47 @@ public class StandardSession
 
         // Deserialize the scalar instance variables (except Manager)
         authType = null;        // Transient only
-        creationTime = ((Long) stream.readObject()).longValue();
+
+        /*
+         * The stream starts with a Long, which indicates the session's
+         * creation time. This Long may optionally be preceded by a Short,
+         * which indicates the session's serializedFormVersion.
+         */
+        Object obj = stream.readObject();
+        short readSerializedFormVersion = 0;
+        if (obj instanceof Short) {
+            readSerializedFormVersion = ((Short) obj).shortValue();
+            creationTime = ((Long) stream.readObject()).longValue();
+        } else {
+           creationTime = ((Long) obj).longValue();
+        }
+
+        readRemainingObject(stream);
+
+        /*
+         * Any additional fields that are to be included in the serialized
+         * representation of this class MUST be written to the end of the
+         * stream (in writeObject), and must be read back in HERE, i.e.,
+         * AFTER readRemainingObject (which is shared by the code that reads
+         * in sessions that were serialized using an earlier, proprietary
+         * format) has returned.
+         */
+        sipAppSessionId = (String) stream.readObject();
+
+        switch (readSerializedFormVersion) {
+        case 0:
+            // Do nothing
+            break;
+        case 1:
+            beKey = (String) stream.readObject();
+            break;
+        default:
+            throw new IOException("Unable to deserialize into "
+                    + getClass().getName()
+                    + " due to unknown serializedFormVersion of "
+                    + readSerializedFormVersion);
+        }
+
         readRemainingObject(stream);
     }
 
@@ -1953,6 +2062,9 @@ public class StandardSession
 	    }
             //end HERCULES:mod
         }
+
+        stream.writeObject(sipAppSessionId);
+        stream.writeObject(beKey);
 
     }
 

v3/web/web-core/src/main/java/org/apache/catalina/core/ContainerBase.java
@@ -231,8 +231,8 @@ public abstract class ContainerBase
     protected Loader loader = null;
 
     private ReadWriteLock lock = new ReentrantReadWriteLock();
-    private Lock readLock = lock.readLock();
-    private Lock writeLock = lock.writeLock();
+    protected Lock readLock = lock.readLock();
+    protected Lock writeLock = lock.writeLock();
 
     /**
      * The Logger implementation with which this Container is associated.

v3/web/web-core/src/main/java/org/apache/catalina/core/StandardContext.java
@@ -4950,6 +4950,25 @@ public class StandardContext
     }
 
     /**
+     * Starts the session manager of this Context.
+     */
+    protected void managerStart() throws LifecycleException {
+        if ((manager != null) && (manager instanceof Lifecycle)) {
+            ((Lifecycle) getManager()).start();
+        }
+    }
+
+
+    /**
+     * Stops the session manager of this Context.
+     */
+    protected void managerStop() throws LifecycleException {
+        if ((manager != null) && (manager instanceof Lifecycle)) {
+            ((Lifecycle) manager).stop();
+        }
+    }
+
+    /**
      * Start this Context component.
      *
      * @exception LifecycleException if a startup error occurs

v3/web/web-core/src/main/java/org/apache/catalina/util/RequestUtil.java
@@ -533,4 +533,37 @@ public final class RequestUtil {
 
         return result;
     }
+
+
+    /**
+     * Gets the string representation of the given session version mappings.
+     *
+     * @param sessionVersions The session version mappings
+     *
+     * @return The string representation of the given session version mappings
+     */
+    public static String makeSessionVersionString(HashMap sessionVersions) {
+
+        if (sessionVersions == null) {
+            return null;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        Iterator<String> iter = sessionVersions.keySet().iterator();
+        boolean first = true;
+        while (iter.hasNext()) {
+            if (first) {
+                first = false;
+            } else {
+                sb.append(':');
+            }
+            String contextPath = iter.next();
+            sb.append(contextPath);
+            sb.append(SESSION_VERSION_SEPARATOR);
+            sb.append(sessionVersions.get(contextPath));
+        }
+
+        return sb.toString();
+    }
+
 }

v3/web/web-glue/src/main/java/com/sun/enterprise/web/WebModuleContextConfig.java
@@ -149,6 +149,13 @@ public class WebModuleContextConfig extends ContextConfig {
     public void setDescriptor(WebBundleDescriptor wbd){
         webBundleDescriptor = wbd;
     }
+
+    /**
+     * Return the WebBundleDescriptor
+     */
+    public WebBundleDescriptor getDescriptor() {
+        return webBundleDescriptor;
+    }
    
     /**
      * Set the DOL object associated with this class.

commit 3fe7d5b09038eb9327dc7b131b30176328d15d01

Fixed session serialization regression that was introduced by r35290 ("Porting the SailFin related changes to V3"). This also fixes the sessionSerializeOnShutdownOnly unit test failure. (Note that v3.0.1 was not affected by this.)
Tests run: QL

svn path=/trunk/; revision=35660


Former-commit-id: feef7ade95c64cf4ff53946a04c1e65229be82c0

v3/web/web-core/src/main/java/org/apache/catalina/session/StandardSession.java
@@ -149,26 +149,6 @@ public class StandardSession
     protected static final Class<?> containerEventTypes[] =
         { String.class, Object.class };
 
-
-    /**
-     * The session identifier of the parent SipApplicationSession, if any
-     */
-    private String sipAppSessionId = null;
-
-
-    /**
-     * The BEKEY of this session, or <tt>null</tt>.
-     *
-     * <p>The BEKEY is used by the Converged Loadbalancer (CLB) in DCR mode
-     * for loadbalancing purposes, and supplied to the web container in the
-     * form of a request header.
-     *
-     * <p>See https://sailfin.dev.java.net/issues/show_bug.cgi?id=1647
-     * for additional details
-     */
-    private String beKey;
-
-
     /**
      * Descriptive information describing this Session implementation.
      */
@@ -192,8 +172,34 @@ public class StandardSession
      */
     protected static HttpSessionContext sessionContext = null;
 
+    /**
+     * Used for serialized format versioning.
+     * 1 = first version where this is being tracked.
+     *
+     * NOTE: You must increment this version whenever any changes are made
+     * to the serialized representation of this class between releases
+     */
+    private static final Short SERIALIZED_FORM_VERSION = Short.valueOf("1");
+
 
     // ----------------------------------------------------- Instance Variables
+
+    /**
+     * The session identifier of the parent SipApplicationSession, if any
+     */
+    private String sipAppSessionId = null;
+
+    /**
+     * The BEKEY of this session, or <tt>null</tt>.
+     *
+     * <p>The BEKEY is used by the Converged Loadbalancer (CLB) in DCR mode
+     * for loadbalancing purposes, and supplied to the web container in the
+     * form of a request header.
+     *
+     * <p>See https://sailfin.dev.java.net/issues/show_bug.cgi?id=1647
+     * for additional details
+     */
+    private String beKey;
 
     /**
      * The collection of user data attributes associated with this Session.
@@ -1836,8 +1842,6 @@ public class StandardSession
                     + " due to unknown serializedFormVersion of "
                     + readSerializedFormVersion);
         }
-
-        readRemainingObject(stream);
     }
 
 
@@ -1938,7 +1942,7 @@ public class StandardSession
      */
     private void writeObject(ObjectOutputStream stream) throws IOException {
 
-        // Write the scalar instance variables (except Manager)
+        stream.writeObject(SERIALIZED_FORM_VERSION);
         stream.writeObject(Long.valueOf(creationTime));
         stream.writeObject(Long.valueOf(lastAccessedTime));
         stream.writeObject(Integer.valueOf(maxInactiveInterval));