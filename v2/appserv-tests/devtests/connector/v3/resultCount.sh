1f7e099c643d1a0c894c017102b12b9351000032
Jagadish Ramu <jagadish.ramu@oracle.com>
2010-02-19 10:21:41 +0000

    Modifying build.xml to verify --cascade=true during undeploy
    svn path=/trunk/; revision=35696
    
    
    Former-commit-id: 3d644d9b68cbba91fc273012d52e4f44350c82c9

--- v2/appserv-tests/devtests/connector/v3/resultCount.sh
+++ v2/appserv-tests/devtests/connector/v3/resultCount.sh
@@ -4,7 +4,7 @@
 FILE=$APS_HOME/test_resultsValid.xml
 echo "input file=$FILE"
 
-TOTAL=163
+TOTAL=166
 PASSED=`grep "pass" $FILE | wc -l`
 FAILED=`grep "fail" $FILE | wc -l`
 TOTAL_RUN=`expr $PASSED + $FAILED `
