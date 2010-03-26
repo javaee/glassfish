f41d969f5426070d998f8c62b58c6c8f346dee9c
dochez <dochez@java.net>
2010-03-26 20:54:04 +0000

    added a convenience API to swap the annotated method for a different setter method (convenient when the annotated method is the getter for instance).

--- hk2/auto-depends/src/main/java/com/sun/hk2/component/InjectionResolver.java
+++ hk2/auto-depends/src/main/java/com/sun/hk2/component/InjectionResolver.java
@@ -1,9 +1,46 @@
+/*
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2007-2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
 package com.sun.hk2.component;
 
 import org.jvnet.hk2.component.ComponentException;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Method;
 
 /**
  * Implementation of this abstract class are handling injection resolution
@@ -20,6 +57,24 @@ public abstract class InjectionResolver<U extends Annotation> {
      */
     public InjectionResolver(Class<U> type) {
         this.type = type;
+    }
+
+    /**
+     * Returns the setter method responsible for setting the resource identified by the
+     * passed annotation on the passed annotated method.
+     *
+     * This is useful when the annotation is specified on the getter for instance (like
+     * Attribute in the config module for instance) while the setter should be used if
+     * values must be set using this injection resolver.
+     *
+     * By default, the setter method is the annotated method.
+     *
+     * @param annotated the annotated method
+     * @param annotation the annotation on the method
+     * @return the setter method to use for injecting the annotation identified resource
+     */
+    public Method getSetterMethod(Method annotated, U annotation) {
+        return annotated;
     }
 
     /**
