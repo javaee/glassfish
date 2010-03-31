6ab536c938cb0b4ada2b4195b3ef60926cb3e5a7
Sheetal Vartak <sheetalv@java.net>
2010-03-31 17:47:57 +0000

    Made a number of changes to this class for simulating asadmin functionality for cluster testing with GMS : - reads from 2 properties files (instanceXXX.properties and cluster.properties to get all GMS related properties - reads the CLUSTER_NAME and INSTANCE_NAME properties as sys properties (set in domain.xml as jvm-options) - starts GMS module only if above 2 sys properties are specified. - This service can be injected in the clients for various even notifications. The GMS API is exposed for use through this service.
    - Reviewed By Joe Fialli
    
    
    svn path=/trunk/; revision=36210
    
    
    Former-commit-id: b0573ec2753082c85aa7f752788f496ea1617606

--- v3/cluster/gms-adapter/src/main/java/org/glassfish/gms/GMSService.java
+++ v3/cluster/gms-adapter/src/main/java/org/glassfish/gms/GMSService.java
@@ -41,8 +41,7 @@ import com.sun.enterprise.ee.cms.core.*;
 import com.sun.enterprise.ee.cms.impl.client.*;
 
 import com.sun.enterprise.mgmt.transport.grizzly.GrizzlyConfigConstants;
-
-import com.sun.logging.LogDomains;
+import com.sun.enterprise.util.SystemPropertyConstants;
 
 import org.jvnet.hk2.component.PostConstruct;
 import org.jvnet.hk2.annotations.Service;
@@ -54,66 +53,177 @@ import org.glassfish.api.event.Events;
 import org.glassfish.gms.logging.LogDomain;
 
 import java.util.Properties;
+import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 /**
  * @author Sheetal.Vartak@Sun.COM
  */
-@Service
+
+@Service(name="GMSService")
+
 public class GMSService implements Startup, PostConstruct, CallBack {
 
-//    @Inject (fail = false)
-//    GMSConfig gmsCfg;
     private static final Logger logger = LogDomain.getLogger(LogDomain.GMS_LOGGER);
     private GroupManagementService gms;
+
+    private final static String INSTANCE_NAME = "INSTANCE_NAME";
+    private final static String CLUSTER_NAME = "CLUSTER_NAME";
+
+    private final static String CORE = "CORE";
+    private final static String SPECTATOR = "SPECTATOR";
+    private final static String MEMBERTYPE_STRING = "MEMBER_TYPE";
+
+    private String instanceName = System.getProperty(INSTANCE_NAME);
+    private String clusterName = System.getProperty(CLUSTER_NAME);
+    
+    private String INSTANCE_PROPS_FILE_LOCATION = "/clusters/" + clusterName + "/" + instanceName + ".properties";
+    private String CLUSTER_PROPS_FILE_LOCATION = "/clusters/" + clusterName + "/cluster.properties";
 
     @Inject
     Events events;
 
     public void usage() {
-        logger.log(Level.CONFIG, "The following properties need to be set : /n" +
-        "INSTANCE_NAME(server for DAS), CLUSTER_NAME, <MEMBER_TYPE>, <TCPSTARTPORT>, <TCPENDPORT>");
+        logger.log(Level.CONFIG, "The following properties need to be set in order to enable GMS : \n" +
+                "INSTANCE_NAME(server for DAS), CLUSTER_NAME, <MEMBER_TYPE>, <TCPSTARTPORT>, <TCPENDPORT>");
     }
     public void postConstruct() {
         try {
             logger.log(Level.CONFIG, "gmsservice.postconstruct");
             initializeGMS();
         } catch (GMSException e) {
-            logger.log(Level.WARNING, "gmsexception.occurred", new Object[] {e});
+            logger.log(Level.WARNING, "gmsexception.occurred", e.getLocalizedMessage());
         }
     }
 
     private void initializeGMS() throws GMSException{
 
-        final String instanceName = System.getProperty("INSTANCE_NAME");
-        final String groupName = System.getProperty("CLUSTER_NAME");
+        logger.setLevel(Level.CONFIG);
 
-        if (instanceName == null && groupName == null) {
-            usage();
+        if (instanceName == null && clusterName == null) {
             return;       //don't enable GMS
         }
-        if (instanceName == null || groupName == null) {
+        if (instanceName == null || clusterName == null) {
             usage();
-            throw new GMSException("Either instanceName or cluster name is null. " +
-                    "Please set the appropriate system property INSTANCE_NAME or CLUSTER_NAME");
+            throw new GMSException("instanceName =  " + instanceName + " clusterName = " + clusterName +
+                    " Please set the appropriate system property INSTANCE_NAME or CLUSTER_NAME to a valid value");
         }
 
+        Properties configProps = getSystemProps();   //setting up Shoal defaults
+
+        configProps = readFromPropsFile(configProps);
+
+        for (Enumeration property = configProps.propertyNames(); property.hasMoreElements();) {
+            String key = (String) property.nextElement();
+            String value = configProps.getProperty(key);
+            if (value != null) {
+                configProps.setProperty(key.trim(), value.trim());
+            }
+        }
+        
+        printProps(configProps);
+
+        String memberType = (String) configProps.get(MEMBERTYPE_STRING);   
+
+        gms = (GroupManagementService) GMSFactory.startGMSModule(instanceName, clusterName,
+                GroupManagementService.MemberType.valueOf(memberType), configProps);
+
+        if (gms != null) {
+            try {
+
+                gms.addActionFactory(new JoinedAndReadyNotificationActionFactoryImpl(this));
+                gms.addActionFactory(new JoinNotificationActionFactoryImpl(this));
+                gms.addActionFactory(new FailureNotificationActionFactoryImpl(this));
+                gms.addActionFactory(new PlannedShutdownActionFactoryImpl(this));
+                gms.addActionFactory(new FailureSuspectedActionFactoryImpl(this));
+
+                events.register(new org.glassfish.api.event.EventListener() {
+                    public void event(Event event) {
+                        if (event.is(EventTypes.SERVER_SHUTDOWN)) {
+                            logger.fine("Calling gms.shutdown()...");
+                            gms.shutdown(GMSConstants.shutdownType.INSTANCE_SHUTDOWN);
+                        }
+                    }
+                });
+
+                gms.join();
+            } catch (GMSException e) {
+                logger.log(Level.WARNING, "gmsexception.occurred", e.getLocalizedMessage());
+            }
+
+            logger.log(Level.CONFIG, "gmsservice.started ", new Object[]{instanceName, clusterName});
+
+        } else throw new GMSException("gms object is null.");
+
+        logger.info(instanceName + " joined group " + clusterName);
+
+    }
+
+    private Properties readFromPropsFile(Properties configProps) {
+        //load default properties
+
+        String install_location= System.getProperty(SystemPropertyConstants.INSTALL_ROOT_PROPERTY);
+        String fileName = install_location + INSTANCE_PROPS_FILE_LOCATION;
+        logger.config("configProps file location ..." + install_location + INSTANCE_PROPS_FILE_LOCATION + "and " +
+                install_location + CLUSTER_PROPS_FILE_LOCATION);
+        FileInputStream in;
+        try {
+
+            in = new FileInputStream(fileName);
+            configProps.load(in);
+            in.close();
+
+        } catch (FileNotFoundException fe) {
+            logger.log(Level.WARNING, "Cannot find the properties file : " + fileName, fe.getLocalizedMessage() + "Using Shoal Defaults..." );
+            return configProps;
+
+        } catch (IOException ioe) {
+            logger.log(Level.WARNING, "Exception while trying to load the properties file.Using GMS default properties", ioe.getLocalizedMessage());
+            return configProps;
+        }
+
+        try {
+            fileName = install_location + CLUSTER_PROPS_FILE_LOCATION;
+
+            in = new FileInputStream(fileName);
+            configProps.load(in);
+            in.close();
+            return configProps;
+        } catch (FileNotFoundException fe) {
+            logger.log(Level.WARNING, "Cannot find the properties file : " + fileName + "Using GMS default properties", fe.getLocalizedMessage());
+            return configProps;
+
+        } catch (IOException ioe) {
+            logger.log(Level.WARNING, "Exception while trying to load the properties file.Using GMS default properties", ioe.getLocalizedMessage());
+            return configProps;
+        }
+        //trim the spaces from the name-value pairs
+
+
+    }
+
+    private Properties getSystemProps() {
+        
         Properties configProps = new Properties();
-        final String MEMBERTYPE_STRING;
+        
 
         logger.log(Level.FINE, "Is initial host=" + System.getProperty("IS_INITIAL_HOST"));
+        configProps.put(INSTANCE_NAME, instanceName);
+        configProps.put(CLUSTER_NAME, clusterName);
 
         if (instanceName.equals("server")) {   //instance is DAS
-            MEMBERTYPE_STRING = "SPECTATOR";
+            configProps.put(MEMBERTYPE_STRING, SPECTATOR);
+
             configProps.put(ServiceProviderConfigurationKeys.IS_BOOTSTRAPPING_NODE.toString(), true);
         } else {
-            MEMBERTYPE_STRING = System.getProperty("MEMBERTYPE", "CORE").toUpperCase();
+            configProps.put(MEMBERTYPE_STRING, System.getProperty(MEMBERTYPE_STRING, CORE).toUpperCase());
             configProps.put(ServiceProviderConfigurationKeys.IS_BOOTSTRAPPING_NODE.toString(),
-                System.getProperty("IS_INITIAL_HOST", "false"));
+                    System.getProperty("IS_INITIAL_HOST", "false"));
         }
-
-        final GroupManagementService.MemberType memberType = GroupManagementService.MemberType.valueOf(MEMBERTYPE_STRING);
 
         configProps.put(ServiceProviderConfigurationKeys.MULTICASTADDRESS.toString(),
                 System.getProperty("MULTICASTADDRESS", "229.9.1.1"));
@@ -152,43 +262,38 @@ public class GMSService implements Startup, PostConstruct, CallBack {
         configProps.put(GrizzlyConfigConstants.TCPSTARTPORT.toString(), System.getProperty("TCPSTARTPORT", "9090"));
         configProps.put(GrizzlyConfigConstants.TCPENDPORT.toString(), System.getProperty("TCPENDPORT", "9120"));
 
-        gms = (GroupManagementService) GMSFactory.startGMSModule(instanceName, groupName, memberType, configProps);
-        if (gms != null) {
-            try {
-                gms.addActionFactory(new JoinedAndReadyNotificationActionFactoryImpl(this));
-                gms.addActionFactory(new JoinNotificationActionFactoryImpl(this));
-                gms.addActionFactory(new FailureNotificationActionFactoryImpl(this));
-                gms.addActionFactory(new PlannedShutdownActionFactoryImpl(this));
-                gms.addActionFactory(new FailureSuspectedActionFactoryImpl(this));
+        return configProps;
+    }
 
-                events.register(new org.glassfish.api.event.EventListener() {
-                    public void event(Event event) {
-                        if (event.is(EventTypes.SERVER_SHUTDOWN)) {
-                            logger.fine("Calling gms.shutdown()...");
-                            gms.shutdown(GMSConstants.shutdownType.INSTANCE_SHUTDOWN);
-                        }
-                    }
-                });
+    private void printProps(Properties prop) {
 
-                gms.join();
-            } catch (GMSException e) {
-                logger.log(Level.WARNING, "gmsexception.occurred", new Object[]{e});
-            }
-            logger.log(Level.CONFIG, "gmsservice.started ", new Object[]{instanceName, groupName});
+        StringBuffer sbuf = new StringBuffer();
+        logger.config("Printing all the properties : ");
+
+        for (Enumeration en = prop.propertyNames(); en.hasMoreElements();) {
+            String key = (String)en.nextElement();
+            sbuf.append(key + " = " + prop.get(key) + "  ");
         }
-        else throw new GMSException("gms object is null.");
+
+        logger.config(sbuf.toString());
     }
 
     public Startup.Lifecycle getLifecycle() {
         return Startup.Lifecycle.SERVER;
     }
 
-    public GroupManagementService getGMS() {
-        return gms;
+    public GroupManagementService getGMS(String groupName) {
+        //return the gms instance for that group
+        try {
+            return GMSFactory.getGMSModule(groupName);
+        } catch (GMSException e) {
+            logger.log(Level.SEVERE, "Exception in getting GMS module for group " + groupName , e.getLocalizedMessage());
+            return null;
+        }
     }
 
     @Override
     public void processNotification(Signal signal) {
-         logger.log(Level.CONFIG, "gmsservice.processNotification", new Object[] {signal.getClass().getName()});
+        logger.log(Level.INFO, "gmsservice.processNotification", signal.getClass().getName());
     }
 }
