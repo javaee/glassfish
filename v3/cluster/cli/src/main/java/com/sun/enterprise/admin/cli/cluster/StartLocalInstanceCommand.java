ebba5745cb64314085f8b74feed62faef6efe9d7
Byron Nevins <byron.nevins@oracle.com>
2010-04-07 01:43:56 +0000

    Massive copy&Paste from start-domain to start-instance. Later the pasted code will somehow be shared...
    svn path=/trunk/; revision=36326
    
    
    Former-commit-id: 1576b9f876785df6e5b758e504232eac2b8ea00c

--- v3/cluster/cli/src/main/java/com/sun/enterprise/admin/cli/cluster/StartLocalInstanceCommand.java
+++ v3/cluster/cli/src/main/java/com/sun/enterprise/admin/cli/cluster/StartLocalInstanceCommand.java
@@ -36,8 +36,17 @@
 
 package com.sun.enterprise.admin.cli.cluster;
 
+import com.sun.enterprise.admin.launcher.GFLauncher;
+import com.sun.enterprise.admin.launcher.GFLauncherException;
+import com.sun.enterprise.admin.launcher.GFLauncherFactory;
+import com.sun.enterprise.admin.launcher.GFLauncherInfo;
+import com.sun.enterprise.security.store.PasswordAdapter;
+import com.sun.enterprise.universal.xml.MiniXmlParserException;
 import java.io.*;
+import java.security.KeyStore;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.jvnet.hk2.annotations.*;
 import org.jvnet.hk2.component.*;
@@ -53,6 +62,15 @@ import com.sun.enterprise.util.ObjectAnalyzer;
 @Service(name = "start-local-instance")
 @Scoped(PerLookup.class)
 public class StartLocalInstanceCommand extends LocalInstanceCommand {
+    @Param(optional = true, defaultValue = "false")
+    private boolean verbose;
+
+    @Param(optional = true, defaultValue = "false")
+    private boolean debug;
+
+    @Param(optional = true, defaultValue = "false")
+    private boolean upgrade;
+
 
     @Param(name = "instance_name", primary = true, optional = true)
     private String instanceName0;
@@ -78,10 +96,163 @@ public class StartLocalInstanceCommand extends LocalInstanceCommand {
             throws CommandException, CommandValidationException {
 
         System.out.println("" + this);
-        throw new CommandException("ZZZZZZZZZZZZZZZZZZZZZZZ   Not implemented yet");
+
+        try {
+            pidFile = new File(new File(instanceDir, "config"), "pid");
+            createLauncher();
+            // this can be slow, 500 msec,
+            // with --passwordfile option it is ~~ 18 msec
+            String mpv = getMasterPassword();
+            info.addSecurityToken(MASTER_PASSWORD, mpv);
+
+            // launch returns very quickly if verbose is not set
+            // if verbose is set then it returns after the domain dies
+            launcher.launch();
+
+            if (verbose || upgrade) { // we can potentially loop forever here...
+                while (launcher.getExitValue() == CLIConstants.RESTART_EXIT_VALUE) {
+                    logger.printMessage(strings.get("restart"));
+
+                    if (CLIConstants.debugMode)
+                        System.setProperty(CLIConstants.WALL_CLOCK_START_PROP,
+                                            "" + System.currentTimeMillis());
+                    launcher.relaunch();
+                }
+                return launcher.getExitValue();
+            } else {
+                //waitForDAS(info.getAdminPorts());
+                //report();
+                return SUCCESS;
+            }
+        } catch (GFLauncherException gfle) {
+            throw new CommandException(gfle.getMessage());
+        } catch (MiniXmlParserException me) {
+            throw new CommandException(me);
+        }
+    }    /**
+     * Create a launcher for the instance specified by arguments to
+     * this command.  The launcher is for a server of the specified type.
+     * Sets the launcher and info fields.
+     */
+    private void createLauncher()
+                        throws GFLauncherException, MiniXmlParserException {
+            launcher = GFLauncherFactory.getInstance(GFLauncherFactory.ServerType.instance);
+            info = launcher.getInfo();
+            info.setInstanceName(instanceName);
+            info.setInstanceRootDir(instanceDir);
+            info.setVerbose(verbose || upgrade);
+            info.setDebug(debug);
+            info.setUpgrade(upgrade);
+
+            info.setRespawnInfo(programOpts.getClassName(),
+                            programOpts.getClassPath(),
+                            programOpts.getProgramArguments());
+
+            launcher.setup();
+    }
+
+        /**
+     * Get the master password, either from a password file or
+     * by asking the user.
+     */
+    private String getMasterPassword() throws CommandException {
+        // Sets the password into the launcher info.
+        // Yes, returning master password as a string is not right ...
+        final int RETRIES = 3;
+        long t0 = System.currentTimeMillis();
+        String mpv  = passwords.get(MASTER_PASSWORD);
+        if (mpv == null) { //not specified in the password file
+            mpv = "changeit";  //optimization for the default case -- see 9592
+            if (!verifyMasterPassword(mpv)) {
+                mpv = readFromMasterPasswordFile();
+                if (!verifyMasterPassword(mpv)) {
+                    mpv = retry(RETRIES);
+                }
+            }
+        } else { // the passwordfile contains AS_ADMIN_MASTERPASSWORD, use it
+            if (!verifyMasterPassword(mpv))
+                mpv = retry(RETRIES);
+        }
+        long t1 = System.currentTimeMillis();
+        logger.printDebugMessage("Time spent in master password extraction: " +
+                                    (t1-t0) + " msec");       //TODO
+        return mpv;
+    }
+
+        private String retry(int times) throws CommandException {
+        String mpv;
+        // prompt times times
+        for (int i = 0 ; i < times; i++) {
+            // XXX - I18N
+            String prompt = strings.get("mp.prompt", (times-i));
+            mpv = super.readPassword(prompt);
+            if (mpv == null)
+                throw new CommandException(strings.get("no.console"));
+                // ignore retries :)
+            if (verifyMasterPassword(mpv))
+                return mpv;
+            if (i < (times-1))
+                logger.printMessage(strings.get("retry.mp"));
+            // make them pay for typos?
+            //Thread.currentThread().sleep((i+1)*10000);
+        }
+        throw new CommandException(strings.get("mp.giveup", times));
+    }
+
+    private boolean verifyMasterPassword(String mpv) {
+        // only tries to open the keystore
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(getJKS());
+            KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+            ks.load(fis, mpv.toCharArray());
+            return true;
+        } catch (Exception e) {
+            logger.printDebugMessage(e.getMessage());
+            return false;
+        } finally {
+            try {
+                if (fis != null)
+                    fis.close();
+            } catch (IOException ioe) {
+                // ignore, I know ...
+            }
+        }
+    }
+
+    private File getJKS() {
+        File mp = new File(new File(instanceDir, "config"), "keystore.jks");
+        if (!mp.canRead())
+            return null;
+        return mp;
+    }
+    protected String readFromMasterPasswordFile() {
+        File mpf = getMasterPasswordFile();
+        if (mpf == null)
+            return null;   // no master password  saved
+        try {
+            PasswordAdapter pw = new PasswordAdapter(mpf.getAbsolutePath(),
+                                "master-password".toCharArray()); // fixed key
+            return pw.getPasswordForAlias("master-password");
+        } catch (Exception e) {
+            logger.printDebugMessage("master password file reading error: " +
+                                        e.getMessage());
+            return null;
+        }
+    }
+
+    protected File getMasterPasswordFile() {
+        File mp = new File(instanceDir, "master-password");
+        if (!mp.canRead())
+            return null;
+        return mp;
     }
 
     public String toString() {
         return ObjectAnalyzer.toStringWithSuper(this);
     }
+    private static final String MASTER_PASSWORD = "AS_ADMIN_MASTERPASSWORD";
+    private GFLauncherInfo info;
+    private GFLauncher launcher;
+    private   File pidFile;
 }
