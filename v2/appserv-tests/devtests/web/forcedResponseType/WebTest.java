4d6a1455f6c9de4d988951bf17f88a9e85d9f0ac
Shing Wai Chan <shing.wai.chan@oracle.com>
2010-04-08 01:51:07 +0000

    relax testing without space
    svn path=/trunk/; revision=36353
    
    
    Former-commit-id: 28cebd718216d18c43bf6c8cc2891672708b6239

--- v2/appserv-tests/devtests/web/forcedResponseType/WebTest.java
+++ v2/appserv-tests/devtests/web/forcedResponseType/WebTest.java
@@ -93,7 +93,8 @@ public class WebTest {
         try{
             while ((line = bis.readLine()) != null) {
                 System.out.println(i++ + ": " + line);
-                if ("Content-Type: test/html; charset=utf-8".equals(line)){
+                if ("Content-Type: test/html; charset=utf-8".equals(line) ||
+                        "Content-Type: test/html;charset=utf-8".equals(line)) {
                     found = true;
                 }
                 if (found) {
