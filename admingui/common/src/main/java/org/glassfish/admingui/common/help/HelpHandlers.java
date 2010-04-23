ef96b425e49e99e21183756779eaa25ed99513b6
Ken Paulsen <kenpaulsen@java.net>
2010-04-23 18:41:36 +0000

    Backport of issue #11384:
    * Inline Help table of contents and indexes were hard-coded to "en". This
    change correctly locates the toc.xml file according to the requested locale
    and does the same for the index.xml file.
    Issue #: 11384
    Reviewed by: Anissa Lam & Imre Oswald
    
    svn path=/branches/3.0.1/; revision=36555
    
    
    Former-commit-id: 1bd0304d5904b3698f5e555a60786395d6cb1352

--- admingui/common/src/main/java/org/glassfish/admingui/common/help/HelpHandlers.java
+++ admingui/common/src/main/java/org/glassfish/admingui/common/help/HelpHandlers.java
@@ -46,6 +46,8 @@ import org.glassfish.admingui.plugin.Index;
 import org.glassfish.admingui.plugin.IntegrationPoint;
 import org.glassfish.admingui.plugin.TOC;
 
+import java.util.Locale;
+
 
 /**
  *  <p>	These handlers help make the help system work.</p>
@@ -55,38 +57,71 @@ import org.glassfish.admingui.plugin.TOC;
 public class HelpHandlers {
 
     /**
-     *	<p> This handler provides access to {@link IntegrationPoint}s for the requested key.</p>
+     *	<p> This handler provides access to {@link IntegrationPoint}s for the
+     *	    requested key.</p>
      *
      *	@param	handlerCtx	The <code>HandlerContext</code>.
      */
     @Handler(id="getHelpTOC",
     	input={
-            @HandlerInput(name="locale", type=String.class)},
+            @HandlerInput(name="locale", type=Locale.class)},
         output={
             @HandlerOutput(name="toc", type=TOC.class)})
     public static void getHelpTOC(HandlerContext handlerCtx) {
-	String locale = (String) handlerCtx.getInputValue("locale");
+	// Get the desired Locale and the ConsolePluginService...
+	Locale locale = (Locale) handlerCtx.getInputValue("locale");
 	ConsolePluginService cps = PluginHandlers.getPluginService(
 		handlerCtx.getFacesContext());
-	handlerCtx.setOutputValue("toc", cps.getHelpTOC(locale));
+
+	// Determine the correct locale for the path...
+	String localePath = getHelpLocalePath(locale, cps);
+
+	handlerCtx.setOutputValue("toc", cps.getHelpTOC(localePath));
     }
 
-
     /**
-     *	<p> This handler provides access to {@link IntegrationPoint}s for the requested key.</p>
+     *	<p> This handler provides access to {@link IntegrationPoint}s for the
+     *	    requested key.</p>
      *
      *	@param	handlerCtx	The <code>HandlerContext</code>.
      */
     @Handler(id="getHelpIndex",
     	input={
-            @HandlerInput(name="locale", type=String.class)},
+            @HandlerInput(name="locale", type=Locale.class)},
         output={
             @HandlerOutput(name="index", type=Index.class)})
     public static void getHelpIndex(HandlerContext handlerCtx) {
-	String locale = (String) handlerCtx.getInputValue("locale");
+	// Get the desired Locale and the ConsolePluginService...
+	Locale locale = (Locale) handlerCtx.getInputValue("locale");
 	ConsolePluginService cps = PluginHandlers.getPluginService(
 		handlerCtx.getFacesContext());
-	handlerCtx.setOutputValue("index", cps.getHelpIndex(locale));
+
+	// Determine the correct locale for the path...
+	String localePath = getHelpLocalePath(locale, cps);
+
+	handlerCtx.setOutputValue("index", cps.getHelpIndex(localePath));
     }
 
+    /**
+     *	<p> This method calculates the correct locale portion of the help path
+     *	    for the requested locale.</p>
+     */
+    private static String getHelpLocalePath(Locale locale, ConsolePluginService cps) {
+	// Use common toc.xml to validate locale path...
+	ClassLoader loader = cps.getModuleClassLoader("common");
+	String path = PluginHandlers.getHelpPathForResource(
+		"toc.xml", locale, loader);
+	if (path == null) {
+	    // Not found, try the system default...
+	    path = PluginHandlers.getHelpPathForResource(
+		"toc.xml", Locale.getDefault(), loader);
+	    if (path == null) {
+		// Default to "en"
+		path = "/en/";
+	    }
+	}
+
+	// Return the locale-portion of the path
+	return path.substring(1, path.indexOf('/', 1));
+    }
 }
