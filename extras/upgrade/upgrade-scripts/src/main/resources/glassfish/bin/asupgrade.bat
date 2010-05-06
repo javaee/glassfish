ad0270b00c92f89f41b54527e130827f07d4c14f
Bobby Bisset <bbissett@java.net>
2010-05-06 14:30:16 +0000

    Fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=11823 Made the scripts work a little more like the rest of the GF/bin family.
    svn path=/branches/3.0.1/; revision=36805
    
    
    Former-commit-id: bda1ab704d7bc55b6bd912a75bf46f83d6f2f886

--- extras/upgrade/upgrade-scripts/src/main/resources/glassfish/bin/asupgrade.bat
+++ extras/upgrade/upgrade-scripts/src/main/resources/glassfish/bin/asupgrade.bat
@@ -6,15 +6,12 @@ REM Use is subject to license terms.
 REM
 
 setlocal
-set DIRNAME=%~dp0
-set AS_INSTALL=%DIRNAME%\..
+set AS_INSTALL=%~dp0..
 set AS_INSTALL_LIB=%AS_INSTALL%\lib
 set AS_INSTALL_MOD=%AS_INSTALL%\modules
 set CONFIG_HOME=%AS_INSTALL%\config
 
 call "%CONFIG_HOME%\asenv.bat"
 
-set AS_JAVA=%JAVA_HOME%
-
-"%AS_JAVA%\bin\java" -Dcom.sun.aas.domainRoot="%DIRNAME%\%AS_DEF_DOMAINS_PATH%"  -Dcom.sun.aas.java.home="%AS_JAVA%" -jar "%AS_INSTALL_MOD%/upgrade-tool.jar" %*
+java -Dcom.sun.aas.domainRoot="%~dp0%AS_DEF_DOMAINS_PATH%" -jar "%AS_INSTALL_MOD%/upgrade-tool.jar" %*
 endlocal
