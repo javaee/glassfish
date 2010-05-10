db9b50a14606fb9e5924a3ba3da934911397e6d3
Bobby Bisset <bbissett@java.net>
2010-05-10 19:55:56 +0000

    Hopefully last commit for https://glassfish.dev.java.net/issues/show_bug.cgi?id=11823. Removed the variables from batch script now that set/endlocal no longer used, and cleaned some leftover junk out of both scripts.
    svn path=/branches/3.0.1/; revision=36860
    
    
    Former-commit-id: d7274d2a4fca4c863026f8ccf4b589fd6acf8c4f

--- extras/upgrade/upgrade-scripts/src/main/resources/glassfish/bin/asupgrade.bat
+++ extras/upgrade/upgrade-scripts/src/main/resources/glassfish/bin/asupgrade.bat
@@ -5,11 +5,4 @@ REM Copyright (c) 1997, 2010 Oracle and/or its affiliates, Inc. All rights reser
 REM Use is subject to license terms.
 REM
 
-set AS_INSTALL=%~dp0..
-set AS_INSTALL_LIB=%AS_INSTALL%\lib
-set AS_INSTALL_MOD=%AS_INSTALL%\modules
-set CONFIG_HOME=%AS_INSTALL%\config
-
-call "%CONFIG_HOME%\asenv.bat"
-
-java -Dcom.sun.aas.domainRoot="%~dp0%AS_DEF_DOMAINS_PATH%" -jar "%AS_INSTALL_MOD%/upgrade-tool.jar" %*
+java -Dcom.sun.aas.domainRoot="%~dp0..\domains" -jar "%~dp0..\modules\upgrade-tool.jar" %*
