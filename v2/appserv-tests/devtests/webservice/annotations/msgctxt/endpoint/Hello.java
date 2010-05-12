6cb93c79a6a785329daf630e108a384f4f8a2e97
Bhakti Mehta <bhaktimehta@java.net>
2010-05-12 23:01:06 +0000

    Fixed more tests
    svn path=/trunk/; revision=36930
    
    
    Former-commit-id: 0bb2f6e4f69d44baaf9f05b7afcb5916c2bcce4d

--- v2/appserv-tests/devtests/webservice/annotations/msgctxt/endpoint/Hello.java
+++ v2/appserv-tests/devtests/webservice/annotations/msgctxt/endpoint/Hello.java
@@ -10,11 +10,11 @@ public class Hello {
     @Resource WebServiceContext wsc;
 
     public String sayHello(String param) {
-	javax.xml.ws.handler.MessageContext m = wsc.getMessageContext();
-	if(m == null)
-		return "WebService Context injection failed";
-	if(wsc != null)
-		return "WebSvcTest-Hello " + param;
-	return "WebService Context injection failed";
+        javax.xml.ws.handler.MessageContext m = wsc.getMessageContext();
+        if(m == null)
+            return "WebService Context injection failed";
+        if(wsc != null)
+            return "WebSvcTest-Hello " + param;
+        return "WebService Context injection failed";
     }
 }
