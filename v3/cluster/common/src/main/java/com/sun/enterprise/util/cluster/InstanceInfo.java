7b6162bb14519100fa3ad52f65e8850dfb85e258
Byron Nevins <byron.nevins@oracle.com>
2010-05-15 19:43:16 +0000

    set connect timeout for list-instances to 2000 msec.  Default is 20,000 msec.
    svn path=/trunk/; revision=37001
    
    
    Former-commit-id: 30a48b902124bcb988ea6701212539c31d70294b

--- v3/cluster/common/src/main/java/com/sun/enterprise/util/cluster/InstanceInfo.java
+++ v3/cluster/common/src/main/java/com/sun/enterprise/util/cluster/InstanceInfo.java
@@ -1,6 +1,37 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
  */
 package com.sun.enterprise.util.cluster;
 
@@ -75,11 +106,11 @@ public class InstanceInfo {
 
     // TODO what about security????
     private String pingInstance() {
-        // TODO force a shorter timeout if instance isn't running -- currently an unacceptable 10-15 seconds
         try {
             RemoteAdminCommand rac = new RemoteAdminCommand("uptime", host, port, false, "admin", null, logger);
+            // default timeout is 20,000 msec!!
+            rac.setConnectTimeout(2000);
             ParameterMap map = new ParameterMap();
-            //map.add("--target", "INSTANCE");
             return rac.executeCommand(map);
         }
         catch (CommandException ex) {
