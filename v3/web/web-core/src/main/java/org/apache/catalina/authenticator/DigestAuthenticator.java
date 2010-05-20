bac6d6652af40ef3cceebc6da67d76280a03d0d4
Shing Wai Chan <shing.wai.chan@oracle.com>
2010-05-20 00:25:42 +0000

    fix CR 6944260: INPT:HDI Header Injection vulnerability in BasicAuthenticator.java port Tomcat fix: http://svn.apache.org/viewvc?view=revision&revision=936540
    svn path=/trunk/; revision=37104
    
    
    Former-commit-id: e1ff318fc344857eddf0b820bdbfe284e4165c7e

--- v3/web/web-core/src/main/java/org/apache/catalina/authenticator/DigestAuthenticator.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/authenticator/DigestAuthenticator.java
@@ -443,8 +443,7 @@ public class DigestAuthenticator
         // Get the realm name
         String realmName = config.getRealmName();
         if (realmName == null)
-            realmName = request.getServerName() + ":"
-                + request.getServerPort();
+            realmName = REALM_NAME;
 
         byte[] buffer = null;
         synchronized (md5Helper) {
