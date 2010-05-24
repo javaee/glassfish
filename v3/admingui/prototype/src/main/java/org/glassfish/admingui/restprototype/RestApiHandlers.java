5637895d873bffe4b932ada40898e5e0499ba3df
Jason Lee <jdlee@java.net>
2010-05-24 20:16:24 +0000

    Create and edit JDBC Connection Pools should now work as expected
    svn path=/trunk/; revision=37207
    
    
    Former-commit-id: 1526cb6f35b59707532774aa9e3d3f402ad0f80d

--- v3/admingui/prototype/src/main/java/org/glassfish/admingui/restprototype/RestApiHandlers.java
+++ v3/admingui/prototype/src/main/java/org/glassfish/admingui/restprototype/RestApiHandlers.java
@@ -33,7 +33,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package org.glassfish.admingui.restprototype;
+package org.glassfish.admingui.common.handlers;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
@@ -44,7 +44,6 @@ import com.sun.jsftemplating.annotation.HandlerInput;
 import com.sun.jsftemplating.annotation.HandlerOutput;
 import com.sun.jsftemplating.layout.descriptors.handler.HandlerContext;
 import org.glassfish.admingui.common.util.GuiUtil;
-import org.glassfish.admingui.common.util.V3AMX;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -58,19 +57,22 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.*;
+import org.glassfish.admin.amx.config.AMXConfigProxy;
+import org.w3c.dom.NamedNodeMap;
 
 /**
  * @author jasonlee
  */
 public class RestApiHandlers {
+
     @Handler(id = "getDefaultProxyAttrsViaRest",
-            input = {
-                    @HandlerInput(name = "parentObjectNameStr", type = String.class, required = true),
-                    @HandlerInput(name = "childType", type = String.class, required = true),
-                    @HandlerInput(name = "orig", type = Map.class)
-            },
-            output = {
-                    @HandlerOutput(name = "valueMap", type = Map.class)})
+             input = {
+        @HandlerInput(name = "parentObjectNameStr", type = String.class, required = true),
+        @HandlerInput(name = "childType", type = String.class, required = true),
+        @HandlerInput(name = "orig", type = Map.class)
+    },
+             output = {
+        @HandlerOutput(name = "valueMap", type = Map.class)})
     public static void getDefaultValues(HandlerContext handlerCtx) {
         try {
             String endpoint = getRestEndPoint((String) handlerCtx.getInputValue("parentObjectNameStr"),
@@ -96,16 +98,38 @@ public class RestApiHandlers {
         }
     }
 
+    /*  Get the simpleAttributes of the bean based on the objectNameString.
+     *  simpleAttributes means those NOT of Element, ie all attributes.
+     *  This requires the use of AMXConfigHelper, which thus causes the limitation that
+     *  this mbean has to be AMXConfigProxy, not runtiime.
+     *  For runtime mbeans, you need to use getRuntimeProxyAttrs.
+     */
+    @Handler(id = "getProxyAttrsViaRest",
+             input = {
+        @HandlerInput(name = "objectNameStr", type = String.class, required = true)},
+             output = {
+        @HandlerOutput(name = "valueMap", type = Map.class)})
+    public static void getProxyAttrs(HandlerContext handlerCtx) {
+        try {
+            String endpoint = getRestEndPoint((String) handlerCtx.getInputValue("objectNameStr"));
+            String entity = get(endpoint, "application/xml");
+
+            handlerCtx.setOutputValue("valueMap", getEntityAttrs(entity));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            handlerCtx.setOutputValue("valueMap", new HashMap());
+        }
+    }
+
     protected static Map<String, String> buildDefaultValueMap(String endpoint) throws ParserConfigurationException, SAXException, IOException {
         Map<String, String> defaultValues = new HashMap<String, String>();
 
         String options = options(endpoint, "application/xml");
-        System.out.println(options);
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(new ByteArrayInputStream(options.getBytes()));
         Element root = doc.getDocumentElement();
-        NodeList nl = root.getElementsByTagName("Message-Parameters");
+        NodeList nl = root.getElementsByTagName("messageParameters");
         if (nl.getLength() > 0) {
             NodeList params = nl.item(0).getChildNodes();
             Node child;
@@ -124,16 +148,16 @@ public class RestApiHandlers {
         return defaultValues;
     }
 
-    @Handler(id = "createProxy",
-            input = {
-                    @HandlerInput(name = "parentObjectNameStr", type = String.class, required = true),
-                    @HandlerInput(name = "childType", type = String.class, required = true),
-                    @HandlerInput(name = "attrs", type = Map.class),
-                    @HandlerInput(name = "skipAttrs", type = List.class),
-                    @HandlerInput(name = "onlyUseAttrs", type = List.class),
-                    @HandlerInput(name = "convertToFalse", type = List.class)},
-            output = {
-                    @HandlerOutput(name = "result", type = String.class)})
+    @Handler(id = "createProxyViaRest",
+             input = {
+        @HandlerInput(name = "parentObjectNameStr", type = String.class, required = true),
+        @HandlerInput(name = "childType", type = String.class, required = true),
+        @HandlerInput(name = "attrs", type = Map.class),
+        @HandlerInput(name = "skipAttrs", type = List.class),
+        @HandlerInput(name = "onlyUseAttrs", type = List.class),
+        @HandlerInput(name = "convertToFalse", type = List.class)},
+             output = {
+        @HandlerOutput(name = "result", type = String.class)})
     public static void createProxy(HandlerContext handlerCtx) {
         Map<String, String> attrs = (Map) handlerCtx.getInputValue("attrs");
         if (attrs == null) {
@@ -143,9 +167,10 @@ public class RestApiHandlers {
                 (String) handlerCtx.getInputValue("childType"));
         List<String> skipAttrs = (List) handlerCtx.getInputValue("skipAttrs");
         List<String> onlyUseAttrs = (List) handlerCtx.getInputValue("onlyUseAttrs");
+        List<String> convertToFalse = (List) handlerCtx.getInputValue("convertToFalse");
 
-        int status = sendCreateRequest(endpoint, attrs, skipAttrs, onlyUseAttrs);
-        if (status != 201) {
+        int status = sendCreateRequest(endpoint, attrs, skipAttrs, onlyUseAttrs, convertToFalse);
+        if ((status != 200) && (status != 201)) {
             GuiUtil.getLogger().severe("CreateProxy failed.  parent=" + endpoint + "; attrs =" + attrs);
             GuiUtil.handleError(handlerCtx, GuiUtil.getMessage("msg.error.checkLog"));
             return;
@@ -154,9 +179,11 @@ public class RestApiHandlers {
 
     }
 
-    protected static int sendCreateRequest(String endpoint, Map<String, String> attrs, List<String> skipAttrs, List<String> onlyUseAttrs) {
+
+
+    protected static int sendCreateRequest(String endpoint, Map<String, String> attrs, List<String> skipAttrs, List<String> onlyUseAttrs, List<String> convertToFalse) {
         //Should specify either skipAttrs or onlyUseAttrs
-//        V3AMX.removeSpecifiedAttr(attrs, skipAttrs);
+        removeSpecifiedAttrs(attrs, skipAttrs);
 
         if (onlyUseAttrs != null) {
             Map newAttrs = new HashMap();
@@ -167,28 +194,10 @@ public class RestApiHandlers {
             }
             attrs = newAttrs;
         }
-
-//        V3AMX.removeElement(attrs);
-
-        /* If user doesn't fill in anything, we need to remove it, otherwise, it is written out as "" in domain.xml and
-        * user will not be able to get the default value.
-        * Another reason is for attributes that is optional but is an enum, eg  transactionIsolationLevel in jdbc connection
-        * pool, (read-uncommitted|read-committed|repeatable-read|serializable)  pass in "" will result in constraints
-        * violation.
-        */
-        Set<Map.Entry<String, String>> attrSet = attrs.entrySet();
-        Iterator<Map.Entry<String, String>> iter = attrSet.iterator();
-        while (iter.hasNext()) {
-            Map.Entry<String, String> oneEntry = iter.next();
-            Object val = oneEntry.getValue();
-            if ((val != null) && (val instanceof String) && (val.equals(""))) {
-                iter.remove();
-            }
-        }
-
+        attrs = convertNullValuesToFalse(attrs, convertToFalse);
         attrs = fixKeyNames(attrs);
-        
-        return  post(endpoint, attrs, "text/html");
+
+        return post(endpoint, attrs, "text/html");
     }
 
     // This method may be a really bad idea. :P
@@ -211,6 +220,10 @@ public class RestApiHandlers {
         return "http://localhost:4848/management" + endpoint;
     }
 
+    protected static String getRestEndPoint(String parent) {
+        return getRestEndPoint(parent, null);
+    }
+
     protected static Map<String, String> fixKeyNames(Map<String, String> map) {
         Map<String, String> results = new HashMap<String, String>();
         for (Map.Entry<String, String> entry : map.entrySet()) {
@@ -222,10 +235,74 @@ public class RestApiHandlers {
         return results;
     }
 
+    protected static void removeSpecifiedAttrs(Map<String, String> attrs, List<String> removeList) {
+        if (removeList == null || removeList.size() <= 0) {
+            return;
+        }
+        Set<Map.Entry<String, String>> attrSet = attrs.entrySet();
+        Iterator<Map.Entry<String, String>> iter = attrSet.iterator();
+        while (iter.hasNext()) {
+            Map.Entry<String, String> oneEntry = iter.next();
+            if (removeList.contains(oneEntry.getKey())) {
+                iter.remove();
+            }
+        }
+    }
+
+    // This is ugly, but I'm trying to figure out why the cleaner code doesn't work :(
+    protected static Map<String, String> convertNullValuesToFalse(Map<String, String> attrs, List<String> convertToFalse) {
+        if (convertToFalse != null) {
+            Map<String, String> newAttrs = new HashMap<String, String>();
+            String key;
+
+            for (Map.Entry<String, String> entry : attrs.entrySet()) {
+                key = entry.getKey();
+                if (convertToFalse.contains(key) && ((entry.getValue() == null) || "null".equals(entry.getValue()))) {
+                    newAttrs.put(key, "false");
+                } else {
+                    newAttrs.put(key, entry.getValue());
+                }
+            }
+            return newAttrs;
+        } else {
+            return attrs;
+        }
+    }
+
+    protected static Map<String, String> getEntityAttrs(String entity) {
+        Map<String, String> attrs = new HashMap<String, String>();
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new ByteArrayInputStream(entity.getBytes()));
+            Element root = doc.getDocumentElement();
+            NamedNodeMap nnm = root.getAttributes();
+            for (int i = 0; i < nnm.getLength(); i++) {
+                Node node = nnm.item(i);
+                attrs.put(upperCaseFirstLetter(node.getNodeName()), node.getNodeValue());
+            }
+        } catch (Exception e) {
+        }
+
+        return attrs;
+    }
+
+    /**
+     * Converts the first letter of the given string to Uppercase.
+     *
+     * @param string the input string
+     * @return the string with the Uppercase first letter
+     */
+    public static String upperCaseFirstLetter(String string) {
+        if (string == null || string.length() <= 0) {
+            return string;
+        }
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
+    }
+
     //******************************************************************************************************************
     // Jersey client methods
     //******************************************************************************************************************
-
     protected static String get(String address, String responseType) {
         return Client.create().resource(address).accept(responseType).get(String.class);
     }
@@ -237,9 +314,7 @@ public class RestApiHandlers {
         for (final Map.Entry<String, String> entry : payload.entrySet()) {
             formData.putSingle(entry.getKey(), entry.getValue());
         }
-        ClientResponse cr = webResource.type("application/x-www-form-urlencoded")
-                .accept(responseType)
-                .post(ClientResponse.class, formData);
+        ClientResponse cr = webResource.type("application/x-www-form-urlencoded").accept(responseType).post(ClientResponse.class, formData);
         return cr.getStatus();
     }
 
