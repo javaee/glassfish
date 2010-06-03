5fd081a6eabc851c09ddc3f325622dfcb248dfef
Byron Nevins <byron.nevins@oracle.com>
2010-06-03 22:02:39 +0000

    No need for this kludge any more.  Thanks for reminding me Bill.
    svn path=/trunk/; revision=37443
    
    
    Former-commit-id: 2a5b052ee943fdaf234ebeda1af97ddd07de7421

--- v2/appserv-tests/copyofparent/startpb.win.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# need $APS_HOME/lib in classpath to pickup Java stored procedure class
-
-$JAVA_HOME/bin/java -classpath $S1AS_HOME/pointbase/lib/pbembedded.jar\;$APS_HOME/lib com.pointbase.net.netServer /port:$@ /pointbase.ini=./build/pointbase.ini /noconsole
--- v2/appserv-tests/copyofparent/stoppb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-$JAVA_HOME/bin/java -classpath ./lib/isql.jar:$S1AS_HOME/pointbase//lib/pbclient.jar:$S1AS_HOME/pointbase/pbembedded.jar isql.ISQL jdbc:pointbase:server://localhost:$@/sqe-samples,new dbuser dbpassword com.pointbase.jdbc.jdbcUniversalDriver shutdownPbs.sql
