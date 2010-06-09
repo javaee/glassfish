97d2dd7b320001989c54689e30da6ab9b22b0d50
Jennifer Galloway <jc129909>
2010-06-09 15:09:40 +0000

    Issue 12181 Addin remote AdminCommands, create-instance/delete-instance to call _register-instance/_unregister-instance.
    svn path=/trunk/; revision=37631
    
    
    Former-commit-id: 1b264adf81213450c035982f9f7233c1ce2753bf

---
+++ v3/cluster/admin/src/main/java/com/sun/enterprise/v3/admin/cluster/CreateInstanceCommand.java
@@ -0,0 +1,109 @@
+/*
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.enterprise.v3.admin.cluster;
+
+import java.util.logging.*;
+import org.glassfish.api.ActionReport;
+import org.glassfish.api.ActionReport.ExitCode;
+import org.glassfish.api.I18n;
+import org.glassfish.api.Param;
+import org.glassfish.api.admin.AdminCommand;
+import org.glassfish.api.admin.AdminCommandContext;
+import org.glassfish.api.admin.CommandRunner;
+import org.glassfish.api.admin.CommandRunner.CommandInvocation;
+import org.glassfish.api.admin.ParameterMap;
+import org.jvnet.hk2.annotations.*;
+import org.glassfish.api.admin.ServerEnvironment;
+import org.jvnet.hk2.component.*;
+
+/**
+ * Remote AdminCommand to create an instance.  This command is run only on DAS.
+ *  1. Register the instance on DAS
+ *  2. Create the file system on the instance node via ssh, node agent, or other
+ *
+ * @author Jennifer Chou
+ */
+@Service(name = "create-instance")
+@I18n("create.instance")
+@Scoped(PerLookup.class)
+public class CreateInstanceCommand implements AdminCommand {
+    @Inject
+    private CommandRunner cr;
+    @Inject
+    private ServerEnvironment env;
+
+    @Param(name="nodeagent")
+    String nodeAgent;
+
+    @Param(name = "config", optional=true)
+    String configRef;
+
+    @Param(name="cluster", optional=true)
+    String clusterName;
+
+    @Param(name = "systemproperties", optional = true, separator = ':')
+    private String systemProperties;
+
+    @Param(name = "instance_name", primary = true)
+    private String instance;
+
+
+    @Override
+    public void execute(AdminCommandContext context) {
+        Logger logger = context.getLogger();
+        ActionReport report = context.getActionReport();
+
+        // Require that we be a DAS ?
+        if(!env.isDas()) {
+            String msg = Strings.get("onlyRunsOnDas", "create-instance");
+            logger.warning(msg);
+            report.setActionExitCode(ExitCode.FAILURE);
+            report.setMessage(msg);
+            return;
+        }
+
+        CommandInvocation ci = cr.getCommandInvocation("_register-instance", report);
+        ParameterMap map = new ParameterMap();
+        map.add("nodeagent", nodeAgent);
+        map.add("config", configRef);
+        map.add("cluster", clusterName);
+        map.add("systemproperties", systemProperties);
+        map.add("DEFAULT", instance);
+        ci.parameters(map);
+        ci.execute();
+    }
+}
