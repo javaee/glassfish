8c1c10a21a17ceeb77b753b5a4d4eb83b199d118
srinik76 <srinik76@localhost>
2010-06-16 06:07:30 +0000

    Converted the JVM GUI Pages to use REST APIs. Reviewed by Jason
    svn path=/trunk/; revision=37817
    
    
    Former-commit-id: d69948c5a98c97f2bbf3715a3478102c348be668

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/RestApiHandlers.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/RestApiHandlers.java
@@ -116,6 +116,27 @@ public class RestApiHandlers {
         }
     }
 
+    @Handler(id = "gf.checkIfEndPointExist",
+            input = {
+                    @HandlerInput(name = "endpoint", type = String.class, required = true)},
+            output = {
+                    @HandlerOutput(name = "edit", type = Boolean.class)
+            })
+    public static void checkIfEndPointExist(HandlerContext handlerCtx) {
+        try {
+            String endpoint = (String) handlerCtx.getInputValue("endpoint");
+            String entity = get(endpoint);
+            Boolean edit = false;
+            Map<String, String> tmp = getEntityAttrs(entity);
+            if (tmp.size() != 0) {
+                edit = true;
+            }
+
+            handlerCtx.setOutputValue("edit", edit);
+        }catch (Exception ex){
+            GuiUtil.handleException(handlerCtx, ex);
+        }
+    }
     /**
      *
      * REST-based version of createProxy
