8c18c24f3eda2a4c17b2ddb5b3be393a9552a868
Jagadish Ramu <jagadish.ramu@oracle.com>
2010-06-19 12:58:33 +0000

    Removing "file:" in build.xml(s) to avoid WARNING messages from ANT
    All connector-dev-tests passed
    
    
    svn path=/trunk/; revision=37935
    
    
    Former-commit-id: f5c12ce63af14a274a1bd0a0a896b555ba646ec8

--- v2/appserv-tests/devtests/connector/v3/SunRaXml/cci/client/WebTest.java
+++ v2/appserv-tests/devtests/connector/v3/SunRaXml/cci/client/WebTest.java
@@ -32,7 +32,7 @@ public class WebTest {
         try {
             long time = System.currentTimeMillis();
             Socket s = new Socket(host, port);
-            s.setSoTimeout(60000);
+            s.setSoTimeout(120000);
             OutputStream os = s.getOutputStream();
 
             contextPath += "?url=" + contextPath;
