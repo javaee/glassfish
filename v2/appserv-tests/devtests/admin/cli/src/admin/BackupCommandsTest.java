197a5cc60cb659f8ddf759816c75f8c46408551c
Yamini K B <yamini.k.b@oracle.com>
2010-06-20 16:37:56 +0000

    Developer tests for DAS recovery commands: -Added 19 tests -Added as separate ant target run-backup-tests
    Reviewed by: Bhakti
    
    
    svn path=/trunk/; revision=37949
    
    
    Former-commit-id: 873a4b4a43d18da3056d9107a94b85a79b1994f8

---
+++ v2/appserv-tests/devtests/admin/cli/src/admin/BackupCommandsTest.java
@@ -0,0 +1,184 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package admin;
+
+import java.io.*;
+import java.net.*;
+
+/*
+ * Dev test for DAS recovery commands (backup-domain, restore-domain, list-backups)
+ * @author Yamini K B
+ */
+public class BackupCommandsTest extends AdminBaseDevTest {
+
+    public BackupCommandsTest() {
+        String host0 = null;
+
+        try {
+            host0 = InetAddress.getLocalHost().getHostName();
+        }
+        catch (Exception e) {
+            host0 = "localhost";
+        }
+        host = host0;
+        System.out.println("Host= " + host);
+        glassFishHome = getGlassFishHome();
+        System.out.println("GF HOME = " + glassFishHome);
+    }
+
+    public static void main(String[] args) {
+        new BackupCommandsTest().run();
+    }
+
+    @Override
+    protected String getTestName() {
+        return "DAS backup and recovery";
+    }
+
+    @Override
+    protected String getTestDescription() {
+        return "Developer tests for backup-domain/restore-domain/list-backups";
+    }
+
+    public void run() {
+        testCommandsWithDefaultOptions();
+        testCommandsWithDASRunning();
+        testCommandsWithOperands();
+        testRestoreWithFileName();
+        testCommandsWithNoDomains();
+        testCommandsWithMultipleDomains();
+	stat.printSummary();
+    }
+
+    private void testCommandsWithDASRunning() {
+        asadmin("start-domain");
+
+        // backup folder doesn't exist
+        // perform a backup
+        report("backup-domain-DAS-running", !asadmin("backup-domain"));
+
+        // list backup
+        report("list-backups-DAS-running", asadmin("list-backups"));
+
+        // restore backup
+        report("restore-domain-DAS-running", !asadmin("restore-domain", "--force"));
+
+        asadmin("stop-domain");
+    }
+
+    private void testCommandsWithDefaultOptions() {
+
+        // perform a backup
+        report("backup-domain-no-operand", asadmin("backup-domain"));
+
+        // list backup
+        report("list-backups-no-operand", asadmin("list-backups"));
+
+        // restore backup
+        report("restore-domain-no-operand", !asadmin("restore-domain"));
+
+        // force restore backup
+        report("force-restore-domain-no-operand", asadmin("restore-domain", "--force"));
+    }
+
+    private void testCommandsWithOperands() {
+
+        // perform a backup
+        report("backup-domain-with-operand", asadmin("backup-domain", "domain1"));
+
+        // list backup
+        report("list-backups-with-operand", asadmin("list-backups", "domain1"));
+
+        // restore backup
+        report("restore-domain-with-operand", asadmin("restore-domain", "domain1"));
+
+    }
+
+    private void testRestoreWithFileName() {
+
+        // specify a different domain name
+        report("restore-domain-with-filename", !asadmin("restore-domain", "--filename", "foo", "domain2"));
+        
+        // force restore
+        report("force-restore-domain-with-operand", !asadmin("restore-domain", "--force", "domain2"));
+    }
+
+    private void testCommandsWithNoDomains() {
+
+        // delete the domain
+        asadmin("delete-domain", "domain1");
+
+        // perform a backup
+        report("backup-domain-empty-domaindir", !asadmin("backup-domain"));
+
+        // list backup
+        report("list-backups-empty-domaindir", !asadmin("list-backups"));
+
+        // restore backup
+        report("restore-domain-empty-domaindir", !asadmin("restore-domain"));
+
+        // put back domain1
+        asadmin("create-domain", "--nopassword", "domain1");
+    }
+
+    private void testCommandsWithMultipleDomains() {
+        
+        // create domain2
+        asadmin("create-domain", "--nopassword", "domain2");
+
+        // perform a backup
+        report("backup-domain-multiple-domains-in-domaindir", !asadmin("backup-domain"));
+
+        // perform a backup
+        asadmin("backup-domain", "domain1");
+
+        // list backup
+        report("list-backups-multiple-domains-in-domaindir", !asadmin("list-backups"));
+
+        // list backup with operand
+        report("list-backups-with-operand-multiple-domains-in-domaindir", asadmin("list-backups", "domain1"));
+
+        // restore backup
+        report("restore-domain-multiple-domains-in-domaindir", !asadmin("restore-domain", "--force"));
+
+        //delete domain2
+        asadmin("delete-domain", "domain2");
+
+    }
+    private final String host;
+    private final File glassFishHome;
+
+}
