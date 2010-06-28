7b712f164a4e41a86826125a51c1e966487ceac9
jeff_trent <jeff_trent@java.net>
2010-06-28 18:43:22 +0000

    Fix Hk2Runner to properly report test descriptions.

--- hk2/auto-depends/src/main/java/org/jvnet/hk2/annotations/Inject.java
+++ hk2/auto-depends/src/main/java/org/jvnet/hk2/annotations/Inject.java
@@ -40,9 +40,7 @@ package org.jvnet.hk2.annotations;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
