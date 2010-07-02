48424b2eeedd57ff969c362e2e558004336ad68d
Carla Mott <carla.mott@oracle.com>
2010-07-02 20:46:45 +0000

    initial code for _update-node command
    svn path=/trunk/; revision=38324
    
    
    Former-commit-id: 253512b7d8a9c975b8817eab0d162877918e6e0c

---
+++ v3/cluster/admin/src/main/java/com/sun/enterprise/v3/admin/cluster/UpdateNodeCommand.java
@@ -0,0 +1,167 @@
+/*
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.enterprise.v3.admin.cluster;
+
+import com.sun.enterprise.config.serverbeans.Node;
+import com.sun.enterprise.config.serverbeans.Nodes;
+import com.sun.enterprise.config.serverbeans.SshConnector;
+import com.sun.enterprise.config.serverbeans.SshAuth;
+import com.sun.enterprise.config.serverbeans.Domain;
+import java.beans.PropertyVetoException;
+
+import org.glassfish.api.ActionReport;
+import org.glassfish.api.I18n;
+import org.glassfish.api.Param;
+import org.glassfish.api.admin.*;
+import org.jvnet.hk2.annotations.*;
+import org.jvnet.hk2.component.*;
+import org.jvnet.hk2.config.TransactionFailure;
+import org.jvnet.hk2.config.*;
+import java.util.logging.Logger;
+
+/**
+ * Remote AdminCommand to update a config node.  This command is run only on DAS.
+ *  Update the config node on DAS
+ *
+ * @author Carla Mott
+ */
+@Service(name = "_update-node")
+@I18n("update.node")
+@Scoped(PerLookup.class)
+@Cluster({RuntimeType.DAS})
+public class UpdateNodeCommand implements AdminCommand {
+
+    @Inject
+    Nodes nodes;
+
+    @Inject
+    Domain domain;
+
+    @Param(name="name", primary = true)
+    String name;
+
+    @Param(name="nodehost", optional=true)
+    String nodehost;
+
+    @Param(name = "nodehome", optional=true)
+    String nodehome;
+
+    @Param(name="sshport", optional=true)
+    String sshport;
+
+    @Param(name="sshuser", optional=true)
+    String sshuser;
+
+    @Param(name="sshnodehost", optional=true)
+    String sshnodehost;
+
+    @Param(name="sshkeyfile", optional=true)
+    String sshkeyfile;
+
+    @Override
+    public void execute(AdminCommandContext context) {
+        ActionReport report = context.getActionReport();
+        Logger logger= context.logger;
+
+        Node node= nodes.getNode(name);
+        if (node == null) {
+            //node doesn't exist
+            String msg = Strings.get("noSuchNode", name);
+            logger.warning(msg);
+            report.setActionExitCode(ActionReport.ExitCode.FAILURE);
+            report.setMessage(msg);
+            return;
+        }
+
+        try {
+            updateNodeElement(name);
+        } catch(TransactionFailure e) {
+            logger.warning("failed.to.update.node " + name);
+            report.setActionExitCode(ActionReport.ExitCode.FAILURE);
+            report.setMessage(e.getMessage());
+        }
+
+        
+    }
+
+
+    public void updateNodeElement(final String nodeName) throws TransactionFailure {
+        ConfigSupport.apply(new SingleConfigCode() {
+            @Override
+            public Object run(ConfigBeanProxy param) throws PropertyVetoException, TransactionFailure {
+                // get the transaction
+                Transaction t = Transaction.getTransaction(param);
+                if (t!=null) {
+                   Nodes nodes = ((Domain)param).getNodes();
+                    Node node = nodes.getNode(nodeName);
+                    Node writeableNode = t.enroll(node);
+                    if (nodehost != null)
+                        writeableNode.setNodeHost(nodehost);
+                    if (nodehome != null)
+                        writeableNode.setNodeHome(nodehome);
+                    if (sshport != null || sshnodehost != null){
+                        SshConnector sshC = writeableNode.getSshConnector();
+                        if (sshC == null)
+                            sshC =writeableNode.createChild(SshConnector.class);
+
+                        if (sshport != null)
+                            sshC.setSshPort(sshport);
+                        if(sshnodehost != null)
+                            sshC.setSshHost(sshnodehost);
+
+                        if (sshuser != null || sshkeyfile != null) {
+                            SshAuth sshA = sshC.getSshAuth();
+                            if (sshA == null)
+                               sshA = sshC.createChild(SshAuth.class);
+                            if (sshuser != null)
+                                sshA.setUserName(sshuser);
+                            if (sshkeyfile != null)
+                                sshA.setKeyfile(sshkeyfile);
+                            sshC.setSshAuth(sshA);
+                        }
+                        writeableNode.setSshConnector(sshC);
+                        
+                    }
+                   
+                }
+                return Boolean.TRUE;
+            }
+
+        }, domain);
+    }
+            
+}
