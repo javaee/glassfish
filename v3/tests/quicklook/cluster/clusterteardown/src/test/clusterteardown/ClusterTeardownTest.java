60f571b41d8082a95c661a2859941776e78855d4
Ming Zhang <mzh777@java.net>
2010-07-02 19:33:12 +0000

    Add cluster setup and helloworld tests.
    svn path=/trunk/; revision=38318
    
    
    Former-commit-id: 55d2592d87a2b902b39e4c5cce57c259f6cd68c9

---
+++ v3/tests/quicklook/cluster/clusterteardown/src/test/clusterteardown/ClusterTeardownTest.java
@@ -0,0 +1,68 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package test.clusterteardown;
+
+import com.sun.appserv.test.AdminBaseDevTest;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class ClusterTeardownTest extends AdminBaseDevTest {
+
+    @Override
+    protected String getTestDescription() {
+        return "QL cluster test helloworld";
+    }
+        final String tn = "QLCluster";
+        final String cname = "eec1";
+        final String i1name = "eein1-with-a-very-very-very-long-name";
+        final String i2name = "eein2";
+        
+        public boolean retStatus, ret1,ret2;
+
+    @Test
+    public void deleteInstanceTest() throws Exception{
+        report(tn + "stop-local-instance1", asadmin("stop-local-instance", i1name));
+        ret1 = report(tn + "stop-local-instance2", asadmin("stop-local-instance", i2name));
+        report(tn + "delete-local-instance1", asadmin("delete-local-instance", i1name));
+        ret2 = report(tn + "delete-local-instance2", asadmin("delete-local-instance", i2name));
+    }
+        
+    @Test(dependsOnMethods = { "deleteInstanceTest" })
+    public void deleteClusterTest() throws Exception{
+        retStatus = report(tn + "delete-cluster", asadmin("delete-cluster", cname));
+        Assert.assertEquals(retStatus&&ret1&&ret2, true, "Cluster unsetup failed ...");
+    }
+}
---
+++ v3/tests/quicklook/cluster/helloworld/web/jsp/hi.jsp
@@ -0,0 +1,9 @@
+<html><head><title>JSP Test</title>
+<%!
+String message = "Hello, World.";
+%>
+</head>
+<body>
+<h2><%= message%></h2>
+<%= new java.util.Date() %>
+</body></html>
