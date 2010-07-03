95486f3fcd74ecdefb4f2e6732f23fa44a9657c0
Byron Nevins <byron.nevins@oracle.com>
2010-07-03 17:54:49 +0000

    ...
    svn path=/trunk/; revision=38347
    
    
    Former-commit-id: 345eaa050330d9854f2ff52896917109f7baa411

--- v2/appserv-tests/devtests/admin/cli/hudson.sh
+++ v2/appserv-tests/devtests/admin/cli/hudson.sh
@@ -1,58 +1,35 @@
+export HUDSON=true
 export ROOT=`pwd`
+if [ -x "/usr/bin/cygpath" ]
+then
+  ROOT=`cygpath -d $ROOT`
+  echo "Windows ROOT: $ROOT"
+  export CYGWIN=nontsec
+fi
 rm -rf glassfishv3
-wget -O glassfish.zip http://hudson.glassfish.org/job/gf-trunk-build-continuous/lastSuccessfulBuild/artifact/bundles/glassfish.zip
+wget -O revision-under-test.html http://gf-hudson.sfbay.sun.com/hudson/job/gf-trunk-build-continuous/lastSuccessfulBuild
+grep 'Build #' revision-under-test.html
+time wget -O glassfish.zip http://gf-hudson.sfbay.sun.com/hudson/job/gf-trunk-build-continuous/lastSuccessfulBuild/artifact/bundles/glassfish.zip
 unzip -q glassfish.zip
-export S1AS_HOME=$ROOT/glassfishv3/glassfish
-export APS_HOME=$ROOT/appserv-tests
-export AS_LOGFILE=$S1AS_HOME/cli.log
-rm -rf $S1AS_HOME/domains/domain1
-cd $APS_HOME
+export S1AS_HOME="$ROOT/glassfishv3/glassfish"
+export APS_HOME="$ROOT/appserv-tests"
+export AS_LOGFILE="$S1AS_HOME/cli.log"
+export ENABLE_REPLICATION=true
 
-echo "AS_ADMIN_PASSWORD=" > temppwd
-$S1AS_HOME/bin/asadmin --user anonymous --passwordfile $APS_HOME/temppwd create-domain --adminport ${ADMIN_PORT} --domainproperties jms.port=${JMS_PORT}:domain.jmxPort=${JMX_PORT}:orb.listener.port=${ORB_PORT}:http.ssl.port=${SSL_PORT}:orb.ssl.port=${ORB_SSL_PORT}:orb.mutualauth.port=${ORB_SSL_MUTUALAUTH_PORT} --instanceport ${INSTANCE_PORT} domain1
-
-#Create 
-echo "admin.domain=domain1
-admin.domain.dir=\${env.S1AS_HOME}/domains
-admin.port=${ADMIN_PORT}
-admin.user=anonymous
-admin.host=localhost
-http.port=${INSTANCE_PORT}
-https.port=${SSL_PORT}
-http.host=localhost
-http.address=127.0.0.1
-http.alternate.port=${ALTERNATE_PORT}
-orb.port=${ORB_PORT}
-admin.password=
-ssl.password=changeit
-master.password=changeit
-admin.password.file=\${env.APS_HOME}/config/adminpassword.txt
-appserver.instance.name=server
-config.dottedname.prefix=server
-resources.dottedname.prefix=domain.resources
-results.mailhost=localhost
-results.mailer=QLTestsForPEInstallOrDASInEEInstall@sun.com
-results.mailee=byron.nevins@oracle.com
-autodeploy.dir=\${env.S1AS_HOME}/domains/\${admin.domain}/autodeploy
-precompilejsp=true
-jvm.maxpermsize=192m
-appserver.instance.dir=\${admin.domain.dir}/\${admin.domain}" > config.properties
-
+cd "$APS_HOME"
 (jps |grep Main |cut -f1 -d" " | xargs kill -9  > /dev/null 2>&1) || true
+cd "$APS_HOME/devtests/admin/cli"
+# run the tests twice -- e.g. Issue # 12127
+time ant -Dverbose=true -Dteststorun=list-jndi-entries all 
+#time ant all
+# don't run twice if we already failed!
+egrep 'FAILED *0' "$APS_HOME/count.txt" >/dev/null || exit 1
 
-cd $APS_HOME/devtests/admin/cli
-
-
-echo ******** here are the props *******
-echo ******** here are the props *******
-cat $APS_HOME/config.properties
-echo ******** here are the props *******
-echo ******** here are the props *******
-
-
-#ant all report-result |tee log.txt
-ant all |tee log.txt
-
-#(cat log.txt | grep  Total |cut -f2 -d']' |sort |uniq -c |grep -v PAS) || true
-
-$S1AS_HOME/bin/asadmin stop-domain domain1
+# bnevins June 9, 2010
+# if the tests can not be run twice  then nobody will use them because it will screw up their installation
+# This will happen when we don't clean-up and return to the original state -- which is a good test in itself!
+# Also we really don't care how long these tests take
+# of course it is not an issue on Hudson.
+(jps |grep Main |cut -f1 -d" " | xargs kill -9  > /dev/null 2>&1) || true
+time ant -Dnum_tests=45 all 
+egrep 'FAILED *0' "$APS_HOME/count.txt" >/dev/null
