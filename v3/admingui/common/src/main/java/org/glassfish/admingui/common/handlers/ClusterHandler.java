8b37a274b146fd7b662aeb43232d72f4121edde8
Anissa Lam <anissa.lam@oracle.com>
2010-07-26 03:46:58 +0000

    _update-node feature
    svn path=/trunk/; revision=39044
    
    
    Former-commit-id: 70fd8891552c572458773584743159bbd2402ab5

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/ClusterHandler.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/ClusterHandler.java
@@ -333,6 +333,27 @@ public class ClusterHandler {
     }
 
 
+    @Handler(id = "gf.nodeAttrToArgs",
+        input = {
+            @HandlerInput(name = "in", type = Map.class)
+            },
+        output = {
+            @HandlerOutput(name = "out", type = Map.class)
+        })
+    public static void nodeAttrToArgs(HandlerContext handlerCtx) {
+        Map in = (Map) handlerCtx.getInputValue("in");
+        Map out = new HashMap();
+        out.put("name", in.get("Name"));
+        out.put("nodehost", in.get("NodeHost"));
+        out.put("nodedir", in.get("NodeDir"));
+        out.put("installdir", in.get("InstallDir"));
+        out.put("sshhost", in.get("SshHost"));
+        out.put("sshport", in.get("SshPort"));
+        out.put("sshuser", in.get("UserName"));
+        out.put("sshkeyfile", in.get("Keyfile"));
+        handlerCtx.setOutputValue("out", out);
+    }
+
 
     public static String CLUSTER_RESOURCE_NAME = "org.glassfish.cluster.admingui.Strings";
 
