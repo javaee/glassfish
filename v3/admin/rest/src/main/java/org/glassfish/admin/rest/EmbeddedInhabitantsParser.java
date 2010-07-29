36e47ce215b152f3b95eddaf5d6d9105dffb52b6
Ludovic Champenois <ludo@java.net>
2010-07-29 10:33:33 +0000

    better imports
    svn path=/trunk/; revision=39182
    
    
    Former-commit-id: 2fd0c7355cce77fa045aba6a9a6e67564a200921

--- v3/admin/rest/src/main/java/org/glassfish/admin/rest/EmbeddedInhabitantsParser.java
+++ v3/admin/rest/src/main/java/org/glassfish/admin/rest/EmbeddedInhabitantsParser.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2009-2010 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -36,10 +36,12 @@
 
 package org.glassfish.admin.rest;
 
+import com.sun.hk2.component.InhabitantsParser;
+import com.sun.hk2.component.InhabitantsParserDecorator;
 import org.glassfish.admin.rest.adapter.RestManagementAdapter;
 import org.glassfish.admin.rest.adapter.RestMonitoringAdapter;
-import com.sun.hk2.component.*;
-import org.kohsuke.*;
+import org.kohsuke.MetaInfServices;
+
 
 /**
  * Inhabitant decorator to removed unwanted services in embedded mode
