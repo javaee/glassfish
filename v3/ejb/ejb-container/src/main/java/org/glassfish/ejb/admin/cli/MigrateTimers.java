ad60596233b6c28b3d4cf66fc6e47395e950a410
Cheng Fang <cf126330@java.net>
2010-08-06 02:52:11 +0000

    add migrate-timers command.
    svn path=/trunk/; revision=39443
    
    
    Former-commit-id: a40a0fcecac29b7786b81d3110830999c44b4502

---
+++ v3/ejb/ejb-container/src/main/java/org/glassfish/ejb/admin/cli/MigrateTimers.java
@@ -0,0 +1,152 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.ejb.admin.cli;
+
+import com.sun.enterprise.config.serverbeans.Cluster;
+import com.sun.enterprise.config.serverbeans.Domain;
+import com.sun.enterprise.config.serverbeans.Server;
+import com.sun.enterprise.util.SystemPropertyConstants;
+import com.sun.enterprise.util.i18n.StringManager;
+import java.util.List;
+
+import org.glassfish.ejb.api.DistributedEJBTimerService;
+import org.glassfish.api.ActionReport;
+import org.glassfish.api.I18n;
+import org.glassfish.api.Param;
+import org.glassfish.api.admin.AdminCommand;
+import org.glassfish.api.admin.AdminCommandContext;
+import org.glassfish.api.admin.RuntimeType;
+import org.glassfish.config.support.CommandTarget;
+import org.glassfish.config.support.TargetType;
+import org.glassfish.internal.api.Target;
+import org.jvnet.hk2.annotations.Inject;
+import org.jvnet.hk2.annotations.Service;
+import org.jvnet.hk2.annotations.Scoped;
+import org.jvnet.hk2.component.PerLookup;
+
+@Service(name = "migrate-timers")
+@Scoped(PerLookup.class)
+@I18n("migrate.timers")
+@org.glassfish.api.admin.Cluster(value = {RuntimeType.INSTANCE})
+@TargetType(value = {CommandTarget.DAS, CommandTarget.CLUSTERED_INSTANCE})
+public class MigrateTimers implements AdminCommand {
+
+    static StringManager localStrings = StringManager.getManager(MigrateTimers.class);
+
+    @Param(name = "target", optional = true, alias="destination")
+    String destination;
+
+    @Param(name = "migrate_from_server_name", primary = true, optional = false)
+    String fromServer;
+
+    @Inject
+    DistributedEJBTimerService timerService;
+
+    @Inject
+    private Domain domain;
+
+    @Inject
+    Target targetUtil;
+
+    /**
+     * Executes the command
+     *
+     * @param context information
+     */
+    @Override
+    public void execute(AdminCommandContext context) {
+        final ActionReport report = context.getActionReport();
+        String error = validate();
+        if (error != null) {
+            report.setMessage(error);
+            report.setActionExitCode(ActionReport.ExitCode.FAILURE);
+            return;
+        }
+        try {
+            int totalTimersMigrated = timerService.migrateTimers(fromServer);
+            report.setMessage(localStrings.getString("migrate.timers.count", totalTimersMigrated));
+            report.setActionExitCode(ActionReport.ExitCode.SUCCESS);
+        } catch (Exception e) {
+            report.setActionExitCode(ActionReport.ExitCode.FAILURE);
+            report.setFailureCause(e);
+        }
+    }
+
+    private String validate() {
+        //verify fromServer is clusteredInstance
+        Cluster fromServerCluster = targetUtil.getClusterForInstance(fromServer);
+        if(fromServerCluster == null) {
+            return localStrings.getString("migrate.timers.fromServerNotClusteredInstance", fromServer);
+        }
+
+        //verify fromServer is not running
+        if (isServerRunning(fromServer)) {
+            return localStrings.getString(
+                    "migrate.timers.migrateFromServerStillRunning", fromServer);
+        }
+        
+        //if destinationServer is not set, or set to DAS, pick a running instance
+        //in the same cluster as fromServer
+        if(destination == null ||
+                destination.equals(SystemPropertyConstants.DEFAULT_SERVER_INSTANCE_NAME)) {
+            List<Server> instances = fromServerCluster.getInstances();
+            for(Server instance : instances) {
+                if(instance.isRunning()) {
+                    destination = instance.getName();
+                }
+            }
+        } else {
+            //verify fromServer and destinationServer are in the same cluster, and
+            //verify destination is a clustered instance.
+            Cluster destinationServerCluster = targetUtil.getClusterForInstance(destination);
+            if (!fromServerCluster.getName().equals(destinationServerCluster.getName())) {
+                return localStrings.getString(
+                        "migrate.timers.fromServerAndTargetNotInSameCluster", fromServer, destination);
+            }
+            //verify destinationServer is running
+            if (!isServerRunning(destination)) {
+                return localStrings.getString("migrate.timers.destinationServerIsNotAlive", destination);
+            }
+        }
+        
+        return null;
+    }
+
+    private boolean isServerRunning(String serverName) {
+        Server server = domain.getServerNamed(serverName);
+        return (server == null) ? false : server.isRunning();
+    }
+}
