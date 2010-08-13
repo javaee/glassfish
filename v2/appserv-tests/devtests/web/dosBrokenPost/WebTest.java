06fb358928dddeac66414799c8b95f14c1b0b8e4
Justin Lee <cheeser@java.net>
2010-08-13 16:19:04 +0000

    fix test
    svn path=/trunk/; revision=39680
    
    
    Former-commit-id: 2352eeace32bf5be2626ce001ecb6e3b3b94065f

--- v2/appserv-tests/devtests/web/dosBrokenPost/WebTest.java
+++ v2/appserv-tests/devtests/web/dosBrokenPost/WebTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -33,77 +33,58 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-import java.lang.*;
-import java.io.*;
-import java.net.*;
 
-import com.sun.ejte.ccl.reporter.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
 
-public class WebTest extends Thread{
-    
-    private static int count = 0;
-    private static int EXPECTED_COUNT = 1;
-    private static boolean executed = false;
-    
-    static SimpleReporterAdapter stat=
-        new SimpleReporterAdapter("appserv-tests");
+import com.sun.appserv.test.util.results.SimpleReporterAdapter;
+
+public class WebTest extends Thread {
+    private static final SimpleReporterAdapter stat = new SimpleReporterAdapter("appserv-tests", "dosBrokenPost");
 
     public static void main(String args[]) {
-
         // The stat reporter writes out the test info and results
         // into the top-level quicklook directory during a run.
-      
         stat.addDescription("Double content-length header");
-
         String host = args[0];
         String portS = args[1];
         String contextRoot = args[2];
-
-        int port = new Integer(portS).intValue();
-        String name;
-        
+        int port = new Integer(portS);
         try {
-            goGet(host, port, contextRoot + "/ServletTest" );
-            
+            goGet(host, port, contextRoot + "/ServletTest");
         } catch (Throwable t) {
-        } finally{
-            if (count != EXPECTED_COUNT){
-                stat.addStatus("dosBrokenPost", stat.FAIL);
-            }           
         }
-
-        stat.printSummary("web/dosBrokenPost---> expect 1 PASS");
+        stat.printSummary();
     }
 
     private static void goGet(String host, int port,
-                              String contextPath)
-         throws Exception {
+        String contextPath)
+        throws Exception {
         Socket s = new Socket(host, port);
         OutputStream os = s.getOutputStream();
-
-        System.out.println(("POST " + contextPath + " HTTP/1.1\n"));
         os.write(("POST " + contextPath + " HTTP/1.1\n").getBytes());
-        os.write(("Host: localhost\r").getBytes());
+        os.write("Host: localhost\r".getBytes());
         os.write("content-length: 0\r".getBytes());
         os.write("content-length: 10\r".getBytes());
         os.write("content-type: application/x-www-form-urlencoded\r".getBytes());
         os.write("a\r\n".getBytes());
         InputStream is = s.getInputStream();
         BufferedReader bis = new BufferedReader(new InputStreamReader(is));
-        String line = null;
-
-        try{
-            int index;
-            int i=0;
+        String line;
+        try {
+            int i = 0;
             while ((line = bis.readLine()) != null) {
                 System.out.println(i + ":" + line);
                 i++;
             }
-            if (i == 0){
-                stat.addStatus("dosBrokenPost", stat.PASS);
+            if (i == 0) {
+                stat.addStatus("dosBrokenPost", SimpleReporterAdapter.PASS);
             }
-        } catch( Exception ex){
-            ex.printStackTrace();   
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-   }
+    }
 }
