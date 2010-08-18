02fe4861a387ce408221efa75cf9f103bf6d38c3
Mahesh Kannan <mahesh.kannan@oracle.com>
2010-08-18 17:01:56 +0000

    Support for passing mapping info between web and ejb containers.
    svn path=/trunk/; revision=39879
    
    
    Former-commit-id: 87fa2ee49258463f8abeaa9af48abf7a9ed84704

---
+++ v3/common/container-common/src/main/java/org/glassfish/ha/common/HACookieInfo.java
@@ -0,0 +1,110 @@
+/*
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.ha.common;
+
+/**
+ * A Class to hold various info for the {KeyMapper}
+ *  from HTTP cookies
+ *
+ * @author Mahesh Kannan
+ *
+ */
+public class HACookieInfo {
+
+    String cookie;
+
+    Object hashKey;
+
+    String replica;
+
+    boolean initialized;
+
+    public HACookieInfo(String cookie) {
+        initialize(cookie);
+    }
+
+    public HACookieInfo initialize(String cookie) {
+        if (cookie != null && cookie.trim().length() > 0) {
+            this.cookie = cookie.trim();
+            String[] args = cookie.split(":");
+            replica = args[0];
+            initialized = true;
+        }
+
+        return this;
+    }
+
+    void reset() {
+        hashKey = null;
+        replica = null;
+        initialized = false;
+    }
+
+    public String getCookie() {
+        return cookie;
+    }
+
+    public boolean isInitialized() {
+        return initialized;
+    }
+
+    public Object getHashKey() {
+        return hashKey;
+    }
+
+    public void setHashKey(Object hashKey) {
+        this.hashKey = hashKey;
+    }
+
+    public String getReplica() {
+        return replica;
+    }
+
+    public void setReplica(String replica) {
+        this.replica = replica;
+    }
+
+    @Override
+    public String toString() {
+        return "HACookieInfo{" +
+                "cookie='" + cookie + '\'' +
+                ", hashKey=" + hashKey +
+                ", replica='" + replica + '\'' +
+                ", initialized=" + initialized +
+                '}';
+    }
+}
