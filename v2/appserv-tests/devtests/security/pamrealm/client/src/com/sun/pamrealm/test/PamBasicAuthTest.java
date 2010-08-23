f4349e39fc9e688078fd8497009b16d73a09588e
Sudarsan Sridhar <nasradu8@java.net>
2010-08-23 11:11:49 +0000

    Print report Summary
    svn path=/trunk/; revision=40034
    
    
    Former-commit-id: 11b39652c930f52c10850c0964e36bc642cf0815

--- v2/appserv-tests/devtests/security/pamrealm/client/src/com/sun/pamrealm/test/PamBasicAuthTest.java
+++ v2/appserv-tests/devtests/security/pamrealm/client/src/com/sun/pamrealm/test/PamBasicAuthTest.java
@@ -1,7 +1,4 @@
 package com.sun.pamrealm.test;
-import java.lang.*;
-import java.io.*;
-import java.net.*;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
@@ -9,32 +6,29 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.auth.AuthScope;
 import com.sun.ejte.ccl.reporter.SimpleReporterAdapter;
 
-
-public class PamBasicAuthTest
-{
+public class PamBasicAuthTest {
 
     public static final int DEFAULT_HTTP_PORT = 8080;
     public static final String DEFAULT_HOST = "localhost";
     public static final String CONTEXT_ROOT = "pamrealmsimpleweb";
-    public static final String testId = "Security: PamRealm";
+    public static final String testId = "SEC: PamRealm";
     public static final String EXPECTED_RESPONSE = "This is a protected page";
 
-    public static void main(String args[])
-    {
-       SimpleReporterAdapter stat=
-            new SimpleReporterAdapter("appserv-tests");
+    public static void main(String args[]) {
+        SimpleReporterAdapter stat =
+                new SimpleReporterAdapter("appserv-tests");
 
         // The stat reporter writes out the test info and results
         // into the top-level quicklook directory during a run.
 
-        stat.addDescription("PamRealm  Web Authentication Test");
+        stat.addDescription("PamRealm Web Authentication Test");
 
         HttpClient client = new HttpClient();
 
         String host = args[0];
 
-        if( host == null) {
-            host = DEFAULT_HOST ;
+        if (host == null) {
+            host = DEFAULT_HOST;
         }
 
         String strPort = args[1];
@@ -42,39 +36,36 @@ public class PamBasicAuthTest
         Integer port;
         try {
             port = Integer.valueOf(strPort);
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             port = DEFAULT_HTTP_PORT;
         }
 
         String userName = args[2];//username
         String password = args[3];//password
 
-        client.getState().setCredentials(new AuthScope(host, port, "pam"), new UsernamePasswordCredentials(userName,password));
+        client.getState().setCredentials(new AuthScope(host, port, "pam"), new UsernamePasswordCredentials(userName, password));
 
-        String url = "http://"+host+":"+port+"/"+CONTEXT_ROOT;
+        String url = "http://" + host + ":" + port + "/" + CONTEXT_ROOT;
 
         GetMethod get = new GetMethod(url);
-        get.setDoAuthentication( true );
+        get.setDoAuthentication(true);
         String response = "";
         try {
-           int status = client.executeMethod(get);
-           response = get.getResponseBodyAsString();
-           System.out.println("Obtained response.."+response);
-        } catch(Exception ex) {
-           ex.printStackTrace();
-            stat.addStatus(testId,stat.FAIL);
+            int status = client.executeMethod(get);
+            response = get.getResponseBodyAsString();
+            System.out.println("Obtained response.." + response);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            stat.addStatus(testId, stat.FAIL);
         }
         if (response.trim().equals(EXPECTED_RESPONSE.trim())) {
-            stat.addStatus(testId,stat.PASS);
-       System.out.println("PASS");
-        }
-        else {
-            stat.addStatus(testId,stat.FAIL);
-          System.out.println("FAIL");
+            stat.addStatus(testId, stat.PASS);
+            System.out.println("PASS");
+        } else {
+            stat.addStatus(testId, stat.FAIL);
+            System.out.println("FAIL");
 
         }
-
+        stat.printSummary();
     }
-
 }
