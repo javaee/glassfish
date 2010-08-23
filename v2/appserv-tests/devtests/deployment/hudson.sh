09d108def64eeb548790378f1c2681f04cba4509
Tim Quinn <tim.quinn@oracle.com>
2010-08-23 06:56:28 +0000

    still trying to get the exit status right
    svn path=/trunk/; revision=40024
    
    
    Former-commit-id: 1e3515f9a7e1d3e707e3dbf0de0156e7644c7c31

--- v2/appserv-tests/devtests/deployment/hudson.sh
+++ v2/appserv-tests/devtests/deployment/hudson.sh
@@ -34,7 +34,9 @@ then
     $S1AS_HOME/bin/asadmin stop-domain
 fi
 egrep '\[FAILED|UNKNOWN\]' client.log >> /dev/null
-if [ $? -eq 0 ]
+sts=$?
+echo Just finished egrep
+if [ $sts -eq 0 ]
 then
   echo Found FAILED or UNKNOWN
   exit 1
