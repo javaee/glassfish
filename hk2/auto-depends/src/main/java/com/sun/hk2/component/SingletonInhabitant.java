50bd70397a0b06a9dd2b958053a21773633a2403
jeff_trent <jeff_trent@java.net>
2010-09-02 21:14:42 +0000

    alternative fix for 13107 due to problems in distribution/web.  Also may address 13108 regression too but unchecked since no repro case was provided

--- hk2/auto-depends/src/main/java/com/sun/hk2/component/SingletonInhabitant.java
+++ hk2/auto-depends/src/main/java/com/sun/hk2/component/SingletonInhabitant.java
@@ -59,23 +59,25 @@ public class SingletonInhabitant<T> extends AbstractWombInhabitantImpl<T> {
         if (object==null) {
             synchronized(this) {
                 if (object==null) {
-                  if (initializing) {
-                    throw new ComponentException("problem initializing - cycle detected involving: " + this);
-                  }
-                  initializing = true;
-                  
                   // we do it in two steps in case there is an initialization error, we 
                   // want to avoid recursing
-                  T object = womb.create(onBehalfOf);
+                  object = womb.create(onBehalfOf);
                   try {
+                    initializing = true;
                     womb.initialize(object, onBehalfOf);
-                    this.object = object;
                   } catch (Throwable e) {
+                    object = null;
                     throw (e instanceof ComponentException) ? 
                         (ComponentException)e : new ComponentException("problem initializing: " + object, e);
                   } finally {
                     initializing = false;
                   }
+                }
+            }
+        } else if (initializing) {
+            synchronized(this) {
+                if (initializing) {
+                  throw new ComponentException("problem initializing - cycle detected involving: " + this);
                 }
             }
         }
