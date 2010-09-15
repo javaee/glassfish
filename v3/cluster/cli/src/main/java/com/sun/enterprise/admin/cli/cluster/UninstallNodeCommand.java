9417a296cf98697b215ae0d74cb0291260ea79e1
Rajiv Mordani <mode@java.net>
2010-09-15 06:45:56 +0000

    Uninstall node command and fix imports for install node command
    svn path=/trunk/; revision=40891
    
    
    Former-commit-id: 6839fa6267401100ef7d8b4144976f5990be782d

--- v3/cluster/cli/src/main/java/com/sun/enterprise/admin/cli/cluster/UninstallNodeCommand.java
+++ v3/cluster/cli/src/main/java/com/sun/enterprise/admin/cli/cluster/UninstallNodeCommand.java
@@ -43,10 +43,9 @@ package com.sun.enterprise.admin.cli.cluster;
 
 import com.sun.enterprise.admin.cli.CLICommand;
 import com.sun.enterprise.admin.cli.remote.RemoteCommand;
-import com.sun.enterprise.util.io.FileListerRelative;
-import com.sun.enterprise.util.zip.ZipFileException;
-import com.sun.enterprise.util.zip.ZipWriter;
+import com.sun.enterprise.config.serverbeans.Node;
 import com.trilead.ssh2.SCPClient;
+import com.trilead.ssh2.SFTPv3DirectoryEntry;
 import org.glassfish.api.Param;
 import org.glassfish.api.admin.CommandException;
 import org.glassfish.api.admin.ExecuteOn;
@@ -59,19 +58,20 @@ import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.component.PerLookup;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Rajiv Mordani
  */
 
 
-@Service(name = "install-node")
+@Service(name = "uninstall-node")
 @Scoped(PerLookup.class)
 @ExecuteOn({RuntimeType.DAS})
-public class InstallNodeCommand extends CLICommand {
+public class UninstallNodeCommand extends CLICommand {
 
     @Param(optional = true)
     private String sshuser;
@@ -82,10 +82,6 @@ public class InstallNodeCommand extends CLICommand {
     @Param(optional = true)
     private String sshkeyfile;
 
-
-    @Param(name="archivedir", optional = true)
-    private String archiveDir;
-
     @Param(optional = false, primary = true, multiple = true)
     private String[] hosts;
 
@@ -95,63 +91,75 @@ public class InstallNodeCommand extends CLICommand {
     private String sshpassword;
 
     private String sshkeypassphrase=null;
-    
+
     private boolean promptPass=false;
 
     @Inject
     SSHLauncher sshLauncher;
 
-    protected void validate() throws CommandException {
+    @Inject
+    Node[] nodeList;
 
+    protected void validate() throws CommandException {
+        for (String host: hosts) {
+            for (Node node: nodeList) {
+                if (node.getNodeHost().equals(host)) {
+                    throw new CommandException("delete-node-ssh needs to be called to delete node for " + host + " before uninstall-node is called");
+                }
+            }
+        }
     }
 
     @Override
     protected int executeCommand() throws CommandException {
         try {
             String baseRootValue = executeLocationsCommand();
-            ArrayList<String>  binDirFiles = new ArrayList<String>();
-            File zipFile = createZipFile(baseRootValue, binDirFiles);
-            copyToHosts(zipFile, baseRootValue, binDirFiles);
+            deleteFromHosts(baseRootValue);
         } catch (IOException ioe) {
             throw new CommandException(ioe);
-        } catch (ZipFileException ze) {
-            throw new CommandException(ze);
-        } catch (InterruptedException e) {
+        }  catch (InterruptedException e) {
             throw new CommandException(e);
         }
 
         return SUCCESS;
     }
 
-    private void copyToHosts(File zipFile, String baseRootValue, ArrayList<String> binDirFiles) throws IOException, InterruptedException {
+    private void deleteFromHosts(String baseRootValue) throws IOException, InterruptedException {
 
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
         if (installLocation == null) {
             installLocation = baseRootValue;
         }
-        
+
         for (String host: hosts) {
             sshLauncher.init(sshuser, host, sshport, sshpassword, sshkeyfile, sshkeypassphrase, logger);
-            String remoteDir = installLocation + "/glassfishv3/glassfish";
 
             SFTPClient sftpClient = sshLauncher.getSFTPClient();
-            SCPClient scpClient = sshLauncher.getSCPClient();
-            
-            if (!sftpClient.exists(remoteDir)) {
-                sftpClient.mkdirs(remoteDir, 0755);
-            }
-            
 
-            scpClient.put(zipFile.getAbsolutePath(), remoteDir);
-            //String unzipCommand = "cd " + remoteDir + "; unzip glassfish.zip; chmod +x bin/*";
-            String unzipCommand = "cd " + remoteDir + "; jar -xvf glassfish.zip";
-            sshLauncher.runCommand(unzipCommand, outStream);
-            sftpClient.rm(remoteDir + "/glassfish.zip");
-            for (String binDirFile: binDirFiles) {
-                sftpClient.chmod((remoteDir + "/" + binDirFile), 0755);
-            }
-            
 
+            if (!sftpClient.exists(installLocation)) {
+                throw new IOException (installLocation + " Directory does not exist");
+            }
+
+            //ArrayList<String> remoteDirectories = new ArrayList<String>();
+            //deleteRemoteFiles(sftpClient, installLocation, remoteDirectories);
+            deleteRemoteFiles(sftpClient, installLocation);
+            sftpClient.rmdir(installLocation);
+        }
+    }
+
+    //private void deleteRemoteFiles(SFTPClient sftpClient, String dir,
+    //ArrayList<String> remoteDirectories) {
+    private void deleteRemoteFiles(SFTPClient sftpClient, String dir)
+    throws IOException {
+        for (SFTPv3DirectoryEntry directoryEntry: (List<SFTPv3DirectoryEntry>)sftpClient.ls(dir)) {
+            if (directoryEntry.filename.equals(".") || directoryEntry.filename.equals("..")) {
+                continue;
+            } else if (directoryEntry.attributes.isDirectory()) {
+                deleteRemoteFiles(sftpClient, dir+"/"+directoryEntry.filename);
+                sftpClient.rmdir(dir  +"/"+directoryEntry.filename);
+            } else {
+                sftpClient.rm(dir+"/"+directoryEntry.filename);
+            }
         }
     }
 
@@ -161,58 +169,6 @@ public class InstallNodeCommand extends CLICommand {
         Map<String, String> attrs =
                 cmd.executeAndReturnAttributes(new String[]{"__locations"});
         return attrs.get("Base-Root_value");
-       
-    }
-
-    private File createZipFile(String baseRootValue, ArrayList<String> binDirFiles) throws IOException, ZipFileException {
-
-        File installRoot = new File(baseRootValue); 
-
-        File zipFileLocation = null;
-
-        if (archiveDir != null) {
-            zipFileLocation = new File(archiveDir);
-            if (!zipFileLocation.canWrite()) {
-                throw new IOException ("Cannot write to " + archiveDir);
-            }
-        } else if (installRoot.canWrite()) {
-            zipFileLocation = installRoot;
-        } else {
-            zipFileLocation = new File(System.getProperty("java.io.tmpdir"));
-        }
-
-
-        FileListerRelative lister = new FileListerRelative(installRoot);
-        //lister.keepEmptyDirectories();	// we want to restore any empty directories too!
-        String[] files = lister.getFiles();
-
-        List<String> resultFiles1 = Arrays.asList(files);
-        ArrayList<String> resultFiles = new ArrayList<String>(resultFiles1);
-
-        Iterator<String> iter = resultFiles.iterator();
-        while(iter.hasNext()) {
-            String fileName = iter.next();
-            if (fileName.contains("domains") || fileName.contains("nodes")) {
-                iter.remove();
-            } else if (fileName.startsWith("bin")) {
-                binDirFiles.add(fileName);
-            }
-        }
-
-        File glassFishZipFile = new File(zipFileLocation, "glassfish.zip");
-        if (glassFishZipFile.exists()) {
-            return glassFishZipFile;
-        }
-
-
-       String [] filesToZip = new String[resultFiles.size()];
-        filesToZip = resultFiles.toArray(filesToZip);
-
-        ZipWriter writer = new ZipWriter(glassFishZipFile.getCanonicalPath(), installRoot.toString(), filesToZip);
-        writer.safeWrite();
-
-        return glassFishZipFile;
 
     }
-
 }
