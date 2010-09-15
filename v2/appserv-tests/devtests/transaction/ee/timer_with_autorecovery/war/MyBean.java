fb3eb3689eeed4bf7981cb448fe54fe7923e5e47
Marina Vatkina <marina.vatkina@oracle.com>
2010-09-15 23:36:43 +0000

    A test to test a failure recovery with a combination of a timer and a failed 2PC
    svn path=/trunk/; revision=40919
    
    
    Former-commit-id: 86c9550e5be032dd91f0fe2cb6d3941502eaa32a

---
+++ v2/appserv-tests/devtests/transaction/ee/timer_with_autorecovery/war/MyBean.java
@@ -0,0 +1,114 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.acme;
+
+import java.sql.*;
+import javax.ejb.*;
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.annotation.Resource;
+import java.util.HashSet;
+
+/**
+ *
+ * @author marina vatkina
+ */
+
+@Stateless
+public class MyBean {
+
+    private static final String DEF_RESOURCE = "jdbc/__default";
+    private static final String XA_RESOURCE = "jdbc/xa";
+
+    private HashSet<String> timers = new HashSet<String>();
+    @Resource TimerService ts;
+
+    @Timeout
+    private void timeout(Timer t) {
+        System.err.println("In ___MyBean:timeout___ "  + t.getInfo() );
+        timers.add(""+t.getInfo());
+    }
+
+
+    public String verifydefault() throws Exception {
+        InitialContext initCtx = new InitialContext();
+        DataSource ds = (DataSource) initCtx.lookup(DEF_RESOURCE);
+
+        return ""+verify(ds);
+   }
+
+    public String verifyxa() throws Exception {
+        InitialContext initCtx = new InitialContext();
+        DataSource ds = (DataSource) initCtx.lookup(XA_RESOURCE);
+
+        return "" + verify(ds) + "+" + timers.size();
+   }
+
+    public int verify(DataSource ds) throws Exception {
+        String selectStatement = "select * from student";
+        Connection c = ds.getConnection();
+        PreparedStatement ps = c.prepareStatement(selectStatement);
+        ResultSet rs = ps.executeQuery();
+        int result = 0;
+        while (rs.next()) {
+            result++;
+            System.out.println("Found: " + rs.getString(1) + " : " + rs.getString(2));
+        }
+        rs.close();
+        ps.close();
+        c.close();
+
+        return result;
+    }
+
+    public boolean create_timer(int id) throws Exception {
+        ts.createTimer(1000, 5000, "timer01 " + id);
+        return true;
+    }
+
+    public boolean testtwo(int id) throws Exception {
+        InitialContext initCtx = new InitialContext();
+        DataSource ds1 = (DataSource) initCtx.lookup(DEF_RESOURCE);
+        DataSource ds2 = (DataSource) initCtx.lookup(XA_RESOURCE);
+
+        boolean res1 = test(id, ds1, true);
+        boolean res2 = test(id, ds2, true);
+        return res1 && res2;
+    }
+
+    private boolean test(int id, DataSource ds, boolean useFailureInducer) throws Exception {
+        String insertStatement = "insert into student values ( ? , ? )";
+        Connection c = ds.getConnection();
+        PreparedStatement ps = c.prepareStatement(insertStatement);
+
+        if (useFailureInducer) {
+            com.sun.jts.utils.RecoveryHooks.FailureInducer.activateFailureInducer();
+            com.sun.jts.utils.RecoveryHooks.FailureInducer.setWaitPoint(com.sun.jts.utils.RecoveryHooks.FailureInducer.PREPARED, 60);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.err.println("Call # " + (i + 1));
+            ps.setString(1, "BAA" + id + i);
+            ps.setString(2, "BBB" + id + i);
+            ps.executeUpdate();
+
+            if (!useFailureInducer) {
+                try {
+                    Thread.sleep(7000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        ps.close();
+        c.close();
+        System.err.println("Insert successfully");
+
+        return true;
+    }
+
+}
