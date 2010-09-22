565b993cfbe8eb7deb6eafa76b543711e4038253
Jagadish Ramu <jagadish.ramu@oracle.com>
2010-09-22 11:29:39 +0000

    Reenabling test
    svn path=/trunk/; revision=41040
    
    
    Former-commit-id: 50a096cad7fcde91b15a1254ad0bbdd74ea8dddf

--- v2/appserv-tests/devtests/connector/v3/resultCount.sh
+++ v2/appserv-tests/devtests/connector/v3/resultCount.sh
@@ -4,7 +4,7 @@
 FILE=$APS_HOME/test_resultsValid.xml
 echo "input file=$FILE"
 
-TOTAL=388
+TOTAL=400
 PASSED=`grep "\"pass\"" $FILE | wc -l`
 FAILED=`grep "\"fail\"" $FILE | wc -l`
 TOTAL_RUN=`expr $PASSED + $FAILED `
