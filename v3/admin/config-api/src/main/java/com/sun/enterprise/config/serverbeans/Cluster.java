20cc0ed55e1415bb313ea04e15a132e28eb1e725
Joe Fialli <joe.fialli@oracle.com>
2010-10-04 23:31:58 +0000

    Unrevert changes that were removed day before MS5.
    Add gmsbindinterfaceaddress as an optional parameter to create-cluster.
    For cases that one may want to set this value directly (such as to set to local loopback when all clustered instances run on one machine),
    setting it on the commandline avoids an otherwise required restart of DAS after setting
    this value after cluster is created with an asadmin set command.
    
    Bobby reviewed change.
    
    
    
    svn path=/trunk/; revision=41362
    
    
    Former-commit-id: 650f25dbc2a27948f24a4c182b32cf9f84dc63b4

--- v3/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/Cluster.java
+++ v3/admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/Cluster.java
@@ -211,6 +211,7 @@ public interface Cluster extends ConfigBeanProxy, Injectable, PropertyBag, Named
      *              {@link String }
      * @throws PropertyVetoException if a listener vetoes the change
      */
+    @Param(name="gmsbindinterfaceaddress", optional=true)
     void setGmsBindInterfaceAddress(String value) throws PropertyVetoException;
 
     /**
@@ -541,10 +542,11 @@ public interface Cluster extends ConfigBeanProxy, Injectable, PropertyBag, Named
                         "Cluster software is not installed"));
             }
             final String instanceName = instance.getName();
-            instance.setGmsBindInterfaceAddress(String.format(
-                "${GMS-BIND-INTERFACE-ADDRESS-%s}",
-                instanceName));
-
+            if (instance.getGmsBindInterfaceAddress() == null) {
+                instance.setGmsBindInterfaceAddress(String.format(
+                    "${GMS-BIND-INTERFACE-ADDRESS-%s}",
+                    instanceName));
+            }
             if (instance.getGmsMulticastAddress() == null) {
                 instance.setGmsMulticastAddress(generateHeartbeatAddress());
             }
