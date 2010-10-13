db1dc517f1fc64b9ba3f1e9cb1a6431877186e07
Jennifer Galloway <jc129909>
2010-10-13 22:08:28 +0000

    Issue 12490 - clean-up
    svn path=/trunk/; revision=41706
    
    
    Former-commit-id: efa66e3b4f864ff95aee2de197dcd95aa9d49b94

--- v3/admin/monitor/src/main/java/org/glassfish/admin/monitor/jvm/JVMMemoryStatsProvider.java
+++ v3/admin/monitor/src/main/java/org/glassfish/admin/monitor/jvm/JVMMemoryStatsProvider.java
@@ -42,7 +42,6 @@ package org.glassfish.admin.monitor.jvm;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
 import org.glassfish.external.statistics.CountStatistic;
 import org.glassfish.external.statistics.impl.CountStatisticImpl;
 import org.glassfish.gmbal.AMXMetadata;
@@ -57,6 +56,7 @@ import org.glassfish.gmbal.ManagedObject;
 @ManagedObject
 @Description( "JVM Memory Statistics" )
 public class JVMMemoryStatsProvider {
+    private MemoryMXBean memBean = ManagementFactory.getMemoryMXBean();
 
     private CountStatisticImpl committedHeap = new CountStatisticImpl(
             "CommittedHeapSize", "bytes",
@@ -89,63 +89,63 @@ public class JVMMemoryStatsProvider {
     @ManagedAttribute(id="committedheapsize-count")
     @Description( "amount of memory in bytes that is committed for the Java virtual machine to use" )
     public CountStatistic getCommittedHeap() {
-        committedHeap.setCount(ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getCommitted());
+        committedHeap.setCount(memBean.getHeapMemoryUsage().getCommitted());
         return committedHeap;
     }
 
     @ManagedAttribute(id="initheapsize-count")
     @Description( "amount of memory in bytes that the Java virtual machine initially requests from the operating system for memory management" )
     public CountStatistic getInitHeap() {
-        initHeap.setCount(ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getInit());
+        initHeap.setCount(memBean.getHeapMemoryUsage().getInit());
         return initHeap;
     }
 
     @ManagedAttribute(id="maxheapsize-count")
     @Description( "maximum amount of memory in bytes that can be used for memory management" )
     public CountStatistic getMaxHeap() {
-        maxHeap.setCount(ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax());
+        maxHeap.setCount(memBean.getHeapMemoryUsage().getMax());
         return maxHeap;
     }
 
     @ManagedAttribute(id="usedheapsize-count")
     @Description( "amount of used memory in bytes" )
     public CountStatistic getUsedHeap() {
-        usedHeap.setCount(ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed());
+        usedHeap.setCount(memBean.getHeapMemoryUsage().getUsed());
         return usedHeap;
     }
 
     @ManagedAttribute(id="committednonheapsize-count")
     @Description( "amount of memory in bytes that is committed for the Java virtual machine to use" )
     public CountStatistic getCommittedNonHeap() {
-        committedNonHeap.setCount(ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getCommitted());
+        committedNonHeap.setCount(memBean.getNonHeapMemoryUsage().getCommitted());
         return committedNonHeap;
     }
 
     @ManagedAttribute(id="initnonheapsize-count")
     @Description( "amount of memory in bytes that the Java virtual machine initially requests from the operating system for memory management" )
     public CountStatistic getInitNonHeap() {
-        initNonHeap.setCount(ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getInit());
+        initNonHeap.setCount(memBean.getNonHeapMemoryUsage().getInit());
         return initNonHeap;
     }
 
     @ManagedAttribute(id="maxnonheapsize-count")
     @Description( "maximum amount of memory in bytes that can be used for memory management" )
     public CountStatistic getMaxNonHeap() {
-        maxNonHeap.setCount(ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getMax());
+        maxNonHeap.setCount(memBean.getNonHeapMemoryUsage().getMax());
         return maxNonHeap;
     }
 
     @ManagedAttribute(id="usednonheapsize-count")
     @Description( "amount of used memory in bytes" )
     public CountStatistic getUsedNonHeap() {
-        usedNonHeap.setCount(ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getUsed());
+        usedNonHeap.setCount(memBean.getNonHeapMemoryUsage().getUsed());
         return usedNonHeap;
     }
 
     @ManagedAttribute(id="objectpendingfinalizationcount-count")
     @Description( "approximate number of objects for which finalization is pending" )
     public CountStatistic getObjectPendingFinalizationCount() {
-        objectPendingFinalizationCount.setCount(ManagementFactory.getMemoryMXBean().getObjectPendingFinalizationCount());
+        objectPendingFinalizationCount.setCount(memBean.getObjectPendingFinalizationCount());
         return objectPendingFinalizationCount;
     }
 }
