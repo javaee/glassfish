6d2bc6b6ea23f02d05308d99f9f98052e41d04a4
Byron Nevins <byron.nevins@oracle.com>
2010-10-18 20:21:24 +0000

    https://glassfish.dev.java.net/issues/show_bug.cgi?id=13316 Changed the message given to user after success
    svn path=/trunk/; revision=41810
    
    
    Former-commit-id: 2f0fb3f55e23d7408967133ad01cc339f19052cc

--- v3/admin/cli-optional/src/main/java/com/sun/enterprise/admin/cli/optional/CreateServiceCommand.java
+++ v3/admin/cli-optional/src/main/java/com/sun/enterprise/admin/cli/optional/CreateServiceCommand.java
@@ -155,7 +155,7 @@ public final class CreateServiceCommand extends CLICommand {
             // Why the messiness?  We don't want to talk about the help
             // file inside the help file thus the complications below...
             String help = service.getSuccessMessage();
-            String tellUserAboutHelp = strings.get("create.service.runtimeHelp", help);
+            String tellUserAboutHelp = strings.get("create.service.runtimeHelp", help, dirs.getServerDir());
             logger.info(tellUserAboutHelp);
             service.writeReadmeFile(help);
 
