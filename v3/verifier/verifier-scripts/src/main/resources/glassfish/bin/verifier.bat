4eddaebe38528ca4039e305b1e4664b01f0a2990
Snjezana Sevo-Zenzerovic <snjezana.sevozenzerovic@oracle.com>
2010-10-22 21:47:56 +0000

    Adjusted javahelp.jar location. Partial fix for issue 8250.
    svn path=/trunk/; revision=42002
    
    
    Former-commit-id: 0ac05ff9112b1ce160f2f7757573f8104c894941

--- v3/verifier/verifier-scripts/src/main/resources/glassfish/bin/verifier.bat
+++ v3/verifier/verifier-scripts/src/main/resources/glassfish/bin/verifier.bat
@@ -39,4 +39,4 @@ REM  only if the new code is made subject to such option by the copyright
 REM  holder.
 REM
 
-java -Dorg.osgi.framework.storage="%TMPDIR\verifier-cache\" -Dorg.jvnet.hk2.osgimain.autostartBundles=osgi-adapter.jar -cp "%~dp0..\modules\glassfish.jar;%~dp0..\modules\verifier.jar" com.sun.enterprise.tools.verifier.VerifierOSGiMain "%*"
+java -Dorg.osgi.framework.storage="%TMPDIR\verifier-cache\" -Dorg.jvnet.hk2.osgimain.autostartBundles=osgi-adapter.jar -cp "%~dp0..\modules\glassfish.jar;%~dp0..\modules\verifier.jar;%~dp0..\lib\javahelp.jar" com.sun.enterprise.tools.verifier.VerifierOSGiMain "%*"
