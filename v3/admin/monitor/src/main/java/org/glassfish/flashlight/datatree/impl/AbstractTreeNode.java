90aff7d021d062dcccce6f3a738ad4ac10dae157
Byron Nevins <byron.nevins@oracle.com>
2010-10-22 21:28:22 +0000

    Added @Overload annotations to the world's fattest interface. The problem with huge interfaces -- it forces the impl. class to be almost completely public...
    svn path=/trunk/; revision=41999
    
    
    Former-commit-id: 9cda151af052fb6bc759c6fdaa37356bef917cd9

--- v3/admin/monitor/src/main/java/org/glassfish/flashlight/datatree/impl/AbstractTreeNode.java
+++ v3/admin/monitor/src/main/java/org/glassfish/flashlight/datatree/impl/AbstractTreeNode.java
@@ -82,10 +82,12 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
     private String STAR = "*";
 
 
+    @Override
     public String getName() {
         return this.name;
     }
 
+    @Override
     public void setName(String name) {
         if (name == null) {
             throw new RuntimeException("Flashlight-utils: Tree Node needs a" +
@@ -95,6 +97,7 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
     }
 
     // should be implemented at the sub-class level
+    @Override
     public Object getValue() {
         if (enabled) {
             return getChildNodes();
@@ -102,14 +105,17 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         return null;
     }
 
+    @Override
     public boolean isEnabled() {
         return this.enabled;
     }
 
+    @Override
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
 
+    @Override
     public TreeNode addChild(TreeNode newChild) {
         if (newChild == null) {
             return null;
@@ -121,6 +127,7 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         return children.put(newChild.getName(), newChild);
     }
 
+    @Override
     public String getCompletePathName() {
 
         if (getParent() != null) {
@@ -131,10 +138,12 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         }
     }
 
+    @Override
     public void setParent(TreeNode parent) {
         this.parent = parent;
     }
 
+    @Override
     public TreeNode getParent() {
         return this.parent;
     }
@@ -143,6 +152,7 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
      * Returns a mutable view of the children
      * @return
      */
+    @Override
     public Collection<TreeNode> getChildNodes() {
         return children.values();
     }
@@ -151,6 +161,7 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
      * Returns a mutable view of the children
      * @return
      */
+    @Override
     public Collection<TreeNode> getEnabledChildNodes() {
         List<TreeNode> childNodes = new ArrayList();
         for (TreeNode child : children.values()) {
@@ -165,11 +176,13 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         return ((ConcurrentHashMap) children).elements();
     }
 
+    @Override
     public boolean hasChildNodes() {
         return !children.isEmpty();
 
     }
 
+    @Override
     public void removeChild(TreeNode oldChild) {
         String child = oldChild.getName();
         if (child != null) {
@@ -177,22 +190,27 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         }
     }
 
+    @Override
     public String getCategory() {
         return category;
     }
 
+    @Override
     public void setCategory(String category) {
         this.category = category;
     }
 
+    @Override
     public String getDescription (){
         return this.description;
     }
 
+    @Override
     public void setDescription (String description){
         this.description = description;
     }
     
+    @Override
     public TreeNode getChild(String childName) {
         if (childName == null) {
             return null;
@@ -201,12 +219,13 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         }
     }
 
+    @Override
     public TreeNode getNode(String completeName) {
 
         if (completeName == null) {
             return null;
         }
-        Pattern pattern = Pattern.compile(this.REGEX);
+        Pattern pattern = Pattern.compile(AbstractTreeNode.REGEX);
         String[] tokens = pattern.split(completeName);
         TreeNode n = findNodeInTree(tokens);
         return n;
@@ -246,6 +265,7 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
      * Returns all the nodes under the current tree
      * @return List of all nodes in the current tree
      */
+    @Override
     public List<TreeNode> traverse(boolean ignoreDisabled) {
 //        System.out.println ("Node: " + this.getName ()+ " is enabled "+isEnabled());
         List<TreeNode> list = new ArrayList<TreeNode>();
@@ -268,6 +288,7 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         return list;
     }
 
+    @Override
     public List<TreeNode> getNodes(String pattern, boolean ignoreDisabled, boolean gfv2Compatible) {
         pattern = pattern.replace("\\.","\\\\\\."); 
         List<TreeNode> regexMatchedTree = new ArrayList<TreeNode>();
@@ -294,13 +315,14 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         return regexMatchedTree;
     }
 
+    @Override
     public List<TreeNode> getNodes (String pattern){
         return getNodes (pattern, true, true);
     }
 
     private String convertGFv2PatternToRegex (String pattern){
         if (pattern.equals (STAR)){
-           return new String (".*");
+           return ".*";
         }
         // Doing this intermediate step as replacing "*" in a pattern with ".*"
         // is too hassling
@@ -310,10 +332,12 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         return regex;
     }
     
+    @Override
     public int compareTo(Object o) {
         return this.getName().compareTo(((TreeNode)o).getName());
     }
 
+    @Override
     public TreeNode getPossibleParentNode(String pattern) {
         // simplify by bailing out early if preconditions are not met...
         if(pattern == null || pattern.length() <= 0 || pattern.indexOf('*') >= 0)
@@ -323,13 +347,13 @@ public abstract class AbstractTreeNode implements TreeNode, Comparable {
         int     longest = 0;
         
         for(TreeNode n : traverse(true)) {
-            String name = n.getCompletePathName();
+            String aname = n.getCompletePathName();
 
-            if(name == null)
+            if(aname == null)
                 continue;   // defensive pgming
 
-            if(pattern.startsWith(name)) {
-                int thisLength = name.length();
+            if(pattern.startsWith(aname)) {
+                int thisLength = aname.length();
 
                 // keep the longest match ONLY!
                 if(node == null || thisLength > longest) {
