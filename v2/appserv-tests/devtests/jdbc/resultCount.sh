b67bdfbadf2abc20a725ae9b9faf1f0b09a47971
Jagadish Ramu <jagadish.ramu@oracle.com>
2010-10-24 04:35:47 +0000

    Enabling jpa-dsd test
    svn path=/trunk/; revision=42028
    
    
    Former-commit-id: d72914f1f200c6f20be10ab6344bd648a9f5ab4d

--- v2/appserv-tests/devtests/jdbc/resultCount.sh
+++ v2/appserv-tests/devtests/jdbc/resultCount.sh
@@ -3,7 +3,7 @@
 FILE=$APS_HOME/test_resultsValid.xml
 echo "input file=$FILE"
 
-TOTAL=204
+TOTAL=208
 PASSED=`grep "\"pass\"" $FILE | wc -l`
 FAILED=`grep "\"fail\"" $FILE | wc -l`
 TOTAL_RUN=`expr $PASSED + $FAILED `
