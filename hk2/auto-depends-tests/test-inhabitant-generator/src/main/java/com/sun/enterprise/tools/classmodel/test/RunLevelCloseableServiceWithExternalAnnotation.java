45840336dab6e009c1765e9661d245c8b0b51759
jeff_trent <jeff_trent@java.net>
2010-10-25 14:25:58 +0000

    uncomment InhabitantsGeneratorTest#testFullHabitatFileGeneration @Ignore to see problem finding contracts from annotations in external module

---
+++ hk2/auto-depends-tests/test-inhabitant-generator/src/main/java/com/sun/enterprise/tools/classmodel/test/RunLevelCloseableServiceWithExternalAnnotation.java
@@ -0,0 +1,11 @@
+package com.sun.enterprise.tools.classmodel.test;
+
+import org.jvnet.hk2.annotations.Service;
+
+import com.sun.enterprise.tools.classmodel.test.external.ExternalAdminMetaAnnotation;
+
+@ExternalAdminMetaAnnotation
+@Service
+public class RunLevelCloseableServiceWithExternalAnnotation {
+
+}
