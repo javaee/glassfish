0e22cef91bb7db67ec49d13f0b7e38f54986993a
Bhavanishankara Sapaliga <bhavanishankara.sapaliga@oracle.com>
2010-10-30 20:17:55 +0000

    Fix for issue 12261
    Specify the contextroot when doing the directory deployment (embedded/common.xml build file change)
    
    In the @WebServlet used to file this issue was missing "urlPatterns="/ServletTest", which was also part of the problem. Fixed that too.
    
    
    svn path=/trunk/; revision=42339
    
    
    Former-commit-id: 9cfedb43e219ebdb0e29349cdaaf25e9f5347bdb

--- v2/appserv-tests/devtests/web/explodedWar/servlet/ServletTest.java
+++ v2/appserv-tests/devtests/web/explodedWar/servlet/ServletTest.java
@@ -45,7 +45,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet("/ServletTest")
+@WebServlet(name="/ServletTest", urlPatterns="/ServletTest")
 public class ServletTest extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
