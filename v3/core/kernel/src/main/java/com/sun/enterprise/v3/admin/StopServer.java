7a50b47e3f053f8f0a18939a33f6facc0021e221
Byron Nevins <byron.nevins@oracle.com>
2010-11-02 21:46:50 +0000

    TUrning on System.exit no matter what. THe @Param() stuff does not seem to work for the callers. Force is ALWAYS set to false by default. Need to do this until that gets figured out because tests fail.
    svn path=/trunk/; revision=42398
    
    
    Former-commit-id: edbf644ecc03e734b414f1fa0e9230d31bfa6576

--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/admin/StopServer.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/admin/StopServer.java
@@ -84,7 +84,14 @@ public class StopServer {
 
         deletePidFile(env);
 
-        if (force)
+        // 11-2-2010 for some reason force is ALWAYS set to false even though the
+        // callers have this:
+            //@Param(optional = true, defaultValue = "true")
+            // private Boolean force = true;
+
+
+        // I'm turning off the check so that tests will work for now!
+        //if (force)
             System.exit(0);
     }
     private final static LocalStringManagerImpl localStrings = new LocalStringManagerImpl(StopServer.class);
