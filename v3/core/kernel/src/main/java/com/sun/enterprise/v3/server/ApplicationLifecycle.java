bd81dcffbf1c9a51fd27091eff73fac743fa8014
Hong Zhang <hong.hz.zhang@oracle.com>
2010-11-02 19:08:21 +0000

    Fixed issue 13979. If the application is already deployed in the domain and another deployment is attempted, we should fail that deployment asking user to use the create-application-ref command instead. Ran QL, EE deployment dev tests.
    svn path=/trunk/; revision=42389
    
    
    Former-commit-id: 05a7d5cad402d1a3f7856f4bd3804e566d38cc6c

--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/server/ApplicationLifecycle.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/server/ApplicationLifecycle.java
@@ -1803,8 +1803,12 @@ public class ApplicationLifecycle implements Deployment, PostConstruct {
         boolean isRedeploy) {
         List<String> referencedTargets = domain.getAllReferencedTargetsForApplication(name);
         if (referencedTargets.isEmpty()) {
-            if (isRegistered(name) && !isRedeploy && target.equals("domain")) {
-                throw new IllegalArgumentException(localStrings.getLocalString("application.alreadyreg.redeploy", "Application with name {0} is already registered. Either specify that redeployment must be forced, or redeploy the application. Or if this is a new deployment, pick a different name.", name));
+            if (isRegistered(name)) {
+                if (!isRedeploy && target.equals("domain")) {
+                    throw new IllegalArgumentException(localStrings.getLocalString("application.alreadyreg.redeploy", "Application with name {0} is already registered. Either specify that redeployment must be forced, or redeploy the application. Or if this is a new deployment, pick a different name.", name));
+                } else {
+                    throw new IllegalArgumentException(localStrings.getLocalString("use.create_app_ref_2", "Application {0} is already deployed in this domain. Please use create application ref to create application reference on target {1}.", name, target));
+                }
             }
             return;
         }
