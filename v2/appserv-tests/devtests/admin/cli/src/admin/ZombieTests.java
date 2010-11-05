5cf76b41f3cceaf0d05eacf5dc5d6382b583580c
Tom Mueller <tmueller@java.net>
2010-11-05 21:49:14 +0000

    fix zombie test for change in when pid file is deleted
    svn path=/trunk/; revision=42523
    
    
    Former-commit-id: 679c0c6e8c71bcd4376595ecf7418b239e01ec27

--- v2/appserv-tests/devtests/admin/cli/src/admin/ZombieTests.java
+++ v2/appserv-tests/devtests/admin/cli/src/admin/ZombieTests.java
@@ -40,10 +40,6 @@ import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * Dev test for DAS recovery commands (backup-domain, restore-domain, list-backups)
- * @author Yamini K B
- */
 public class ZombieTests extends AdminBaseDevTest {
 
     public ZombieTests() {
@@ -81,7 +77,7 @@ public class ZombieTests extends AdminBaseDevTest {
             report("RunZombieApp", false);
         }
         undeploy(false);
-        stopDomain();
+        report("failed-stop-domain", !asadmin("stop-domain"));
         // TODO -- add common-utils so I can use ProcessManager etc.
         stopDomainForce();
         stat.printSummary();
@@ -104,128 +100,6 @@ public class ZombieTests extends AdminBaseDevTest {
         report("deploy-zombie", asadmin("deploy", zombieWarFile.getAbsolutePath()));
     }
 
-    private void testCommandsWithDefaultOptions() {
-
-        // perform a backup
-        report("backup-domain-no-operand", asadmin("backup-domain"));
-
-        // list backup
-        report("list-backups-no-operand", asadmin("list-backups"));
-
-        // restore backup
-        report("restore-domain-no-operand", !asadmin("restore-domain"));
-
-        // force restore backup
-        report("force-restore-domain-no-operand", asadmin("restore-domain", FORCE_OPTION));
-    }
-
-    private void testCommandsWithOperands() {
-
-        // perform a backup
-        report("backup-domain-with-operand", asadmin("backup-domain", DOMAIN1));
-
-        // list backup
-        report("list-backups-with-operand", asadmin("list-backups", DOMAIN1));
-
-        // restore backup
-        report("restore-domain-with-operand", asadmin("restore-domain", DOMAIN1));
-
-    }
-
-    private void testBackupDirOption() {
-
-        // perform a backup
-        report("backup-domain-with-backupdir", asadmin("backup-domain", "--backupdir", BACKUP_DIR, DOMAIN1));
-
-        // list backup
-        report("list-backups-with-backupdir", asadmin("list-backups", "--backupdir", BACKUP_DIR));
-
-        // list backup invalid domain
-        report("list-backups-with-invalid-operand", !asadmin("list-backups", "--backupdir", BACKUP_DIR, "foo"));
-
-        //test for absolute path
-        report("list-backups-with-invalid-backupdir", !asadmin("list-backups", "--backupdir", "foo"));
-
-        cleanupBackupDir();
-    }
-
-    private void cleanupBackupDir() {
-        File path = new File(BACKUP_DIR);
-        for (File f : path.listFiles()) {
-            f.delete();
-        }
-        path.delete();
-    }
-
-    private void testRestoreWithFileName() {
-
-        // specify a different domain name
-        report("restore-domain-with-filename", !asadmin("restore-domain", FILENAME_OPTION, "foo", DOMAIN2));
-
-        // force restore
-        report("force-restore-domain-with-operand", !asadmin("restore-domain", FORCE_OPTION, DOMAIN2));
-
-        report("restore-domain-with-filename-1", !asadmin("restore-domain", FILENAME_OPTION, BACKUP_FILE));
-
-        report("restore-domain-with-filename-2", asadmin("restore-domain", FILENAME_OPTION, BACKUP_FILE, FORCE_OPTION));
-
-        report("restore-domain-with-filename-3", asadmin("restore-domain", FILENAME_OPTION, BACKUP_FILE, FORCE_OPTION, "r-domain2"));
-
-        //remove the domains
-        asadmin("delete-domain", "r-domain2");
-        asadmin("delete-domain", DOMAIN2);
-    }
-
-    private void testCommandsWithNoDomains() {
-
-        // delete the domain
-        asadmin("delete-domain", DOMAIN1);
-
-        // perform a backup
-        report("backup-domain-empty-domaindir", !asadmin("backup-domain"));
-
-        // list backup
-        report("list-backups-empty-domaindir", !asadmin("list-backups"));
-
-        // restore backup
-        report("restore-domain-empty-domaindir", !asadmin("restore-domain"));
-
-        // put back domain1
-        asadmin("create-domain", "--nopassword", DOMAIN1);
-    }
-
-    private void testCommandsWithMultipleDomains() {
-
-        // create domain2
-        asadmin("create-domain", "--nopassword", DOMAIN2);
-
-        // perform a backup
-        report("backup-domain-multiple-domains-in-domaindir", !asadmin("backup-domain"));
-
-        // perform a backup
-        asadmin("backup-domain", DOMAIN1);
-
-        // list backup
-        report("list-backups-multiple-domains-in-domaindir", !asadmin("list-backups"));
-
-        // list backup with operand
-        report("list-backups-with-operand-multiple-domains-in-domaindir", asadmin("list-backups", DOMAIN1));
-
-        // restore backup
-        report("restore-domain-multiple-domains-in-domaindir", !asadmin("restore-domain", FORCE_OPTION));
-
-        asadmin("start-domain", DOMAIN1);
-
-        // perform a backup on domain2 while domain1 is running (13463)
-        report("backup-domain-multiple-domains-in-domaindir-13463", asadmin("backup-domain", DOMAIN2));
-
-        asadmin("stop-domain", DOMAIN1);
-
-        //delete domain2
-        asadmin("delete-domain", DOMAIN2);
-
-    }
-
     private void makeDasUndead() throws MalformedURLException, IOException {
         URL zombieUrl = new URL("http://localhost:8080/Zombie");
         URLConnection conn = zombieUrl.openConnection();
@@ -242,6 +116,6 @@ public class ZombieTests extends AdminBaseDevTest {
     private static final String BACKUP_DIR = System.getenv("APS_HOME") + "devtests/admin/cli/backupdir";
 
     private void stopDomainForce() {
-        report("forced-stop", asadmin("stop-domain", "--force"));
+        report("forced-stop", asadmin("stop-domain", "--kill"));
     }
 }
