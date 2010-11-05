7f64fec487a5f2651ce8f4daef51551323ea9898
Marina Vatkina <marina.vatkina@oracle.com>
2010-11-05 03:05:02 +0000

    Test for failure during tx recovery
    svn path=/trunk/; revision=42477
    
    
    Former-commit-id: 3c651bf899e2e703f452a6f5a68e59b01f47a3eb

---
+++ v2/appserv-tests/devtests/transaction/ee/resendautorecovery/war/VerifyServlet.java
@@ -0,0 +1,55 @@
+package com.acme;
+
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.naming.*;
+
+@WebServlet(urlPatterns="/VerifyServlet", loadOnStartup=1)
+public class VerifyServlet extends HttpServlet {
+
+    @EJB private MyBean bean;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+	resp.setContentType("text/html");
+
+        String type = req.getQueryString();
+        out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet VerifyServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+        try {
+	    out.println("RESULT:" + ((type == null)? bean.verifydefault() : bean.verifyxa()));
+        }catch(Throwable e){
+            out.println("got exception");
+            out.println(e);
+            e.printStackTrace();
+        } finally {
+            out.println("</body>");
+            out.println("</html>");
+
+            out.close();
+            out.flush();
+
+        }
+
+    }
+
+}
