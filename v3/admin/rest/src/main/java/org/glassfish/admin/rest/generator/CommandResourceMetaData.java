632b3df6778fb76d361dec3404c285632db44d03
Ludovic Champenois <ludo@java.net>
2010-11-09 21:18:59 +0000

    fixing https://glassfish.dev.java.net/issues/show_bug.cgi?id=14549
    svn path=/trunk/; revision=42649
    
    
    Former-commit-id: b9db7b9370e97cab7b2677c853a72b71c5caae91

--- v3/admin/rest/src/main/java/org/glassfish/admin/rest/generator/CommandResourceMetaData.java
+++ v3/admin/rest/src/main/java/org/glassfish/admin/rest/generator/CommandResourceMetaData.java
@@ -184,6 +184,8 @@ ListLbConfig
             {"Domain", "create-instance", "POST", "create-instance", "Create Instance"},
             {"Domain", "create-password-alias", "POST", "create-password-alias", "create-password-alias"},
             {"Domain", "delete-password-alias", "POST", "delete-password-alias", "delete-password-alias"},
+            {"Domain", "list-password-alias", "GET", "list-password-alias", "list-password-alias"},
+            {"Domain", "update-password-alias", "POST", "update-password-alias", "update-password-alias"},
             {"Domain", "disable-monitoring", "POST", "disable-monitoring", "Disable Monitoring"},
             {"Domain", "disable-secure-admin", "POST", "disable-secure-admin", "disable-secure-admin"},
             {"Domain", "enable-monitoring", "POST", "enable-monitoring", "Enable Monitoring"},
