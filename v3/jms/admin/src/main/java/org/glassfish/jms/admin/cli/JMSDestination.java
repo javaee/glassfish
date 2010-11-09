f227e1bb1b5add16fdd4de6b67b4f0e5cfb883b4
Satish Kumar <sats@java.net>
2010-11-09 13:25:33 +0000

    issue:14180: Moved from using CLUSTER_MONITOR_MBEAN_NAME to CLUSTER_CONFIG_MBEAN_NAME
    svn path=/trunk/; revision=42620
    
    
    Former-commit-id: 1028f3da66c8d6491d60af5373217ab28d032a46

--- v3/jms/admin/src/main/java/org/glassfish/jms/admin/cli/JMSDestination.java
+++ v3/jms/admin/src/main/java/org/glassfish/jms/admin/cli/JMSDestination.java
@@ -95,10 +95,10 @@ public abstract class JMSDestination {
 				+ ":type=" + "DestinationManager"
 				+ ",subtype=Config";
            
-	   protected static final String CLUSTER_MONITOR_MBEAN_NAME
+	   protected static final String CLUSTER_CONFIG_MBEAN_NAME
     			= MBEAN_DOMAIN_NAME
 				+ ":type=" + "Cluster"
-				+ ",subtype=Monitor";
+				+ ",subtype=Config";
             // Queue destination type
             protected static final String DESTINATION_TYPE_QUEUE= "q";
             //Topic destination type
