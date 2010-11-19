c30227ba1c12dacf7a61c05b058c7e9a62a70347
Sanjeeb Sahoo <sanjeeb.sahoo@oracle.com>
2010-11-19 01:03:03 +0000

    Issue 14004: Reimplementing osgi app deployment through asadmin command. Tested in cluster as well.
    svn path=/trunk/; revision=42957
    
    
    Former-commit-id: a3a614ec8c3ad2df95473ed178680b8b6c6ad4cc

--- v3/extras/osgi-container/src/main/java/org/glassfish/extras/osgicontainer/OSGiDeployer.java
+++ v3/extras/osgi-container/src/main/java/org/glassfish/extras/osgicontainer/OSGiDeployer.java
@@ -40,16 +40,18 @@
 
 package org.glassfish.extras.osgicontainer;
 
-import org.glassfish.internal.deployment.GenericDeployer;
-import org.glassfish.internal.deployment.GenericApplicationContainer;
-import org.glassfish.api.container.Container;
-import org.glassfish.api.deployment.*;
+import org.glassfish.api.deployment.Deployer;
+import org.glassfish.api.deployment.DeploymentContext;
+import org.glassfish.api.deployment.MetaData;
+import org.glassfish.api.deployment.OpsParams;
 import org.jvnet.hk2.annotations.Service;
-import org.jvnet.hk2.annotations.Inject;
-import org.jvnet.hk2.component.PreDestroy;
-import com.sun.enterprise.module.*;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.BundleReference;
+import org.osgi.service.packageadmin.PackageAdmin;
 
-import java.util.Collection;
+import java.io.File;
 
 /**
  * OSGi deployer, takes care of loading and cleaning modules from the OSGi runtime.
@@ -59,47 +61,29 @@ import java.util.Collection;
 @Service
 public class OSGiDeployer implements Deployer<OSGiContainer, OSGiDeployedBundle> {
 
-    @Inject
-    ModulesRegistry registry;
-
-    @Inject
-    OSGiArchiveHandler archiveHandler;
+    private Bundle bundle;
 
     public OSGiDeployedBundle load(OSGiContainer container, DeploymentContext context) {
-
-        Collection<Module> modules = registry.getModules(context.getAppProps().getProperty("module-name"));
-        final Module module = (modules.size()>0?modules.iterator().next():null);
-        if (module==null) {
-            throw new RuntimeException("Cannot install OSGi bundle in repository, is this an OSGi bundle ?"); 
-        }
-        // I am ensure I have a RefCountingClassLoader...
-        final RefCountingClassLoader loader = archiveHandler.getClassLoader(null, module);
-        // and release the one I got from the context.
-        if (context.getFinalClassLoader() instanceof PreDestroy) {
-            ((PreDestroy) context.getFinalClassLoader()).preDestroy();
-        }
-
-        return new OSGiDeployedBundle(module, loader);
+        return new OSGiDeployedBundle(bundle);
     }
 
     public void unload(OSGiDeployedBundle appContainer, DeploymentContext context) {
-        appContainer.cl.preDestroy();
-        context.addModuleMetaData(appContainer.m);
     }
 
-
-
     public void clean(DeploymentContext context) {
-        OpsParams params = context.getCommandParameters(OpsParams.class);
-        if (params!=null) {
-            if (params.origin== OpsParams.Origin.undeploy) {
-                Module m = context.getModuleMetaData(Module.class);
-                if (m!=null && m.getState()!= ModuleState.NEW) {
-                    m.uninstall();
-                }
-            }
+        try {
+            bundle.uninstall();
+            getPA().refreshPackages(new Bundle[]{bundle});
+            System.out.println("Uninstalled " + bundle);
+            bundle = null;
+        } catch (BundleException e) {
+            throw new RuntimeException(e);
         }
+    }
 
+    private PackageAdmin getPA() {
+        final BundleContext context = getBundleContext();
+        return (PackageAdmin) context.getService(context.getServiceReference(PackageAdmin.class.getName()));
     }
 
     public MetaData getMetaData() {
@@ -111,6 +95,18 @@ public class OSGiDeployer implements Deployer<OSGiContainer, OSGiDeployedBundle>
     }
 
     public boolean prepare(DeploymentContext context) {
+        File file = context.getSourceDir();
+        try {
+            assert(file.isDirectory());
+            bundle = getBundleContext().installBundle("reference:" + file.toURI().toString());
+            System.out.println("Installed " + bundle + " from " + bundle.getLocation());
+        } catch (BundleException e) {
+            throw new RuntimeException(e);
+        }
         return true; 
+    }
+
+    private BundleContext getBundleContext() {
+        return BundleReference.class.cast(getClass().getClassLoader()).getBundle().getBundleContext();
     }
 }
