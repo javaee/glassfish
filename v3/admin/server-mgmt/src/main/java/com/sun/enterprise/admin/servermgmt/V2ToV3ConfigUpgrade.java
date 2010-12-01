63115090b314b3def74323cd6056ab6157e49c06
Bobby Bisset <bbissett@java.net>
2010-12-01 20:25:22 +0000

    Actual fix for issue http://java.net/jira/browse/GLASSFISH-14854. Tom points out that this code could overwrite some values, e.g. if someone has a different osgi telnet port in a 3.0.1 installation and an upgrade is run. May file a separate issue/rfe to track that.
    svn path=/trunk/; revision=43295
    
    
    Former-commit-id: eeea1e2724cc42ca6482a1797569bbdf2882e5cb

--- v3/admin/server-mgmt/src/main/java/com/sun/enterprise/admin/servermgmt/V2ToV3ConfigUpgrade.java
+++ v3/admin/server-mgmt/src/main/java/com/sun/enterprise/admin/servermgmt/V2ToV3ConfigUpgrade.java
@@ -83,8 +83,11 @@ public class V2ToV3ConfigUpgrade implements ConfigurationUpgrade, PostConstruct 
                 if (jc == null) {
                     continue;
                 }
+
+                // fix issues where each new config gets 2x, 3x, 4x the data
+                newJvmOptions.clear();
                 oldJvmOptions = Collections.unmodifiableList(jc.getJvmOptions());
-                doAdditions();
+                doAdditions("server-config".equals(c.getName()));
                 doRemovals();
                 ConfigSupport.apply(new JavaConfigChanger(), jc);
             }
@@ -106,9 +109,18 @@ public class V2ToV3ConfigUpgrade implements ConfigurationUpgrade, PostConstruct 
         }
     }
 
-    private void doAdditions() {
+    private void doAdditions(boolean isDas) {
         // add new options
-        for(String s : ADD_LIST) {
+        doAdditionsFrom(ADD_LIST);
+        if (isDas) {
+            doAdditionsFrom(ADD_LIST_DAS);
+        } else {
+            doAdditionsFrom(ADD_LIST_NOT_DAS);
+        }
+    }
+
+    private void doAdditionsFrom(String [] strings) {
+        for (String s : strings) {
             newJvmOptions.add(s);
         }
     }
@@ -140,12 +152,12 @@ public class V2ToV3ConfigUpgrade implements ConfigurationUpgrade, PostConstruct 
             "-XX:LogFile",
         };
 
+    // these are added to all configs
     private static final String[] ADD_LIST = new String[] {
         "-XX:+UnlockDiagnosticVMOptions",
         "-XX:+LogVMOutput",
         "-XX:LogFile=${com.sun.aas.instanceRoot}/logs/jvm.log",
         "-DANTLR_USE_DIRECT_CLASS_LOADING=true",
-        "-Dosgi.shell.telnet.port=6666",
         "-Dosgi.shell.telnet.maxconn=1",
         "-Dosgi.shell.telnet.ip=127.0.0.1",
         "-Dfelix.fileinstall.dir=${com.sun.aas.installRoot}/modules/autostart/",
@@ -154,7 +166,16 @@ public class V2ToV3ConfigUpgrade implements ConfigurationUpgrade, PostConstruct 
         "-Dfelix.fileinstall.bundles.new.start=true",
         "-Djavax.management.builder.initial=com.sun.enterprise.v3.admin.AppServerMBeanServerBuilder",
         "-Dorg.glassfish.web.rfc2109_cookie_names_enforced=false",
-        "-Dosgi.shell.telnet.port=${OSGI_SHELL_TELNET_PORT}",
+    };
+
+    // these are added to DAS only
+    private static final String[] ADD_LIST_DAS = new String[] {
+        "-Dosgi.shell.telnet.port=6666"
+    };
+
+    // these are added to instances
+    private static final String[] ADD_LIST_NOT_DAS = new String[] {
+        "-Dosgi.shell.telnet.port=${OSGI_SHELL_TELNET_PORT}"
     };
 
     private static final List<String> REMOVAL_LIST = new ArrayList<String>();
