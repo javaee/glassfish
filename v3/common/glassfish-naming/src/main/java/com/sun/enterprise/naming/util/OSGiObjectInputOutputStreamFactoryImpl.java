3fa5b3a21313dadd300a099a5edc7d1f08e5d5dd
Mahesh Kannan <mahesh.kannan@oracle.com>
2010-12-03 18:55:12 +0000

    Fix for 14735. The fix is to maintain a mapping between bundle names and ids (using a BundleTracker)
    svn path=/trunk/; revision=43411
    
    
    Former-commit-id: 2b2a6b71ffc32a595d240c8be8e2e511f052c19d

--- v3/common/glassfish-naming/src/main/java/com/sun/enterprise/naming/util/OSGiObjectInputOutputStreamFactoryImpl.java
+++ v3/common/glassfish-naming/src/main/java/com/sun/enterprise/naming/util/OSGiObjectInputOutputStreamFactoryImpl.java
@@ -42,8 +42,11 @@ package com.sun.enterprise.naming.util;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleEvent;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.packageadmin.PackageAdmin;
+import org.osgi.util.tracker.BundleTracker;
+import org.osgi.util.tracker.BundleTrackerCustomizer;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -51,6 +54,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
 import java.io.ObjectStreamClass;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Sanjeeb.Sahoo@Sun.COM
@@ -61,14 +67,68 @@ public class OSGiObjectInputOutputStreamFactoryImpl
     private BundleContext ctx;
     PackageAdmin pkgAdm;
 
+    static Logger _logger = LogFacade.getLogger();
+
+    private ConcurrentHashMap<String, Long> name2Id = new ConcurrentHashMap<String, Long>();
+
     // Since bundle id starts with 0, we use -1 to indicate a non-bundle
     private static final long NOT_A_BUNDLE_ID = -1;
+    private static final String NOT_A_BUNDLE_KEY = ":";
 
     public OSGiObjectInputOutputStreamFactoryImpl(BundleContext ctx)
     {
         this.ctx = ctx;
         ServiceReference ref = ctx.getServiceReference(PackageAdmin.class.getName());
         pkgAdm = PackageAdmin.class.cast(ctx.getService(ref));
+
+        BundleTracker bt = new BundleTracker(ctx, Bundle.INSTALLED | Bundle.RESOLVED | Bundle.STARTING | Bundle.ACTIVE, new BundleTrackerCustomizer() {
+            
+            @Override
+            public void modifiedBundle(Bundle bundle, BundleEvent bundleEvent, Object o) {
+            }
+
+            @Override
+            public Object addingBundle(Bundle bundle, BundleEvent bundleEvent) {
+                String key = makeKey(bundle);
+                name2Id.put(key, bundle.getBundleId());
+                if (_logger.isLoggable(Level.FINE)) {
+                    _logger.log(Level.FINE, "BundleTracker.addingBundle BUNDLE " + key + " ==> " + bundle.getBundleId() + "  for " + bundle.getSymbolicName());
+                }
+                return null;
+            }
+
+            @Override
+            public void removedBundle(Bundle bundle, BundleEvent bundleEvent, Object o) {
+                String key = makeKey(bundle);
+                Long bundleID = name2Id.remove(key);
+                if (_logger.isLoggable(Level.FINE)) {
+                    _logger.log(Level.FINE, "BundleTracker.removedBundle BUNDLE " + key + "  ==> " + bundle.getSymbolicName());
+                }
+                if (bundleID == null) {
+                    _logger.log(Level.WARNING, "BundleTracker.removedBundle null bundleID for ==> " + key);
+                }
+            }
+            /*
+            @Override
+            public Object addingBundle(Bundle bundle, BundleEvent event) {
+                System.out.println("ADDING BUNDLE ==> " + bundle.getSymbolicName());
+                return super.addingBundle(bundle, event);    //To change body of overridden methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public void removedBundle(Bundle bundle, BundleEvent event, Object object) {
+                System.out.println("REMOVING BUNDLE ==> " + bundle.getSymbolicName());                
+                super.removedBundle(bundle, event, object);    //To change body of overridden methods use File | Settings | File Templates.
+            }
+            */
+        });
+
+        bt.open();
+
+    }
+
+    private String makeKey(Bundle bundle) {
+        return bundle.getSymbolicName() + ":" + bundle.getVersion();
     }
 
     public ObjectInputStream createObjectInputStream(InputStream in)
@@ -126,39 +186,41 @@ public class OSGiObjectInputOutputStreamFactoryImpl
     public Class<?> resolveClass(ObjectInputStream in, final ObjectStreamClass desc)
             throws IOException, ClassNotFoundException
     {
-        long bundleId = in.readLong();
-        if (bundleId != NOT_A_BUNDLE_ID) {
-            final Bundle b = ctx.getBundle(bundleId);
+        String key = in.readUTF();
 
-            if (System.getSecurityManager() == null) {
-                return b.loadClass(desc.getName());
-            } else {
-                try {
-                    return (Class) java.security.AccessController.doPrivileged(
-                        new java.security.PrivilegedExceptionAction()  {
-                            public java.lang.Object run() throws ClassNotFoundException {
-                               return b.loadClass(desc.getName());
-                            }
-                        });
-                } catch(java.security.PrivilegedActionException pae) {
-                    throw (ClassNotFoundException) pae.getException();    
+        if (! NOT_A_BUNDLE_KEY.equals(key)) {
+            Long bundleId = name2Id.get(key);
+            if (bundleId != null) {
+                final Bundle b = ctx.getBundle(bundleId);
+
+                if (System.getSecurityManager() == null) {
+                    return b.loadClass(desc.getName());
+                } else {
+                    try {
+                        return (Class) java.security.AccessController.doPrivileged(
+                                new java.security.PrivilegedExceptionAction() {
+                                    public java.lang.Object run() throws ClassNotFoundException {
+                                        return b.loadClass(desc.getName());
+                                    }
+                                });
+                    } catch (java.security.PrivilegedActionException pae) {
+                        throw (ClassNotFoundException) pae.getException();
+                    }
                 }
             }
-        } else {
-            return null;
         }
+
+        return null;
     }
 
     public void annotateClass(ObjectOutputStream out, Class<?> cl) throws IOException
     {
-        long id = NOT_A_BUNDLE_ID;
+        String key = NOT_A_BUNDLE_KEY;
         Bundle b = pkgAdm.getBundle(cl);
         if (b != null) {
-            id = b.getBundleId();
+            key = makeKey(b);
         }
-        out.writeLong(id);
+        out.writeUTF(key);
     }
-
-
 
 }
