1bca474e0eb59d40ac81e357423c32189e5d3020
Marina Vatkina <marina.vatkina@oracle.com>
2010-12-08 23:23:54 +0000

    Make test work with both, DerbyControl (default) and NetworkServerControl (run via ant -Dderby-type=ntw)
    svn path=/trunk/; revision=43592
    
    
    Former-commit-id: dbab520cb2c22a4a1f7fe34e90fed628195b5e59

--- v2/appserv-tests/devtests/transaction/ee/cli/kill_database.sh
+++ v2/appserv-tests/devtests/transaction/ee/cli/kill_database.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo process id `$JAVA_HOME/bin/jps -mlVv | grep NetworkServerControl | cut -f1 -d' '`
-kill -9 `$JAVA_HOME/bin/jps -mlVv | grep NetworkServerControl | cut -f1 -d' '`
+echo process id `$JAVA_HOME/bin/jps -mlVv | grep ${1} | cut -f1 -d' '`
+kill -9 `$JAVA_HOME/bin/jps -mlVv | grep ${1} | cut -f1 -d' '`
 
