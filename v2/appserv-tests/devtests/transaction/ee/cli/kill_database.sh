54e996dba2bc2bc71adcb0e741a0329824045213
Marina Vatkina <marina.vatkina@oracle.com>
2010-12-08 21:44:44 +0000

    Fix kill_database script
    svn path=/trunk/; revision=43588
    
    
    Former-commit-id: f2220096b63d60403994090de6504ec31f514f13

--- v2/appserv-tests/devtests/transaction/ee/cli/kill_database.sh
+++ v2/appserv-tests/devtests/transaction/ee/cli/kill_database.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo process id `$JAVA_HOME/bin/jps -mlVv | grep ASMain | cut -f1 -d' '`
-kill -9 `$JAVA_HOME/bin/jps -mlVv | grep ASMain cut -f1 -d' '`
+echo process id `$JAVA_HOME/bin/jps -mlVv | grep NetworkServerControl | cut -f1 -d' '`
+kill -9 `$JAVA_HOME/bin/jps -mlVv | grep NetworkServerControl | cut -f1 -d' '`
 
