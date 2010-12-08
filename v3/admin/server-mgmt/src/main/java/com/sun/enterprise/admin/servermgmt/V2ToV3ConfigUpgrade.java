ab1d5bf7616b4f71f2234841df84a50887e01190
Bhakti Mehta <bhaktimehta@java.net>
2010-12-08 21:31:37 +0000

    Fix for issue 14905 Reviewed and verified by Bobby
    svn path=/trunk/; revision=43587
    
    
    Former-commit-id: c4e02123609c277f80c7c63e8baf036ed58144b3

--- v3/admin/server-mgmt/src/main/java/com/sun/enterprise/admin/servermgmt/V2ToV3ConfigUpgrade.java
+++ v3/admin/server-mgmt/src/main/java/com/sun/enterprise/admin/servermgmt/V2ToV3ConfigUpgrade.java
@@ -169,6 +169,7 @@ public class V2ToV3ConfigUpgrade implements ConfigurationUpgrade, PostConstruct 
         "-Dfelix.fileinstall.log.level=2",
         "-Djavax.management.builder.initial=com.sun.enterprise.v3.admin.AppServerMBeanServerBuilder",
         "-Dorg.glassfish.web.rfc2109_cookie_names_enforced=false",
+        "-Djava.ext.dirs=${com.sun.aas.javaRoot}/lib/ext${path.separator}${com.sun.aas.javaRoot}/jre/lib/ext${path.separator}${com.sun.aas.instanceRoot}/lib/ext",
     };
 
     // these are added to DAS only
