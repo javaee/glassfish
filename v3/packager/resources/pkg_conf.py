d24399713bc8b8c11d79791258ef8c2e50baf624
Snjezana Sevo-Zenzerovic <snjezana.sevozenzerovic@oracle.com>
2010-12-29 19:39:10 +0000

    Syncing up shoal package version with recent integration.
    svn path=/trunk/; revision=44149
    
    
    Former-commit-id: 32abbe120f17c35ee02c3285408460ad08bd2f9d

--- v3/packager/resources/pkg_conf.py
+++ v3/packager/resources/pkg_conf.py
@@ -68,7 +68,7 @@ jsf_version="2.1.0,0-9"
 grizzly_version="1.9.27,0-1"
 metro_version="2.1,0-23"
 javahelp_version="2.0.2,0-1"
-shoal_version="1.5.21,0-0"
+shoal_version="1.5.25,0-0"
 
 #description
 glassfish_description="GlassFish Application Server"
