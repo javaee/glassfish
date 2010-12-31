53f6cc783b0d5740d4fb7892ff9cbd9bec295893
Byron Nevins <byron.nevins@oracle.com>
2010-12-31 07:31:21 +0000

    FORMATTED WHITE SPACE  -- NO CODE CHAnge --
    svn path=/trunk/; revision=44173
    
    
    Former-commit-id: 0c07a3345edbe21749845fd53d8d36abdb20866d

--- v3/flashlight/framework/src/main/java/org/glassfish/flashlight/impl/provider/DTraceMethodFinder.java
+++ v3/flashlight/framework/src/main/java/org/glassfish/flashlight/impl/provider/DTraceMethodFinder.java
@@ -42,10 +42,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.glassfish.flashlight.impl.provider;
 
-import com.sun.enterprise.universal.i18n.LocalStringsImpl;
 import com.sun.enterprise.util.LocalStringManagerImpl;
 import com.sun.logging.LogDomains;
 
@@ -60,18 +58,19 @@ import org.glassfish.flashlight.provider.FlashlightProbe;
  * @author bnevins
  */
 class DTraceMethodFinder {
+
     private static final Logger logger =
-        LogDomains.getLogger(DTraceMethodFinder.class, LogDomains.MONITORING_LOGGER);
+            LogDomains.getLogger(DTraceMethodFinder.class, LogDomains.MONITORING_LOGGER);
     public final static LocalStringManagerImpl localStrings =
-                            new LocalStringManagerImpl(DTraceMethodFinder.class);
+            new LocalStringManagerImpl(DTraceMethodFinder.class);
 
     DTraceMethodFinder(FlashlightProbe p, Object t) {
-        probe               = p;
-        targetObject        = t;
-        targetClass         = targetObject.getClass();
-        probeParamTypes     = probe.getParamTypes();
+        probe = p;
+        targetObject = t;
+        targetClass = targetObject.getClass();
+        probeParamTypes = probe.getParamTypes();
 
-        if(probeParamTypes == null)
+        if (probeParamTypes == null)
             probeParamTypes = new Class[0];
 
         numProbeParams = probeParamTypes.length;
@@ -82,60 +81,56 @@ class DTraceMethodFinder {
 
         // bnevins - the contract is that you do not call me with a hidden probe
         // If you do anyway -- I'll throw an unchecked exception as punishment.
-        if(probe.isHidden())
+        if (probe.isHidden())
             throw new RuntimeException(localStrings.getLocalString("dtrace_cantfind",
-                            "The probe is  hidden.  DTrace will ignore it.", metname));
+                    "The probe is  hidden.  DTrace will ignore it.", metname));
 
-        for(Method m : targetClass.getMethods()) {
-            if(!m.getName().equals(metname))
+        for (Method m : targetClass.getMethods()) {
+            if (!m.getName().equals(metname))
                 continue;
 
             // we have a name match!!
             Class[] paramTypes = m.getParameterTypes(); // guaranteed non null
 
-            if(paramTypes.length != numProbeParams)
+            if (paramTypes.length != numProbeParams)
                 continue; // overloaded method
 
-            if(!compareParams(probeParamTypes, paramTypes))
+            if (!compareParams(probeParamTypes, paramTypes))
                 continue; // overloaded method
 
             // we have a match!!!
-            return  m;
+            return m;
         }
         String errStr = localStrings.getLocalString("dtrace_cantfind",
-                            "Can not match the Probe method ({0}) with any method in the DTrace object.", metname);
+                "Can not match the Probe method ({0}) with any method in the DTrace object.", metname);
         throw new RuntimeException(errStr);
     }
 
     private boolean compareParams(Class[] probep, Class[] dtracep) {
         // the lengths are guaranteed to be the same!
-        for(int i = 0; i < probep.length; i++) {
+        for (int i = 0; i < probep.length; i++) {
             Class probeClass = probep[i];
             Class dtraceClass = dtracep[i];
 
-            if(probeClass.equals(dtraceClass))
+            if (probeClass.equals(dtraceClass))
                 continue;
-
             // something that can be coverted to String...
-            else if(dtraceClass.equals(String.class) && !FlashlightUtils.isIntegral(probeClass))
+            else if (dtraceClass.equals(String.class) && !FlashlightUtils.isIntegral(probeClass))
                 continue;
-
             // check for something like Short.class versus short.class
             // JDK will handle the boxing/unboxing
-            else if(FlashlightUtils.compareIntegral(dtraceClass, probeClass))
+            else if (FlashlightUtils.compareIntegral(dtraceClass, probeClass))
                 continue;
-            
             else
                 return false;
         }
-        
+
         return true;
     }
-
-    private final   FlashlightProbe probe;
-    private final   Object          targetObject;
-    private final   Class           targetClass;
-    private final   int             numProbeParams;
-    private         Method          method;
-    private         Class[]         probeParamTypes;
+    private final FlashlightProbe probe;
+    private final Object targetObject;
+    private final Class targetClass;
+    private final int numProbeParams;
+    private Method method;
+    private Class[] probeParamTypes;
 }
