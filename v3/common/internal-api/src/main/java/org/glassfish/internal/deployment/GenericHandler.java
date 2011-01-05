6c5e9a191f19cc8e536e5648ac2062fb20bcce05
Hong Zhang <hong.hz.zhang@oracle.com>
2011-01-05 19:04:55 +0000

    Fixed issue 15440. There was some code assuming the DeploymentContext will always be non-null, this is not the case for the java -jar glassfish.jar launching application path when calculating application name. Added the necessary null checks for that code path. Approved by Chris, reviewed by Tim. Ran QL, deployment dev tests.
    svn path=/trunk/; revision=44243
    
    
    Former-commit-id: 06ef1a380727e7b077820b3071ac97a9b96a95ce

--- v3/common/internal-api/src/main/java/org/glassfish/internal/deployment/GenericHandler.java
+++ v3/common/internal-api/src/main/java/org/glassfish/internal/deployment/GenericHandler.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -126,7 +126,13 @@ public abstract class GenericHandler implements ArchiveHandler {
         // first try to get the name from ApplicationInfoProvider if 
         // we can find an implementation of this service
         ApplicationInfoProvider nameProvider = habitat.getComponent(ApplicationInfoProvider.class);
-        DeploymentTracing tracing = context.getModuleMetaData(DeploymentTracing.class);
+
+        DeploymentTracing tracing = null;
+
+        if (context != null) {
+            tracing = context.getModuleMetaData(DeploymentTracing.class);
+        }
+
         if (tracing!=null) {
             tracing.addMark(DeploymentTracing.Mark.APPINFO_PROVIDED);
         }
--- v3/deployment/javaee-core/src/main/java/org/glassfish/javaee/core/deployment/DolProvider.java
+++ v3/deployment/javaee-core/src/main/java/org/glassfish/javaee/core/deployment/DolProvider.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -231,12 +231,17 @@ public class DolProvider implements ApplicationMetaDataProvider<Application>,
                 archive.exists("META-INF/application-client.xml") || 
                 archive.exists("META-INF/ra.xml")) {
                 application = applicationFactory.createApplicationFromStandardDD(archive);
-                DeploymentTracing tracing = context.getModuleMetaData(DeploymentTracing.class);
+                DeploymentTracing tracing = null; 
+                if (context != null) {
+                    tracing = context.getModuleMetaData(DeploymentTracing.class);
+                }
                 if (tracing!=null) {
                     tracing.addMark(DeploymentTracing.Mark.DOL_LOADED);
                 }
                 ApplicationHolder holder = new ApplicationHolder(application);
-                context.addModuleMetaData(holder);
+                if (context != null) {
+                    context.addModuleMetaData(holder);
+                }
 
                 return application.getAppName();
             }
