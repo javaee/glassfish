c84baec733eaaa1236c7bcc628c9791a1d3b7435
Jason Lee <jdlee@java.net>
2011-01-06 18:06:29 +0000

    Changes to make TransactionServiceTest work
    svn path=/trunk/; revision=44267
    
    
    Former-commit-id: e7c58fe571f205f6cd686253ee087736c57aee8d

--- v3/admingui/devtests/src/test/java/org/glassfish/admingui/devtests/BaseSeleniumTestClass.java
+++ v3/admingui/devtests/src/test/java/org/glassfish/admingui/devtests/BaseSeleniumTestClass.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -103,6 +103,7 @@ public class BaseSeleniumTestClass {
         put("i18n",  "org.glassfish.admingui.core.Strings"); // core
         put("i18nc",  "org.glassfish.common.admingui.Strings"); // common -- apparently we use both in the app :|
         put("i18ncs", "org.glassfish.cluster.admingui.Strings"); // cluster
+        put("i18n_jts" ,"org.glassfish.jts.admingui.Strings"); // JTS
     }};
     
     @BeforeClass
