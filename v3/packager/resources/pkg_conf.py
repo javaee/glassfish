e8fb5684122a726fe40ec98fc9a383dfc0d4f227
Ken Cavanaugh <kcavanaugh@java.net>
2011-01-07 01:28:34 +0000

    - Updated ORB to version 3.1.0-b021. - Fixes and issues partly addressed:     - 15931: Fixed     - 15932: Fixed     - 15436: Fixed     - 14704: ORB error fixed, re-assigning to Cheng (and possibly Kumar)       for further investigation in naming. - Tests: ORB regression, QL web, QL, EJB dev. - Changes reviewed by Marina
    svn path=/trunk/; revision=44286
    
    
    Former-commit-id: 25db13fdc98302bf427ed9189456444e80b30459

--- v3/packager/resources/pkg_conf.py
+++ v3/packager/resources/pkg_conf.py
@@ -63,7 +63,7 @@
 glassfish_version="3.1,0-${build.id}"
 felix_version="3.0.7,0-0"
 javadb_version="10.5.3.0,0-0"
-corba_version="3.1.0,0-19"
+corba_version="3.1.0,0-21"
 jsf_version="2.1.0,0-9"
 grizzly_version="1.9.27,0-1"
 metro_version="2.1,0-25"
