d1a335ce956f5ae175939ff15801321ac447beec
Cheng Fang <cf126330@java.net>
2011-01-07 19:59:20 +0000

    update hudson.sh to support custom derby port; DB_PORT and DB_PORT_2 are provided by hudson.
    svn path=/trunk/; revision=44319
    
    
    Former-commit-id: 8710e4a9e5b12ec76db97593da82fc616054f48b

--- v2/appserv-tests/devtests/ejb/hudson.sh
+++ v2/appserv-tests/devtests/ejb/hudson.sh
@@ -33,6 +33,7 @@ rm -rf $S1AS_HOME/domains/domain1
 cd $APS_HOME
 
 echo "AS_ADMIN_PASSWORD=" > temppwd
+cat $APS_HOME/temppwd
 $S1AS_HOME/bin/asadmin --user anonymous --passwordfile $APS_HOME/temppwd create-domain --adminport ${ADMIN_PORT} --domainproperties jms.port=${JMS_PORT}:domain.jmxPort=${JMX_PORT}:orb.listener.port=${ORB_PORT}:http.ssl.port=${SSL_PORT}:orb.ssl.port=${ORB_SSL_PORT}:orb.mutualauth.port=${ORB_SSL_MUTUALAUTH_PORT} --instanceport ${INSTANCE_PORT} domain1
 
 #Create 
@@ -64,14 +65,30 @@ appserver.instance.dir=\${admin.domain.dir}/\${admin.domain}" > config.propertie
 
 (jps |grep Main |cut -f1 -d" " | xargs kill -9  > /dev/null 2>&1) || true
 
+cd $S1AS_HOME/domains/domain1/config/
+sed "s/1527/${DB_PORT}/g" domain.xml > domain.xml.replaced
+mv domain.xml.replaced domain.xml
+grep PortNumber domain.xml
+
+cd $APS_HOME/config
+(rm derby.properties.replaced  > /dev/null 2>&1) || true
+sed "s/1527/${DB_PORT}/g" derby.properties > derby.properties.replaced
+rm derby.properties
+sed "s/1528/${DB_PORT_2}/g" derby.properties.replaced > derby.properties
+cat derby.properties
+
 pushd $APS_HOME/devtests/ejb
+rm count.txt || true
 
-ant all report-result |tee log.txt
-# ant all |tee log.txt
+ant all report-result -Ddb.port=${DB_PORT} -Ddb.port.2=${DB_PORT_2} |tee log.txt
+cat $S1AS_HOME/databases/derby.log
 
-(cat log.txt | grep  Total |cut -f2 -d']' |sort |uniq -c |grep -v PAS) || true
-popd
-cd $S1AS_HOME/bin/
+egrep 'FAILED= *0' count.txt
+egrep 'DID NOT RUN= *0' count.txt
+
+#(cat log.txt | grep  Total |cut -f2 -d']' |sort |uniq -c |grep -v PAS) || true
+#popd
+#cd $S1AS_HOME/bin/
 #./asadmin stop-domain
 
 #(jps |grep ASMain |cut -f1 -d" " | xargs kill -9  > /dev/null 2>&1) || true
