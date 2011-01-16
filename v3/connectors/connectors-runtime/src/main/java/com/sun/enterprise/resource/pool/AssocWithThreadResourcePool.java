50ba078ecc92310ccf6ec1f34e56f3c37e78ec3e
Jagadish Ramu <jagadish.ramu@oracle.com>
2011-01-16 15:14:05 +0000

    Issue Number : GLASSFISH-15586 Description : Parameter associate-with-thread fails when activated for more than one pool.
    Tests Passed : QL (Web, Classic), connector-dev, jdbc-dev, resources-admin-cli, connector-sqe, connector-standalone-cts (Web, Classic)
    
    Approved by : Nazrul Islam
    
    svn path=/trunk/; revision=44526
    
    
    Former-commit-id: e5cca9a9d33729308df0b9c0218e2bf9a267841d

--- v3/connectors/connectors-runtime/src/main/java/com/sun/enterprise/resource/pool/AssocWithThreadResourcePool.java
+++ v3/connectors/connectors-runtime/src/main/java/com/sun/enterprise/resource/pool/AssocWithThreadResourcePool.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -61,7 +61,7 @@ import java.util.Hashtable;
  */
 public class AssocWithThreadResourcePool extends ConnectionPool {
 
-    private static ThreadLocal<AssocWithThreadResourceHandle> localResource =
+    private ThreadLocal<AssocWithThreadResourceHandle> localResource =
             new ThreadLocal<AssocWithThreadResourceHandle>();
 
     public AssocWithThreadResourcePool(PoolInfo poolInfo, Hashtable env)
