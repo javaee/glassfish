7922832512cfd0426cdbee7681e059a28a7f875a
Bhakti Mehta <bhaktimehta@java.net>
2011-01-19 21:17:17 +0000

    Test fixes for hudson
    svn path=/trunk/; revision=44623
    
    
    Former-commit-id: e72509ea9ea59944ddf5914d360aaf34b4d1066a

--- v2/appserv-tests/devtests/webservice/annotations/postconstruct/webclient/Client.java
+++ v2/appserv-tests/devtests/webservice/annotations/postconstruct/webclient/Client.java
@@ -11,7 +11,7 @@ import javax.xml.ws.soap.*;
 public class Client extends HttpServlet {
 
 
-       @WebServiceRef(wsdlLocation="http://localhost:8080/postconstruct/webservice/HelloService?WSDL")
+       @WebServiceRef(wsdlLocation="http://HTTP_HOST:HTTP_PORT/postconstruct/webservice/HelloService?WSDL")
         HelloService service;
 
        public void doGet(HttpServletRequest req, HttpServletResponse resp)
