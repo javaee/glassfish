468003f6d08a5b4fde8c9b3d3e8d0c2e9429f980
Marek Potociar <marek.potociar@oracle.com>
2011-01-20 22:06:29 +0000

    Metro 2.1-b26 integrated
    svn path=/trunk/; revision=44651
    
    
    Former-commit-id: 9b77e637b7261cd22e871d0e6654628b75c5ac62

--- v3/packager/resources/pkg_conf.py
+++ v3/packager/resources/pkg_conf.py
@@ -66,7 +66,7 @@ javadb_version="10.5.3.0,0-0"
 corba_version="3.1.0,0-23"
 jsf_version="2.1.0,0-10"
 grizzly_version="1.9.28,0-1"
-metro_version="2.1,0-25"
+metro_version="2.1,0-26"
 javahelp_version="2.0.2,0-1"
 shoal_version="1.5.28,0-0"
 
