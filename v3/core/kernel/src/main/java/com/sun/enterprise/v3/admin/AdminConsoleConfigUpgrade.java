ba53e1538928da835d9d20793637699e2a03d769
Jane Young <janey@java.net>
2011-01-20 19:01:49 +0000

    revert svn rev 44632 since it's causing QL failure in adminconsole.
    svn path=/trunk/; revision=44647
    
    
    Former-commit-id: 4562fdafa3723c51151d3cf6605fef4ad731b426

--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/admin/AdminConsoleConfigUpgrade.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/admin/AdminConsoleConfigUpgrade.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -83,11 +83,6 @@ public class AdminConsoleConfigUpgrade
 
     @Inject
     Configs configs;
-
-    // This will force the Grizzly upgrade code to run before
-    // AdminConsoleConfigUpgrade runs. Issue GLASSFISH-15599
-    @Inject(name="grizzlyconfigupgrade", optional=true)
-    ConfigurationUpgrade precondition = null;
 
     @Override
     public void postConstruct() {
