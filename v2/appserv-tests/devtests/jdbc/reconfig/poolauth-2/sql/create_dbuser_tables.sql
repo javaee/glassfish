4511928189f090bbed441c4c24357625933241ac
Jagadish Ramu <jagadish.ramu@oracle.com>
2011-01-22 12:01:33 +0000

    Adding a simple test-case for pool-reconfiguration, application disable, enable actions.
    svn path=/trunk/; revision=44668
    
    
    Former-commit-id: 1e1c8a51722c9e0ffda6f75b9751d669f17af38b

---
+++ v2/appserv-tests/devtests/jdbc/reconfig/poolauth-2/sql/create_dbuser_tables.sql
@@ -0,0 +1,9 @@
+Drop table DBUSERTABLE;
+
+CREATE TABLE DBUSERTABLE (
+    c_id            integer not null,
+    c_phone         char(16)
+);
+
+INSERT INTO DBUSERTABLE VALUES(1, 'wxyz');
+INSERT INTO DBUSERTABLE VALUES(2, 'pqrs');
