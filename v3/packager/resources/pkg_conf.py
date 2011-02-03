8d228a17480b219f29f50a52ae018405abb76d86
Marek Potociar <marek.potociar@oracle.com>
2011-02-03 15:55:04 +0000

    Integrated Metro 2.1 FCS
    svn path=/trunk/; revision=44866
    
    
    Former-commit-id: a152f8dfebba8e6fb68356ce2c4f03326c189c36

--- v3/packager/resources/pkg_conf.py
+++ v3/packager/resources/pkg_conf.py
@@ -66,7 +66,7 @@ javadb_version="10.5.3.0,0-0"
 corba_version="3.1.0,0-24"
 jsf_version="2.1.0,0-11"
 grizzly_version="1.9.30,0-1"
-metro_version="2.1,0-27"
+metro_version="2.1,0-30"
 javahelp_version="2.0.2,0-1"
 shoal_version="1.5.29,0-0"
 
