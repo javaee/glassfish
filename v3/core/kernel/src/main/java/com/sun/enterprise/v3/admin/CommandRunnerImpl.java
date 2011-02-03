f639186554df483482fa59bc7b30ea01e844974e
Tim Quinn <tim.quinn@oracle.com>
2011-02-03 00:48:50 +0000

    Workaround/fix for 15773
    Large archive uploads during deployment with secure admin enabled cause problems in Grizzly buffering of the request's input stream if there is no client cert in the message when the server invokes request.getUserPrincipal.  In by far the most cases, user admin messages to the DAS (whether from asadmin, IDEs, or other admin clients) do not use client SSL authentication.  
    
    So as a workaround/fix, these changes accomplish two basic things:
    
    1. The AdminAdapter does not invoke request.getUserPrincipal if it is running on the DAS.  (The user can override this by setting org.glassfish.admin.DASCheckAdminCert for the DAS.)
    
    2. The RemoteAdminCommand now records if the first request it sends (to retrieve the command metadata) is redirected to https.  If so, the second request (to submit the actual command) is sent via https.  This avoids redirection of the actual command request, which carries with it the uploaded archive.
    
    Approved: Chris
    Reviewed: Tom
    Tests: QL, upload of large archives succeeded with the property undefined, failed with the property defined - as expected
    
    svn path=/trunk/; revision=44842
    
    
    Former-commit-id: b7ee8808760b5f45cf76e7b2c4e2e43a57e95c65

--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/admin/CommandRunnerImpl.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/admin/CommandRunnerImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -1127,7 +1127,7 @@ public class CommandRunnerImpl implements CommandRunner {
             }
 
         } catch (Exception ex) {
-            logger.severe(ex.getMessage());
+            logger.log(Level.SEVERE, "", ex);
                 report.setActionExitCode(ActionReport.ExitCode.FAILURE);
                 report.setMessage(ex.getMessage());
                 report.setFailureCause(ex);
