57c0f4c691129c159af10723f86ca49cae2b152e
Mitesh Meswani <mm110999@java.net>
2011-02-05 00:28:41 +0000

    Fix for issue 15769. Disable weaving for Embedded EJB container
    svn path=/trunk/; revision=44918
    
    
    Former-commit-id: f96b2da908f910afa53505744773e1182a92f4e4

--- v3/common/internal-api/src/main/java/org/glassfish/internal/embedded/Server.java
+++ v3/common/internal-api/src/main/java/org/glassfish/internal/embedded/Server.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2009-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -235,6 +235,7 @@ public class Server {
 
     private void setGlassFishProperties(GlassFishProperties props, EmbeddedFileSystem fs) {
         props.setProperty("-type", "EMBEDDED");
+        props.setProperty("org.glassfish.persistence.embedded.weaving.enabled", "false");
         if (fs != null) {
             String instanceRoot = fs.instanceRoot != null ? fs.instanceRoot.getAbsolutePath() : null;
             if (instanceRoot != null) {
