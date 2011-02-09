0e96fd7713d0b55dcc4c57df67050ebe74e84ecb
Byron Nevins <byron.nevins@oracle.com>
2011-02-09 02:51:16 +0000

    More monitoring devtests
    svn path=/trunk/; revision=45008
    
    
    Former-commit-id: 13423bceaa33883ab866386964946931ec340052

--- v2/appserv-tests/devtests/admin/cli/src/admin/MonitoringTests.java
+++ v2/appserv-tests/devtests/admin/cli/src/admin/MonitoringTests.java
@@ -7,7 +7,6 @@ package admin;
 import admin.AdminBaseDevTest;
 
 /**
- * This class is like the Ring of Power from LOTR.  It calls the other Rings (classes).
  * Created on February 8, 2011
  * @author Byron Nevins
  */
@@ -17,7 +16,16 @@ public class MonitoringTests extends AdminBaseDevTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new MonitoringTests().runTests();
+        // top level try here!!
+        MonitoringTests mt = new MonitoringTests();
+        
+        try {
+            mt.runTests();
+        }
+        catch (Exception e) {
+            mt.report("GotException-" + e.getClass().getName(), false);
+        }
+        mt.stat.printSummary();
     }
 
     @Override
@@ -32,26 +40,96 @@ public class MonitoringTests extends AdminBaseDevTest {
     }
 
     public void runTests() {
+        String configName = "server-config";
         startDomain();
 
         // if we started it -- make very sure that we at least attempt 
         // to stop it in all cases!
+        // Also let's not forget disabling monitoring
 
         try {
+            enableMonitoring(configName, true);
             runTestsThatNeedARunningDomain();
         }
         finally {
             try {
+                enableMonitoring(configName, false);
                 stopDomain();
             }
             catch (Exception e) {
                 report("BigProblemInMonitoringTests", false);
                 System.out.println("" + e);
             }
-            stat.printSummary();
         }
     }
 
     private void runTestsThatNeedARunningDomain() {
+        runGetTests();
     }
+
+    private void runGetTests() {
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     *
+     * @param on if true it means turn on all levels to HIGH
+     * if false it means set all to LOW
+     * Also verify that they are currently set to the opposite
+     */
+    private void enableMonitoring(String configName, boolean high) {
+        final String metName = "enableMonitoring";
+
+        String prepend = MON_LEVEL_PREPEND + configName + MON_LEVEL_APPEND;
+        String desiredValue = "=";
+        desiredValue += high ? HIGH : OFF;
+        String metFullName = metName + "-set-" + desiredValue + "-";
+
+        for (String monItem : MON_LEVEL_ITEMS) {
+            String fullitemname = prepend + monItem;
+            String reportname = metFullName + monItem;
+            report(reportname, asadmin("set", (fullitemname + desiredValue)));
+        }
+        // make sure they were all set properly
+        verifyMonitoring(metName, high, configName);
+    }
+
+    /**
+     * Verify that ALL levels are OFF or HIGH depending on the param
+     * @param b
+     */
+    private void verifyMonitoring(String parentMethod, boolean high, String configName) {
+        String prepend = MON_LEVEL_PREPEND + configName + MON_LEVEL_APPEND;
+        String desiredValue = high ? HIGH : OFF;
+        String metName = parentMethod + "-verify-" + desiredValue + "-";
+
+        for (String monItem : MON_LEVEL_ITEMS) {
+            String fullitemname = prepend + monItem;
+            String reportname = metName + monItem;
+            report(reportname, doesGetMatch(fullitemname, desiredValue));
+        }
+    }
+    private static final String MON_LEVEL_PREPEND = "configs.config.";
+    private static final String MON_LEVEL_APPEND = ".monitoring-service.module-monitoring-levels.";
+    private static final String MON_LEVEL_ITEMS[] = new String[]{
+        "http-service",
+        "connector-connection-pool",
+        "connector-service",
+        "deployment",
+        "ejb-container",
+        "http-service",
+        "jdbc-connection-pool",
+        "jersey",
+        "jms-service",
+        "jpa",
+        "jvm",
+        "orb",
+        "security",
+        "thread-pool",
+        "transaction-service",
+        "web-container",
+        "web-services-container"
+    };
+    private final static String HIGH = "HIGH";
+    private final static String OFF = "OFF";
 }
