4aaadbb4afd9ac89f3b07dae44e1bd587f677ad8
Tim Quinn <tim.quinn@oracle.com>
2011-02-11 23:00:24 +0000

    Fix for 15963
    Work done earlier to help optimize synchronization of uploaded archives to remote instances introduced a bug which broke the .reload feature.  Files uploaded during deployment are placed into a temporary directory under the domain's applications directory.  The code at fault incorrectly assumed that a file anywhere under the domain's applications directory was an uploaded file.  Deployment expands all archives and, except for the sync feature, the uploaded archive is no longer needed.  So the code moved what it thought was the uploaded file to a separate directory for use by synchronization.  But when a user touches the .reload file in the applications/(appName) directory for an already-deployed app, that directory is within the applications directory so this logic was moving the expanded directory to this separate directory, causing rather some havoc.
    
    This change repairs the problem by not moving a file under the applications directory if it is itself a directory.  This is the lowest-risk fix at this point in 3.1.
    
    Approved: Nazrul
    Reviewed: Hong
    Tests: QL, deployment devtests including a new test for the .reload feature
    
    svn path=/trunk/; revision=45068
    
    
    Former-commit-id: 10f6454dfd9ee7539181662d7501b98ff01d11d8

--- v3/deployment/admin/src/main/java/org/glassfish/deployment/admin/DeployCommand.java
+++ v3/deployment/admin/src/main/java/org/glassfish/deployment/admin/DeployCommand.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -507,7 +507,7 @@ public class DeployCommand extends DeployCommandParameters implements AdminComma
          */
         File result = fileParam;
         
-        if ( ! appsDir.toURI().relativize(fileParam.toURI()).isAbsolute()) {
+        if ( ! fileParam.isDirectory() && ! appsDir.toURI().relativize(fileParam.toURI()).isAbsolute()) {
             /*
              * The file lies within the apps directory, so it was 
              * uploaded.
