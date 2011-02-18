f1f34d3f1c229d9f367b9e43ae1ebca4b2f339f4
Rama Pulavarthi <ramapulavarthi@java.net>
2011-02-18 23:10:22 +0000

    GLASSFISH-16035: WLWebServicesDeploymentDescriptorFile expects only WebServicesDescriptor in the constructor.
    svn path=/trunk/; revision=45182
    
    
    Former-commit-id: 3a254ff09714e0222ec7549a5334ef813d4c849e

--- v3/deployment/dol/src/main/java/com/sun/enterprise/deployment/archivist/Archivist.java
+++ v3/deployment/dol/src/main/java/com/sun/enterprise/deployment/archivist/Archivist.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -1089,7 +1089,7 @@ public abstract class Archivist<T extends RootDeploymentDescriptor> {
     private void writeWLWebServicesDescriptors(BundleDescriptor desc, WritableArchive out)
             throws IOException {
         if (desc.hasWebServices()) {
-            DeploymentDescriptorFile webServicesDD = new WLWebServicesDeploymentDescriptorFile(desc);
+            DeploymentDescriptorFile webServicesDD = new WLWebServicesDeploymentDescriptorFile(desc.getWebServices());
             OutputStream os = out.putNextEntry(webServicesDD.getDeploymentDescriptorPath());
             webServicesDD.write(desc.getWebServices(), os);
             out.closeEntry();
