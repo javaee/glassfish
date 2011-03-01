232d4b9bb5e303c4b55ea9fcbb4a7822bbbdff6c
Jason Lee <jdlee@java.net>
2011-03-01 14:25:46 +0000

    Fix copyright headers
    svn path=/trunk/; revision=45337
    
    
    Former-commit-id: 7579f51b5564fa9a613a63bc5546f67e0faf1f44

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/MonitoringHandlers.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/MonitoringHandlers.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -183,7 +183,7 @@ public class MonitoringHandlers {
                         String queuesize = null;
                         String thresholds = "--";
 
-                        if (monAttrs.size() != 0) {
+                        if (!monAttrs.isEmpty()) {
 
                             if (monAttrs.containsKey("name")) {
                                 mname = (String) monAttrs.get("name");
@@ -793,7 +793,7 @@ public class MonitoringHandlers {
                 }
             }
         } catch (Exception ex) {
-            GuiUtil.getLogger().severe("Error in getMonitoringStatInfo ; \nendpoint = " + endpoint + "attrs=" + "method=GET");
+            GuiUtil.getLogger().log(Level.SEVERE,"Error in getMonitoringStatInfo ; \nendpoint = {0}" + "attrs=" + "method=GET", endpoint);
             //we don't need to call GuiUtil.handleError() because thats taken care of in restRequest() when we pass in the handler.
         }
         return monitorInfoMap;
