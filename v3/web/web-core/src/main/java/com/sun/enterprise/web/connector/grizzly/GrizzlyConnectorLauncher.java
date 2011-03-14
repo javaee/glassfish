8ef6ba8266fb74eaf1183efe214a2816d12f7382
Shing Wai Chan <shing.wai.chan@oracle.com>
2011-03-14 22:46:10 +0000

    fix FindBugs issue: SIC_INNER_SHOULD_BE_STATIC, UPM_UNCALLED_PRIVATE_METHOD
    svn path=/trunk/; revision=45559
    
    
    Former-commit-id: c3538ab69e3e4d9dbfd9a47d09996399c1653e1b

--- v3/web/web-core/src/main/java/com/sun/enterprise/web/connector/grizzly/GrizzlyConnectorLauncher.java
+++ v3/web/web-core/src/main/java/com/sun/enterprise/web/connector/grizzly/GrizzlyConnectorLauncher.java
@@ -935,7 +935,7 @@ public class GrizzlyConnectorLauncher extends CoyoteConnectorLauncher {
     /**
      * JMX Wrapper around a JMX implementation.
      */
-    class ModelerManagement implements Management{
+    static class ModelerManagement implements Management{
 
         @SuppressWarnings("deprecation")
         public void registerComponent(Object bean, ObjectName oname, String type) 
@@ -967,14 +967,4 @@ public class GrizzlyConnectorLauncher extends CoyoteConnectorLauncher {
     public void setProxiedProtocols(String proxiedProtocols) {
         this.proxiedProtocols = proxiedProtocols;
     }
-
-    
-    /**
-     * Configure the domain.xml proxied protocols.
-     */
-    private void configureProxiedProtocols() {
-        //TO: Do we need to implement Port Unification here or directly
-        // When hk2 boot Grizzly?
-    }
 }
-
