ac3202b610f20f4337afd0f02422360d1eeafea8
Tom Mueller <tmueller@java.net>
2011-03-18 23:31:33 +0000

    Issue 15552 - allow version to work with both --terse and --verbose at the same time
    svn path=/trunk/; revision=45626
    
    
    Former-commit-id: b870c840ad0f7461305da51c8adcac6ad238bb19

--- v3/admin/cli/src/main/java/com/sun/enterprise/admin/cli/VersionCommand.java
+++ v3/admin/cli/src/main/java/com/sun/enterprise/admin/cli/VersionCommand.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2008-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -77,13 +77,6 @@ public class VersionCommand extends CLICommand {
 
     private static final LocalStringsImpl strings =
             new LocalStringsImpl(VersionCommand.class);
-
-    @Override
-    protected void validate() throws CommandException {
-        if (terse && verbose) {
-            throw new CommandValidationException(strings.get("version.optionerror1"));
-        }
-    }
 
     @Override
     protected int executeCommand() throws CommandException {
