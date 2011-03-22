795cfa959458ef12ae976a29beb7570dbe0321f2
srinik76 <srinik76@localhost>
2011-03-22 06:08:17 +0000

    Issue : 15517 "Restart Required" when no changes are made to JVM options
    Comments : Before saving check the new list of jvm options with old list of jvm options, if it is same do not save the list
    
    
    svn path=/trunk/; revision=45650
    
    
    Former-commit-id: a54dcaa7c01691f670b2882e7798a4fe98fcc5f8

--- v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/InstanceHandler.java
+++ v3/admingui/common/src/main/java/org/glassfish/admingui/common/handlers/InstanceHandler.java
@@ -119,6 +119,15 @@ public class InstanceHandler {
         String target = (String) handlerCtx.getInputValue("target");
         try {
             List<Map> options = (List<Map>) handlerCtx.getInputValue("options");
+            ArrayList<String> newList = new ArrayList();
+            for (Map oneRow : options) {
+                newList.add((String) oneRow.get(PROPERTY_VALUE));
+            }
+            ArrayList<String> oldList = getJvmOptions(handlerCtx);
+            if (newList.equals(oldList)) {
+                // if old list is same as new list, return without saving anything
+                return;
+            }
             Map<String, Object> payload = new HashMap<String, Object>();
             if (endpoint.contains("profiler")) {
                 payload.put("profiler", "true");
