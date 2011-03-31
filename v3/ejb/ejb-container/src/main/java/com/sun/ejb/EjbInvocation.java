144a681a8c15d5b4f49881cf0f0167564c286a00
Marina Vatkina <marina.vatkina@oracle.com>
2011-03-31 21:52:39 +0000

    FindBugs - no need of doing instanceof check.
    svn path=/trunk/; revision=45824
    
    
    Former-commit-id: 9c594e2af470a28a4ff0c16f43fd50000a4033f4

--- v3/ejb/ejb-container/src/main/java/com/sun/ejb/EjbInvocation.java
+++ v3/ejb/ejb-container/src/main/java/com/sun/ejb/EjbInvocation.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -693,8 +693,8 @@ public class EjbInvocation
 
    public ResourceHandler getResourceHandler() {
        ResourceHandler rh = super.getResourceHandler();
-       if (rh == null && context instanceof ResourceHandler) {
-           rh = (ResourceHandler)context;
+       if (rh == null) {
+           rh = context;
        }
 
        return rh;
