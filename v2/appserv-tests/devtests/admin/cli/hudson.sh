c5c82886444c64e2f8ebf7763668f03670775f72
Byron Nevins <byron.nevins@oracle.com>
2011-04-05 20:10:57 +0000

    Speed up Hudson - run tests only once but create 45 instances.
    svn path=/trunk/; revision=45941
    
    
    Former-commit-id: 2c5fed15d61a1cf7cc16ac9aa5872fce748f114a

--- v2/appserv-tests/devtests/admin/cli/hudson.sh
+++ v2/appserv-tests/devtests/admin/cli/hudson.sh
@@ -27,20 +27,8 @@ export APS_HOME="$ROOT/appserv-tests"
 export AS_LOGFILE="$S1AS_HOME/cli.log"
 
 cd "$APS_HOME"
-(jps |grep Main |cut -f1 -d" " | xargs kill -9  > /dev/null 2>&1) || true
+(jps |grep ASMain |cut -f1 -d" " | xargs kill -9  > /dev/null 2>&1) || true
 cd "$APS_HOME/devtests/admin/cli"
-# run the tests twice -- e.g. Issue # 12127
-ant clean
-time ant all
-# don't run twice if we already failed!
-egrep 'FAILED *0' "$APS_HOME/count.txt" >/dev/null || exit 1
-
-# bnevins June 9, 2010
-# if the tests can not be run twice  then nobody will use them because it will screw up their installation
-# This will happen when we don't clean-up and return to the original state -- which is a good test in itself!
-# Also we really don't care how long these tests take
-# of course it is not an issue on Hudson.
-(jps |grep Main |cut -f1 -d" " | xargs kill -9  > /dev/null 2>&1) || true
 ant clean
 time ant -Dnum_tests=45 all 
 egrep 'FAILED *0' "$APS_HOME/count.txt" >/dev/null
