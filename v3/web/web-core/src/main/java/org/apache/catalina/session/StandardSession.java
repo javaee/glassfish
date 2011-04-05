2f7c42116d09ae70d090feb10c194d7ec892dac7
Shing Wai Chan <shing.wai.chan@oracle.com>
2011-04-05 16:45:08 +0000

    fix FindBugs issue: CAST_INTEGER_MULTIPLY_CAST_TO_LONG
    svn path=/trunk/; revision=45935
    
    
    Former-commit-id: d8fe2f841768ea49faa0d7268a4fa433df62f763

--- v3/web/web-core/src/main/java/org/apache/catalina/session/StandardSession.java
+++ v3/web/web-core/src/main/java/org/apache/catalina/session/StandardSession.java
@@ -1019,7 +1019,7 @@ public class StandardSession
 
         if (maxInactiveInterval >= 0
                 && (System.currentTimeMillis() - thisAccessedTime >=
-                    maxInactiveInterval * 1000)) {
+                    maxInactiveInterval * 1000L)) {
             return true;
         } else {
             return false;
