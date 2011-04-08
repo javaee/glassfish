36973efa4c885dee349d0abd217553c6e38af27c
Shing Wai Chan <shing.wai.chan@oracle.com>
2011-04-08 20:21:20 +0000

    fix Findbugs issues: ES_COMPARING_STRINGS_WITH_EQ, NP_LOAD_OF_KNOWN_NULL_VALUE
    svn path=/trunk/; revision=46045
    
    
    Former-commit-id: 799ae9b4e4fb1ffaf460fed6b1f636b4c4688d8b

--- v3/web/web-core/src/main/java/org/apache/tomcat/util/digester/Digester.java
+++ v3/web/web-core/src/main/java/org/apache/tomcat/util/digester/Digester.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -1521,7 +1521,7 @@ public class Digester extends DefaultHandler {
             if (systemId == null) {
                 // cannot resolve
                 if (log.isLoggable(Level.FINE)) {
-                    log.fine(" Cannot resolve entity: '" + entityURL + "'");
+                    log.fine(" Cannot resolve entity: '" + publicId + "'");
                 }
                 return (null);
                 
@@ -2870,7 +2870,7 @@ public class Digester extends DefaultHandler {
             try {
                 String newValue = 
                     IntrospectionUtils.replaceProperties(value, null, source);
-                if (value != newValue) {
+                if (!value.equals(newValue)) {
                     newAttrs.setValue(i, newValue);
                 }
             }
