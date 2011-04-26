5638f19a1d8007f615dadc3c6b520bdb4b576c4f
Byron Nevins <byron.nevins@oracle.com>
2011-04-26 17:42:37 +0000

    FindBugs does not like the use of Boolean's as objects that can have three values:  true, false, null.
    svn path=/trunk/; revision=46458
    
    
    Former-commit-id: 5acaae53d3aa34775025e14cc17eac6cd80b6e00

--- v3/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherInfo.java
+++ v3/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherInfo.java
@@ -317,7 +317,7 @@ public class GFLauncherInfo {
 
         File f = null;
         String s = null;
-        Boolean b = null;
+        ThreeStateBoolean tsb = null;
 
         // pick out file props
         // annoying -- cli uses "domaindir" to represent the parent of the 
@@ -350,17 +350,29 @@ public class GFLauncherInfo {
         }
 
         // finally, do the booleans
-        if ((b = getBoolean("debug")) != null) {
-            debug = b;
-        }
+        // getting ugly.  Findbugs does not like using regular Boolean object
+        // a three-state boolean
+        // we do NOT want to disturb the existing values of these variables if the
+        // user has not explicitly overridden them.
 
-        if ((b = getBoolean("verbose")) != null) {
-            verbose = b;
-        }
+        tsb = getBoolean("debug");
 
-        if ((b = getBoolean("upgrade")) != null) {
-            upgrade = b;
-        }
+        if(tsb.isTrue())
+            debug = true;
+        else if(tsb.isFalse())
+            debug = false;
+
+        tsb = getBoolean("verbose");
+        if(tsb.isTrue())
+            debug = true;
+        else if(tsb.isFalse())
+            debug = false;
+
+        tsb = getBoolean("upgrade");
+        if(tsb.isTrue())
+            debug = true;
+        else if(tsb.isFalse())
+            debug = false;
     }
 
     private void finalSetup() throws GFLauncherException {
@@ -465,14 +477,14 @@ public class GFLauncherInfo {
         return files[0].getName();
     }
 
-    private Boolean getBoolean(String key) {
+    private ThreeStateBoolean getBoolean(String key) {
         // 3 return values -- true, false, null
         String s = getValueIgnoreCommandDelimiter(key);
 
-        if (s != null)
-            return Boolean.valueOf(s);
+        if (s != null) // guaranteed true or false
+            return new ThreeStateBoolean(Boolean.valueOf(s));
         else
-            return null;
+            return new ThreeStateBoolean(null);
     }
 
     private File getFile(String key) {
@@ -531,4 +543,23 @@ public class GFLauncherInfo {
     private final static String CONFIG_FILENAME = "domain.xml";
     //password tokens -- could be multiple -- launcher should *just* write them onto stdin of server
     final List<String> securityTokens = new ArrayList<String>(); // note: it's package private
+
+    final private static class ThreeStateBoolean {
+
+        ThreeStateBoolean(Boolean b) {
+            this.b = b;
+        }
+        boolean isNull() {
+            return b == null;
+        }
+        boolean isTrue() {
+            return !isNull() && b.booleanValue();
+        }
+        boolean isFalse() {
+            return !isNull() && !b.booleanValue();
+        }
+        
+        Boolean b;
+    }
+
 }
