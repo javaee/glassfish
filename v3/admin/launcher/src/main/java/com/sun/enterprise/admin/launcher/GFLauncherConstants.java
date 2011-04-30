351d2284f5bbaedceb828e7d79fdd00f896b5e9b
Byron Nevins <byron.nevins@oracle.com>
2011-04-30 06:02:18 +0000

    Use Flashlight Agent if BTrace agent jar doesn't exist.
    svn path=/trunk/; revision=46558
    
    
    Former-commit-id: 8a9af78e29ea393b75561d9f32dbca9d66c17b9d

--- v3/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherConstants.java
+++ v3/admin/launcher/src/main/java/com/sun/enterprise/admin/launcher/GFLauncherConstants.java
@@ -56,5 +56,6 @@ class GFLauncherConstants {
     static final String NATIVE_LIB_PREFIX           = "native-library-path-prefix";
     static final String NATIVE_LIB_SUFFIX           = "native-library-path-suffix";
     static final String BTRACE_PATH                 = "lib/monitor/btrace-agent.jar";
+    static final String FLASHLIGHT_AGENT_PATH       = "modules/flashlight-agent.jar";
     static final String DEFAULT_LOGFILE             = "logs/server.log";
 }
