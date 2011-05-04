d5181a75dc0a3ce6590b3f165a1770be9aa22ead
Jagadish Ramu <jagadish.ramu@oracle.com>
2011-05-04 15:47:47 +0000

    Issue : GLASSFISH-16350 Description : On instance restart Application cannot be loaded anymore Tests Passed : QL(Web, Classic), connector-dev, jdbc-dev, connector-standalone-cts (Web, Classic), connector-sqe, resources-admin-cli
    svn path=/branches/3.1.1/; revision=46655
    
    
    Former-commit-id: c4a24bfd24757b6caedc1a254bd56961b6f19f95

--- connectors/connectors-runtime/src/main/java/com/sun/enterprise/resource/recovery/ConnectorsRecoveryResourceHandler.java
+++ connectors/connectors-runtime/src/main/java/com/sun/enterprise/resource/recovery/ConnectorsRecoveryResourceHandler.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -458,7 +458,12 @@ public class ConnectorsRecoveryResourceHandler implements RecoveryResourceHandle
         if (ConnectorAdminServiceUtils.isEmbeddedConnectorModule(rarModuleName)) {
              cr.createActiveResourceAdapterForEmbeddedRar(rarModuleName);
         } else {
-            String moduleDir = ConfigBeansUtilities.getLocation(rarModuleName);
+            String moduleDir ;
+            if (ConnectorsUtil.belongsToSystemRA(rarModuleName)) {
+                moduleDir = ConnectorsUtil.getSystemModuleLocation(rarModuleName);
+            }else{
+                moduleDir = ConfigBeansUtilities.getLocation(rarModuleName);
+            }
             ClassLoader loader = cr.createConnectorClassLoader(moduleDir, null, rarModuleName);
             cr.createActiveResourceAdapter(moduleDir, rarModuleName, loader);
         }
