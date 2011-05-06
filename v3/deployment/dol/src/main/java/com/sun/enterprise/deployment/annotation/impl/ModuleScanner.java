8c4301a617cb837b9976b2e45b74b77df54e80f8
Hong Zhang <hong.hz.zhang@oracle.com>
2011-05-06 20:09:12 +0000

    Issue 16560. Performance improvement for library jar annotation scanning. Annotations were already scanned in the beginning of the deployment and we should not try to scan it again (use addScanJar instead of addScanURI so we don't try to scan/introspect the classes). Ran QL, deployment dev, ejb dev tests.
    svn path=/trunk/; revision=46729
    
    
    Former-commit-id: 47bc08b9e1dba1565414d6c4109c74be6fefa04c

--- v3/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/impl/ModuleScanner.java
+++ v3/deployment/dol/src/main/java/com/sun/enterprise/deployment/annotation/impl/ModuleScanner.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -379,7 +379,7 @@ public abstract class ModuleScanner<T> extends JavaEEScanner implements Scanner<
             try {
                 File libFile = new File(url.toURI());;
                 if (libFile.isFile()) {
-                    addScanURI(libFile.toURI());
+                    addScanJar(libFile);
                 } else if (libFile.isDirectory()) {
                     addScanDirectory(libFile);
                 }
