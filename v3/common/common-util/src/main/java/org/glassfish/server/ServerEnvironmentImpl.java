fc33b56fe8e9e8f670dca778b61bf65fa344e342
Byron Nevins <byron.nevins@oracle.com>
2011-05-10 21:47:13 +0000

    Removed a WILDLY inappropriate comment.
    svn path=/trunk/; revision=46770
    
    
    Former-commit-id: ef80759b7e76fc9d98cae02ab4ee072f8c286855

--- v3/common/common-util/src/main/java/org/glassfish/server/ServerEnvironmentImpl.java
+++ v3/common/common-util/src/main/java/org/glassfish/server/ServerEnvironmentImpl.java
@@ -118,7 +118,7 @@ public class ServerEnvironmentImpl implements ServerEnvironment, PostConstruct {
         // todo : dochez : this will need to be reworked...
         String installRoot = startupContext.getArguments().getProperty(INSTALL_ROOT_PROP_NAME);
         if (installRoot == null) {
-            // Sahoo: This is truely a bad piece of code. During unit testing, we find an empty StartupContext.
+            // During unit testing, we find an empty StartupContext.
             // To be consistent with earlier code (i.e., code that relied on StartupContext.getRootDirectory()),
             // I am setting user.dir as installRoot.
             installRoot = System.getProperty("user.dir");
