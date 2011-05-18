5de5a9de1edcc387aca4c51378e34e62590832c0
Amy Moon <amy.roh@oracle.com>
2011-05-18 16:26:47 +0000

    Fix GLASSFISH 16652 in 3.1.1.
    Ran QL and devtests.
    
    
    svn path=/branches/3.1.1/; revision=46919
    
    
    Former-commit-id: 94ad9ac8adb9ae7ca758811d0504d32e7a872376

--- web/web-glue/src/main/java/com/sun/enterprise/web/WebContainer.java
+++ web/web-glue/src/main/java/com/sun/enterprise/web/WebContainer.java
@@ -2807,7 +2807,10 @@ public class WebContainer implements org.glassfish.api.container.Container, Post
             docroot = vsBean.getDocroot();
         }
         if (docroot != null) {
-            updateDocroot(docroot, vs, vsBean);
+            // Only update docroot if it is modified
+            if (!vs.getDocRoot().getAbsolutePath().equals(docroot)) {
+                updateDocroot(docroot, vs, vsBean);
+            }
         }
 
         List<Property> props = vs.getProperties();
