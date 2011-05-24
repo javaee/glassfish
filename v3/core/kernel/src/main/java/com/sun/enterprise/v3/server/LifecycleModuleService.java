83d5513b39a624123409fd1e8c9babbb8f3dd249
Hong Zhang <hong.hz.zhang@oracle.com>
2011-05-24 00:45:35 +0000

    Port the changes from 3.1.1 branch. Fixed issue 16703. Handle the case where user creates a lifecycle module through admin console and does not enter a value for load order (ignore the empty field for load-order instead of trying to parse it). Ran QL.
    svn path=/trunk/; revision=47041
    
    
    Former-commit-id: 0b50e7061a5c6429d5814524ab3f0ea84a163f69

--- v3/core/kernel/src/main/java/com/sun/enterprise/v3/server/LifecycleModuleService.java
+++ v3/core/kernel/src/main/java/com/sun/enterprise/v3/server/LifecycleModuleService.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -158,7 +158,7 @@ public class LifecycleModuleService implements Startup, PreDestroy, PostConstruc
                     ServerTags.LOAD_ORDER); 
 
                 int order = Integer.MAX_VALUE;
-                if (strOrder != null) {
+                if (strOrder != null && strOrder.length() > 0) {
                     try {
                         order = Integer.parseInt(strOrder);
                     } catch(NumberFormatException nfe) {
