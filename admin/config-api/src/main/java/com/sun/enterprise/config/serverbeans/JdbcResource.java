46644598bb26fe49c7b5aed99b5c95f70b1bf36b
Shalini Muthukrishnan <sm157516@java.net>
2011-05-30 08:13:05 +0000

    Fix for issue 15734. Modifying the custom validators for connection pools and resources to support I18n.
    svn path=/branches/3.1.1/; revision=47189
    
    
    Former-commit-id: 8d3335ec72faf473eed15a310fc86064cd742322

--- admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/JdbcResource.java
+++ admin/config-api/src/main/java/com/sun/enterprise/config/serverbeans/JdbcResource.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -49,6 +49,7 @@ import org.jvnet.hk2.config.Element;
 
 import java.beans.PropertyVetoException;
 import java.util.List;
+import javax.validation.Payload;
 
 import org.glassfish.api.admin.config.PropertiesDesc;
 import org.jvnet.hk2.config.types.Property;
@@ -76,8 +77,9 @@ import javax.validation.constraints.NotNull;
  @RestRedirect(opType = RestRedirect.OpType.POST, commandName = "create-jdbc-resource"),
  @RestRedirect(opType = RestRedirect.OpType.DELETE, commandName = "delete-jdbc-resource")
 })
-@ResourcePoolReferenceConstraint
-public interface JdbcResource extends ConfigBeanProxy, Injectable, Resource, PropertyBag, BindableResource {
+@ResourcePoolReferenceConstraint(message="{resource.invalid}", payload=JdbcResource.class)
+public interface JdbcResource extends ConfigBeanProxy, Injectable, Resource, 
+        PropertyBag, BindableResource, Payload {
     
     /**              
      * Gets the value of the poolName property.
