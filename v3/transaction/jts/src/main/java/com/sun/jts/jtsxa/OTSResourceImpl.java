352c50c607976657f13052f9a53657b82fbd18c3
Marina Vatkina <marina.vatkina@oracle.com>
2011-05-31 20:33:13 +0000

    Handle XA_HEURRB during rollback
    svn path=/trunk/; revision=47224
    
    
    Former-commit-id: f0a998f0dd07a43a8784cfacfa45f870b0caa478

--- v3/transaction/jts/src/main/java/com/sun/jts/jtsxa/OTSResourceImpl.java
+++ v3/transaction/jts/src/main/java/com/sun/jts/jtsxa/OTSResourceImpl.java
@@ -361,6 +361,13 @@ public class OTSResourceImpl extends OTSResourcePOA implements OTSResource {
                 _logger.log(Level.WARNING, "jts.unexpected_error_occurred_twopc_rollback", ex);
                 throw new TRANSACTION_ROLLEDBACK(0, CompletionStatus.COMPLETED_MAYBE);
             }
+            if (e.errorCode == XAException.XA_HEURRB) {
+                // Can't throw HeuristicRollback exception because org.omg.CosTransactions.ResourceOperations#rollback
+                // doesn't declare it
+                HeuristicHazard hh = new HeuristicHazard(ex.getMessage());
+                hh.initCause(ex);
+                throw hh;
+            }
             INTERNAL internal =  new INTERNAL(0,CompletionStatus.COMPLETED_MAYBE);
             internal.initCause(ex);
             _logger.log(Level.WARNING, "jts.unexpected_error_occurred_twopc_rollback", ex);
