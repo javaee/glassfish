a98b49604f458b5ffb4b18833f02018fa1d06fbe
Jagadish Ramu <jagadish.ramu@oracle.com>
2011-05-31 10:43:40 +0000

    Issue : GLASSFISH-16749 Description : Missing API to get shutdown timeout
    Tests Passed : QL (Web, Classic), connector-dev, jdbc-dev, resources-admin-cli, connector-standalone-cts (Web, Classic)
    
    svn path=/branches/3.1.1/; revision=47204
    
    
    Former-commit-id: a46107447ec4e7d00d5351097b6d68fbace1684b

--- connectors/connectors-internal-api/src/main/java/com/sun/appserv/connectors/internal/api/ConnectorRuntime.java
+++ connectors/connectors-internal-api/src/main/java/com/sun/appserv/connectors/internal/api/ConnectorRuntime.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -712,4 +712,11 @@ public interface ConnectorRuntime extends ConnectorConstants{
      */
     public void associateResourceAdapter(String rarName, ResourceAdapterAssociation raa)
             throws ResourceException;
+
+    /**
+     * Gets the shutdown-timeout attribute configured in <code>connector-service</code>
+     * @return long shutdown timeout (in milli-seconds)
+     */
+    public long getShutdownTimeout() ;
+
 }
