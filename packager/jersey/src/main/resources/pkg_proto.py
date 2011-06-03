8b20cc4cff2048310da543ed5f46b66535c17f59
Pavel Bucek <pavel.bucek@oracle.com>
2011-06-03 17:23:37 +0000

    jersey 1.8-ea02 integration
    svn path=/branches/3.1.1/; revision=47303
    
    
    Former-commit-id: 4fa74ee5f61492efbbcd91c876b867438ce247aa

--- packager/jersey/src/main/resources/pkg_proto.py
+++ packager/jersey/src/main/resources/pkg_proto.py
@@ -46,7 +46,7 @@ conf = imp.load_source("pkg_conf", "../pkg_conf.py")
 
 pkg = {
     "name"          : "jersey",
-    "version"       : "1.5,0-1.0",
+    "version"       : "1.8,0-0.5",
     "attributes"    : { "pkg.summary" : "Jersey Core, RESTful Web services for GlassFish",
                         "pkg.description" : 
 "Jersey core runtime libraries including some 3rd party dependencies. \
