46dd53561349f9882df23e32c1af280be64687ad
Jason Lee <jdlee@java.net>
2011-06-06 20:15:10 +0000

    Fix (and test) for GLASSFISH-16765
    svn path=/trunk/; revision=47333
    
    
    Former-commit-id: 2bd5e1f88bad2aaf90ffbc05ef303b48c5a6c691

--- v3/admin/rest/src/main/java/org/glassfish/admin/rest/generator/CommandResourceMetaData.java
+++ v3/admin/rest/src/main/java/org/glassfish/admin/rest/generator/CommandResourceMetaData.java
@@ -307,7 +307,7 @@ public class CommandResourceMetaData {
         {"JmsService", "list-jms-hosts", "GET", "list-jms-hosts", "list-jms-hosts"},
         {"JmsService", "list-jms-resources", "GET", "list-jms-resources", "list-jms-resources"},
         {"JmsHost", "delete-jms-host", "DELETE", "delete-jms-host", "Delete JMS Host", "id=$parent"},
-        {"LbConfig", "create-http-lb-ref", "POST", "create-http-lb-ref", "create-http-lb-ref"},
+        {"LbConfig", "create-http-lb-ref", "POST", "create-http-lb-ref", "create-http-lb-ref", "config=$parent"},
         {"LbConfig", "create-http-listener", "POST", "create-http-listener", "create-http-listener"},
         {"LbConfig", "delete-http-lb-ref", "POST", "delete-http-lb-ref", "delete-http-lb-ref"},
         {"LbConfig", "delete-http-listener", "POST", "delete-http-listener", "delete-http-listener"},
