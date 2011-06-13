e404e076ee2518abaedc8907feb67adb7665787c
Fabian Ritzmann <ritzmann@java.net>
2011-06-13 13:47:20 +0000

    GLASSFISH-16748: Integrating Metro 2.1.1-b06 and Woodstox 4.1.1.
    svn path=/branches/3.1.1/; revision=47443
    
    
    Former-commit-id: 9d9fef9a73e4efc75c7ba87988fa919b7e03e057

--- packager/resources/pkg_conf.py
+++ packager/resources/pkg_conf.py
@@ -66,7 +66,7 @@ javadb_version="10.5.3.0,0-0"
 corba_version="3.2.0,0-2"
 jsf_version="2.1.2,0-4"
 grizzly_version="1.9.35,0-1"
-metro_version="2.1.1,0-1"
+metro_version="2.1.1,0-6"
 javahelp_version="2.0.2,0-1"
 shoal_version="1.5.32,0-0"
 
