0b7fd5369e9143a7159d3d91d19daad06d3709c6
Shalini Muthukrishnan <sm157516@java.net>
2011-06-22 05:00:04 +0000

    Better test report for jdbc41 tests to reflect what individual tests do.
    svn path=/trunk/; revision=47606
    
    
    Former-commit-id: 4205ce9962cdc66d31b72d1d516668f20aa9dc30

--- v2/appserv-tests/devtests/jdbc/jdbc41-devtests/client/Client.java
+++ v2/appserv-tests/devtests/jdbc/jdbc41-devtests/client/Client.java
@@ -14,7 +14,7 @@ public class Client {
         throws Exception {
         
 	SimpleReporterAdapter stat = new SimpleReporterAdapter();
-	String testSuite = " JDBC 41 tests on JDK7 ";
+	String testSuite = " JDBC41 tests on JDK7 ";
 
         InitialContext ic = new InitialContext();
         Object objRef = ic.lookup("java:comp/env/ejb/SimpleSessionHome");
@@ -24,27 +24,27 @@ public class Client {
 	stat.addDescription("Running JDBC41 testSuite ");
         SimpleSession simpleSession = simpleSessionHome.create();
         if (simpleSession.test1() ) {
-	    stat.addStatus( testSuite + " test1 : " , stat.PASS );
+	    stat.addStatus( testSuite + " test1 : Connection API/abort Test : " , stat.PASS );
 	} else {
-	    stat.addStatus( testSuite + " test1 : " , stat.FAIL );
+	    stat.addStatus( testSuite + " test1 : Connection API/abort Test : " , stat.FAIL );
 	}
     
         if (simpleSession.test2() ) {
-	    stat.addStatus( testSuite + " test2 : " , stat.PASS );
+	    stat.addStatus( testSuite + " test2 : Connection API/abort usertx Test : " , stat.PASS );
 	} else {
-	    stat.addStatus( testSuite + " test2 : " , stat.FAIL );
+	    stat.addStatus( testSuite + " test2 : Connection API/abort usertx Test : " , stat.FAIL );
 	}
 	
         if (simpleSession.test3() ) {
-	    stat.addStatus( testSuite + " test3 : " , stat.PASS );
+	    stat.addStatus( testSuite + " test3 : DatabaseMetaData Test : " , stat.PASS );
 	} else {
-	    stat.addStatus( testSuite + " test3 : " , stat.FAIL );
+	    stat.addStatus( testSuite + " test3 : DatabaseMetaData Test : " , stat.FAIL );
 	}
     
         if (simpleSession.test4() ) {
-	    stat.addStatus( testSuite + " test4 : " , stat.PASS );
+	    stat.addStatus( testSuite + " test4 : Statement API/Connection abort usertx Test : " , stat.PASS );
 	} else {
-	    stat.addStatus( testSuite + " test4 : " , stat.FAIL );
+	    stat.addStatus( testSuite + " test4 : Statement API/Connection abort usertx Test : " , stat.FAIL );
 	}
 	stat.printSummary();
     }
