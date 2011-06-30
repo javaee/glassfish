581553adb838b8e2aff05c1e5174d39e552fb538
Sarada Kommalapati <sarada.kommalapati@oracle.com>
2011-06-30 14:28:28 +0000

    Added comments to the persistence util classes.
    svn path=/trunk/; revision=47785
    
    
    Former-commit-id: 731a1bc952b67d007911b2b560649b30c27d2e29

--- v2/appserv-tests/devtests/persistence/util/ServletUtil.java
+++ v2/appserv-tests/devtests/persistence/util/ServletUtil.java
@@ -1,5 +1,22 @@
 /*
- * ServletUtil.java
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * glassfish/bootstrap/legal/CDDLv1.0.txt or
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * HEADER in each file and include the License file at
+ * glassfish/bootstrap/legal/CDDLv1.0.txt.  If applicable,
+ * add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your
+ * own identifying information: Portions Copyright [yyyy]
+ * [name of copyright owner]
  */
 
 package util;
@@ -16,6 +33,15 @@ import javax.annotation.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+
+/** ServletUtil.java
+  * This program is generic servlet which process the 
+  * HTTP request and invoke the 
+  * proper methods based on the request parameters.
+  *
+  * @author      Sarada Kommalapati
+  */
+
 
 public class ServletUtil extends HttpServlet{
 
--- v2/appserv-tests/devtests/persistence/util/WebTestUtil.java
+++ v2/appserv-tests/devtests/persistence/util/WebTestUtil.java
@@ -1,3 +1,23 @@
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the "License").  You may not use this file except
+ * in compliance with the License.
+ *
+ * You can obtain a copy of the license at
+ * glassfish/bootstrap/legal/CDDLv1.0.txt or
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * HEADER in each file and include the License file at
+ * glassfish/bootstrap/legal/CDDLv1.0.txt.  If applicable,
+ * add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your
+ * own identifying information: Portions Copyright [yyyy]
+ * [name of copyright owner]
+ */
 
 package util;
 
@@ -6,6 +26,20 @@ import java.net.*;
 import java.util.*;
 
 import com.sun.ejte.ccl.reporter.SimpleReporterAdapter;
+
+/** WebTestUtil.java
+  * This program opens HttpURLconnection,sends the request to the 
+  * servlet , & receives the response from the servlet.
+  * Using commandline args the user can specify for WebTestUtil
+  * 1. test suite name
+  * 2. host name
+  * 3. port no
+  * 4. context root of the servlet that is defined in web.xml
+  * 5. url pattern of the servlet that is defined in web.xml
+  *
+  * @author      Sarada Kommalapati
+  */
+
 
 public class WebTestUtil {
 
@@ -33,6 +67,7 @@ public class WebTestUtil {
     public void test( String c) throws Exception {
       this.test( c, "");
     }
+
 
     public void test( String c, String params) throws Exception {
         String EXPECTED_RESPONSE = c + ":pass";
