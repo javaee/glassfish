ccd14842f44684b3e78f891661f784294158774b
Bhavanishankara Sapaliga <bhavanishankara.sapaliga@oracle.com>
2011-06-30 10:40:08 +0000

    Adding a wrapper ANT build.xml to be able to run jdbc devtests with Embedded Glassfish.
    svn path=/trunk/; revision=47779
    
    
    Former-commit-id: 484a997ffcacc8fe28e0f093a570b867e0b41d7c

---
+++ v2/appserv-tests/devtests/jdbc/v3/embedded/unsetup-jdbc-dev-tests_embedded.sh
@@ -0,0 +1,60 @@
+v3home=${S1AS_HOME}
+databaseshome=/tmp/jdbc_devtests/databases
+cd $v3home
+
+echo undeploying application .....
+./bin/asadmin undeploy v3_jdbc_dev_tests
+echo "\n"
+
+echo deleting jdbc-resources .....
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-multiple-user-test-resource
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-app-auth-test-resource
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-common-resource
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-stmt-timeout-test-resource
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-max-conn-usage-test-resource
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-conn-leak-tracing-test-resource
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-associate-with-thread-test-resource
+#./bin/asadmin delete-jdbc-resource jdbc/jdbc-simple-xa-test-resource
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-simple-xa-test-resource-1
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-simple-xa-test-resource-2
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-lazy-assoc-test-resource
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-lazy-enlist-resource-1
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-lazy-enlist-resource-2
+./bin/asadmin delete-jdbc-resource jdbc/double-resource-reference-resource-1
+./bin/asadmin delete-jdbc-resource jdbc/double-resource-reference-resource-2
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-dev-test-resource
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-reconfig-test-resource-1
+./bin/asadmin delete-jdbc-resource jdbc/jdbc-reconfig-test-resource-2
+./bin/asadmin delete-jdbc-resource jdbc/res1
+./bin/asadmin delete-jdbc-resource jdbc/res2
+echo "\n"
+
+echo deleting jdbc-connection-pools .....
+./bin/asadmin delete-jdbc-connection-pool jdbc-multiple-user-test-pool
+./bin/asadmin delete-jdbc-connection-pool jdbc-app-auth-test-pool
+./bin/asadmin delete-jdbc-connection-pool jdbc-common-pool
+./bin/asadmin delete-jdbc-connection-pool jdbc-statement-timeout-test-pool
+./bin/asadmin delete-jdbc-connection-pool jdbc-max-conn-usage-test-pool
+./bin/asadmin delete-jdbc-connection-pool jdbc-conn-leak-tracing-test-pool
+./bin/asadmin delete-jdbc-connection-pool jdbc-associate-with-thread-test-pool
+#./bin/asadmin delete-jdbc-connection-pool jdbc-simple-xa-test-pool
+./bin/asadmin delete-jdbc-connection-pool jdbc-lazy-assoc-test-pool
+./bin/asadmin delete-jdbc-connection-pool jdbc-simple-xa-test-pool-1
+./bin/asadmin delete-jdbc-connection-pool jdbc-simple-xa-test-pool-2
+./bin/asadmin delete-jdbc-connection-pool jdbc-lazy-enlist-pool-1
+./bin/asadmin delete-jdbc-connection-pool jdbc-lazy-enlist-pool-2
+./bin/asadmin delete-jdbc-connection-pool double-resource-reference-pool-1
+./bin/asadmin delete-jdbc-connection-pool jdbc-dev-test-pool
+./bin/asadmin delete-jdbc-connection-pool jdbc-reconfig-test-pool-1
+./bin/asadmin delete-jdbc-connection-pool jdbc-reconfig-test-pool-2
+./bin/asadmin delete-jdbc-connection-pool pool1
+./bin/asadmin delete-jdbc-connection-pool pool2
+echo "\n"
+
+echo Deleting files...
+rm -f ./SetDerbyAuthentication.class
+rm -rf $databaseshome
+cd $APS_HOME/devtests/jdbc/v3/embedded
+rm -rf sun-appserv-samples/ lazyenlist-test-1/ lazyenlist-test-2/ *.class derby.log
+echo "\n"
+
