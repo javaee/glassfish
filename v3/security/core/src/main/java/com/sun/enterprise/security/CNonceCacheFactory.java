37f51f63f3f295ea4b927fd2660f85ae655b0b7d
Kumar Jayanti <v.b.kumar.jayanti@oracle.com>
2011-07-11 06:35:20 +0000

    Fixed copyright.
    svn path=/trunk/; revision=47954
    
    
    Former-commit-id: 01c3620085321cc947d5ed18f27c1a4221d9b006

--- v3/security/core/src/main/java/com/sun/enterprise/security/CNonceCacheFactory.java
+++ v3/security/core/src/main/java/com/sun/enterprise/security/CNonceCacheFactory.java
@@ -36,8 +36,6 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
- *
- *
  */
 package com.sun.enterprise.security;
 
