3a1b8f639195541130268ef15b7e3cb361ff524d
Amy Moon <amy.roh@oracle.com>
2011-07-15 03:25:11 +0000

    Fix dynamic reconfig regression.  Update HttpService for HttpService related events.
    Ran QL and devtests.
    
    svn path=/branches/3.1.1/; revision=48052
    
    
    Former-commit-id: 142739c8d976b82eeab36e7b97c3648799116317

--- web/web-glue/src/main/java/com/sun/enterprise/web/reconfig/WebConfigListener.java
+++ web/web-glue/src/main/java/com/sun/enterprise/web/reconfig/WebConfigListener.java
@@ -120,14 +120,16 @@ public class WebConfigListener implements ConfigListener, MapperUpdateListener {
      */
     @Override
     public UnprocessedChangeEvents changed(PropertyChangeEvent[] events) {
-        return ConfigSupport.sortAndDispatch(events, new Changed() {         
+        return ConfigSupport.sortAndDispatch(events, new Changed() {
             @Override
             public <T extends ConfigBeanProxy> NotProcessed changed(TYPE type, Class<T> tClass, T t) {
                 if (logger.isLoggable(Level.FINE)) {
                     logger.fine("Web container config changed "+type+" "+tClass+" "+t);
                 }
                 try {
-                    if (tClass == NetworkListener.class) {
+                    if (tClass == HttpService.class) {
+                        container.updateHttpService((HttpService) t);
+                    } else if (tClass == NetworkListener.class) {
                         if (type==TYPE.ADD) {
                             container.addConnector((NetworkListener) t, httpService, true);
                         } else if (type==TYPE.REMOVE) {
