67f91fd9bbf460f351456383e154281f4f097bf0
Anissa Lam <anissa.lam@oracle.com>
2011-08-17 03:15:14 +0000

    change steps and wizard header and side color.
    svn path=/branches/console.next/; revision=48828

--- webapp/src/main/java/org/glassfish/admingui/console/WizardBean.java
+++ webapp/src/main/java/org/glassfish/admingui/console/WizardBean.java
@@ -20,11 +20,12 @@ import javax.faces.bean.ViewScoped;
 public class WizardBean {
     private int step = 0;
     private List<String> stepLabels = Collections.unmodifiableList(new ArrayList<String>(){{ 
-        add("Upload archive");
-        add("Pick Virtualization Template");
-        add("Configure Database");
-        add("Configure Load Balancer");
-        add("Configure Elasticity Settings");
+        add("1. Upload archive");
+        add("2. Pick Virtualization Template");
+        add("3. Configure Database");
+        add("4. Configure Load Balancer");
+        add("5. Configure Elasticity Settings");
+        add("6. Review and Deploy");
     }});
     private List<String> stepPages = Collections.unmodifiableList(new ArrayList<String>(){{ 
         add("/demo/wizard/upload.xhtml");
