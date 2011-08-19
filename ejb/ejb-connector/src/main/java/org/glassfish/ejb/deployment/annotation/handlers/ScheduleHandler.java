a8db0eadd9228daf6f4c7e70f826584663f680b9
Cheng Fang <cf126330@java.net>
2011-08-19 15:15:36 +0000

    Fix issue 17038 3.1.1 deployment performance - ejb container module get loaded for a pure web application. Move annotation handler classes, archivist classes, scanner classes and related from ejb-container to ejb-connector. Reviewed by Hong and Shing Wai, ran QL and ejb devtests.
    svn path=/branches/3.1.2/; revision=48926
    
    
    Former-commit-id: c7a7a795ccaa43e1c917eb24e57f3beac2f88f11

--- ejb/ejb-connector/src/main/java/org/glassfish/ejb/deployment/annotation/handlers/ScheduleHandler.java
+++ ejb/ejb-connector/src/main/java/org/glassfish/ejb/deployment/annotation/handlers/ScheduleHandler.java
@@ -52,7 +52,7 @@ import javax.ejb.*;
 import com.sun.enterprise.deployment.EjbDescriptor;
 import com.sun.enterprise.deployment.MethodDescriptor;
 import com.sun.enterprise.deployment.ScheduledTimerDescriptor;
-import com.sun.ejb.containers.TimerSchedule;
+//import com.sun.ejb.containers.TimerSchedule;
 
 import org.glassfish.apf.AnnotationInfo;
 import org.glassfish.apf.AnnotatedElementHandler;
@@ -116,6 +116,9 @@ public class ScheduleHandler extends AbstractAttributeHandler {
 
                     if (logger.isLoggable(Level.FINE)) {
                         logger.fine("@@@ Found Schedule on " + annMethod);
+                        
+                        /* Issue 17038, commented out for now since it references 
+                         * TimerSchedule class in ejb-container module.
                         TimerSchedule ts = new TimerSchedule(sd, 
                                 annMethod.getName(), annMethod.getParameterTypes().length);
 
@@ -123,6 +126,8 @@ public class ScheduleHandler extends AbstractAttributeHandler {
                         logger.fine("@@@ First timeout: " + 
                                 ((ts.isValid(date))? date.getTime() : "NEVER"));
                         logger.fine("@@@ Schedule : " + ts.getScheduleAsString());
+                         
+                         */
                         logger.fine("@@@ TimerConfig : " + 
                                 ((sd.getInfo() != null && !sd.getInfo().equals(""))? sd.getInfo() : null) + 
                                 " # " + sd.getPersistent());
