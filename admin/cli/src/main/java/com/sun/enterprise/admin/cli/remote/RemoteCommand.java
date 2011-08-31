80e34c528fd182aef777fa684c6bad8c624428e2
Tom Mueller <tmueller@java.net>
2011-08-31 16:26:33 +0000

    Make certificate prompting related to secure admin pay attention to the --interactive option to asadmin.  With this change, the prompting that happens when there is a self-signed cert can be overridden with the --interactive=false option. Passed QL.
    svn path=/branches/3.1.2/; revision=49143
    
    
    Former-commit-id: e214f24b136b21d4027439a87c9b5e24d4a4db7e

--- admin/cli/src/main/java/com/sun/enterprise/admin/cli/remote/RemoteCommand.java
+++ admin/cli/src/main/java/com/sun/enterprise/admin/cli/remote/RemoteCommand.java
@@ -492,6 +492,7 @@ public class RemoteCommand extends CLICommand {
                 programOpts.isSecure(), programOpts.getUser(),
                 programOpts.getPassword(), logger, programOpts.getAuthToken());
             rac.setFileOutputDirectory(outputDir);
+            rac.setInteractive(programOpts.isInteractive());;
         }
     }
 
