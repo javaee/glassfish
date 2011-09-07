50d448b1c638faa5a9ea745c6236c007c51bf78c
Jason Lee <jdlee@java.net>
2011-09-07 22:41:12 +0000

    Quasi-experimental effort to reduce time spent in getters for tables
    svn path=/branches/console.next/; revision=49329

--- webapp/src/main/java/org/glassfish/admingui/console/Overview.java
+++ webapp/src/main/java/org/glassfish/admingui/console/Overview.java
@@ -4,14 +4,13 @@
  */
 package org.glassfish.admingui.console;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import org.glassfish.admingui.console.rest.RestUtil;
 import org.glassfish.admingui.console.util.CommandUtil;
@@ -20,60 +19,69 @@ import org.glassfish.admingui.console.util.CommandUtil;
  *
  * @author anilam
  */
-
 @ManagedBean
-@SessionScoped
-public class Overview  {
+@ViewScoped
+public class Overview {
+
+    private List<Map> services;
+    private final List<Map> apps = new ArrayList<Map>();
 
     public List<Map> getApplications() {
-        List<Map> apps = new ArrayList();
-        Map sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
-        List<String> deployingApps = (List)sessionMap.get("_deployingApps");
-        String endPoint = "http://localhost:4848/management/domain/applications/list-applications";
-        Map attrs = new HashMap();
-        attrs.put("target", "domain");  //specify domain to get Paas deployed app.
-        Map appData = (Map)RestUtil.restRequest(endPoint, attrs, "GET", null, null, false, true).get("data");
-        Map<String, String> props = (Map)appData.get("properties");
-        if (props != null){
-            for (String appName : props.keySet()) {
-                Map app = new HashMap();
-                app.put("appName", appName);
-                app.put("notExist", false);
-                apps.add(app);
-                if (deployingApps != null && deployingApps.contains(appName)){
-                    deployingApps.remove(appName);
+        synchronized (apps) {
+            if (apps.isEmpty()) {
+                Map sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+                List<String> deployingApps = (List) sessionMap.get("_deployingApps");
+                String endPoint = "http://localhost:4848/management/domain/applications/list-applications";
+                Map attrs = new HashMap();
+                attrs.put("target", "domain");  //specify domain to get Paas deployed app.
+                Map appData = (Map) RestUtil.restRequest(endPoint, attrs, "GET", null, null, false, true).get("data");
+                Map<String, String> props = (Map) appData.get("properties");
+                if (props != null) {
+                    for (String appName : props.keySet()) {
+                        Map app = new HashMap();
+                        app.put("appName", appName);
+                        app.put("notExist", false);
+                        apps.add(app);
+                        if (deployingApps != null && deployingApps.contains(appName)) {
+                            deployingApps.remove(appName);
+                        }
+                    }
                 }
-            }
-        }
-        if (deployingApps != null){
-            for(String one :deployingApps){
-                Map app = new HashMap();
-                app.put("appName", one);
-                app.put("notExist", true);
-                apps.add(app);
+                if (deployingApps != null) {
+                    for (String one : deployingApps) {
+                        Map app = new HashMap();
+                        app.put("appName", one);
+                        app.put("notExist", true);
+                        apps.add(app);
+                    }
+                }
             }
         }
         return apps;
     }
 
     public List<Map> getServices() {
-        List services =  CommandUtil.listServices(null, null, null);
+        synchronized (this) {
+            if (services == null) {
+                services = CommandUtil.listServices(null, null, null);
 
-        //In the overview page, we don't want to show clusterInstance type.
-        Iterator iter = services.iterator();
-        while (iter.hasNext()) {
-            Map oneEntry = (Map)iter.next();
-            if ("ClusterInstance".equals(oneEntry.get("serverType"))) {
-                iter.remove();
-            }
-            if ("Cluster".equals(oneEntry.get("serverType"))){
-                oneEntry.put("serverType", "JavaEE");
+                //In the overview page, we don't want to show clusterInstance type.
+                Iterator iter = services.iterator();
+                while (iter.hasNext()) {
+                    Map oneEntry = (Map) iter.next();
+                    if ("ClusterInstance".equals(oneEntry.get("serverType"))) {
+                        iter.remove();
+                    }
+                    if ("Cluster".equals(oneEntry.get("serverType"))) {
+                        oneEntry.put("serverType", "JavaEE");
+                    }
+                }
             }
         }
         return services;
     }
 
-    public List<Map>getEnvironments(){
+    public List<Map> getEnvironments() {
         return CommandUtil.getEnvironments();
     }
 }
