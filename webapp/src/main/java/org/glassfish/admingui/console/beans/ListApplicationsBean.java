ae52f2a0c445ccbd0ee6980e55545df4c33d9671
Siraj Ghaffar <siraj.ghaffar@oracle.com>
2011-09-13 14:07:44 +0000

    Remove calls to overview, remove elasticity
    svn path=/branches/console.next/; revision=49471

--- webapp/src/main/java/org/glassfish/admingui/console/beans/ListApplicationsBean.java
+++ webapp/src/main/java/org/glassfish/admingui/console/beans/ListApplicationsBean.java
@@ -11,16 +11,18 @@ import javax.faces.bean.ViewScoped;
 import java.util.*;
 import org.glassfish.admingui.console.rest.RestUtil;
 import org.glassfish.admingui.console.util.CommandUtil;
+import javax.faces.context.FacesContext;
 
 @ManagedBean(name="listApplicationsBean")
 @ViewScoped
 public class ListApplicationsBean {
     private Map appData;
+    transient private final List<Map> apps = new ArrayList<Map>();
 
     public ListApplicationsBean() {
 //        applications = getApplications();
     }
-
+/**
     public List<Application> getApplications() {
         List<Application> apps = new ArrayList<Application>();
         String endPoint = "http://localhost:4848/management/domain/applications/list-applications";
@@ -33,6 +35,42 @@ public class ListApplicationsBean {
         }
         return apps;
     }
+**/
+
+    public List<Map> getApplications() {
+        synchronized (apps) {
+            if (apps.isEmpty()) {
+                Map sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+                List<String> deployingApps = (List) sessionMap.get("_deployingApps");
+                String endPoint = "http://localhost:4848/management/domain/applications/list-applications";
+                Map attrs = new HashMap();
+                attrs.put("target", "domain");  //specify domain to get Paas deployed app.
+                Map appData = (Map) RestUtil.restRequest(endPoint, attrs, "GET", null, null, false, true).get("data");
+                Map<String, String> props = (Map) appData.get("properties");
+                if (props != null) {
+                    for (String appName : props.keySet()) {
+                        Map app = new HashMap();
+                        app.put("appName", appName);
+                        app.put("notExist", false);
+                        apps.add(app);
+                        if (deployingApps != null && deployingApps.contains(appName)) {
+                            deployingApps.remove(appName);
+                        }
+                    }
+                }
+                if (deployingApps != null) {
+                    for (String one : deployingApps) {
+                        Map app = new HashMap();
+                        app.put("appName", one);
+                        app.put("notExist", true);
+                        apps.add(app);
+                    }
+                }
+            }
+        }
+        return apps;
+    }
+
 
     //get list of services for the specified application
     
