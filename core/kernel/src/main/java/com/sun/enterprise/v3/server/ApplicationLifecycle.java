423df9a1e40706868f2ceead9c28a7e755d62bbf
Hong Zhang <hong.hz.zhang@oracle.com>
2011-10-17 16:35:11 +0000

    Fixed issue 16995 (port/merge fix to 3.1.2 branch). Send the disable event synchronously so we don't run into problems when deploying JPA application with high availability. Ran QL, deployment dev tests.
    svn path=/branches/3.1.2/; revision=50278
    
    
    Former-commit-id: 6e90ba8a9d92ae2b222458df312bc0e64d70a6b9

--- core/kernel/src/main/java/com/sun/enterprise/v3/server/ApplicationLifecycle.java
+++ core/kernel/src/main/java/com/sun/enterprise/v3/server/ApplicationLifecycle.java
@@ -998,7 +998,7 @@ public class ApplicationLifecycle implements Deployment, PostConstruct {
         if (info.isLoaded()) {
             info.stop(context, context.getLogger());
             info.unload(context);
-            events.send(new Event<ApplicationInfo>(Deployment.APPLICATION_DISABLED, info));
+            events.send(new Event<ApplicationInfo>(Deployment.APPLICATION_DISABLED, info), false);
         }
 
         return info;
