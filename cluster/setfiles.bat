bdde35f1992310a6b9546e12be694dac44c5d534
Byron Nevins <byron.nevins@oracle.com>
2011-10-27 02:04:52 +0000

    temp files
    svn path=/branches/3.1.2/; revision=50516
    
    
    Former-commit-id: 6a8b0356c18671c87d875930d971cd53ac4f297e

---
+++ cluster/setfiles.bat
@@ -0,0 +1,11 @@
+set a=admin\src\main\java\com\sun\enterprise\v3\admin\cluster\CreateNodeSshCommand.java
+set b=admin\src\main\java\com\sun\enterprise\v3\admin\cluster\CreateRemoteNodeCommand.java
+set c=admin\src\main\java\com\sun\enterprise\v3\admin\cluster\dcom\CreateNodeDcom.java
+set d=admin\src\main\java\com\sun\enterprise\v3\admin\cluster\dcom\UpdateNodeDcomCommand.java
+set e=admin\src\main\java\com\sun\enterprise\v3\admin\cluster\ValidateDcom.java
+set f=cli\src\main\java\com\sun\enterprise\admin\cli\cluster\InstallNodeDcomCommand.java
+set g=cli\src\main\java\com\sun\enterprise\admin\cli\cluster\NativeRemoteCommandsBase.java
+set h=cli\src\main\java\com\sun\enterprise\admin\cli\cluster\UninstallNodeDcomCommand.java
+set i=ssh\src\main\java\org\glassfish\cluster\ssh\util\DcomUtils.java
+set tr=d:\gf\trunk\main\nucleus\cluster
+set br=d:\gf\branches\3.1.2\cluster
