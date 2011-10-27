4ec360144fafa35d95e0b250e593065b0ac3f628
Byron Nevins <byron.nevins@oracle.com>
2011-10-27 01:30:07 +0000

    temp files
    svn path=/branches/3.1.2/; revision=50512
    
    
    Former-commit-id: ca32b29939ba477c124c3d690067d229d445934d

---
+++ cluster/compare.bat
@@ -0,0 +1,21 @@
+@echo off
+call setfiles.bat
+
+echo ******  %a%
+diff %br%\%a% %tr%\%a%
+echo ******  %b%
+diff %br%\%b% %tr%\%b%
+echo ******  %c%
+diff %br%\%c% %tr%\%c%
+echo ******  %d%
+diff %br%\%d% %tr%\%d%
+echo ******  %e%
+diff %br%\%e% %tr%\%e%
+echo ******  %f%
+diff %br%\%f% %tr%\%f%
+echo ******  %g%
+diff %br%\%g% %tr%\%g%
+echo ******  %h%
+diff %br%\%h% %tr%\%h%
+echo ******  %i%
+diff %br%\%i% %tr%\%i%
---
+++ cluster/copyy.bat
@@ -0,0 +1,12 @@
+@echo off
+call setfiles.bat
+
+copy %br%\%a% %tr%\%a%
+copy %br%\%b% %tr%\%b%
+copy %br%\%c% %tr%\%c%
+copy %br%\%d% %tr%\%d%
+copy %br%\%e% %tr%\%e%
+copy %br%\%f% %tr%\%f%
+copy %br%\%g% %tr%\%g%
+copy %br%\%h% %tr%\%h%
+copy %br%\%i% %tr%\%i%
