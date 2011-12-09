c2b428e889a8e2d332cdaa7cb14248f893cf8cfd
Byron Nevins <byron.nevins@oracle.com>
2011-12-09 07:26:25 +0000

    http://java.net/jira/browse/GLASSFISH-17946 Added a check for JDK on the remote Windows node.  It runs
    javac -version
    
    and returns the result.
    Annoyingly, javac writes this to stderr not stdout so I had to make plenty of changes.
    
    
    svn path=/branches/3.1.2/; revision=51408
    
    
    Former-commit-id: e3f580e30be8230675935b2937c3f643cbf034a7

--- cluster/setfiles.bat
+++ cluster/setfiles.bat
@@ -21,6 +21,8 @@ rem set g=ssh\src\main\java\org\glassfish\cluster\ssh\util\DcomInfo.java
 rem set h=admin\src\main\java\com\sun\enterprise\v3\admin\cluster\StopInstanceCommand.java
 rem set i=ssh\src\main\java\org\glassfish\cluster\ssh\connect\NodeRunnerDcom.java
 
-set a=admin\src\main\java\com\sun\enterprise\v3\admin\cluster\NodeUtils.java
+set a=admin\src\main\java\com\sun\enterprise\v3\admin\cluster\ValidateDcom.java
+set b=common\src\main\java\com\sun\enterprise\util\cluster\windows\process\WindowsRemoteScripter.java
+set c=admin\src\main\java\com\sun\enterprise\v3\admin\cluster\LocalStrings.properties
 set tr=d:\gf\trunk\main\nucleus\cluster
 set br=d:\gf\branches\3.1.2\cluster
