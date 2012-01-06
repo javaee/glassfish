6f1f6f030d26f8f840273dbab62b6f26d3f125f9
Amy Moon <amy.roh@oracle.com>
2012-01-06 03:23:29 +0000

    Port fix for GLASSFISH-18134.
    Approved by Joe Di Pol. Ran embedded tests.
    
    
    svn path=/branches/3.1.2/; revision=51919
    
    
    Former-commit-id: c3e95905a517e00a9f50f76c82cb9869e0b2cd6f

--- web/web-embed/impl/src/main/java/org/glassfish/web/embed/impl/WebContainerImpl.java
+++ web/web-embed/impl/src/main/java/org/glassfish/web/embed/impl/WebContainerImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -329,6 +329,10 @@ public class WebContainerImpl implements WebContainer {
             if (sslConfig.getHandshakeTimeout() > 0) {
                 newSsl.setSSLInactivityTimeout(sslConfig.getHandshakeTimeout());
             }
+
+            newSsl.setCertNickname("s1as");
+            newSsl.setClassname("com.sun.enterprise.security.ssl.GlassfishSSLImpl");
+
         } else {
             log.severe("HttpsListener required for https protocol");
         }
