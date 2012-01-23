5717f94751d032cd3ec0db8aeb78987bdbe10e77
tbeerbower <tbeerbower@java.net>
2012-01-23 18:28:23 +0000

    HK2-25 : Services.forContract(...).all() returns duplicate entries

--- hk2/auto-depends/src/test/java/org/glassfish/hk2/tests/basic/BasicInjectionTest.java
+++ hk2/auto-depends/src/test/java/org/glassfish/hk2/tests/basic/BasicInjectionTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -162,7 +162,6 @@ public class BasicInjectionTest {
     }
 
     @Test
-    @Ignore
     public void testAllContractBindingsRetrieval() {
         assertEquals(3, services.forContract(MultiBoundContract.class).all().size());
 
