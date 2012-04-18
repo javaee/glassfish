aa1f3dfd9ae501a5c9de5deedf894f9d950a359f
Marina Vatkina <marina.vatkina@oracle.com>
2012-04-18 19:21:02 +0000

    Add a test for GLASSFISH-18475
    svn path=/trunk/; revision=53565
    
    
    Former-commit-id: da2e93c7a90777cb7bd10c306a8d95e86a0d7d1f

---
+++ v2/appserv-tests/devtests/ejb/ejb31/timer31/schedule_on_ejb_timeout/ejb/Stles.java
@@ -0,0 +1,7 @@
+package com.sun.s1asdev.ejb31.timer.schedule_on_ejb_timeout;
+
+public interface Stles {
+
+    public void verifyTimers() throws Exception;
+    public void createTimer() throws Exception;
+}
