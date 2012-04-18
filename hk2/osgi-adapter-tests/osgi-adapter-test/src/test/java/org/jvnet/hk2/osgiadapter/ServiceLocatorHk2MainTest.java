1efb7b2759f84627450035484f37971be259fc53
mtaube <mtaube@java.net>
2012-04-18 18:27:24 +0000

    Reenabling test to see if http proxy settings helped hudson

--- hk2/osgi-adapter-tests/osgi-adapter-test/src/test/java/org/jvnet/hk2/osgiadapter/ServiceLocatorHk2MainTest.java
+++ hk2/osgi-adapter-tests/osgi-adapter-test/src/test/java/org/jvnet/hk2/osgiadapter/ServiceLocatorHk2MainTest.java
@@ -37,7 +37,6 @@ import org.osgi.framework.*;
 import org.osgi.util.tracker.ServiceTracker;
 
 @RunWith(org.ops4j.pax.exam.junit.JUnit4TestRunner.class)
-@Ignore
 public class ServiceLocatorHk2MainTest {
 
 	private static final String GROUP_ID = "org.glassfish.hk2";
