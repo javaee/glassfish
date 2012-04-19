17a36b9b44863a5d259f8ea350d458de6aaf3b3f
tbeerbower <tbeerbower@java.net>
2012-04-19 21:53:07 +0000

    adding more AliasDescriptor tests

--- hk2/core/src/main/java/org/glassfish/hk2/core/utilities/AliasDescriptor.java
+++ hk2/core/src/main/java/org/glassfish/hk2/core/utilities/AliasDescriptor.java
@@ -1,3 +1,43 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
 package org.glassfish.hk2.core.utilities;
 
 import org.glassfish.hk2.api.ActiveDescriptor;
@@ -9,9 +49,11 @@ import org.glassfish.hk2.utilities.NamedImpl;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
 
 /**
  * A descriptor class that serves as an alias for another descriptor.
@@ -45,11 +87,18 @@ public class AliasDescriptor<T> extends AbstractActiveDescriptor<T> {
      */
     private boolean initialized = false;
 
+
     // ----- Constants ------------------------------------------------------
 
+    /**
+     * Empty set of contracts used to construct this descriptor.
+     */
     private static final Set<Type> EMPTY_CONTRACT_SET = new HashSet<Type>();
-    private static final Set<Annotation> EMPTY_ANNOTATION_SET = new HashSet<Annotation>();
 
+    /**
+     * Empty set of annotations used to construct this descriptor.
+     */
+    private static final Set<Annotation> EMPTY_ANNOTATION_SET = new HashSet<Annotation>();
 
     // ----- Constructors ---------------------------------------------------
 
@@ -61,11 +110,15 @@ public class AliasDescriptor<T> extends AbstractActiveDescriptor<T> {
      * @param contract    the contact
      * @param name        the name
      */
-    public AliasDescriptor(ServiceLocator locator, ActiveDescriptor<T> descriptor, Class<?> contract, String name) {
-
-        super(EMPTY_CONTRACT_SET, null, name,
-                EMPTY_ANNOTATION_SET, descriptor.getDescriptorType(),
-                descriptor.getRanking());
+    public AliasDescriptor(ServiceLocator locator,
+                           ActiveDescriptor<T> descriptor,
+                           Class<?> contract,
+                           String name) {
+        // pass in an empty contract set, an empty annotation set and a null
+        // scope since we are not really reified and we don't want to reify
+        // the given descriptor yet
+        super(EMPTY_CONTRACT_SET, null, name, EMPTY_ANNOTATION_SET,
+                descriptor.getDescriptorType(), descriptor.getRanking());
         this.locator    = locator;
         this.descriptor = descriptor;
         this.contract   = contract;
@@ -73,11 +126,37 @@ public class AliasDescriptor<T> extends AbstractActiveDescriptor<T> {
         super.setScope(descriptor.getScope());
     }
 
+
+    // ----- ActiveDescriptor -----------------------------------------------
+
+    /* (non-Javadoc)
+     * @see org.glassfish.hk2.api.ActiveDescriptor#getImplementationClass()
+     */
+    @Override
+    public Class<?> getImplementationClass() {
+        ensureInitialized();
+        return descriptor.getImplementationClass();
+    }
+
+    /* (non-Javadoc)
+     * @see org.glassfish.hk2.api.ActiveDescriptor#create(org.glassfish.hk2.api.ServiceHandle)
+     */
+    @Override
+    public T create(ServiceHandle<?> root) {
+        ensureInitialized();
+        return locator.getServiceHandle(descriptor).getService();
+    }
+
+
+    // ----- AbstractActiveDescriptor overrides -----------------------------
+
     /* (non-Javadoc)
      * @see org.glassfish.hk2.api.ActiveDescriptor#isReified()
      */
     @Override
     public boolean isReified() {
+        // always return true to get past the addActiveDescriptor checks
+        // even though the underlying descriptor may not be reified yet
         return true;
     }
 
@@ -87,15 +166,6 @@ public class AliasDescriptor<T> extends AbstractActiveDescriptor<T> {
     @Override
     public String getImplementation() {
         return descriptor.getImplementation();
-    }
-
-    /* (non-Javadoc)
-     * @see org.glassfish.hk2.api.ActiveDescriptor#getImplementationClass()
-     */
-    @Override
-    public Class<?> getImplementationClass() {
-        ensureInitialized();
-        return descriptor.getImplementationClass();
     }
 
     /* (non-Javadoc)
@@ -140,21 +210,24 @@ public class AliasDescriptor<T> extends AbstractActiveDescriptor<T> {
     }
 
     /* (non-Javadoc)
-     * @see org.glassfish.hk2.api.ActiveDescriptor#create(org.glassfish.hk2.api.ServiceHandle)
-     */
-    @Override
-    public T create(ServiceHandle<?> root) {
-        ensureInitialized();
-        return locator.getServiceHandle(descriptor).getService();
-    }
-
-    /* (non-Javadoc)
      * @see org.glassfish.hk2.api.ActiveDescriptor#dispose(java.lang.Object, org.glassfish.hk2.api.ServiceHandle)
      */
     @Override
     public void dispose(T instance) {
         ensureInitialized();
         descriptor.dispose(instance);
+    }
+
+
+    // ----- Utility methods ------------------------------------------------
+
+    /**
+     * Get the descriptor being aliased.
+     *
+     * @return the descriptor
+     */
+    protected ActiveDescriptor<T> getDescriptor() {
+        return descriptor;
     }
 
     /**
--- hk2/core/src/test/java/org/glassfish/hk2/core/utilities/AliasDescriptorTest.java
+++ hk2/core/src/test/java/org/glassfish/hk2/core/utilities/AliasDescriptorTest.java
@@ -6,14 +6,22 @@ import org.glassfish.hk2.api.DynamicConfigurationService;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.api.ServiceLocatorFactory;
 import org.glassfish.hk2.utilities.BuilderHelper;
+import org.glassfish.hk2.utilities.NamedImpl;
 import org.junit.Test;
 import org.jvnet.hk2.annotations.Service;
 
 import javax.inject.Singleton;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for the AliasDescriptor.
@@ -62,6 +70,102 @@ public class AliasDescriptorTest {
         assertNull(locator.getService(MyInterface3.class, ""));
         assertNull(locator.getService(MyInterface3.class, "foo"));
     }
+
+    @Test
+    public void testIsReified() throws Exception {
+        AliasDescriptor<MyService> aliasDescriptor = getAliasDescriptor();
+
+        assertTrue(aliasDescriptor.isReified());
+    }
+
+    @Test
+    public void testGetImplementation() throws Exception {
+        AliasDescriptor<MyService> aliasDescriptor = getAliasDescriptor();
+
+        assertEquals(MyService.class.getName(), aliasDescriptor.getImplementation());
+    }
+
+    @Test
+    public void testGetContractTypes() throws Exception {
+        AliasDescriptor<MyService> aliasDescriptor = getAliasDescriptor();
+
+        final Set<Type> contractTypes = aliasDescriptor.getContractTypes();
+
+        assertEquals(1, contractTypes.size());
+        assertEquals(MyInterface2.class, contractTypes.iterator().next());
+    }
+
+    @Test
+    public void testGetScopeAnnotation() throws Exception {
+        AliasDescriptor<MyService> aliasDescriptor = getAliasDescriptor();
+
+        assertEquals(Singleton.class, aliasDescriptor.getScopeAnnotation());
+    }
+
+    @Test
+    public void testGetQualifierAnnotations() throws Exception {
+        AliasDescriptor<MyService> aliasDescriptor = getAliasDescriptor();
+
+        final Set<Annotation> qualifierAnnotations = aliasDescriptor.getQualifierAnnotations();
+
+        assertEquals(1, qualifierAnnotations.size());
+        Annotation annotation = qualifierAnnotations.iterator().next();
+        assertTrue(annotation instanceof NamedImpl);
+        assertEquals("foo", ((NamedImpl)annotation).value());
+    }
+
+    @Test
+    public void testGetImplementationClass() throws Exception {
+        AliasDescriptor<MyService> aliasDescriptor = getAliasDescriptor();
+
+        ActiveDescriptor<MyService> descriptor = aliasDescriptor.getDescriptor();
+        assertFalse(descriptor.isReified());
+
+        assertEquals(MyService.class, aliasDescriptor.getImplementationClass());
+
+        assertTrue(descriptor.isReified());
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        ServiceLocator locator = ServiceLocatorFactory.getInstance().create("testAddDescriptor");
+        DynamicConfigurationService dcs = locator.getService(DynamicConfigurationService.class);
+        DynamicConfiguration config = dcs.createDynamicConfiguration();
+
+        ActiveDescriptor<MyService> descriptor =
+                (ActiveDescriptor<MyService>)config.bind(BuilderHelper.link(MyService.class).
+                        to(MyInterface1.class).in(Singleton.class.getName()).build());
+
+        AliasDescriptor<MyService> aliasDescriptor =
+                new AliasDescriptor<MyService>(locator, descriptor, MyInterface2.class, "foo");
+
+        config = dcs.createDynamicConfiguration();
+        config.addActiveDescriptor(aliasDescriptor);
+        config.commit();
+
+        MyService s1 = locator.getService(descriptor, null);
+        MyService s2 = aliasDescriptor.create(null);
+        assertSame(s1, s2);
+    }
+
+    // ----- Utility methods ------------------------------------------------
+
+    private AliasDescriptor<MyService> getAliasDescriptor() {
+        ServiceLocator locator = ServiceLocatorFactory.getInstance().create("testAddDescriptor");
+        DynamicConfigurationService dcs = locator.getService(DynamicConfigurationService.class);
+        DynamicConfiguration config = dcs.createDynamicConfiguration();
+
+        ActiveDescriptor<MyService> descriptor =
+                (ActiveDescriptor<MyService>)config.bind(BuilderHelper.link(MyService.class).
+                        to(MyInterface1.class).in(Singleton.class.getName()).build());
+
+        config.commit();
+
+        return new AliasDescriptor<MyService>(locator, descriptor, MyInterface2.class, "foo");
+    }
+
+
+    // ----- inner classes --------------------------------------------------
 
     public interface MyInterface1 {
         public void doSomething();
