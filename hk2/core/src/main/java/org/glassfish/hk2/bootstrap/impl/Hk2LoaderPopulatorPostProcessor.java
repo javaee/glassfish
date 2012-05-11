15d0b69afbc77e82da424d3dbc7f3d7a05278ad8
jwells <jwells@java.net>
2012-05-11 12:07:26 +0000

    Another couple of minor fixes based on small interface change

--- hk2/core/src/main/java/org/glassfish/hk2/bootstrap/impl/Hk2LoaderPopulatorPostProcessor.java
+++ hk2/core/src/main/java/org/glassfish/hk2/bootstrap/impl/Hk2LoaderPopulatorPostProcessor.java
@@ -1,6 +1,44 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2007-2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
 package org.glassfish.hk2.bootstrap.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.glassfish.hk2.api.HK2Loader;
@@ -17,12 +55,22 @@ import org.glassfish.hk2.utilities.DescriptorImpl;
  */
 public class Hk2LoaderPopulatorPostProcessor implements PopulatorPostProcessor {
 
-	final ClassLoader classLoader;
+	private final ClassLoader classLoader;
 	
+	/**
+	 * Creates the post-processor to use the given classloader
+	 * 
+	 * @param classLoader The classloader to use, may not be null
+	 */
+	public Hk2LoaderPopulatorPostProcessor(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+	
+	/**
+	 * Uses the given classloader to load the class from the descriptor
+	 */
 	@Override
 	public List<DescriptorImpl> process(DescriptorImpl descriptorImpl) {
-		List<DescriptorImpl> returnList = new ArrayList<DescriptorImpl> ();
-		
 		descriptorImpl.setLoader(new HK2Loader() {
 			
 			@Override
@@ -35,14 +83,10 @@ public class Hk2LoaderPopulatorPostProcessor implements PopulatorPostProcessor {
 		    }
 		});
 		
-		returnList.add(descriptorImpl);
-		return returnList;
+		return null;
 	}
 
-	public Hk2LoaderPopulatorPostProcessor(ClassLoader classLoader) {
-		super();
-		this.classLoader = classLoader;
-	}
+	
 
 	
 }
