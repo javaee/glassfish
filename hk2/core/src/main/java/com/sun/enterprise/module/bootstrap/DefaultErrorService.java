d206589c654b36bab952603ca84d30169b45e788
mtaube <mtaube@java.net>
2012-05-14 20:43:11 +0000

    HK2-54 quiet when there is no Injectee

--- hk2/core/src/main/java/com/sun/enterprise/module/bootstrap/DefaultErrorService.java
+++ hk2/core/src/main/java/com/sun/enterprise/module/bootstrap/DefaultErrorService.java
@@ -2,19 +2,19 @@ package com.sun.enterprise.module.bootstrap;
 
 import javax.inject.Singleton;
 
-import org.glassfish.hk2.api.ActiveDescriptor;
 import org.glassfish.hk2.api.ErrorInformation;
 import org.glassfish.hk2.api.ErrorService;
-import org.glassfish.hk2.api.Injectee;
 import org.glassfish.hk2.api.MultiException;
 
 @Singleton
 public class DefaultErrorService implements ErrorService {
 	@Override
 	public void onFailure(ErrorInformation ei) throws MultiException {
-		System.err.println(ei.getDescriptor());
-		System.err.println(ei.getInjectee());
-		ei.getAssociatedException().printStackTrace();
+		if (ei.getInjectee() != null) {
+			System.err.println(ei.getDescriptor());
+			System.err.println(ei.getInjectee());
+			ei.getAssociatedException().printStackTrace();
+		}
     }
 	
 }
