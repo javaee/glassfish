eb2f88190bbce951fff85ac115f1f4bd68d72d0d
tbeerbower <tbeerbower@java.net>
2012-06-22 14:58:59 +0000

    add test for generic factories - currently @Ignore

--- hk2/hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/factory/ParameterizedFactoryTest.java
+++ hk2/hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/factory/ParameterizedFactoryTest.java
@@ -1,26 +1,46 @@
 package org.glassfish.hk2.tests.locator.factory;
 
 import junit.framework.Assert;
+import org.glassfish.hk2.api.Descriptor;
 import org.glassfish.hk2.api.DynamicConfiguration;
 import org.glassfish.hk2.api.Factory;
+import org.glassfish.hk2.api.FactoryDescriptors;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.api.TypeLiteral;
 import org.glassfish.hk2.tests.locator.utilities.LocatorHelper;
 import org.glassfish.hk2.tests.locator.utilities.TestModule;
+import org.glassfish.hk2.utilities.AbstractActiveDescriptor;
+import org.glassfish.hk2.utilities.ActiveDescriptorBuilder;
 import org.glassfish.hk2.utilities.BuilderHelper;
+import org.glassfish.hk2.utilities.reflection.ParameterizedTypeImpl;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.inject.Singleton;
+import java.lang.reflect.Type;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Test for complex parameterized type factories.
  */
 public class ParameterizedFactoryTest {
 
+    private static final List<String>  STRING_LIST  = new LinkedList<String> ();
+    private static final List<Integer> INTEGER_LIST = new LinkedList<Integer> ();
+
     @Test
     public void testParameterizedFactory() {
-        ServiceLocator locator = LocatorHelper.create("testParameterizedFactory", new Module());
+        ServiceLocator locator = LocatorHelper.create("testParameterizedFactory", new TestModule(){
+            @Override
+            public void configure(DynamicConfiguration config) {
+                config.bind(BuilderHelper.link(FooFactory.class).to(Foo.class).buildFactory());
+                config.bind(BuilderHelper.link(BarFactory.class).to(Bar.class).buildFactory());
+                config.bind(BuilderHelper.link(Injectee.class).in(Singleton.class).build());
+            }
+        });
 
         FooFactory fooFactory
                 = locator.getService((new TypeLiteral<Factory<Foo>>() {}).getType());
@@ -52,18 +72,61 @@ public class ParameterizedFactoryTest {
         Assert.assertTrue(injectee.getInjectedBar() instanceof BarImpl);
     }
 
-    public static class Module implements TestModule {
+    @Test @Ignore
+    public void testGenericFactories() {
+        ServiceLocator locator = LocatorHelper.create("testGenericFactories", new TestModule(){
 
-        /* (non-Javadoc)
-        * @see org.glassfish.hk2.api.Module#configure(org.glassfish.hk2.api.Configuration)
-        */
-        @Override
-        public void configure(DynamicConfiguration configurator) {
-            configurator.bind(BuilderHelper.link(FooFactory.class).to(Foo.class).buildFactory());
-            configurator.bind(BuilderHelper.link(BarFactory.class).to(Bar.class).buildFactory());
-            configurator.bind(BuilderHelper.link(Injectee.class).in(Singleton.class).build());
-        }
+            @Override
+            public void configure(DynamicConfiguration config) {
+                config.bind(createConstantFactoryDescriptor(getFactory(STRING_LIST), (new TypeLiteral<List<String>>() {}).getType()));
+                config.bind(createConstantFactoryDescriptor(getFactory(INTEGER_LIST), (new TypeLiteral<List<Integer>>() {}).getType()));
+                config.bind(BuilderHelper.link(Injectee2.class).in(Singleton.class).build());
+            }
+        });
+
+        Factory<List<String>> stringListFactory
+                = locator.getService((new TypeLiteral<Factory<List<String>>>() {}).getType());
+
+        Assert.assertNotNull(stringListFactory);
+
+        List<String> stringList = stringListFactory.provide();
+
+        Assert.assertSame(STRING_LIST, stringList);
+
+        // -----
+
+        Factory<List<Integer>> integerListFactory
+                = locator.getService((new TypeLiteral<Factory<List<Integer>>>() {}).getType());
+
+        Assert.assertNotNull(integerListFactory);
+
+        List<Integer> integerList = integerListFactory.provide();
+
+        Assert.assertSame(INTEGER_LIST, integerList);
+
+        // -----
+
+        Injectee2 injectee = locator.getService(Injectee2.class);
+        Assert.assertNotNull(injectee);
+
+        stringList = injectee.getInjectedStringList();
+
+        Assert.assertSame(STRING_LIST, stringList);
+
+        stringList = injectee.getStringListFromProvider();
+
+        Assert.assertSame(STRING_LIST, stringList);
+
+        integerList = injectee.getInjectedIntegerList();
+
+        Assert.assertSame(INTEGER_LIST, integerList);  // fails here ...  integerList == STRING_LIST
+
+        integerList = injectee.getIntegerListFromProvider();
+
+        Assert.assertSame(INTEGER_LIST, integerList);
     }
+
+    // ----- inner test classes ---------------------------------------------
 
     public static interface Foo {
     }
@@ -71,13 +134,11 @@ public class ParameterizedFactoryTest {
     public static class FooImpl implements Foo {
     }
 
-
     public static interface Bar {
     }
 
     public static class BarImpl implements Bar {
     }
-
 
     public static abstract class AbstractFactory<T> extends EmptyClass1 implements Factory<T> {
         @Override
@@ -105,6 +166,24 @@ public class ParameterizedFactoryTest {
         @Override
         public Bar get() {
             return new BarImpl();
+        }
+    }
+
+    public static class BasicFactory<T> implements Factory<T> {
+        private T value;
+
+        protected BasicFactory(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public T provide() {
+            return value;
+        }
+
+        @Override
+        public void dispose(T instance) {
+            //do nothing
         }
     }
 
@@ -138,5 +217,82 @@ public class ParameterizedFactoryTest {
         public Bar getInjectedBar() {
             return injectedBar;
         }
+    }
+
+    @Singleton
+    public static class Injectee2 {
+        @Inject
+        List<String> stringList;
+
+        @Inject
+        Provider<List<String>> stringListProvider;
+
+        @Inject
+        List<Integer> integerList;
+
+        @Inject
+        Provider<List<Integer>> integerListProvider;
+
+        public List<String> getInjectedStringList() {
+            return stringList;
+        }
+
+        public List<String> getStringListFromProvider() {
+            return stringListProvider.get();
+        }
+
+        public List<Integer> getInjectedIntegerList() {
+            return integerList;
+        }
+
+        public List<Integer> getIntegerListFromProvider() {
+            return integerListProvider.get();
+        }
+    }
+
+    // ----- static helper methods ------------------------------------------
+
+    public static <T> Factory<T> getFactory(T initialValue) {
+        return new BasicFactory<T>(initialValue);
+    }
+
+    public static <T> FactoryDescriptors createConstantFactoryDescriptor(
+            Factory<T> factory,
+            Type... types) {
+
+        final AbstractActiveDescriptor serviceDescriptor = BuilderHelper.createConstantDescriptor(factory);
+        final ActiveDescriptorBuilder  factoryBuilder    = BuilderHelper.activeLink(factory.getClass());
+
+        serviceDescriptor.addContractType(factory.getClass());
+
+        for (Type type : types) {
+            factoryBuilder.to(type);
+            serviceDescriptor.addContractType(new ParameterizedTypeImpl(Factory.class, type));
+        }
+
+        final Descriptor factoryDescriptor = factoryBuilder.buildFactory();
+
+        FactoryDescriptors f = new FactoryDescriptors(){
+            @Override
+            public Descriptor getFactoryAsAService() {
+                return serviceDescriptor;
+            }
+
+            @Override
+            public Descriptor getFactoryAsAFactory() {
+                return factoryDescriptor;
+            }
+
+            public String toString() {
+                return "FactoryDescriptorsImpl(\n" +
+                        serviceDescriptor + ",\n" +
+                        factoryDescriptor + ",\n\t" +
+                        System.identityHashCode(this) + ")";
+            }
+        };
+
+        System.out.println("\n\n\n#!## ParameterizedFactoryTest.createConstantFactoryDescriptor : f=" +f);
+
+        return f;
     }
 }
