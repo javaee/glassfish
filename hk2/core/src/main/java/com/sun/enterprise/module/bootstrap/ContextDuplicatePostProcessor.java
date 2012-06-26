c9633d3c668c45a4910f00c4b8fe7441997caf93
jwells <jwells@java.net>
2012-06-26 21:59:43 +0000

    Make sure that if there are multiple jars on the classpath that the duplicate contexts do not make it in

---
+++ hk2/core/src/main/java/com/sun/enterprise/module/bootstrap/ContextDuplicatePostProcessor.java
@@ -0,0 +1,84 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.enterprise.module.bootstrap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.glassfish.hk2.api.Context;
+import org.glassfish.hk2.bootstrap.PopulatorPostProcessor;
+import org.glassfish.hk2.utilities.DescriptorImpl;
+
+import javax.inject.Singleton;
+
+/**
+ * This class will weed out and duplicate implementations of Context, which can happen
+ * if the same jar is on the classpath twice.  This service must be put into the
+ * ServiceLocator prior to populating from the inhabitant files, and hence is not
+ * annotated with &#64;Service
+ * 
+ * @author jwells
+ *
+ */
+@Singleton
+public class ContextDuplicatePostProcessor implements PopulatorPostProcessor {
+    private final HashSet<String> contextImpl = new HashSet<String>();
+    
+
+    /* (non-Javadoc)
+     * @see org.glassfish.hk2.bootstrap.PopulatorPostProcessor#process(org.glassfish.hk2.utilities.DescriptorImpl)
+     */
+    @Override
+    public DescriptorImpl process(DescriptorImpl descriptorImpl) {
+        Set<String> contracts = descriptorImpl.getAdvertisedContracts();
+        if (!contracts.contains(Context.class.getName())) {
+            return descriptorImpl;
+        }
+        
+        String implementation = descriptorImpl.getImplementation();
+        if (contextImpl.contains(implementation)) {
+            return null;
+        }
+        contextImpl.add(implementation);
+        
+        return descriptorImpl;
+    }
+
+}
