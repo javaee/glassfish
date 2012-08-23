7913e784233051aa077cf9427454e23306fdcfd4
Tom Mueller <tmueller@java.net>
2012-08-23 14:37:19 +0000

    Turn on debugging for cluster devtest
    svn path=/trunk/; revision=55603
    
    
    Former-commit-id: 83b1d7514acfec5713d2788a82764cde97b86828

--- v2/appserv-tests/devtests/cluster/test-bigcluster.sh
+++ v2/appserv-tests/devtests/cluster/test-bigcluster.sh
@@ -68,7 +68,9 @@ benchmark_commands() {
   bench list-applications
   bench stop-domain $DOMAIN
   bench start-domain $DOMAIN
+  export AS_DEBUG=true
   bench restart-domain $DOMAIN
+  export AS_DEBUG=false
   grep 'time to parse domain.xml' $GFHOME/glassfish/domains/$DOMAIN/logs/server.log | 
     sed -e 's/^.*Total //' -e 's/|#]//' | tail -1
   echo 'size of domain.xml: ' `ls -l $GFHOME/glassfish/domains/$DOMAIN/config/domain.xml | awk '{ print $5 }'`
