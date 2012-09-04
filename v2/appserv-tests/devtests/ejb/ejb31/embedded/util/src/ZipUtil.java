a09b72267ecfee5df36dce8c15d661303808e2ad
Marina Vatkina <marina.vatkina@oracle.com>
2012-09-04 21:47:11 +0000

    Adding ZipUtil to zip temp dir if needed
    svn path=/trunk/; revision=55790
    
    
    Former-commit-id: f89e0b1967e017b78d1ab953d9d7944da2e8e187

---
+++ v2/appserv-tests/devtests/ejb/ejb31/embedded/util/src/ZipUtil.java
@@ -0,0 +1,91 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2012 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package embedded.util;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
+
+public class ZipUtil {
+
+    public static void zipInstanceDirectory() {
+        zipInstanceDirectory("tmp");
+    }
+    public static void zipInstanceDirectory(String name) {
+        try {
+            ZipOutputStream zos = new ZipOutputStream( new FileOutputStream( new File(name + "-instance-dir.zip") ) );
+            File directory = new File(System.getProperty("com.sun.aas.instanceRoot"));
+            zip( directory, directory.getParentFile(), zos );
+            zos.close();
+        } catch (IOException e) {
+            System.out.println("ERROR preserving the directory:");
+            e.printStackTrace();
+        }
+    }
+   
+    private static final void zip(File directory, File base, ZipOutputStream zos) throws IOException {
+        File[] files = directory.listFiles();
+        byte[] buffer = new byte[8192];
+        int read = 0;
+        for (int i = 0, n = files.length; i < n; i++) {
+            if (files[i].getCanonicalPath().indexOf("osgi") != -1 ||
+                    files[i].getCanonicalPath().indexOf("ejbtimer") != -1 ) {
+                continue;
+            } else {
+                ZipEntry entry = new ZipEntry(files[i].getPath().substring(
+                      base.getPath().length() + 1));
+                zos.putNextEntry(entry);
+                if (files[i].isDirectory()) {
+                    zip(files[i], base, zos);
+                } else {
+                    FileInputStream in = new FileInputStream(files[i]);
+                    while (-1 != (read = in.read(buffer))) {
+                        zos.write(buffer, 0, read);
+                    }
+                    in.close();
+                }
+            }
+        }
+    }
+}
