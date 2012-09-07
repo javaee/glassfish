a9f823c2e6169f68ae49bfdbfe7470ffafb35305
David Zhao <liang.x.zhao@oracle.com>
2012-09-07 04:28:20 +0000

    Add unit test cases for JMS Injection.
    svn path=/trunk/; revision=55829
    
    
    Former-commit-id: ab1b3fd8c48d35a44796978628203afc19779b25

---
+++ v2/appserv-tests/devtests/jms/resultCount.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+FILE=$APS_HOME/test_resultsValid.xml
+echo "input file=$FILE"
+
+TOTAL=6
+TOTAL_LITE=6
+
+if [ $# -eq 1 ] && [ $1 = "lite" ]
+then TOTAL=$TOTAL_LITE
+echo "JMS Lite Test"
+fi
+
+PASSED=`grep "status value" $FILE | grep "pass" | wc -l`
+FAILED=`grep "status value" $FILE | grep "fail" | wc -l`
+TOTAL_RUN=`expr $PASSED + $FAILED `
+DNR=`expr $TOTAL - $TOTAL_RUN `
+
+echo ""
+echo "************************"
+echo "PASSED=   $PASSED"
+echo "------------  ========="
+echo "FAILED=   $FAILED"
+echo "------------  ========="
+echo "DID NOT RUN=   $DNR"
+echo "------------  ========="
+echo "Total Expected=$TOTAL"
+echo "************************"
+echo ""
+
+echo "************************">>$APS_HOME/devtests/jms/count.txt;
+date>>$APS_HOME/devtests/jms/count.txt;
+echo "-----------------------">>$APS_HOME/devtests/jms/count.txt;
+echo "PASSED=   $PASSED">>$APS_HOME/devtests/jms/count.txt;
+echo "------------  =========">>$APS_HOME/devtests/jms/count.txt;
+echo "FAILED=   $FAILED">>$APS_HOME/devtests/jms/count.txt;
+echo "------------  =========">>$APS_HOME/devtests/jms/count.txt;
+echo "DID NOT RUN=   $DNR">>$APS_HOME/devtests/jms/count.txt;
+echo "------------  =========">>$APS_HOME/devtests/jms/count.txt;
+echo "Total Expected=$TOTAL">>$APS_HOME/devtests/jms/count.txt;
+echo "************************">>$APS_HOME/devtests/jms/count.txt;
