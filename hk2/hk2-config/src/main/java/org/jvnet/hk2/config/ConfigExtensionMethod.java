fd86e8bd3272d5b9709e00a71bb231f14a6962f8
mk111283 <mk111283@java.net>
2012-09-26 23:01:48 +0000

    Add Retention & Target to the annotation

--- hk2/hk2-config/src/main/java/org/jvnet/hk2/config/ConfigExtensionMethod.java
+++ hk2/hk2-config/src/main/java/org/jvnet/hk2/config/ConfigExtensionMethod.java
@@ -3,11 +3,11 @@ package org.jvnet.hk2.config;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
-@Target(TYPE)
+@Target(METHOD)
 public @interface ConfigExtensionMethod {
     public String value() default "basic-config-extension-handler";
 }
