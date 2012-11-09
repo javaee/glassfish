2832cd48e31dd4e83015b128986271132c9bb5ac
Marina Vatkina <marina.vatkina@oracle.com>
2012-11-09 00:55:27 +0000

    Add test for Local/Remote interface calculation. SingletonBean1 is wrong and needs to be fixed when ejb-container code is fixed to catch it.
    svn path=/trunk/; revision=56936
    
    
    Former-commit-id: 6151fed3b062a0974b23d435928e6de359df8f5d

---
+++ v2/appserv-tests/devtests/ejb/ejb32/intfces/ejb/St1.java
@@ -0,0 +1,6 @@
+package ejb32.intrfaces;
+
+public interface St1 {
+
+    public String st1() throws Exception;
+}
---
+++ v2/appserv-tests/devtests/ejb/ejb32/intfces/ejb/St6.java
@@ -0,0 +1,7 @@
+package ejb32.intrfaces;
+
+@javax.ejb.Local
+public interface St6 {
+
+    public String st6() throws Exception;
+}
