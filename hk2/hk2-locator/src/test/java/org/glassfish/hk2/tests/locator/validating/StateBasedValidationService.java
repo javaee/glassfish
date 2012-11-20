f2ddd7f5ae31c466752d0c8c452426c7be29060c
jwells <jwells@java.net>
2012-11-20 13:37:07 +0000

    Add failing test to be fixed

---
+++ hk2/hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/validating/StateBasedValidationService.java
@@ -0,0 +1,105 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.hk2.tests.locator.validating;
+
+import javax.inject.Singleton;
+
+import org.glassfish.hk2.api.ActiveDescriptor;
+import org.glassfish.hk2.api.Descriptor;
+import org.glassfish.hk2.api.Filter;
+import org.glassfish.hk2.api.ValidationInformation;
+import org.glassfish.hk2.api.ValidationService;
+import org.glassfish.hk2.api.Validator;
+
+/**
+ * This service bases its decision on the current state of the system
+ * 
+ * @author jwells
+ *
+ */
+@Singleton
+public class StateBasedValidationService implements ValidationService {
+    private int currentState = 0;
+    
+    public void setCurrentState(int state) {
+        currentState = state;
+    }
+
+    /* (non-Javadoc)
+     * @see org.glassfish.hk2.api.ValidationService#getLookupFilter()
+     */
+    @Override
+    public Filter getLookupFilter() {
+        return new Filter() {
+
+            @Override
+            public boolean matches(Descriptor d) {
+                return (d.getAdvertisedContracts().contains(DynamicService.class.getName()));
+            }
+            
+        };
+    }
+
+    /* (non-Javadoc)
+     * @see org.glassfish.hk2.api.ValidationService#getValidator()
+     */
+    @Override
+    public Validator getValidator() {
+        return new Validator() {
+
+            @Override
+            public boolean validate(ValidationInformation info) {
+                ActiveDescriptor<?> candidate = info.getCandidate();
+                String impl = candidate.getImplementation();
+                System.out.println("JRW(10) checking on " + impl + " in state " + currentState);
+                
+                switch (currentState) {
+                case 1:
+                    return impl.contains("1");
+                case 2:
+                    return impl.contains("2");
+                default:
+                    return false;
+                }
+            }
+        };
+    }
+
+}
