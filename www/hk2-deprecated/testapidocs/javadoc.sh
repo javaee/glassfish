799b9730804a022a0d98142ad02eda060818e509
jwells <jwells@java.net>
2013-01-22 16:41:29 +0000

    Update web pages

--- www/hk2-deprecated/testapidocs/javadoc.sh
+++
@@ -1,3 +0,0 @@
-/scratch/jwells/wls/dev/src1212/build/x86_64/jrockit-jdk1.6.0_29/jre/../bin/javadoc -J-Dhttp.proxySet=true -J-Dhttp.proxyHost=www-proxy.us.oracle.com -J-Dhttp.proxyPort=80 "-J-Dhttp.nonProxyHosts=\"adc2170275|*.oracle.com|*.oracleads.com|*.us.oracle.com|
-                       *.uk.oracle.com|*.ca.oracle.com|*.oraclecorp.com|
-                       *.oracleportal.com\"" @options @packages
