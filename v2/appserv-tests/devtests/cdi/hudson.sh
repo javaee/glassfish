23fcdd61754c0b0f84f574a3806f65b28ce882e5
Mason Taube <mason.taube@oracle.com>
2013-02-06 18:16:58 +0000

    adding a hudson script
    svn path=/trunk/; revision=59189
    
    
    Former-commit-id: 4ca9cbecd0e3d65b2bcf8426ab23b027246567c8

---
+++ v2/appserv-tests/devtests/cdi/hudson.sh
@@ -0,0 +1,63 @@
+date
+
+java -version
+ant -version
+
+rm -rf glassfish-v3-image
+mkdir glassfish-v3-image
+pushd glassfish-v3-image
+
+export http_proxy=http://www-proxy.us.oracle.com:80
+
+# download the latest GF 
+#wget http://rator.sfbay/maven/repositories/glassfish//org/glassfish/distributions/glassfish/3.0-SNAPSHOT/glassfish-3.0-SNAPSHOT.zip
+#wget http://javaweb.sfbay/java/re/glassfish/v3/promoted/trunk-latest/archive/bundles/glassfish-ri.zip
+#wget -O web.zip http://hudson.sfbay/job/glassfish-v3-devbuild/lastSuccessfulBuild/artifact/v3/distributions/web/target/web.zip
+#wget http://hudson.sfbay/job/gfv3-build-test/lastSuccessfulBuild/artifact/v3/distributions-ips/glassfish/target/glassfish.zip
+
+wget --no-proxy http://gf-hudson.us.oracle.com/hudson/job/gf-trunk-build-continuous/lastSuccessfulBuild/artifact/bundles/glassfish.zip
+
+#wget http://hudson.glassfish.org/job/gf-trunk-build-continuous/lastSuccessfulBuild/artifact/bundles/glassfish.zip
+
+date
+
+unzip -q glassfish.zip
+
+date
+
+export S1AS_HOME=$PWD/glassfish3/glassfish
+popd
+export APS_HOME=$PWD/appserv-tests
+export AS_LOGFILE=$S1AS_HOME/cli.log 
+
+rm -rf $S1AS_HOME/domains/domain1
+
+cd $APS_HOME
+
+(jps |grep Main |cut -f1 -d" " | xargs kill -9  > /dev/null 2>&1) || true
+
+pushd $APS_HOME/devtests/cdi
+rm count.txt || true
+
+rm -rf bin .classpath  .project; ant clean; rm RepRunConf.txt; rm *.output
+rm alltests.res
+rm $APS_HOME/test_results*
+#clean all RepRunConf.txt
+find . -name "RepRunConf.txt" | xargs rm
+
+# start GlassFish
+$S1AS_HOME/bin/asadmin start-domain domain1
+# start Derby
+$S1AS_HOME/bin/asadmin start-database
+
+ant all
+
+- start GlassFish
+$S1AS_HOME/bin/asadmin stop-domain domain1
+- start Derby
+$S1AS_HOME/bin/asadmin stop-database
+
+egrep 'FAILED= *0' count.txt
+egrep 'DID NOT RUN= *0' count.txt
+
+
