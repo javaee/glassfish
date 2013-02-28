53477bcee181bbe8a1692bd9075e16c58e9286ce
Mahesh Kannan <mahesh.kannan@oracle.com>
2013-02-28 06:43:54 +0000

    Add a simple test case
    svn path=/trunk/; revision=59900
    
    
    Former-commit-id: 9b5a9e4c1be575b965d5b76599830974a4cdced4

---
+++ v2/appserv-tests/devtests/batch/simple-job/ejb/com/oracle/javaee7/samples/batch/simple/IdGenerator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.oracle.javaee7.samples.batch.simple;
+
+/**
+ *
+ * @author makannan
+ */
+public interface IdGenerator {
+
+    public String nextId();
+    
+}
