b095fe4a065059832da3e678ef32a672e72c1001
Oleksiy Stashok <oleksiy.stashok@oracle.com>
2013-03-09 00:41:51 +0000

    fix maxKeepAliveRequests test
    svn path=/trunk/; revision=60243
    
    
    Former-commit-id: 6c1c97c37051ac11299893d5384a9d463b75fc9a

--- v2/appserv-tests/devtests/web/maxKeepAliveRequests/WebTest.java
+++ v2/appserv-tests/devtests/web/maxKeepAliveRequests/WebTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -122,8 +122,11 @@ public class WebTest {
             os.write("\n".getBytes());
             os.flush();
 
+	    found = true;
+
             try{
                 while ((line = bis.readLine()) != null) {
+	            found = false;
                     System.out.println("line: " + line);
                 }
             }catch (SocketTimeoutException t){
