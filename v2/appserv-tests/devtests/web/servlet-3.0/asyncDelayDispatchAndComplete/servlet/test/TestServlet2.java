beb49c7c9ca495c413800652fbebf3125320fb2c
Shing Wai Chan <shing.wai.chan@oracle.com>
2013-04-13 03:24:13 +0000

    add test for GlassFish-20239
    svn path=/trunk/; revision=61412
    
    
    Former-commit-id: fb89865410326370ae0466838d93050e50e65c37

---
+++ v2/appserv-tests/devtests/web/servlet-3.0/asyncDelayDispatchAndComplete/servlet/test/TestServlet2.java
@@ -0,0 +1,102 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package test;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+@WebServlet(urlPatterns={"/test2"}, asyncSupported=true)
+public class TestServlet2 extends HttpServlet implements AsyncListener {
+    private static final String NAME = "text";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+
+        addInfo(req, "S2i");
+        final AsyncContext asyncContext = req.startAsync();
+        asyncContext.addListener(this);
+
+        new Thread() {
+            @Override
+            public void run() {
+                    asyncContext.complete();
+            }
+        }.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            //
+        }
+        addInfo(req, "S2o");
+    }
+
+    public void onComplete(AsyncEvent event) throws IOException {
+        AsyncContext asyncContext = event.getAsyncContext();
+        String text = (String)asyncContext.getRequest().getAttribute(NAME);
+        asyncContext.getResponse().getWriter().println(text + "AC");
+    }
+
+    public void onTimeout(AsyncEvent event) throws IOException {
+        // do nothing
+    }
+
+    public void onError(AsyncEvent event) throws IOException {
+        // do nothing
+    }
+
+    public void onStartAsync(AsyncEvent event) throws IOException {
+        // do nothing
+    }
+
+    static void addInfo(ServletRequest req, String text) {
+        String attr = (String)req.getAttribute(NAME);
+        if (attr == null) {
+            attr = "";
+        }
+        System.out.println("XXX --> " + attr + text);
+        req.setAttribute(NAME, attr + text);
+    }
+}
