eef8f629befca5bdd7357dfa530d2ba9f50b5988
Shing Wai Chan <shing.wai.chan@oracle.com>
2013-04-18 23:54:54 +0000

    remove unused import statement
    svn path=/trunk/; revision=61547
    
    
    Former-commit-id: 272798ae13dd54ff4ce4660d326bdcdbdc880115

--- v2/appserv-tests/devtests/web/servlet-3.1/upgradeEcho/servlet/test/TestServlet.java
+++ v2/appserv-tests/devtests/web/servlet-3.1/upgradeEcho/servlet/test/TestServlet.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -41,7 +41,6 @@
 package test;
 
 import java.io.IOException;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
