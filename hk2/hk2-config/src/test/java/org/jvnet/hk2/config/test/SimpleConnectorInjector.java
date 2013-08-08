4a2d8e75daaaf4328b82e79300f4d3cddb4797ce
Andriy Zhdanov <azhdanov@gmail.com>
2013-08-08 17:13:32 +0200

    finally testDisposedNested working

--- hk2/hk2-config/src/test/java/org/jvnet/hk2/config/test/SimpleConnectorInjector.java
+++ hk2/hk2-config/src/test/java/org/jvnet/hk2/config/test/SimpleConnectorInjector.java
@@ -5,7 +5,7 @@ import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.config.InjectionTarget;
 import org.jvnet.hk2.config.NoopConfigInjector;
 
-@Service(name = "simple-connector", metadata = "target=org.jvnet.hk2.config.test.SimpleConnector,@port=optional,@port=default:8080,@port=datatype:java.lang.String,@port=leaf,<ejb-container-availability>=org.jvnet.hk2.config.test.EjbContainerAvailability,<web-container-availability>=org.jvnet.hk2.config.test.WebContainerAvailability,<generic-container>=org.jvnet.hk2.config.test.GenericContainer,<*>=collection:org.jvnet.hk2.config.test.GenericContainer")
+@Service(name = "simple-connector", metadata = "target=org.jvnet.hk2.config.test.SimpleConnector,@port=optional,@port=default:8080,@port=datatype:java.lang.String,@port=leaf,<ejb-container-availability>=org.jvnet.hk2.config.test.EjbContainerAvailability,<web-container-availability>=org.jvnet.hk2.config.test.WebContainerAvailability,<*>=collection:org.jvnet.hk2.config.test.GenericContainer")
 @InjectionTarget(SimpleConnector.class)
 public class SimpleConnectorInjector
     extends NoopConfigInjector
