ec2cf796bf9365c37f176c9b4b6163dab5959216
Andriy Zhdanov <azhdanov@gmail.com>
2013-08-15 12:48:05 +0200

    fix confusion with element and '*' extension

--- hk2/hk2-config/src/test/java/org/jvnet/hk2/config/test/GenericConfigInjector.java
+++ hk2/hk2-config/src/test/java/org/jvnet/hk2/config/test/GenericConfigInjector.java
@@ -5,7 +5,7 @@ import org.jvnet.hk2.annotations.Service;
 import org.jvnet.hk2.config.InjectionTarget;
 import org.jvnet.hk2.config.NoopConfigInjector;
 
-@Service(name = "generic-config", metadata = "target=org.jvnet.hk2.config.test.GenericConfig,@name=optional,@name=datatype:java.lang.String,@name=leaf,key=@name,keyed-as=org.jvnet.hk2.config.test.GenericConfig,<generic-config>=org.jvnet.hk2.config.test.GenericConfig,<*>=collection:org.jvnet.hk2.config.test.GenericContainer")
+@Service(name = "generic-config", metadata = "target=org.jvnet.hk2.config.test.GenericConfig,@name=optional,@name=datatype:java.lang.String,@name=leaf,key=@name,keyed-as=org.jvnet.hk2.config.test.GenericConfig,<generic-config>=org.jvnet.hk2.config.test.GenericConfig")
 @InjectionTarget(GenericConfig.class)
 public class GenericConfigInjector
     extends NoopConfigInjector
