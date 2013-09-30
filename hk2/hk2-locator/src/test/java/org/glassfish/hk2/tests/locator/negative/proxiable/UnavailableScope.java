c57fedd26e7aa327903508f5a14df2707f946db2
JOHN.WELLS <john.wells@oracle.com>
2013-09-30 06:50:49 -0700

    Fix the exception coming from the internal cache, it needed to be unwrapped better from the ExecutionException.  Add a test that ensures consistency when there is no active context for a proxied service

--- hk2/hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/negative/proxiable/UnavailableScope.java
+++ hk2/hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/negative/proxiable/UnavailableScope.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -39,33 +39,27 @@
  */
 package org.glassfish.hk2.tests.locator.negative.proxiable;
 
-import javax.inject.Singleton;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import org.glassfish.hk2.api.Context;
-import org.glassfish.hk2.api.DynamicConfiguration;
-import org.glassfish.hk2.tests.locator.utilities.TestModule;
-import org.glassfish.hk2.utilities.BuilderHelper;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Scope;
+
+import org.glassfish.hk2.api.Proxiable;
 
 /**
+ * This is a proxiable scope annotation, but there is NO corresponding
+ * context implementation
+ * 
  * @author jwells
  *
  */
-public class NegativeProxyModule implements TestModule {
-
-    /* (non-Javadoc)
-     * @see org.glassfish.hk2.tests.locator.utilities.TestModule#configure(org.glassfish.hk2.api.DynamicConfiguration)
-     */
-    @Override
-    public void configure(DynamicConfiguration config) {
-        config.bind(BuilderHelper.link(BadScopeContext.class.getName()).
-            to(Context.class.getName()).
-            in(Singleton.class.getName()).
-            build());
-        
-        config.bind(BuilderHelper.link(UnavailableScopeService.class.getName()).
-                in(UnavailableScope.class.getName()).
-                build());
-
-    }
+@Scope
+@Proxiable
+@Retention(RUNTIME)
+@Target( { TYPE })
+public @interface UnavailableScope {
 
 }
