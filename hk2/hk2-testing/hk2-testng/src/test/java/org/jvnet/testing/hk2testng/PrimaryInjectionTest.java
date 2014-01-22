8e249b98f42b30fb23dd7193c7a2cc2b79e3113c
JOHN.WELLS <john.wells@oracle.com>
2014-01-22 06:03:18 -0800

    HK2-179 Fix testng to give individual test classes their own ServiceLocator by default, but even if two test classes choose the same name ensure that they do not double-populate the ServiceLocator

--- hk2/hk2-testing/hk2-testng/src/test/java/org/jvnet/testing/hk2testng/PrimaryInjectionTest.java
+++ hk2/hk2-testing/hk2-testng/src/test/java/org/jvnet/testing/hk2testng/PrimaryInjectionTest.java
@@ -39,20 +39,32 @@
  */
 package org.jvnet.testing.hk2testng;
 
+import java.util.List;
+
+import org.glassfish.hk2.api.ActiveDescriptor;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.BuilderHelper;
 import org.jvnet.testing.hk2testng.service.PrimaryService;
+
 import javax.inject.Inject;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.testng.annotations.Test;
 
 /**
  *
  * @author saden
  */
-@HK2
+@HK2(PrimaryInjectionTest.SAME_LOCATOR_NAME)
 public class PrimaryInjectionTest {
+    public final static String SAME_LOCATOR_NAME = "Same";
 
     @Inject
     PrimaryService primaryService;
+    
+    @Inject
+    ServiceLocator serviceLocator;
 
     @Test
     public void assertPrimaryServiceInjecton() {
@@ -62,5 +74,22 @@ public class PrimaryInjectionTest {
     @Test
     public void assertSecondaryService() {
         assertThat(primaryService.getSecondaryService()).isNotNull();
+    }
+    
+    /**
+     * The exact same test is found in ConfigurationMethodInjectionTest.  The
+     * intent of the test is to ensure that there are not multiple
+     * descriptors for PrimaryService due to the fact that multiple
+     * test classes are using the same service locator.  If the
+     * service locator was being populated by both test classes then
+     * one of these two identical tests would see multiple instances
+     * of the primaryService in the IterableProvider
+     */
+    @Test
+    public void assertNoMultiplePrimaries() {
+        List<ActiveDescriptor<?>> allPrimaryServices = serviceLocator.getDescriptors(
+                BuilderHelper.createContractFilter(PrimaryService.class.getName()));
+        
+        assertThat(allPrimaryServices.size()).isEqualTo(1);
     }
 }
