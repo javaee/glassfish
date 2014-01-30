e6c024fa4a00caa6e8129f86c3d4d928d2b34c38
JOHN.WELLS <john.wells@oracle.com>
2014-01-30 08:41:04 -0800

    Some code coverage analysis showed some outages.  hk2-locator down to 88%, trying to get it higher than 90 again

--- hk2/hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/interception2/ConstructorInterceptorTest.java
+++ hk2/hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/interception2/ConstructorInterceptorTest.java
@@ -40,12 +40,21 @@
 package org.glassfish.hk2.tests.locator.interception2;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.LinkedList;
 import java.util.List;
 
+import javax.inject.Singleton;
+
+import org.aopalliance.intercept.ConstructorInterceptor;
+import org.aopalliance.intercept.ConstructorInvocation;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.glassfish.hk2.api.Filter;
+import org.glassfish.hk2.api.InterceptionService;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.tests.locator.utilities.LocatorHelper;
+import org.glassfish.hk2.utilities.BuilderHelper;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -62,7 +71,7 @@ public class ConstructorInterceptorTest {
                 RecordingInterceptorService.class,
                 RecordingInterceptor.class);
         
-        SimpleService service = locator.getService(SimpleService.class);
+        locator.getService(SimpleService.class);
         
         RecordingInterceptor recorder = locator.getService(RecordingInterceptor.class);
         
@@ -70,5 +79,95 @@ public class ConstructorInterceptorTest {
         Assert.assertEquals(1, cCalled.size());
         
     }
+    
+    /**
+     * Ensures that all of the interceptors in a chain get called
+     */
+    @Test
+    public void testMultipleConstructorInterceptors() {
+        ServiceLocator locator = LocatorHelper.getServiceLocator(SimpleService.class,
+                ThreeInterceptionService.class);
+        
+        ThreeInterceptionService tis = locator.getService(ThreeInterceptionService.class);
+        
+        locator.getService(SimpleService.class);
+        
+        List<ConstructorInterceptor> interceptors = tis.getConstructorInterceptors(null);
+        
+        InterceptorOne i1 = (InterceptorOne) interceptors.get(0);
+        InterceptorTwo i2 = (InterceptorTwo) interceptors.get(1);
+        InterceptorThree i3 = (InterceptorThree) interceptors.get(2);
+        
+        Assert.assertEquals(1, i1.wasCalled());
+        Assert.assertEquals(1, i2.wasCalled());
+        Assert.assertEquals(1, i3.wasCalled());
+    }
+    
+    private static class InterceptorOne implements ConstructorInterceptor {
+        private int called;
 
+        @Override
+        public Object construct(ConstructorInvocation invocation) throws Throwable {
+            called++;
+            return invocation.proceed();
+        }
+        
+        private int wasCalled() { return called; }
+        
+    }
+    
+    private static class InterceptorTwo implements ConstructorInterceptor {
+        private int called;
+
+        @Override
+        public Object construct(ConstructorInvocation invocation) throws Throwable {
+            called++;
+            return invocation.proceed();
+        }
+        
+        private int wasCalled() { return called; }
+        
+    }
+    
+    private static class InterceptorThree implements ConstructorInterceptor {
+        private int called;
+
+        @Override
+        public Object construct(ConstructorInvocation invocation) throws Throwable {
+            called++;
+            return invocation.proceed();
+        }
+        
+        private int wasCalled() { return called; }
+    }
+    
+    @Singleton
+    private static class ThreeInterceptionService implements InterceptionService {
+        private final LinkedList<ConstructorInterceptor> interceptors;
+        
+        private ThreeInterceptionService() {
+            interceptors = new LinkedList<ConstructorInterceptor>();
+            
+            interceptors.add(new InterceptorOne());
+            interceptors.add(new InterceptorTwo());
+            interceptors.add(new InterceptorThree());
+        }
+
+        @Override
+        public Filter getDescriptorFilter() {
+            return BuilderHelper.createContractFilter(SimpleService.class.getName());
+        }
+
+        @Override
+        public List<MethodInterceptor> getMethodInterceptors(Method method) {
+            return null;
+        }
+
+        @Override
+        public List<ConstructorInterceptor> getConstructorInterceptors(
+                Constructor<?> constructor) {
+            return interceptors;
+        }
+        
+    }
 }
