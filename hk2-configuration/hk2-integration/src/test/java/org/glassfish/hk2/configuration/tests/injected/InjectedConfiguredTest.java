66deefb2ba77ca94c4ec91df0cef08534585b55c
JOHN.WELLS <john.wells@oracle.com>
2014-05-19 07:14:10 -0700

    Start of new wave of test cases for configured service use cases

---
+++ hk2-configuration/hk2-integration/src/test/java/org/glassfish/hk2/configuration/tests/injected/InjectedConfiguredTest.java
@@ -0,0 +1,174 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.hk2.configuration.tests.injected;
+
+import java.util.HashMap;
+
+import javax.inject.Inject;
+
+import org.glassfish.hk2.api.MultiException;
+import org.glassfish.hk2.configuration.api.ConfigurationUtilities;
+import org.glassfish.hk2.configuration.hub.api.Hub;
+import org.glassfish.hk2.configuration.hub.api.WriteableBeanDatabase;
+import org.glassfish.hk2.configuration.hub.api.WriteableType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.jvnet.hk2.testing.junit.HK2Runner;
+
+/**
+ * @author jwells
+ *
+ */
+public class InjectedConfiguredTest extends HK2Runner {
+    /* package */ static final String CTEST_ONE_TYPE = "InjectedConfiguredTestType1";
+    
+    /* package */ static final String ALICE = "Alice";
+    /* package */ static final String BOB = "Bob";
+    
+    private final static String NAME_KEY = "name";
+    
+    @Inject
+    private Hub hub;
+    
+    @Before
+    public void before() {
+        super.before();
+        
+        ConfigurationUtilities.enableConfigurationSystem(testLocator);
+    }
+    
+    private void addNamedBean(String typeName, String name) {
+        WriteableBeanDatabase wbd = hub.getWriteableDatabaseCopy();
+        
+        WriteableType wt = wbd.findOrAddWriteableType(typeName);
+        
+        HashMap<String, Object> namedBean = new HashMap<String, Object>();
+        namedBean.put(NAME_KEY, name);
+        
+        wt.addInstance(name, namedBean);
+        
+        wbd.commit();
+    }
+    
+    private void removeType(String typeName) {
+        WriteableBeanDatabase wbd = hub.getWriteableDatabaseCopy();
+        
+        wbd.removeType(typeName);
+        
+        wbd.commit();
+    }
+    
+    private void removeNamedBean(String typeName, String name) {
+        WriteableBeanDatabase wbd = hub.getWriteableDatabaseCopy();
+        
+        WriteableType wt = wbd.findOrAddWriteableType(typeName);
+        
+        wt.removeInstance(name);
+        
+        wbd.commit();
+    }
+    
+    /**
+     * Tests that a service specifically injected with a named
+     * configured service works when the service is there and
+     * does not work when it is not there
+     */
+    @Test
+    public void testSpecificInjectionOfConfiguredService() {
+        try {
+            testLocator.getService(AliceInjectedService.class);
+            Assert.fail("There is no ConfiguredContract with name alice, should have failed");
+        }
+        catch (MultiException me) {
+            // expected
+        }
+        
+        try {
+            addNamedBean(CTEST_ONE_TYPE, BOB);
+            
+            try {
+                testLocator.getService(AliceInjectedService.class);
+                Assert.fail("Should still fail, only Bob is available");
+            }
+            catch (MultiException me) {
+                // expected
+            }
+            
+            addNamedBean(CTEST_ONE_TYPE, ALICE);
+            
+            {
+                AliceInjectedService aliceService = testLocator.getService(AliceInjectedService.class);
+                Assert.assertNotNull(aliceService);
+                
+                ConfiguredContract aliceContract = aliceService.getAlice();
+                Assert.assertNotNull(aliceContract);
+                Assert.assertEquals(ALICE, aliceContract.getName());
+            }
+            
+            removeNamedBean(CTEST_ONE_TYPE, BOB);
+            
+            {
+                AliceInjectedService aliceService = testLocator.getService(AliceInjectedService.class);
+                Assert.assertNotNull(aliceService);
+                
+                ConfiguredContract aliceContract = aliceService.getAlice();
+                Assert.assertNotNull(aliceContract);
+                Assert.assertEquals(ALICE, aliceContract.getName());
+            }
+            
+            removeNamedBean(CTEST_ONE_TYPE, ALICE);
+            
+            try {
+                testLocator.getService(AliceInjectedService.class);
+                Assert.fail("Alice was removed, new instance should not get created");
+            }
+            catch (MultiException me) {
+                // expected
+            }
+            
+        }
+        finally {
+            removeType(CTEST_ONE_TYPE);
+        }
+        
+    }
+
+}
