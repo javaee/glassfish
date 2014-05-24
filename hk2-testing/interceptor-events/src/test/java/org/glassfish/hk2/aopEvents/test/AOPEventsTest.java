df1bcc25421544fc91fb0400a34c15a8a44f7be2
JOHN.WELLS <john.wells@oracle.com>
2014-05-24 10:58:30 -0700

    Add in a test for the combination of events and interception, test is failing but not for the reason I thought it would fail

---
+++ hk2-testing/interceptor-events/src/test/java/org/glassfish/hk2/aopEvents/test/AOPEventsTest.java
@@ -0,0 +1,83 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.glassfish.hk2.aopEvents.test;
+
+import org.glassfish.hk2.aopEvents.Event;
+import org.glassfish.hk2.aopEvents.EventPublisher;
+import org.glassfish.hk2.aopEvents.EventSubscriberService;
+import org.glassfish.hk2.aopEvents.MethodInterceptorImpl;
+import org.glassfish.hk2.api.TypeLiteral;
+import org.glassfish.hk2.api.messaging.Topic;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.jvnet.hk2.testing.junit.HK2Runner;
+
+/**
+ * Tests for the combo of AOP interception and event processing
+ * 
+ * @author jwells
+ *
+ */
+public class AOPEventsTest extends HK2Runner {
+    @Before
+    public void before() {
+        super.before();
+        
+        ServiceLocatorUtilities.enableTopicDistribution(testLocator);
+    }
+    
+    @Test @org.junit.Ignore
+    public void testSimpleInterceptedEvent() {
+        MethodInterceptorImpl mi = testLocator.getService(MethodInterceptorImpl.class);
+        mi.getAndZeroCounter();
+        
+        EventSubscriberService subscriber = testLocator.getService(EventSubscriberService.class);
+        EventPublisher publisher = testLocator.getService(EventPublisher.class);
+        
+        Event event = new Event();
+        publisher.publish(event);
+        
+        // Intercepted and invoked
+        Assert.assertEquals(event, subscriber.getLastEvent());
+        Assert.assertEquals(1, mi.getAndZeroCounter());
+    }
+}
