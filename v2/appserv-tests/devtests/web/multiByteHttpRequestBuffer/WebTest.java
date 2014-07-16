e7264eedc84fa9919504fd68abe10a3eec9b7df0
Oleksiy Stashok <oleksiy.stashok@oracle.com>
2014-07-16 19:00:41 +0000

    [appserv-tests] + don't forget to flush the output stream
    svn path=/trunk/; revision=63508
    
    
    Former-commit-id: 8334aeb6064eda9e6bb5843b58d7b80de4c0062d

--- v2/appserv-tests/devtests/web/multiByteHttpRequestBuffer/WebTest.java
+++ v2/appserv-tests/devtests/web/multiByteHttpRequestBuffer/WebTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -128,6 +128,9 @@ public class WebTest {
         os.write("Host: localhost\r\n".getBytes());
         os.write("Connection: close\r\n".getBytes());
         os.write("\r\n".getBytes());
+
+        os.flush();
+
         InputStream is = sock.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
