5580f9611a472b4aa36bc1426618ee186c9ebe9f
Romain Grecourt <romain.grecourt@oracle.com>
2014-08-07 14:29:19 +0200

    fix typo in uname for osx

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
-# Copyright (c) 2010-2013 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2010-2014 Oracle and/or its affiliates. All rights reserved.
 #
 # The contents of this file are subject to the terms of either the GNU
 # General Public License Version 2 only ("GPL") or the Common Development
@@ -90,7 +90,7 @@ else
 fi
 
 # on OSX, -r is -E
-if [ `uname | grep -i darwmin | wc -l` -eq 1 ]
+if [ `uname | grep -i darwin | wc -l` -eq 1 ]
 then
 	SED_OPTS="-Ee"
 else
