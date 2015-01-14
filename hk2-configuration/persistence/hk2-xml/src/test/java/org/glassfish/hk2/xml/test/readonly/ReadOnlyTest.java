5ad1d9a1e2a045d910684deb1f1b35572d709223
JOHN.WELLS <john.wells@oracle.com>
2015-01-14 06:42:46 -0800

    Add failing test for read-only beans

--- hk2-configuration/persistence/hk2-xml/src/test/java/org/glassfish/hk2/xml/test/readonly/ReadOnlyTest.java
+++ hk2-configuration/persistence/hk2-xml/src/test/java/org/glassfish/hk2/xml/test/readonly/ReadOnlyTest.java
@@ -39,6 +39,13 @@
  */
 package org.glassfish.hk2.xml.test.readonly;
 
+import java.net.URL;
+
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.xml.api.XmlRootHandle;
+import org.glassfish.hk2.xml.api.XmlService;
+import org.glassfish.hk2.xml.test.utilities.Utilities;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -46,9 +53,62 @@ import org.junit.Test;
  *
  */
 public class ReadOnlyTest {
-    @Test
-    public void testReadOnlyBeans() {
+    private final static String LIBRARY1_FILE = "library1.xml";
+    
+    private final static String LIBRARY_NAME = "Sesame Street Library";
+    
+    private final static String GEEK_SUBLIME_ISBN = "978-1-55597-685-9";
+    private final static String GEEK_SUBLIME_NAME = "Geek Sublime";
+    
+    private final static String ESL_ISBN = "1-592-40087-6";
+    private final static String ESL_NAME = "Eats, Shoots and Leaves";
+    
+    private final static String ALIEN_NAME = "Alien";
+    private final static String HANNA_NAME = "Hannah and her Sisters";
+    private final static String WOODS_NAME = "Into the Woods";
+    
+    private final static String ADDRESS_LINE_1 = "555 Sesame Street";
+    private final static String ADDRESS_LINE_2 = "CO Grover";
+    private final static String TOWN = "New York";
+    private final static String STATE = "NY";
+    private final static int ZIP = 10128;
+    
+    @Test @org.junit.Ignore
+    public void testReadOnlyBeans() throws Exception {
+        ServiceLocator locator = Utilities.createLocator();
+        XmlService xmlService = locator.getService(XmlService.class);
         
+        URL url = getClass().getClassLoader().getResource(LIBRARY1_FILE);
+        
+        XmlRootHandle<LibraryBean> rootHandle = xmlService.unmarshall(url.toURI(), LibraryBean.class);
+        LibraryBean library = rootHandle.getRoot();
+        
+        Assert.assertEquals(LIBRARY_NAME, library.getName());
+        
+        {
+            BookBean geekSublimeBook = library.getBooks().get(0);
+            Assert.assertEquals(GEEK_SUBLIME_ISBN, geekSublimeBook.getISBN());
+            Assert.assertEquals(GEEK_SUBLIME_NAME, geekSublimeBook.getName());
+        }
+        
+        {
+            BookBean eslBook = library.getBooks().get(1);
+            Assert.assertEquals(ESL_ISBN, eslBook.getISBN());
+            Assert.assertEquals(ESL_NAME, eslBook.getName());
+        }
+        
+        Assert.assertEquals(ALIEN_NAME, library.getMovies().get(0).getName());
+        Assert.assertEquals(HANNA_NAME, library.getMovies().get(1).getName());
+        Assert.assertEquals(WOODS_NAME, library.getMovies().get(1).getName());
+        
+        AddressBean address = library.getAddress();
+        Assert.assertNotNull(address);
+        
+        Assert.assertEquals(ADDRESS_LINE_1, address.getStreetAddress1());
+        Assert.assertEquals(ADDRESS_LINE_2, address.getStreetAddress2());
+        Assert.assertEquals(TOWN, address.getTown());
+        Assert.assertEquals(STATE, address.getState());
+        Assert.assertEquals(ZIP, address.getZipCode());
     }
 
 }
