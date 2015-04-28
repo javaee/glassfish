8028eec4608296a141276c2d18c00cbed9a7f1c6
JOHN.WELLS <john.wells@oracle.com>
2015-04-28 11:48:34 -0700

    Start of test for having a factory know about who it is being injected into

--- hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/utilities/LocatorHelper.java
+++ hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/utilities/LocatorHelper.java
@@ -43,84 +43,107 @@ import junit.framework.Assert;
 
 import org.glassfish.hk2.api.DynamicConfiguration;
 import org.glassfish.hk2.api.DynamicConfigurationService;
+import org.glassfish.hk2.api.Factory;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.api.ServiceLocatorFactory;
 
 /**
  * @author jwells
- *
+ * 
  */
 public class LocatorHelper {
     /** This should be thrown from negative tests */
     public final static String EXPECTED = "Expected Exception";
-    
-    private final static ServiceLocatorFactory factory = ServiceLocatorFactory.getInstance();
-    
+
+    private final static ServiceLocatorFactory factory = ServiceLocatorFactory
+            .getInstance();
+
     /**
      * Creates an unnamed, untracked service locator
+     * 
      * @return An unnamed, untracked service locator
      */
     public static ServiceLocator create() {
         return factory.create(null);
     }
-    
+
     /**
      * Creates an unnamed, untracked service locator with the given parent
-     * @param parent The non-null parent to be associated with this locator
+     * 
+     * @param parent
+     *            The non-null parent to be associated with this locator
      * @return An unnamed, untracked service locator with the given parent
      */
     public static ServiceLocator create(ServiceLocator parent) {
         return factory.create(null, parent);
     }
-    
+
     /**
-     * Will create a ServiceLocator after doing test-specific bindings from the TestModule
+     * Will create a ServiceLocator after doing test-specific bindings from the
+     * TestModule
      * 
-     * @param name The name of the service locator to create.  Should be unique per test, otherwise
-     * this method will fail.
-     * @param module The test module, that will do test specific bindings.  May be null
+     * @param name
+     *            The name of the service locator to create. Should be unique
+     *            per test, otherwise this method will fail.
+     * @param module
+     *            The test module, that will do test specific bindings. May be
+     *            null
      * @return A service locator with all the test specific bindings bound
      */
     public static ServiceLocator create(String name, TestModule module) {
         return create(name, null, module);
     }
-    
+
     /**
-     * Will create a ServiceLocator after doing test-specific bindings from the TestModule
+     * Will create a ServiceLocator after doing test-specific bindings from the
+     * TestModule
      * 
-     * @param name The name of the service locator to create.  Should be unique per test, otherwise
-     * this method will fail.
-     * @param parent The parent locator this one should have.  May be null
-     * @param module The test module, that will do test specific bindings.  May be null
+     * @param name
+     *            The name of the service locator to create. Should be unique
+     *            per test, otherwise this method will fail.
+     * @param parent
+     *            The parent locator this one should have. May be null
+     * @param module
+     *            The test module, that will do test specific bindings. May be
+     *            null
      * @return A service locator with all the test specific bindings bound
      */
-    public static ServiceLocator create(String name, ServiceLocator parent, TestModule module) {
+    public static ServiceLocator create(String name, ServiceLocator parent,
+            TestModule module) {
         ServiceLocator retVal = factory.find(name);
-        Assert.assertNull("There is already a service locator of this name, change names to ensure a clean test: " + name, retVal);
-        
+        Assert.assertNull(
+                "There is already a service locator of this name, change names to ensure a clean test: "
+                        + name, retVal);
+
         retVal = factory.create(name, parent);
-        
+
         if (module == null) return retVal;
-        
-        DynamicConfigurationService dcs = retVal.getService(DynamicConfigurationService.class);
-        Assert.assertNotNull("Their is no DynamicConfigurationService.  Epic fail", dcs);
-        
+
+        DynamicConfigurationService dcs = retVal
+                .getService(DynamicConfigurationService.class);
+        Assert.assertNotNull(
+                "Their is no DynamicConfigurationService.  Epic fail", dcs);
+
         DynamicConfiguration dc = dcs.createDynamicConfiguration();
         Assert.assertNotNull("DynamicConfiguration creation failure", dc);
-        
+
         module.configure(dc);
-        
+
         dc.commit();
-        
+
         return retVal;
     }
-    
+
     /**
-     * Creates a ServiceLocator equipped with a RunLevelService and the set of classes given
+     * Creates a ServiceLocator equipped with a RunLevelService and the set of
+     * classes given
      * 
-     * @param classes The set of classes to also add to the descriptor (should probably contain some run level services, right?)
+     * @param classes
+     *            The set of classes to also add to the descriptor (should
+     *            probably contain some run level services, right?)
      * @return The ServiceLocator to use
      */
+    @SuppressWarnings("unchecked")
     public static ServiceLocator getServiceLocator(Class<?>... classes) {
         ServiceLocator locator = ServiceLocatorFactory.getInstance().create(null);
         
@@ -128,12 +151,18 @@ public class LocatorHelper {
         DynamicConfiguration config = dcs.createDynamicConfiguration();
         
         for (Class<?> clazz : classes) {
-            config.addActiveDescriptor(clazz);
+            if (Factory.class.isAssignableFrom(clazz)) {
+                Class<? extends Factory<Object>> fClass = (Class<? extends Factory<Object>>) clazz;
+                
+                config.addActiveFactoryDescriptor(fClass);
+            }
+            else {
+                config.addActiveDescriptor(clazz);
+            }
         }
         
         config.commit();
         
         return locator;
     }
-
 }
