33211ff1bfbba04dfcb613f6575326a862d3ca7d
Romain Grecourt <romain.grecourt@oracle.com>
2015-05-06 12:27:25 -0700

    Revert "Revert previous commit, 2nd attempt. fix findbugs low priority error in AbstractConfigGeneratorMojo, and exclude some NP_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD / UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD, as findbug does not know about plexus"
    This reverts commit ff9b0d3d5688fc5c8238a08c7e0b493af337a3f0.

--- hk2-configuration/persistence/hk2-xml-dom/config-generator/src/main/java/org/jvnet/hk2/config/generator/maven/AbstractConfigGeneratorMojo.java
+++ hk2-configuration/persistence/hk2-xml-dom/config-generator/src/main/java/org/jvnet/hk2/config/generator/maven/AbstractConfigGeneratorMojo.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012-2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -107,11 +107,8 @@ public abstract class AbstractConfigGeneratorMojo extends AbstractMojo {
                 || !getSourceDirectory().isDirectory()){
             return;
         }
-
-        if(!getGeneratedDirectory().mkdirs()){
-          throw new MojoExecutionException(
-            "Unable to create directory "+getGeneratedDirectory().getAbsolutePath());
-        }        
+        
+        getGeneratedDirectory().mkdirs();
         String outputPath = getGeneratedDirectory().getAbsolutePath();
 
         // prepare command line arguments
