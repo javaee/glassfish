8a88633164038aeb4e54a2808f5f411d37dd9a5b
Oleksiy Stashok <oleksiy.stashok@oracle.com>
2016-03-08 08:15:08 +0000

    + fix issue https://java.net/jira/browse/GLASSFISH-21238
    svn path=/trunk/; revision=64251
    
    
    Former-commit-id: 64820310080126bf300cac8f284ffaa323d90a7c

--- main/nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/GlassfishNetworkListener.java
+++ main/nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/GlassfishNetworkListener.java
@@ -270,7 +270,8 @@ public class GlassfishNetworkListener extends GenericGrizzlyListener {
             final DelayedExecutor delayedExecutor,
             final int maxRequestHeaders, final int maxResponseHeaders) {
         
-        return new GlassfishHttpCodecFilter(
+        final org.glassfish.grizzly.http.HttpServerFilter httpCodecFilter =
+                new GlassfishHttpCodecFilter(
                 http == null || Boolean.parseBoolean(http.getXpoweredBy()),
                 isChunkedEnabled,
                 headerBufferLengthBytes,
@@ -279,8 +280,17 @@ public class GlassfishNetworkListener extends GenericGrizzlyListener {
                 delayedExecutor,
                 maxRequestHeaders,
                 maxResponseHeaders);
+        
+        if (http != null) { // could be null for HTTP redirect
+            httpCodecFilter.setMaxPayloadRemainderToSkip(
+                    Integer.parseInt(http.getMaxSwallowingInputBytes()));
+            
+            httpCodecFilter.setAllowPayloadForUndefinedHttpMethods(
+                    Boolean.parseBoolean(http.getAllowPayloadForUndefinedHttpMethods()));
+        }
+        
+        return httpCodecFilter;
     }
-
 
 
     protected void registerMonitoringStatsProviders() {
