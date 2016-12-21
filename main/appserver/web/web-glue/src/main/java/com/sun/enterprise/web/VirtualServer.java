5eede7e54993986dfe17ea5de1cd5515b7f3340d
Shing Wai Chan <shing.wai.chan@oracle.com>
2016-12-21 19:42:02 +0000

    fix issue https://java.net/jira/browse/GLASSFISH-21642 ("access-logging-bad-requests web devtests fails in Hudson")
    svn path=/trunk/; revision=64372
    
    
    Former-commit-id: 328f95d485b702b3ad4226298097b6bdea976442

--- main/appserver/web/web-glue/src/main/java/com/sun/enterprise/web/VirtualServer.java
+++ main/appserver/web/web-glue/src/main/java/com/sun/enterprise/web/VirtualServer.java
@@ -297,8 +297,6 @@ public class VirtualServer extends StandardHost
 
     private ActionReport report = null;
 
-    private HttpProbeImpl httpProbe = null;
-
     // ------------------------------------------------------------- Properties
 
     /**
@@ -1644,23 +1642,7 @@ public class VirtualServer extends StandardHost
     }
 
     void addProbes(boolean globalAccessLoggingEnabled) {
-
-        List<String> listenerList = StringUtils.parseStringList(
-                vsBean.getNetworkListeners(), ",");
-        String[] listeners = (listenerList != null) ?
-                listenerList.toArray(new String[listenerList.size()]) :
-                new String[0];
-        List<NetworkListener> networkListeners = new ArrayList<NetworkListener>();
-
-        for (String listener : listeners) {
-            for (NetworkListener networkListener :
-                    serverConfig.getNetworkConfig().getNetworkListeners().getNetworkListener()) {
-                if (networkListener.getName().equals(listener)) {
-                    networkListeners.add(networkListener);
-                }
-            }
-        }
-        for (final NetworkListener listener : networkListeners) {
+        for (final NetworkListener listener : getGrizzlyNetworkListeners()) {
             try {
                 final GrizzlyProxy proxy = (GrizzlyProxy) grizzlyService.lookupNetworkProxy(listener);
                 if (proxy != null) {
@@ -1675,7 +1657,7 @@ public class VirtualServer extends StandardHost
                     } else {
                         for (HttpCodecFilter codecFilter : codecFilters) {
                             if (codecFilter.getMonitoringConfig().getProbes().length == 0) {
-                                httpProbe = new HttpProbeImpl(listener, isAccessLoggingEnabled(globalAccessLoggingEnabled));
+                                HttpProbeImpl httpProbe = new HttpProbeImpl(listener, isAccessLoggingEnabled(globalAccessLoggingEnabled));
                                 codecFilter.getMonitoringConfig().addProbes(httpProbe);
                             }
                         }
@@ -1755,8 +1737,9 @@ public class VirtualServer extends StandardHost
                 webcontainerFeatureFactory);
             if (restart) {
                 accessLogValve.start();
-                if (httpProbe != null)
-                    httpProbe.enableAccessLogging();
+                for (HttpProbeImpl p : getHttpProbeImpl()) {
+                    p.enableAccessLogging();
+                }
             }
         } catch (LifecycleException le) {
             _logger.log(Level.SEVERE, LogFacade.UNABLE_RECONFIGURE_ACCESS_LOG, le);
@@ -1784,8 +1767,9 @@ public class VirtualServer extends StandardHost
                     accessLogValve.stop();
                 }
                 accessLogValve.start();
-                if (httpProbe != null)
-                    httpProbe.enableAccessLogging();
+                for (HttpProbeImpl p : getHttpProbeImpl()) {
+                    p.enableAccessLogging();
+                }
             } catch (LifecycleException le) {
                 _logger.log(Level.SEVERE, LogFacade.UNABLE_RECONFIGURE_ACCESS_LOG, le);
             }
@@ -1798,8 +1782,9 @@ public class VirtualServer extends StandardHost
      */
     void disableAccessLogging() {
         removeValve(accessLogValve);
-        if (httpProbe != null)
-            httpProbe.disableAccessLogging();
+        for (HttpProbeImpl p : getHttpProbeImpl()) {
+            p.disableAccessLogging();
+        }
     }
 
     /**
@@ -2524,6 +2509,50 @@ public class VirtualServer extends StandardHost
     }
 
 
+    private List<NetworkListener> getGrizzlyNetworkListeners() {
+        List<String> listenerList = StringUtils.parseStringList(
+                vsBean.getNetworkListeners(), ",");
+        String[] listeners = (listenerList != null) ?
+                listenerList.toArray(new String[listenerList.size()]) :
+                new String[0];
+        List<NetworkListener> networkListeners = new ArrayList<NetworkListener>();
+
+        for (String listener : listeners) {
+            for (NetworkListener networkListener :
+                    serverConfig.getNetworkConfig().getNetworkListeners().getNetworkListener()) {
+                if (networkListener.getName().equals(listener)) {
+                    networkListeners.add(networkListener);
+                }
+            }
+        }
+
+        return networkListeners;
+    }
+
+    private List<HttpProbeImpl> getHttpProbeImpl() {
+        List<HttpProbeImpl> httpProbes = new ArrayList<>();
+        for (final NetworkListener listener : getGrizzlyNetworkListeners()) {
+            final GrizzlyProxy proxy = (GrizzlyProxy) grizzlyService.lookupNetworkProxy(listener);
+            if (proxy != null) {
+                 GenericGrizzlyListener grizzlyListener = (GenericGrizzlyListener) proxy.getUnderlyingListener();
+                 List<HttpCodecFilter> codecFilters = grizzlyListener.getFilters(HttpCodecFilter.class);
+                 if (codecFilters != null && !codecFilters.isEmpty()) {
+                     for (HttpCodecFilter codecFilter : codecFilters) {
+                         HttpProbe[] probes = codecFilter.getMonitoringConfig().getProbes();
+                         if (probes != null) {
+                             for (HttpProbe probe : probes) {
+                                 if (probe instanceof HttpProbeImpl) {
+                                     httpProbes.add((HttpProbeImpl)probe);
+                                 }
+                             }
+                         }
+                     }
+                 }
+            }
+        }
+
+        return httpProbes;
+    }
 
 
     // ---------------------------------------------------------- Nested Classes
