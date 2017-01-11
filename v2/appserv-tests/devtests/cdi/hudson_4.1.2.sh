b4daeed0de9df695a563bfce165fa5fef6173a87
Ankur Kathuria <ankur.kathuria@oracle.com>
2017-01-11 12:37:23 +0000

    added hudson script for GF 4.1.2. Primarily added to use 4.1.2 specific jobs for getting glassfish.zip
    svn path=/trunk/; revision=64390
    
    
    Former-commit-id: 9618c5afc7e68c641d563f4b100309d755281fa4

---
+++ v2/appserv-tests/devtests/cdi/hudson_4.1.2.sh
@@ -0,0 +1,65 @@
+date
+
+java -version
+ant -version
+
+rm -rf glassfish-v3-image
+mkdir glassfish-v3-image
+pushd glassfish-v3-image
+
+export http_proxy=http://www-proxy.us.oracle.com:80
+
+# download the latest GF 
+#wget http://rator.sfbay/maven/repositories/glassfish//org/glassfish/distributions/glassfish/3.0-SNAPSHOT/glassfish-3.0-SNAPSHOT.zip
+#wget http://javaweb.sfbay/java/re/glassfish/v3/promoted/trunk-latest/archive/bundles/glassfish-ri.zip
+#wget -O web.zip http://hudson.sfbay/job/glassfish-v3-devbuild/lastSuccessfulBuild/artifact/v3/distributions/web/target/web.zip
+#wget http://hudson.sfbay/job/gfv3-build-test/lastSuccessfulBuild/artifact/v3/distributions-ips/glassfish/target/glassfish.zip
+
+wget --no-proxy http://gf-hudson.us.oracle.com/hudson/job/gf-4.1.2-build-continuous/lastSuccessfulBuild/artifact/bundles/glassfish.zip
+
+#wget http://hudson.glassfish.org/job/gf-trunk-build-continuous/lastSuccessfulBuild/artifact/bundles/glassfish.zip
+
+date
+
+unzip -q glassfish.zip
+
+date
+
+export S1AS_HOME=$PWD/glassfish4/glassfish
+popd
+export APS_HOME=$PWD/appserv-tests
+export AS_LOGFILE=$S1AS_HOME/cli.log 
+
+cd $APS_HOME
+
+(jps |grep Main |cut -f1 -d" " | xargs kill -9  > /dev/null 2>&1) || true
+
+pushd $APS_HOME/devtests/cdi
+rm count.txt || true
+
+rm -rf bin .classpath  .project 
+
+ant clean || true;
+
+rm -f RepRunConf.txt; rm -f *.output
+
+rm -f alltests.res
+rm -f $APS_HOME/test_results*
+#clean all RepRunConf.txt
+find . -name "RepRunConf.txt" | xargs rm -f
+
+# start GlassFish
+$S1AS_HOME/bin/asadmin start-domain domain1
+# start Derby
+$S1AS_HOME/bin/asadmin start-database
+
+ant all
+
+# start GlassFish
+$S1AS_HOME/bin/asadmin stop-domain domain1
+# start Derby
+$S1AS_HOME/bin/asadmin stop-database
+
+touch $APS_HOME/test_resultsValid.xml
+
+
