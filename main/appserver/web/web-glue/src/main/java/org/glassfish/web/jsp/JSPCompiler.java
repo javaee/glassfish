dd72072a98dff30e767c9d7cd79c6ab43a6de987
Kokil Jain <kokil.jain@oracle.com>
2017-02-06 08:58:30 +0000

    Fixed GLASSFISH-21574: asadmin deploy --precompilejsp doesn't check compilerSourceVM and compilerTargetVM in glassfish-web.xml.
    svn path=/trunk/; revision=64500
    
    
    Former-commit-id: d496609040deefd1319ad250e889e8c5ad3b6ffa

--- main/appserver/web/web-glue/src/main/java/org/glassfish/web/jsp/JSPCompiler.java
+++ main/appserver/web/web-glue/src/main/java/org/glassfish/web/jsp/JSPCompiler.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2016 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -316,6 +316,10 @@ public final class JSPCompiler {
             } else if ("ignoreJspFragmentErrors".equals(pName)) {
                 jspc.setIgnoreJspFragmentErrors(
                     Boolean.valueOf(pValue).booleanValue());
+            } else if ("compilerSourceVM".equals(pName)) {
+                jspc.setCompilerSourceVM(pValue);
+            } else if ("compilerTargetVM".equals(pName)) {
+                jspc.setCompilerTargetVM(pValue);
             }
         }
 
