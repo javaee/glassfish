058b91bc4c7d26c585d576430ffccb00202f7e60
Arindam Bandyopadhyay <arindam.bandyopadhyay@oracle.com>
2017-02-06 17:52:39 +0000

    Fix GLASSFISH-21671  - Switch appserver build to source=1.8 and target=1.8
    svn path=/trunk/; revision=64509
    
    
    Former-commit-id: 15638237c955d6b1a02edce52b66c840f2a79370

--- main/appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/interceptors/InterceptorManager.java
+++ main/appserver/ejb/ejb-container/src/main/java/com/sun/ejb/containers/interceptors/InterceptorManager.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -239,7 +239,7 @@ public class InterceptorManager {
         }
 
 
-        List<InterceptorDescriptor> list = (ejbDesc != null) ?
+        List<? extends InterceptorDescriptor> list = (ejbDesc != null) ?
                 ejbDesc.getAroundInvokeInterceptors(mDesc) :
                 interceptorInfo.getAroundInvokeInterceptors(beanMethod);
 
