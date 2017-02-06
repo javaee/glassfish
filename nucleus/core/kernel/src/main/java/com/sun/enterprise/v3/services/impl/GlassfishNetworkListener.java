9685b820e26ff440aca6d6cb99b9d3b2510e3724
Srinivas Krishnan <srinivas.krishnan@oracle.com>
2017-02-06 04:14:13 +0000

    Backporting to 4.1.2 - GLASSFISH-21238 Response code 400 on a DELETE request with a body
    svn path=/branches/4.1.2/; revision=64494
    
    
    Former-commit-id: b00486d1ddcdeed0b8ae0471cfb4f9a5c2d94f94

--- nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/GlassfishNetworkListener.java
+++ nucleus/core/kernel/src/main/java/com/sun/enterprise/v3/services/impl/GlassfishNetworkListener.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2007-2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -269,8 +269,9 @@ public class GlassfishNetworkListener extends GenericGrizzlyListener {
             final String defaultResponseType, final KeepAlive keepAlive,
             final DelayedExecutor delayedExecutor,
             final int maxRequestHeaders, final int maxResponseHeaders) {
-        
-        return new GlassfishHttpCodecFilter(
+
+        final org.glassfish.grizzly.http.HttpServerFilter httpCodecFilter =
+                new GlassfishHttpCodecFilter(
                 http == null || Boolean.parseBoolean(http.getXpoweredBy()),
                 isChunkedEnabled,
                 headerBufferLengthBytes,
@@ -279,6 +280,16 @@ public class GlassfishNetworkListener extends GenericGrizzlyListener {
                 delayedExecutor,
                 maxRequestHeaders,
                 maxResponseHeaders);
+
+        if (http != null) { // could be null for HTTP redirect
+            httpCodecFilter.setMaxPayloadRemainderToSkip(
+                    Integer.parseInt(http.getMaxSwallowingInputBytes()));
+
+            httpCodecFilter.setAllowPayloadForUndefinedHttpMethods(
+                    Boolean.parseBoolean(http.getAllowPayloadForUndefinedHttpMethods()));
+        }
+
+        return httpCodecFilter;
     }
 
 
