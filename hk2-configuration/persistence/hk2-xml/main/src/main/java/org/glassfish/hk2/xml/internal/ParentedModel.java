1f2f9c44e806061aa6c1c2e352333ae52d9de157
John Wells <john.wells@oracle.com>
2017-02-21 14:05:57 -0800

    Fix copyrights

--- hk2-configuration/persistence/hk2-xml/main/src/main/java/org/glassfish/hk2/xml/internal/ParentedModel.java
+++ hk2-configuration/persistence/hk2-xml/main/src/main/java/org/glassfish/hk2/xml/internal/ParentedModel.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2015-2016 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -40,7 +40,6 @@
 package org.glassfish.hk2.xml.internal;
 
 import java.io.Serializable;
-import java.util.Map;
 
 import org.glassfish.hk2.utilities.general.GeneralUtilities;
 
--- hk2-configuration/persistence/hk2-xml/main/src/main/java/org/glassfish/hk2/xml/internal/alt/clazz/AnnotationAltAnnotationImpl.java
+++ hk2-configuration/persistence/hk2-xml/main/src/main/java/org/glassfish/hk2/xml/internal/alt/clazz/AnnotationAltAnnotationImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2015-2016 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -41,7 +41,6 @@ package org.glassfish.hk2.xml.internal.alt.clazz;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
