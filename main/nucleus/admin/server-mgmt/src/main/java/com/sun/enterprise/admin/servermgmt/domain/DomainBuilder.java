8271fcf90bc7e92a88416b62d9416748a98ceedf
Yamini K B <yamini.k.b@oracle.com>
2017-04-06 06:13:10 +0000

    GLASSFISH-21266: Start domain does not follow savelogin/savemasterpassword rules
    Tests: Manual, QL passed
    
    
    svn path=/trunk/; revision=64916
    
    
    Former-commit-id: cf5d7cf436a83e72256bd488c6060d4bc3f70409

--- main/nucleus/admin/server-mgmt/src/main/java/com/sun/enterprise/admin/servermgmt/domain/DomainBuilder.java
+++ main/nucleus/admin/server-mgmt/src/main/java/com/sun/enterprise/admin/servermgmt/domain/DomainBuilder.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -296,7 +296,7 @@ public class DomainBuilder {
                         fos.close();
                 }
             }
-            domainSecurity.changeMasterPasswordInMasterPasswordFile(new File(configDir, DomainConstants.MASTERPASSWORD_FILE), masterPassword, 
+            domainSecurity.changeMasterPasswordInMasterPasswordFile(new File(domainDir, DomainConstants.MASTERPASSWORD_FILE), masterPassword,
                     saveMasterPassword);
             domainSecurity.createPasswordAliasKeystore(new File(configDir, DomainConstants.DOMAIN_PASSWORD_FILE), masterPassword);
 
