bfe3ff6c7e14f1dda6987a039f2251323de12f35
Sameer Pandit <sameer.pandit@oracle.com>
2017-05-04 15:47:04 +0530

    remove unnecessary changes, add copyrights

--- main/appserver/tests/common_test.sh
+++ main/appserver/tests/common_test.sh
@@ -73,8 +73,8 @@ test_init(){
 	ANT_HOME=/net/gf-hudson/scratch/gf-hudson/export2/hudson/tools/ant-1.7.1; export ANT_HOME
 	APS_HOME=$WORKSPACE/main/appserver/tests/appserv-tests; export APS_HOME
 	TEST_RUN_LOG=tests-run.log; export TEST_RUN_LOG
-  #workaround for OSGI timestamp issue
-  find $S1AS_HOME -type f | xargs touch > /dev/null
+        #workaround for OSGI timestamp issue
+        find $S1AS_HOME -type f | xargs touch > /dev/null
 	echo S1AS_HOME is $S1AS_HOME
 	echo ANT_HOME is $ANT_HOME
 	echo APS_HOME is $APS_HOME
