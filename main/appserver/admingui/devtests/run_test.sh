a951a4bfaad59baf423765c70d329b1f91bcff3d
kokil12 <kokil.jain@oracle.com>
2017-05-25 18:05:20 +0530

    Changed run_test.sh again

--- main/appserver/admingui/devtests/run_test.sh
+++ main/appserver/admingui/devtests/run_test.sh
@@ -48,7 +48,7 @@ test_run(){
   $S1AS_HOME/bin/asadmin start-domain
   cd $WORKSPACE/main/appserver/admingui/devtests/
   pwd
-  mvn -Dmaven.repo.local=$WORKSPACE/repository -Dtest=ConfigTest test | tee $TEST_RUN_LOG
+  mvn -Dtest=ConfigTest test | tee $TEST_RUN_LOG
   $S1AS_HOME/bin/asadmin stop-domain	
 }
 
