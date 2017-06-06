674109e7009523e42306aaf83948c245ac0caf0b
kokil12 <kokil.jain@oracle.com>
2017-06-06 11:58:27 +0530

    Added opends java home

--- main/appserver/tests/appserv-tests/devtests/security/run_test.sh
+++ main/appserver/tests/appserv-tests/devtests/security/run_test.sh
@@ -53,20 +53,25 @@ test_run(){
 
 	# Workaround for JDK7 and OpenDS
 	cp $APS_HOME/devtests/security/ldap/opends/X500Signer.jar $OPENDS_HOME/lib
-	echo $PATH
-	echo $JAVA_HOME
-	export OLD_JAVA_HOME=$JAVA_HOME
-	export OLD_PATH=$PATH	
-	export JAVA_HOME=/gf-hudson-tools/jdk/7/latest/bin
-	export PATH=$JAVA_HOME:$PATH
-	echo $PATH
-	java -version
+	
+	#echo $PATH
+	#echo $JAVA_HOME
+	#export OLD_JAVA_HOME=$JAVA_HOME
+	#export OLD_PATH=$PATH	
+	#export JAVA_HOME=/gf-hudson-tools/jdk/7/latest/bin
+	#export PATH=$JAVA_HOME:$PATH
+	#echo $PATH
+	#java -version
+	rm -rf $OPENDS_HOME/lib/set-java-home
+	export OPENDS_JAVA_HOME=/gf-hudson-tools/jdk/7/latest
+
 	# Configure and start OpenDS using the default ports
 	$OPENDS_HOME/setup -i -v -n -p 1389 --adminConnectorPort 4444 -x 1689 -w dmanager -b "dc=sfbay,dc=sun,dc=com" -Z 1636 --useJavaKeystore $S1AS_HOME/domains/domain1/config/keystore.jks -W changeit -N s1as
 
-	export PATH=$OLD_PATH
-	export JAVA_HOME=$OLD_JAVA_HOME	
-	java -version
+	#export PATH=$OLD_PATH
+	#export JAVA_HOME=$OLD_JAVA_HOME	
+	#java -version
+	
 	$S1AS_HOME/bin/asadmin start-database
 	$S1AS_HOME/bin/asadmin start-domain
 	pushd $APS_HOME/devtests/security
