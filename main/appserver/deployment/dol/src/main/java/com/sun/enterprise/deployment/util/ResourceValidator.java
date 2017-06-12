e6423678abb0c5af47e169443985b1ad2e9ac128
mskdeepak-oracle <sai.krishna.deepak.maram@oracle.com>
2017-06-12 13:11:05 +0530

    Extract resources out of dc

--- main/appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/util/ResourceValidator.java
+++ main/appserver/deployment/dol/src/main/java/com/sun/enterprise/deployment/util/ResourceValidator.java
@@ -1,23 +1,47 @@
 package com.sun.enterprise.deployment.util;
 
 import com.sun.enterprise.config.serverbeans.*;
+import com.sun.enterprise.deployment.*;
 import com.sun.enterprise.deployment.Application;
 import org.glassfish.api.deployment.DeployCommandParameters;
 import org.glassfish.api.deployment.DeploymentContext;
 import org.glassfish.api.event.EventListener;
 import org.glassfish.api.event.Events;
+import org.glassfish.deployment.common.DeploymentException;
 import org.glassfish.internal.deployment.Deployment;
+import org.glassfish.logging.annotation.LogMessageInfo;
 import org.jvnet.hk2.annotations.Service;
 
 import javax.inject.Inject;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by mskd on 6/9/17.
  */
 @Service
 public class ResourceValidator implements EventListener, ResourceValidatorVisitor {
+
+    public static final Logger deplLogger = com.sun.enterprise.deployment.util.DOLUtils.deplLogger;
+
+    @LogMessageInfo(
+            message = "JNDI lookup failed for the resource with jndi name: {0}",
+            level = "SEVERE",
+            cause = "JNDI lookup for the specified resource failed.",
+            action = "Create the necessary object before deploying the application.",
+            comment = "For the method validateJNDIRefs of com.sun.enterprise.deployment.util.ResourceValidator."
+    )
+    private static final String RESOURCE_REF_JNDI_LOOKUP_FAILED = "AS-DEPLOYMENT-00026";
+
+    String target;
+
+    DeploymentContext dc;
+
+    Application application;
+
     @Inject
     private Events events;
 
@@ -31,22 +55,50 @@ public class ResourceValidator implements EventListener, ResourceValidatorVisito
     @Override
     public void event(Event event) {
         if (event.is(Deployment.AFTER_APPLICATION_CLASSLOADER_CREATION)) {
-            DeploymentContext dc = (DeploymentContext) event.hook();
-            processResources(dc);
+            dc = (DeploymentContext) event.hook();
+            application = dc.getModuleMetaData(Application.class);
+            DeployCommandParameters commandParams = dc.getCommandParameters(DeployCommandParameters.class);
+            target = commandParams.target;
+            processResources();
         }
     }
 
-    //TODO: Extract resources out of dc
-    private void processResources(DeploymentContext dc) {
-        DeployCommandParameters commandParams = dc.getCommandParameters(DeployCommandParameters.class);
-        Application application = dc.getModuleMetaData(Application.class);
-        validateJNDIRefs("", commandParams.target, dc);
+    //TODO: Extract resources completely out of dc
+    private void processResources() {
+        for (BundleDescriptor bd : application.getBundleDescriptorsOfType(DOLUtils.warType())) {
+            accept(bd);
+        }
+        for (BundleDescriptor bd : application.getBundleDescriptorsOfType(DOLUtils.carType())) {
+            accept(bd);
+        }
+        for (BundleDescriptor bd : application.getBundleDescriptorsOfType(DOLUtils.ejbType())) {
+            accept(bd);
+        }
     }
 
-    private void validateJNDIRefs(String jndiName, String target, DeploymentContext dc) {
+    private void accept(BundleDescriptor bundleDescriptor) {
+        if (bundleDescriptor instanceof JndiNameEnvironment) {
+            JndiNameEnvironment nameEnvironment = (JndiNameEnvironment) bundleDescriptor;
+            for (Iterator<ResourceReferenceDescriptor> itr = nameEnvironment.getResourceReferenceDescriptors().iterator(); itr.hasNext();) {
+                accept(itr.next());
+            }
+            for (Iterator<MessageDestinationDescriptor> itr = bundleDescriptor.getMessageDestinations().iterator(); itr.hasNext();) {
+                accept(itr.next());
+            }
+        }
+    }
+
+    private void accept(NamedDescriptor resRef) {
+        validateJNDIRefs(resRef.getJndiName());
+    }
+
+    // check in the domain.xml and in the app scoped resources
+    private void validateJNDIRefs(String jndiName) {
         if(!validateResource(target, jndiName)) {
             if (!validateAppScopedResource(jndiName, dc)) {
-                //throw new IllegalStateException(String.format("JNDI resource not present: %s", jndiName));
+                deplLogger.log(Level.SEVERE, RESOURCE_REF_JNDI_LOOKUP_FAILED,
+                        new Object[] {jndiName});
+                throw new DeploymentException(String.format("JNDI resource not present: %s", jndiName));
             }
         }
     }
@@ -61,22 +113,12 @@ public class ResourceValidator implements EventListener, ResourceValidatorVisito
             return cluster.isResourceRefExists(jndiName);
         }
         return false;
-        // throw new IllegalStateException(String.format("Where am I? %s", target));
     }
 
     // TODO: Understand application scoped resources and validate using resourcesList
     private boolean validateAppScopedResource(String jndiName, DeploymentContext dc) {
-        com.sun.enterprise.config.serverbeans.Application app = dc.getTransientAppMetaData(ServerTags.APPLICATION, com.sun.enterprise.config.serverbeans.Application.class);
         Map<String, Map<String, List>> resourcesList =
                 (Map<String, Map<String, List>>) dc.getTransientAppMetadata().get("app-scoped-resources-map");
-
-        List<Resource> resources = app.getResources().getResources();
-
-        for (Resource resource: resources) {
-            if (resource.getIdentity() == jndiName) return true;
-        }
         return false;
     }
-
-
 }
