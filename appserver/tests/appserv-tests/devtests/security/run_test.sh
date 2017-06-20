8840d84d747f42592eb5b2b2978e1b5592a3b5a6
Sameer Pandit <sameer.pandit@oracle.com>
2017-06-20 21:13:48 +0530

    fix proxy

--- appserver/tests/appserv-tests/devtests/security/run_test.sh
+++ appserver/tests/appserv-tests/devtests/security/run_test.sh
@@ -62,6 +62,8 @@ test_run(){
 	$S1AS_HOME/bin/asadmin start-domain
 	pushd $APS_HOME/devtests/security	
 	rm count.txt || true
+  PROXY_HOST=`echo ${http_proxy} | cut -d':' -f1`
+  PROXY_PORT=`echo ${http_proxy} | cut -d':' -f2`
   ANT_OPTS="${ANT_OPTS} \
   -Dhttp.proxyHost=${PROXY_HOST} \
   -Dhttp.proxyPort=${PROXY_PORT} \
