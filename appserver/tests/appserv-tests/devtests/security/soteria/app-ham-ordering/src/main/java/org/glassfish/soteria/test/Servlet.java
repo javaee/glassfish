621f42c3877e83801ce6f1bbc615985c02ffc696
SAMEER PANDIT <sameer.pandit@oracle.com>
2017-06-23 09:13:59 +0530

    Fixes #21808:add soteria sample test to security devtests (#21809)
    * add security Context and default role mapping tests
    * add undefined behavior test app-double-ham
    * add test for verification of order for HAM methods
    * add app-ham-ordering to module
    * add common dependencies
    * change soteria artifactId and version
    * enable role-mapping with comment, download maven repo

---
+++ appserver/tests/appserv-tests/devtests/security/soteria/app-ham-ordering/src/main/java/org/glassfish/soteria/test/Servlet.java
@@ -0,0 +1,128 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://oss.oracle.com/licenses/CDDL+GPL-1.1
+ * or LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.glassfish.soteria.test;
+
+import java.io.IOException;
+
+import javax.annotation.security.DeclareRoles;
+import javax.inject.Inject;
+import javax.security.enterprise.SecurityContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.glassfish.soteria.SecurityContextImpl;
+import java.util.stream.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+/**
+ * Test Servlet that prints out the name of the authenticated caller and whether
+ * this caller is in any of the roles {foo, bar, kaz}
+ * 
+ *
+ */
+@DeclareRoles({ "foo", "bar", "kaz" })
+@WebServlet("/servlet")
+public class Servlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Inject
+    private SecurityContext securityContext;
+
+    private void tagHttpRequest(HttpServletRequest request,String methodName){
+
+        if(request.getAttribute("methodInvList") == null){
+            List<String> invList= new ArrayList<>();
+            invList.add(methodName);
+            request.setAttribute("methodInvList", invList.stream().collect(Collectors.joining(",")));
+        }else{
+            String invListStr = (String)request.getAttribute("methodInvList");
+            List<String> invList = new ArrayList(Arrays.asList(invListStr.split(",")));
+            invList.add(methodName);
+            request.setAttribute("methodInvList", invList.stream().collect(Collectors.joining(",")));
+        }
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        tagHttpRequest(request,"doGet");
+        response.getWriter().write("This is a servlet \n");
+
+        String webName = null;
+        if (request.getUserPrincipal() != null) {
+            webName = request.getUserPrincipal().getName();
+        }
+
+        response.getWriter().write("web username: " + webName + "\n");
+
+        response.getWriter().write("web user has role \"foo\": " + request.isUserInRole("foo") + "\n");
+        response.getWriter().write("web user has role \"bar\": " + request.isUserInRole("bar") + "\n");
+        response.getWriter().write("web user has role \"kaz\": " + request.isUserInRole("kaz") + "\n");
+        
+        String contextName = null;
+        if (securityContext.getCallerPrincipal() != null) {
+            contextName = securityContext.getCallerPrincipal().getName();
+        }
+        
+        response.getWriter().write("context username: " + contextName + "\n");
+
+        
+        response.getWriter().write("context user has role \"foo\": " + securityContext.isCallerInRole("foo") + "\n");
+        response.getWriter().write("context user has role \"bar\": " + securityContext.isCallerInRole("bar") + "\n");
+        response.getWriter().write("context user has role \"kaz\": " + securityContext.isCallerInRole("kaz") + "\n");
+        
+        response.getWriter().write("has access " + securityContext.hasAccessToWebResource("/protectedServlet") + "\n");
+        
+        response.getWriter().write("All declared roles of user " + ((SecurityContextImpl)securityContext).getAllDeclaredCallerRoles() + "\n");
+        String invListStr = (String)request.getAttribute("methodInvList");
+        response.getWriter().write("OrderingList: " + invListStr);
+        boolean doLogout = Boolean.parseBoolean(request.getParameter("doLogout"));
+        if(doLogout)
+            request.logout();
+    }
+
+}
---
+++ appserver/tests/appserv-tests/devtests/security/soteria/common/src/main/java/org/glassfish/soteria/test/ArquillianBase.java
@@ -0,0 +1,171 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://oss.oracle.com/licenses/CDDL+GPL-1.1
+ * or LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.glassfish.soteria.test;
+
+import static java.util.logging.Level.SEVERE;
+import static org.apache.http.HttpStatus.SC_MULTIPLE_CHOICES;
+import static org.apache.http.HttpStatus.SC_OK;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+
+public class ArquillianBase {
+    
+    private static final Logger logger = Logger.getLogger(ArquillianBase.class.getName());
+    
+    private WebClient webClient;
+    private String response;
+
+	@ArquillianResource
+    private URL base;
+	
+    @Rule
+    public TestWatcher ruleExample = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            super.failed(e, description);
+            
+            logger.log(SEVERE, 
+                "\n\nTest failed: " + 
+                description.getClassName() + "." + description.getMethodName() +
+                
+                "\nMessage: " + e.getMessage() +
+                
+                "\nLast response: " +
+                
+                "\n\n"  + response + "\n\n");
+            
+        }
+    };
+
+    @Before
+    public void setUp() {
+        response = null;
+        webClient = new WebClient() {
+            
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void printContentIfNecessary(WebResponse webResponse) {
+                int statusCode = webResponse.getStatusCode();
+                if (getOptions().getPrintContentOnFailingStatusCode() && !(statusCode >= SC_OK && statusCode < SC_MULTIPLE_CHOICES)) {
+                    logger.log(SEVERE, webResponse.getWebRequest().getUrl().toExternalForm());
+                }
+                super.printContentIfNecessary(webResponse);
+            }
+        };
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+    }
+
+    @After
+    public void tearDown() {
+        webClient.getCookieManager().clearCookies();
+        webClient.close();
+    }
+    
+    protected String readFromServer(String path) {
+        response = "";
+        WebResponse localResponse = responseFromServer(path);
+        if (localResponse != null) {
+            response = localResponse.getContentAsString();
+        }
+        
+    	return response;
+    }
+    
+    protected WebResponse responseFromServer(String path) {
+        
+        WebResponse webResponse = null;
+        
+        Page page = pageFromServer(path);
+        if (page != null) {
+            webResponse = page.getWebResponse();
+            if (webResponse != null) {
+                response = webResponse.getContentAsString();
+            }
+        }
+        
+        return webResponse;
+    }
+    
+    protected <P extends Page> P pageFromServer(String path) {
+    	
+    	if (base.toString().endsWith("/") && path.startsWith("/")) {
+    		path = path.substring(1);
+    	}
+    	
+        try {
+            response = "";
+            
+            P page = webClient.getPage(base + path);
+            
+            if (page != null) {
+                WebResponse localResponse = page.getWebResponse();
+                if (localResponse != null) {
+                    response = localResponse.getContentAsString();
+                }
+            }
+            
+            return page;
+            
+        } catch (FailingHttpStatusCodeException | IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+    
+    protected WebClient getWebClient() {
+ 		return webClient;
+ 	}
+    
+}
