b8eefe004114144412c2519f2cc6ee8c2032dd8d
Ankur Kathuria <ankur.kathuria@oracle.com>
2017-06-27 14:53:28 +0530

    weld3 integration code (#21895)
    * changing name of test suit in order to remove conflict
    
    * weld 3 integration
    
    * repackaging osgi-cdi bundle in order to correct the version
    
    * updating CDI and Weld version to Final release
    
    * cannot add local to remote interface, bug related to EJB spec
    
    * fixing cdi-tck failure
    
    * reverting repackaging logic and using osg-cdi 1.0.4 from maven repo
    
    * Gf weld tck2 (#2)
    
    Gf weld tck2
    
    * fixes #21849 Jboss logging needs to be updated (#21850)
    
    * correcting pom for weld3 integration
    
    * removing main from branch
    
    * adding for weld 3 integration
    
    * Fixes #21897: Update commons-fileupload version to 1.3.2 (#21901)
    
    * Revert "Fixing - The nucleus_admin_all: runConfigureManagedJobsTest(org.glassfish.nucleus.admin.progress.JobManagerTest)" (#21902)
    
    * Revert "Fixes #21897: Update commons-fileupload version to 1.3.2 (#21901)"
    
    This reverts commit abdb14e042223c1a1dcec6b1f1aca1a0778d9a7b.
    
    * Revert "fixes #21849 Jboss logging needs to be updated (#21850)"
    
    This reverts commit c60b80610b01a317df384f7ebbfb811d29a1dc70.
    
    * Revert "21709 :Added support for boolean get method names (#21835)"
    
    This reverts commit aa18e79ca6f97f8f9a3842303122ee84a81ad9f8.
    
    * Revert "Fixes #21875: Handle Proxy header in CGI Servlet (#21876)"
    
    This reverts commit 758a7fff1e07c71cb01706222034f8ea67509ecc.
    
    * Revert "Fixes #21830: Added merge function in run_test.sh of security devtests (#21842)"
    
    This reverts commit 8aa4828463e130f7b578425ca1f6a50780d06d2b.
    
    * Revert "Issue: 21848 Adding 'connector-service' as default-config. (#21856)"
    
    This reverts commit 594fbb6a684747732e66949261c7d20e48f4a2c5.
    
    * Revert "Fixing The nucleus_admin_all: runConfigureManagedJobsTest(org.glassfish.nucleus.admin.progress.JobManagerTest). (#21872)"
    
    This reverts commit ab64767a0bfd430f982ca2b6c35da79d852a3fed.
    
    * Fixes #21878 SSLv3 protocol is enabled by default (#21879)
    
    * Fixes #21882 deserialization flaw (#21884)
    
    * updated jbatch version and license url.

--- appserver/deployment/dol/src/test/java/com/sun/enterprise/deployment/EjbInterceptorTest.java
+++ appserver/deployment/dol/src/test/java/com/sun/enterprise/deployment/EjbInterceptorTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -9,11 +9,11 @@
  * may not use this file except in compliance with the License.  You can
  * obtain a copy of the License at
  * https://oss.oracle.com/licenses/CDDL+GPL-1.1
- * or LICENSE.txt.  See the License for the specific
+ * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  *
  * When distributing the software, include this License Header Notice in each
- * file and include the License file at LICENSE.txt.
+ * file and include the License file at packager/legal/LICENSE.txt.
  *
  * GPL Classpath Exception:
  * Oracle designates this particular file as subject to the "Classpath"
@@ -37,51 +37,31 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-
 package com.sun.enterprise.deployment;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNull;
+
+import static junit.framework.Assert.assertSame;
+import static org.easymock.EasyMock.*;
+
+import org.easymock.EasyMockSupport;
 
 import javax.enterprise.inject.spi.Interceptor;
 
-/**
- * Contains information about 1 ejb interceptor.
- */ 
+public class EjbInterceptorTest {
 
-public class EjbInterceptor extends InterceptorDescriptor
-{
+  @Test
+  public void testGetSetInterceptor() throws Exception {
+    EjbInterceptor ejbInterceptor = new EjbInterceptor();
+    assertNull( ejbInterceptor.getInterceptor() );
 
-    private EjbBundleDescriptor ejbBundleDescriptor;
-    private boolean cdiInterceptor = false;
-    private Interceptor interceptor;
+    EasyMockSupport mockSupport = new EasyMockSupport();
+    Interceptor interceptor = mockSupport.createMock( Interceptor.class );
 
-    public EjbBundleDescriptor getEjbBundleDescriptor() {
-	    return ejbBundleDescriptor;
-    }
+    ejbInterceptor.setInterceptor( interceptor );
+    assertSame( interceptor, ejbInterceptor.getInterceptor() );
+  }
 
-    public void setEjbBundleDescriptor(EjbBundleDescriptor bundleDescriptor) {
-	    ejbBundleDescriptor = bundleDescriptor;
-        super.setBundleDescriptor(bundleDescriptor);
-    }
-
-    public String toString() {
-        return "EjbInterceptor class = " + getInterceptorClassName();
-    }
-
-    public void setCDIInterceptor(boolean flag) {
-        cdiInterceptor = flag;
-    }
-
-    public boolean isCDIInterceptor() {
-        return cdiInterceptor;
-    }
-
-    /**
-     * @return The interceptor.  May be null when CDI is not enabled.
-     */
-    public Interceptor getInterceptor() {
-        return interceptor;
-    }
-
-    public void setInterceptor( Interceptor interceptor ) {
-        this.interceptor = interceptor;
-    }
 }
