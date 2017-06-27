0da59b9154e9f974c9f6029064ecb45afbc0a662
Naman Mehta <naman.mehta@oracle.com>
2017-06-27 22:33:02 +0530

    Enabling Connector Tests for RQ Job.

--- appserver/tests/v2-tests/appserv-tests/devtests/connector/resultCount.sh
+++ appserver/tests/v2-tests/appserv-tests/devtests/connector/resultCount.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-total=25
-OUTPUT_FILE=connector.output
-
-for each in `grep "Total PASS" ${OUTPUT_FILE} | awk '{print $4}'`
+total=28
+OUTPUT_FILE=$ROOT/connector.output
+echo "------------------------"
+which grep
+echo "------------------------"
+which awk
+echo "------------------------"
+echo  ${OUTPUT_FILE}
+echo "------------------------"
+ps -p $$
+for each in `grep "Total PASS:" ${OUTPUT_FILE} | awk '{print $4}'`
 do
      pass=$(( $pass + $each ))
 done
 
-for each in `grep "Total FAIL" ${OUTPUT_FILE} | awk '{print $4}'`
+for each in `grep "Total FAIL:" ${OUTPUT_FILE} | awk '{print $4}'`
 do
      fail=$(( $fail + $each ))
 done
@@ -20,3 +27,11 @@ echo "PASSED = ${pass}"
 echo "FAILED = ${fail}"
 echo "------------------------"
 
+if [ $fail -gt 0 ]
+then
+   echo "One or More Test(s) Failed."
+   exit 1;
+else
+   echo "Tests Successful."
+   exit 0;
+fi
