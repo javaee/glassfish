5d8ce3356e639ea8a340e403aa506ffd16521d2b
Debayan Gupta <debayan.gupta@oracle.com>
2017-07-10 13:27:24 +0530

    Tests have been split into five test ids to reduce execution time while running parallely

--- appserver/tests/appserv-tests/devtests/admin/cli/run_test.sh
+++ appserver/tests/appserv-tests/devtests/admin/cli/run_test.sh
@@ -67,9 +67,6 @@ run_test_id(){
 	check_successful_run
     generate_junit_report $1
     change_junit_report_class_names
-}
-
-post_test_run(){
     copy_test_artifects
     upload_test_results
     delete_bundle
@@ -81,12 +78,15 @@ get_test_target(){
 		admin_cli_all )
 			TARGET=all
 			export TARGET;;
-	esac
+	        * )
+                       TARGET=$1
+                       export TARGET;;
+        esac
 
 }
 
 list_test_ids(){
-	echo admin_cli_all
+	echo admin_cli_all admin-cli-group-1 admin-cli-group-2 admin-cli-group-3 admin-cli-group-4 admin-cli-group-5
 }
 
 OPT=$1
@@ -96,6 +96,5 @@ case $OPT in
 	list_test_ids )
 		list_test_ids;;
 	run_test_id )
-		trap post_test_run EXIT
 		run_test_id $TEST_ID ;;
 esac
