dfd8986ea6e6234f812d3cff9a739d93d782ea26
h2002044 <jagadish.ramu@oracle.com>
2017-07-12 13:01:56 +0530

    quicklook test for bean-validation

---
+++ appserver/tests/quicklook/bean-validator/simple-bv-servlet/src/java/simple_bv_servlet/Employee.java
@@ -0,0 +1,54 @@
+package simple_bv_servlet;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.List;
+
+public class Employee implements Serializable {
+
+  @Max(60)
+  @Min(20)
+  private int age;
+
+  public void setAge(int age){
+    this.age = age;
+  }
+
+  public int getAge(){
+    return age;
+  }
+
+  @NotNull
+  private String firstName;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  @NotNull
+  private String lastName;
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  private List<@NotNull String> listOfString;
+
+  public List<String> getListOfString() {
+    return listOfString;
+  }
+
+  public void setListOfString(List<String> listOfString) {
+    this.listOfString = listOfString;
+  }
+}
