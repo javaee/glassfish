21fed7928503364fd0cdb0c24fd186e3cef14f0c
Naman Mehta <naman.mehta@oracle.com>
2017-07-15 19:58:44 +0530

    Fixing compilation error.

--- appserver/tests/appserv-tests/devtests/connector/v3/beanvalidation-simple-resources-xml/ra/src/connector/SimpleResourceAdapterImpl.java
+++ appserver/tests/appserv-tests/devtests/connector/v3/beanvalidation-simple-resources-xml/ra/src/connector/SimpleResourceAdapterImpl.java
@@ -42,7 +42,6 @@ implements ResourceAdapter, java.io.Serializable {
     private BootstrapContext ctx;
     private WorkManager wm;
     
-    @NotEmpty 
     private String testName;
 
     private boolean debug = true;
