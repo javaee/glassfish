0e3a0c1501df633b3bf8427726f1068bb3459122
Naman Mehta <naman.mehta@oracle.com>
2017-07-17 22:37:26 +0530

    Fixing copyright header mistakes in all files.

--- appserver/tests/appserv-tests/devtests/connector/v3/SunRaXml/cci-embedded/client/SampleExternalMethods.java
+++ appserver/tests/appserv-tests/devtests/connector/v3/SunRaXml/cci-embedded/client/SampleExternalMethods.java
@@ -38,15 +38,7 @@
  * holder.
  */
 
-package client;/*
-
- *
- * Copyright 2002 Sun Microsystems, Inc. All Rights Reserved.
- *
- * This software is the proprietary information of Sun Microsystems, Inc.
- * Use is subject to license terms.
- *
- */
+package client;
 
 import java.sql.*;
 
