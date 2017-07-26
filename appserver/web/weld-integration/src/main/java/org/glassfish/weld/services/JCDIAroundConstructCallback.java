54f3084a5c36149418b3213480060487a30abfed
ankur-pc <ankur.kathuria@oracle.com>
2017-07-26 17:03:53 +0530

    changes for Around construct
    This reverts commit 3b2c561b68d12c459323e8ecf08d6aeaff7e1cdd.

--- appserver/web/weld-integration/src/main/java/org/glassfish/weld/services/JCDIAroundConstructCallback.java
+++ appserver/web/weld-integration/src/main/java/org/glassfish/weld/services/JCDIAroundConstructCallback.java
@@ -10,6 +10,7 @@ import org.jboss.weld.exceptions.WeldException;
 import javax.enterprise.inject.spi.AnnotatedConstructor;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.*;
 
 /**
  * This calls back into the ejb container to perform the around construct interception.  When that's finished the
@@ -18,6 +19,14 @@ import java.util.Map;
 public class JCDIAroundConstructCallback<T> implements AroundConstructCallback<T> {
     private BaseContainer container;
     private EJBContextImpl ejbContext;
+
+    // The AroundConstruct interceptor method can access the constructed instance using
+    // InvocationContext.getTarget method after the InvocationContext.proceed completes.
+    private final AtomicReference<T> target = new AtomicReference<T>();
+
+    private ConstructionHandle<T> handle;
+    private Object[] parameters;
+
     public JCDIAroundConstructCallback(BaseContainer container, EJBContextImpl ejbContext) {
         this.container = container;
         this.ejbContext = ejbContext;
@@ -25,17 +34,28 @@ public class JCDIAroundConstructCallback<T> implements AroundConstructCallback<T
 
     @Override
     public T aroundConstruct(final ConstructionHandle<T> handle, AnnotatedConstructor<T> constructor, Object[] parameters, Map<String, Object> data) {
+        this.handle = handle;
+        this.parameters = parameters;
         T ejb;
         try {
             container.intercept( LifecycleCallbackDescriptor.CallbackType.AROUND_CONSTRUCT, ejbContext );
 
             // all the interceptors were invoked, call the constructor now
-            ejb = handle.proceed( parameters, new HashMap<String, Object>() );
+            if ( target.get() == null ) {
+                ejb = handle.proceed( parameters, new HashMap<String, Object>() );
+                target.set( ejb );
+            }
         } catch (RuntimeException e) {
             throw e;
         } catch (Throwable e) {
             throw new WeldException(e);
         }
-            return ejb;
+        return target.get();
+    }
+
+    public T createEjb() {
+        T instance = handle.proceed(parameters, new HashMap<String, Object>() );
+        target.set(instance);
+        return instance;
     }
 }
