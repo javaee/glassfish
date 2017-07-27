22dcb9b5ee3926efab74e719281a6093aca41a96
Debayan Gupta <debayan.gupta@oracle.com>
2017-07-27 14:08:44 +0530

    Removing debug printing

--- appserver/tests/cts_smoke/run_test.sh
+++ appserver/tests/cts_smoke/run_test.sh
@@ -206,8 +206,7 @@ run_test_id(){
 	elif [[ $1 = "servlet_tck_"* ]]; then
 		TEST_DIR_PROPERTIES=`dirname $0`/test_dir.properties
 		TEST_DIR_PROP_KEY=(`echo $1 | sed 's/servlet_tck_//'`)
-		TEST_DIR=(`cat ${TEST_DIR_PROPERTIES} | grep ${TEST_DIR_PROP_KEY} | cut -d'=' -f2`)		
-		echo $TEST_DIR
+		TEST_DIR=(`cat ${TEST_DIR_PROPERTIES} | grep ${TEST_DIR_PROP_KEY} | cut -d'=' -f2`)
 		test_run_servlet_tck $TEST_DIR
 		result=$WORKSPACE/results/tests.log
 	else
