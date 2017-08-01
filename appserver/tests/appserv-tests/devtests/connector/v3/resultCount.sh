e893abd8b2372a5ed2da2388e31480ec27bf9496
Naman Mehta <naman.mehta@oracle.com>
2017-08-01 22:29:33 +0530

    Adding proper result count.

--- appserver/tests/appserv-tests/devtests/connector/v3/resultCount.sh
+++ appserver/tests/appserv-tests/devtests/connector/v3/resultCount.sh
@@ -43,7 +43,7 @@
 FILE=$APS_HOME/test_resultsValid.xml
 echo "input file=$FILE"
 
-TOTAL=255
+TOTAL=380
 PASSED=`grep "\"pass\"" $FILE | wc -l`
 FAILED=`grep "\"fail\"" $FILE | wc -l`
 TOTAL_RUN=`expr $PASSED + $FAILED `
