27e7e9fa9d614f331aafc6046cd2bb62cdb0dc4d
Debayan Gupta <debayan.gupta@oracle.com>
2017-08-05 16:20:28 +0530

    Replacing new lines in properties file for temporary test

--- appserver/tests/cts_smoke/run_test.sh
+++ appserver/tests/cts_smoke/run_test.sh
@@ -119,13 +119,15 @@ test_run_cts_smoke(){
 	$S1AS_HOME/bin/asadmin create-jvm-options "-Djava.security.manager"
 	$S1AS_HOME/bin/asadmin stop-domain
         if [[ -n $1 ]]; then
-                 cd $TS_HOME/bin/xml/impl/glassfish
-		 $TS_HOME/tools/ant/bin/ant  -Dgroups.count=5 -Dgroup.id=$1 -Dgroups.work.dir=/tmp -f ./smoke-groups.xml smoke.split.groups
-                 cat /tmp/javaee-smoke-group$1.properties
-		 cd -
-                 $TS_HOME/tools/ant/bin/ant -Dreport.dir=$WORKSPACE/$BUILD_NUMBER/JTReport -Dwork.dir=$WORKSPACE/$BUILD_NUMBER/JTWork -propertyfile /tmp/javaee-smoke-group$1.properties -f smoke.xml smoke
+		cd $TS_HOME/bin/xml/impl/glassfish
+		$TS_HOME/tools/ant/bin/ant  -Dgroups.count=5 -Dgroup.id=$1 -Dgroups.work.dir=/tmp -f ./smoke-groups.xml smoke.split.groups
+		cat /tmp/javaee-smoke-group$1.properties
+		sed -i ':a;N;$!ba;s/\n/ /g' /tmp/javaee-smoke-group$1.properties
+		cat /tmp/javaee-smoke-group$1.properties
+		cd -
+		$TS_HOME/tools/ant/bin/ant -Dreport.dir=$WORKSPACE/$BUILD_NUMBER/JTReport -Dwork.dir=$WORKSPACE/$BUILD_NUMBER/JTWork -propertyfile /tmp/javaee-smoke-group$1.properties -f smoke.xml smoke
         else
-		 $TS_HOME/tools/ant/bin/ant -Dreport.dir=$WORKSPACE/$BUILD_NUMBER/JTReport -Dwork.dir=$WORKSPACE/$BUILD_NUMBER/JTWork -f smoke.xml smoke
+		$TS_HOME/tools/ant/bin/ant -Dreport.dir=$WORKSPACE/$BUILD_NUMBER/JTReport -Dwork.dir=$WORKSPACE/$BUILD_NUMBER/JTWork -f smoke.xml smoke
 	fi
 
 	#POST CLEANUPS
