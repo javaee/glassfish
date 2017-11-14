85b0cc3b59aa1fc87f1fc83bb8837ff12034803b
Yamini K B <yamini.k.b@oracle.com>
2017-11-14 13:26:43 +0530

    Removed extra import

--- appserver/verifier/verifier-impl/src/main/java/com/sun/enterprise/tools/verifier/tests/util/BundledOptPkgHasDependencies.java
+++ appserver/verifier/verifier-impl/src/main/java/com/sun/enterprise/tools/verifier/tests/util/BundledOptPkgHasDependencies.java
@@ -47,9 +47,9 @@
 package com.sun.enterprise.tools.verifier.tests.util;
 
 import com.sun.enterprise.tools.verifier.Result;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
