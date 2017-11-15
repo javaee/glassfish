26fa94c62a61089bcdaf917028461d54c5dc76ce
Sameer Pandit <sameer.pandit@oracle.com>
2017-11-15 12:44:25 +0530

    use lastStableBuild instead of lastSuccessfullBuild

--- appserver/tests/cts_smoke/run_test.sh
+++ appserver/tests/cts_smoke/run_test.sh
@@ -56,7 +56,7 @@ archive_cts(){
 test_run_cts_smoke(){
 	TS_HOME=$WORKSPACE/javaee-smoke
 	if [[ -z ${CTS_SMOKE_URL} ]]; then
-		CTS_SMOKE=${JENKINS_URL}job/gf-cts-promotion/lastSuccessfulBuild/artifact
+		CTS_SMOKE=${JENKINS_URL}job/gf-cts-promotion/lastStableBuild/artifact
 	else
 		CTS_SMOKE=${CTS_SMOKE_URL}
 	fi
