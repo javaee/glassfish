9282f47bfd90c9e1c2f0335faf9210e47cd5c959
Kokil Jain <kokil.jain@oracle.com>
2018-02-22 17:47:48 +0530

    PR: Updating copyright of multiple modules of glassfish (#22449)

--- nucleus/common/common-util/src/test/resources/config/asenv.bat
+++ nucleus/common/common-util/src/test/resources/config/asenv.bat
@@ -1,30 +1,30 @@
-REM
+REM  
 REM  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-REM 
-REM  Copyright (c) 2004-2017 Oracle and/or its affiliates. All rights reserved.
+REM
+REM  Copyright (c) 2004-2018 Oracle and/or its affiliates. All rights reserved.
 REM
 REM  The contents of this file are subject to the terms of either the GNU
 REM  General Public License Version 2 only ("GPL") or the Common Development
 REM  and Distribution License("CDDL") (collectively, the "License").  You
 REM  may not use this file except in compliance with the License.  You can
 REM  obtain a copy of the License at
-REM  https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
-REM  or packager/legal/LICENSE.txt.  See the License for the specific
+REM  https://oss.oracle.com/licenses/CDDL+GPL-1.1
+REM  or LICENSE.txt.  See the License for the specific
 REM  language governing permissions and limitations under the License.
-REM 
+REM
 REM  When distributing the software, include this License Header Notice in each
-REM  file and include the License file at packager/legal/LICENSE.txt.
-REM 
+REM  file and include the License file at LICENSE.txt.
+REM
 REM  GPL Classpath Exception:
 REM  Oracle designates this particular file as subject to the "Classpath"
 REM  exception as provided by Oracle in the GPL Version 2 section of the License
 REM  file that accompanied this code.
-REM 
+REM
 REM  Modifications:
 REM  If applicable, add the following below the License Header, with the fields
 REM  enclosed by brackets [] replaced by your own identifying information:
 REM  "Portions Copyright [year] [name of copyright owner]"
-REM 
+REM
 REM  Contributor(s):
 REM  If you wish your version of this file to be governed by only the CDDL or
 REM  only the GPL Version 2, indicate your decision by adding "[Contributor]
