c8b7c839737bf9189e4ad7b54ad6cf56e4f49e4d
Jayasheelan Kumar <jayasheelan.kumar@oracle.com>
2018-07-19 14:38:36 +0530

    Removed sources from commonsLoggingLocalAndGlobal test

---
+++ appserver/tests/appserv-tests/devtests/web/commonsLoggingLocalAndGlobal/lib/test/MyTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MyTest {
+
+    public String toString() {
+        Log log = LogFactory.getLog(MyTest.class);
+	return "SUCCESS";
+    }
+}
+
