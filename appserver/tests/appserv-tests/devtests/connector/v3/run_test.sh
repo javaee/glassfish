b1e0fb04222a0258f2aaf8b9d1b8ab5179f9aba4
arindam-bandyopadhyay <arindam.bandyopadhyay@oracle.com>
2018-08-04 15:52:00 +0530

    Fixing connector tests in CI

--- appserver/tests/appserv-tests/devtests/connector/v3/run_test.sh
+++ appserver/tests/appserv-tests/devtests/connector/v3/run_test.sh
@@ -54,7 +54,6 @@ test_run(){
   echo Running target: $TARGET
   time ant clean-all start-record $TARGET stop-record report | tee $TEST_RUN_LOG
   antStatus=$?
-  cp connector.output tests-run.log
   ant stopDomain stopDerby
   if [ $antStatus -ne 0 ]
   then
